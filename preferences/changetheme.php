e4f7581453229ecbb12a27f4b27ba7669c8cf574
seek3r <seek3r@alumni.egroupware.org>
2000-08-18 20:28:58 +0000

    added new theme

--- preferences/changetheme.php
+++ preferences/changetheme.php
@@ -16,7 +16,7 @@
 
   $phpgw_flags["currentapp"] = "preferences";
   include("../header.inc.php");
-  $installed_themes = array('default','blue','mojo','heaven','yellows');
+  $installed_themes = array('default','blue','mojo','heaven','yellows', 'rose');
 
   if ($phpgw_info["user"]["permissions"]["anonymous"]) {
      Header("Location: " . $phpgw->link($phpgw_info["server"]["webserver_url"] . "/"));
