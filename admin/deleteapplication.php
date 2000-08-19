d3dc289015739aac90e9ee39e545eb10ae38da8b
jengo <jengo@alumni.egroupware.org>
2000-08-19 19:48:29 +0000

    The application editor is now working

---
+++ admin/deleteapplication.php
@@ -0,0 +1,51 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare - administration                                            *
+  * http://www.phpgroupware.org                                              *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+
+  /* $Id$ */
+
+  $phpgw_flags = array("noheader" => True, "nonavbar" => True);
+
+  if (! $app_name)
+     Header("Location: " . $phpgw->link("applications.php"));
+
+  $phpgw_flags["currentapp"] = "admin";
+  include("../header.inc.php");
+
+  if ($confirm) {
+        $phpgw->db->query("delete from applications where app_name='$app_name'");
+
+        Header("Location: " . $phpgw->link("applications.php"));
+        exit;
+  }
+
+  $phpgw->common->header();
+  $phpgw->common->navbar();
+  ?>
+     <center>
+      <table border=0 with=65%>
+       <tr colspan=2>
+        <td align=center>
+         <?php echo lang_admin("Are you sure you want to delete this application ?"); ?>
+        <td>
+       </tr>
+       <tr>
+         <td>
+           <a href="<?php echo $phpgw->link("applications.php") . "\">" . lang_common("No") . "</a>"; ?>
+         </td>
+         <td>
+           <a href="<?php echo $phpgw->link("deleteapplication.php","app_name=" . urlencode($app_name) . "&confirm=True") . "\">" . lang_common("Yes") . "</a>"; ?>
+         </td>
+       </tr>
+      </table>
+     </center>
+     <?
+     include($phpgw_info["server"]["api_dir"] . "/footer.inc.php");
+
