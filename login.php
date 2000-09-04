941d05d15c3fa563fbd45214e137f231dad78a11
jengo <jengo@alumni.egroupware.org>
2000-09-04 02:20:18 +0000

    Fixed a trailling / in kp3

--- login.php
+++ login.php
@@ -86,7 +86,7 @@
     if (! $sessionid) {
        Header("Location: " . $phpgw_info["server"]["webserver_url"] . "/login.php?cd=5");
     } else {
-       Header("Location: " . $phpgw->link($phpgw_info["server"]["webserver_url"]) . "/", "cd=yes");
+       Header("Location: " . $phpgw->link($phpgw_info["server"]["webserver_url"] . "/", "cd=yes"));
     }
 
   } else {
