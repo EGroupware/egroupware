1299230dd828b1eaf1924c8d38c1b2f34d950264
jengo <jengo@alumni.egroupware.org>
2000-11-19 23:18:19 +0000

    Added in phpGroupWares header and cvs change tag

--- version.inc.php
+++ version.inc.php
@@ -1,1 +1,15 @@
-<?php  $phpgw_info["server"]["version"] = "0.9.4pre4"; ?>
+<?php
+  /**************************************************************************\
+  * phpGroupWare                                                             *
+  * http://www.phpgroupware.org                                              *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+
+  /* $Id$ */
+
+  $phpgw_info["server"]["version"] = "0.9.4pre4";  
+?>
