778f22cc0281616d4510918ea6c322421ddfe293
skeeter <skeeter@alumni.egroupware.org>
2000-11-19 17:18:25 +0000

    fixes for printer friendly

--- calendar/day.php
+++ calendar/day.php
@@ -26,6 +26,30 @@
 
   include("../header.inc.php");
 
+  if(isset($friendly) && $friendly) {
+    if(!isset($phpgw_info["user"]["preferences"]["calendar"]["weekdaystarts"]))
+      $phpgw_info["user"]["preferences"]["calendar"]["weekdaystarts"] = "Sunday";
+
+    if (isset($date) && strlen($date) > 0) {
+       $thisyear  = substr($date, 0, 4);
+       $thismonth = substr($date, 4, 2);
+       $thisday   = substr($date, 6, 2);
+    } else {
+       if (!isset($day) || !$day)
+          $thisday = $phpgw->calendar->today["day"];
+       else
+          $thisday = $day;
+       if (!isset($month) || !$month)
+          $thismonth = $phpgw->calendar->today["month"];
+       else
+          $thismonth = $month;
+       if (!isset($year) || !$year)
+          $thisyear = $phpgw->calendar->today["year"];
+       else
+          $thisyear = $year;
+    }
+  }
+
   $view = "day";
 
   $now	= $phpgw->calendar->splitdate(mktime (2, 0, 0, $thismonth, $thisday, $thisyear));
--- calendar/week.php
+++ calendar/week.php
@@ -19,11 +19,35 @@
   if (isset($friendly) && $friendly){
      $phpgw_info["flags"]["noheader"] = True;
      $phpgw_info["flags"]["nonavbar"] = True;
-     $phpgw_info["flags"]["nocalendarheader"] = True;
+//     $phpgw_info["flags"]["nocalendarheader"] = True;
   } else {
      $friendly = 0;
   }
   include("../header.inc.php");
+
+  if(isset($friendly) && $friendly) {
+    if(!isset($phpgw_info["user"]["preferences"]["calendar"]["weekdaystarts"]))
+      $phpgw_info["user"]["preferences"]["calendar"]["weekdaystarts"] = "Sunday";
+
+    if (isset($date) && strlen($date) > 0) {
+       $thisyear  = substr($date, 0, 4);
+       $thismonth = substr($date, 4, 2);
+       $thisday   = substr($date, 6, 2);
+    } else {
+       if (!isset($day) || !$day)
+          $thisday = $phpgw->calendar->today["day"];
+       else
+          $thisday = $day;
+       if (!isset($month) || !$month)
+          $thismonth = $phpgw->calendar->today["month"];
+       else
+          $thismonth = $month;
+       if (!isset($year) || !$year)
+          $thisyear = $phpgw->calendar->today["year"];
+       else
+          $thisyear = $year;
+    }
+  }
 
   $next = $phpgw->calendar->splitdate(mktime(2,0,0,$thismonth,$thisday + 7,$thisyear));
   $prev = $phpgw->calendar->splitdate(mktime(2,0,0,$thismonth,$thisday - 7,$thisyear));
@@ -76,7 +100,7 @@
   $phpgw->template->set_var("week_display",$phpgw->calendar->display_large_week($thisday,$thismonth,$thisyear,true));
 
   if (!$friendly) {
-    $param = "year=".$now["year"]."&month=".$now["month"]."&friendly=1";
+    $param = "year=".$thisyear."&month=".$thismonth."&day=".$thisday."&friendly=1";
     $phpgw->template->set_var("print","<a href=\"".$phpgw->link($PHP_SELF,$param)."\" TARGET=\"cal_printer_friendly\" onMouseOver=\"window."
 	   . "status = '" . lang("Generate printer-friendly version"). "'\">[". lang("Printer Friendly") . "]</A>");
     $phpgw->template->parse("out","week");
