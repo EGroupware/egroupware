f504d8f6a72b2b81b796d0764fed5e35c9c01ec5
seek3r <seek3r@alumni.egroupware.org>
2000-12-29 22:10:24 +0000

    adding in some of the special setup files and dirs

---
+++ admin/version.inc.php
@@ -0,0 +1,14 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare                                                             *
+  * http://www.phpgroupware.org                                              *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+
+  /* $Id$ */
+
+  $phpgw_info["server"]["versions"]["admin"] = $phpgw_info["server"]["versions"]["phpgwapi"];
