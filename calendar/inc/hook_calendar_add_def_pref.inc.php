4638fe9f4df1a96b2a0cdb8082983d8b693783de
skeeter <skeeter@alumni.egroupware.org>
2001-01-04 19:22:52 +0000

    minor fix for default calendar

--- calendar/inc/hook_calendar_add_def_pref.inc.php
+++ calendar/inc/hook_calendar_add_def_pref.inc.php
@@ -3,7 +3,7 @@
   $pref->change("calendar","weekstarts","Monday");
   $pref->change("calendar","workdaystarts","9");
   $pref->change("calendar","workdayends","17");
-  $pref->change("calendar","defaultcalendar","index.php");
+  $pref->change("common","defaultcalendar","index.php");
   $pref->change("calendar","defaultfilter","all");
   $pref->change("calendar","mainscreen_showevents","Y");
 ?>
