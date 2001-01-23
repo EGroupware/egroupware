9c554ba6d9c2347811238883ad62229bac19a1f3
skeeter <skeeter@alumni.egroupware.org>
2001-01-23 12:01:55 +0000

    cosmetic fixes

--- phpgwapi/inc/class.accounts_ldap.inc.php
+++ phpgwapi/inc/class.accounts_ldap.inc.php
@@ -179,8 +179,8 @@
            $db->query("select group_name from groups where group_id=".$group[0],__LINE__,__FILE__);
            $db->next_record();
            $group_names[$i][0] = $group[0];
-	   $group_names[$i][1] = $db->f("group_name");
-	   $group_names[$i++][2] = $group[1];
+           $group_names[$i][1] = $db->f("group_name");
+           $group_names[$i++][2] = $group[1];
        }
        if (! $lid)
           $this->group_names = $group_names;
