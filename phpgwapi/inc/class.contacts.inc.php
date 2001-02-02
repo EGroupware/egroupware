d97225c0c17ae809f282a06f01853f6cadd411bf
seek3r <seek3r@alumni.egroupware.org>
2001-02-02 07:06:08 +0000

    fixed problem with default settings

--- phpgwapi/inc/class.contacts.inc.php
+++ phpgwapi/inc/class.contacts.inc.php
@@ -1,4 +1,4 @@
 <?php
-  if (!$phpgw_info["server"]["contact_application"]) { $phpgw_info["server"]["contact_application"] = "addressbook"; }
+  if (!$phpgw_info["server"]["contacts_application"]) { $phpgw_info["server"]["contacts_application"] = "addressbook"; }
   include($phpgw_info["server"]["include_root"]."/".$phpgw_info["server"]["contact_application"]."/inc/class.contacts.inc.php");
 ?>
