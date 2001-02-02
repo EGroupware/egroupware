a31ba57a861c406741bd2e1bba2f62eec7458867
seek3r <seek3r@alumni.egroupware.org>
2001-02-02 07:00:58 +0000

    moving the files to where they belong

---
+++ addressbook/inc/class.contacts.inc.php
@@ -0,0 +1,4 @@
+<?php
+  include($phpgw_info["server"]["include_root"]."/addressbook/inc/class.contacts_".$phpgw_info["server"]["contacts_repository"].".inc.php");
+  include($phpgw_info["server"]["include_root"]."/addressbook/inc/class.contacts_shared.inc.php");
+?>
