355a4b730e4c5fcc439b6f5ce8ef58f98b5167cf
chrwei <chrwei@alumni.egroupware.org>
2001-02-17 19:25:51 +0000

    Simple Notify for phpGW Client programs

---
+++ notify_simple.php
@@ -0,0 +1,19 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare                                                             *
+  * http://www.phpgroupware.org                                              *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+
+  /* $Id$ */
+
+	$phpgw_info["flags"] = array("noheader" => True, "nonavbar" => True, "currentapp" => "notifywindow");
+	include("header.inc.php");
+	echo '<html><body>';
+	$phpgw->common->hook("simple",array("email"));
+	echo '</body></html>';
+?>
