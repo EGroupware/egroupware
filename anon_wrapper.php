05109bbdba295f690285390de5ac3186fa59e7ef
jengo <jengo@alumni.egroupware.org>
2001-02-17 10:37:32 +0000

    Added login program that is just for anonymous sessions.

---
+++ anon_wrapper.php
@@ -0,0 +1,33 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare                                                             *
+	* http://www.phpgroupware.org                                              *
+	* Written by Dan Kuykendall <seek3r@phpgroupware.org>                      *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
+	// TODO:
+	//	Limit which users can access this program (ACL check)
+	//	Global disabler
+	//	Detect bad logins and passwords, spit out generic message
+
+	// If you plan on using this program, remove the following line.
+	echo "\n"; exit;
+
+	$phpgw_info['flags'] = array(
+		'disable_template_class' => True,
+		'login' => True,
+		'currentapp' => 'login',
+		'noheader'  => True);
+
+	include('./header.inc.php');
+
+	$sessionid = $phpgw->session->create($login,$passwd);
+	$phpgw->redirect($phpgw->link($phpgw_info['server']['webserver_url'] . '/index.php'));
+?>
