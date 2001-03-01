ee5e38588a3cd70d901860782fb6425e36ffb67d
Miles Lott <milosch@alumni.egroupware.org>
2001-03-01 01:44:49 +0000

    Update contacts class again

--- version.inc.php
+++ version.inc.php
@@ -11,5 +11,5 @@
 
   /* $Id$ */
 
-  $phpgw_info["server"]["versions"]["phpgwapi"] = "0.9.10pre15";
+  $phpgw_info["server"]["versions"]["phpgwapi"] = "0.9.10pre16";
   $phpgw_info["server"]["versions"]["current_header"] = "1.11";
