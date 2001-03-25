25f13d5072e82e92abe99c8ba65084423fa57797
Miles Lott <milosch@alumni.egroupware.org>
2001-03-25 02:48:11 +0000

    Add category editing code, based on Notes' code, for prefs

---
+++ addressbook/deletecategory.php
@@ -0,0 +1,59 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare - Addressbook                                               *
+  * (http://www.phpgroupware.org)                                            *
+  * Written by Bettina Gille [ceb@phpgroupware.org]                          *    
+  * -----------------------------------------------                          *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+  /* $Id$ */
+
+    if ($confirm) {
+    $phpgw_info["flags"] = array('noheader' => True, 
+                                  'nonavbar' => True);
+    }
+
+    $phpgw_info["flags"]["currentapp"] = "addressbook";
+    include("../header.inc.php");
+
+    $c = CreateObject('phpgwapi.categories');
+
+    if (! $cat_id) {
+    Header('Location: ' . $phpgw->link('/addressbook/categories.php'));
+    }
+
+    if ($confirm) {
+    $c->delete($cat_id);    
+    Header('Location: ' . $phpgw->link('/addressbook/categories.php'));
+    }
+    else {
+	$hidden_vars = "<input type=\"hidden\" name=\"cat_id\" value=\"$cat_id\">\n";
+    $t = CreateObject('phpgwapi.Template',$phpgw->common->get_tpl_dir('addressbook'));
+    $t->set_file(array('category_delete' => 'delete.tpl'));
+    $t->set_var('deleteheader',lang('Are you sure you want to delete this category ?'));
+
+    $nolinkf = $phpgw->link('/addressbook/categories.php',"cat_id=$cat_id");
+    $nolink = "<a href=\"$nolinkf\">" . lang('No') ."</a>";
+    $t->set_var("nolink",$nolink);
+
+    $yeslinkf = $phpgw->link('/addressbook/deletecategory.php',"cat_id=$cat_id&confirm=True");
+    $yeslinkf = "<FORM method=\"POST\" name=yesbutton action=\"".$phpgw->link('/addressbook/deletecategory.php') . "\">"
+                 . $hidden_vars
+                 . "<input type=hidden name=cat_id value=$cat_id>"
+		 . "<input type=hidden name=confirm value=True>"
+                 . "<input type=submit name=yesbutton value=Yes>"
+                 . "</FORM><SCRIPT>document.yesbutton.yesbutton.focus()</SCRIPT>";
+
+    $yeslink = "<a href=\"$yeslinkf\">" . lang('Yes') ."</a>";
+    $yeslink = $yeslinkf;
+
+    $t->set_var('yeslink',$yeslink);
+
+    $t->pparse('out','category_delete');
+    }
+    
+    $phpgw->common->phpgw_footer();
+?>
---
+++ addressbook/editcategory.php
@@ -0,0 +1,75 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare - Addressbook                                               *
+  * http://www.phpgroupware.org                                              *
+  * Written by Bettina Gille [ceb@phpgroupware.org]                          *
+  * -----------------------------------------------                          *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+  /* $Id$ */
+
+  $phpgw_info["flags"]["currentapp"] = 'addressbook';
+  include("../header.inc.php");
+  
+  if (! $cat_id) {                                                                                                                                                              
+     Header("Location: " . $phpgw->link('/addressbook/categories.php',"sort=$sort&order=$order&query=$query&start=$start"                                                                                                             
+					. "&filter=$filter"));
+          }
+  
+    $t = CreateObject('phpgwapi.Template',$phpgw->common->get_tpl_dir('addressbook'));
+    $t->set_file(array('form' => 'category_form.tpl'));
+    $t->set_block('form','add','addhandle');                                                                                                                          
+    $t->set_block('form','edit','edithandle');  
+
+    $c = CreateObject('phpgwapi.categories');
+
+    if ($submit) {
+    $errorcount = 0;
+    if (!$cat_name) { $error[$errorcount++] = lang('Please enter a name for that category !'); }
+    $phpgw->db->query("SELECT count(*) from phpgw_categories WHERE cat_name='$cat_name' AND cat_id !='$cat_id' AND cat_appname='"
+		     . $phpgw_info["flags"]["currentapp"] ."'");
+    $phpgw->db->next_record();
+    if ($phpgw->db->f(0) != 0) { $error[$errorcount++] = lang('That category name has been used already !'); }
+
+    $cat_name = addslashes($cat_name);
+    $cat_description = addslashes($cat_description);
+
+    if (! $error) { $c->edit($cat_id,$cat_parent,$cat_name,$cat_description,$cat_data);	}
+    }
+
+    if ($errorcount) { $t->set_var('message',$phpgw->common->error_list($error)); }
+    if (($submit) && (! $error) && (! $errorcount)) { $t->set_var('message',lang("Category $cat_name has been updated !")); }
+    if ((! $submit) && (! $error) && (! $errorcount)) { $t->set_var('message',''); }
+
+    $cats = $c->return_single($cat_id);
+
+    $t->set_var('font',$font);
+    $t->set_var('user_name',$phpgw_info["user"]["fullname"]);
+    $t->set_var('title_categories',lang('Edit category for'));
+    $t->set_var('lang_action',lang('Edit category'));
+    $t->set_var('actionurl',$phpgw->link('/addressbook/editcategory.php'));
+    $t->set_var('deleteurl',$phpgw->link('/addressbook/deletecategory.php'));
+    $hidden_vars = "<input type=\"hidden\" name=\"cat_id\" value=\"$cat_id\">\n";
+    $t->set_var('hidden_vars',$hidden_vars);
+    $t->set_var('lang_name',lang('Category name'));
+    $t->set_var('lang_descr',lang('Category description'));
+
+    $cat_id = $cats[0]['id'];
+
+    $t->set_var('cat_name',$phpgw->strip_html($cats[0]['name']));
+    $t->set_var('cat_description',$phpgw->strip_html($cats[0]['description']));
+
+    $t->set_var('lang_edit',lang('Edit'));
+    $t->set_var('lang_delete',lang('Delete'));
+
+    $t->set_var('edithandle','');
+    $t->set_var('addhandle','');
+
+    $t->pparse('out','form');
+    $t->pparse('edithandle','edit');
+
+    $phpgw->common->phpgw_footer();
+?>
