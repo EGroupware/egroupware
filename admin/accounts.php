7b81319a3cdc20ac93da9d98c4ad2722c0cb1234
skeeter <skeeter@alumni.egroupware.org>
2001-03-25 13:50:40 +0000

    Fix for bug #411158 - Improper link() calls

--- admin/accounts.php
+++ admin/accounts.php
@@ -14,7 +14,7 @@
   $phpgw_info = array();
   $phpgw_info['flags'] = array('currentapp' => 'admin', 'enable_nextmatchs_class' => True);  
   include('../header.inc.php');
-  include(PHPGW_APP_INC . '/accounts_' . $phpgw_info['server']['account_repository'] . '.inc.php');
+//  include(PHPGW_APP_INC . '/accounts_' . $phpgw_info['server']['account_repository'] . '.inc.php');
 
   $p = CreateObject('phpgwapi.Template',$phpgw->common->get_tpl_dir('admin'));
   
@@ -29,13 +29,13 @@
   $p->set_var('bg_color',$phpgw_info['theme']['bg_color']);
   $p->set_var('th_bg',$phpgw_info['theme']['th_bg']);
 
-  $p->set_var('left_next_matchs',$phpgw->nextmatchs->left('accounts.php',$start,$total));
+  $p->set_var('left_next_matchs',$phpgw->nextmatchs->left('admin/accounts.php',$start,$total));
   $p->set_var('lang_user_accounts',lang('user accounts'));
-  $p->set_var('right_next_matchs',$phpgw->nextmatchs->right('accounts.php',$start,$total));
+  $p->set_var('right_next_matchs',$phpgw->nextmatchs->right('admin/accounts.php',$start,$total));
 
-  $p->set_var('lang_loginid',$phpgw->nextmatchs->show_sort_order($sort,'account_lid',$order,'accounts.php',lang('LoginID')));
-  $p->set_var('lang_lastname',$phpgw->nextmatchs->show_sort_order($sort,'account_lastname',$order,'accounts.php',lang('last name')));
-  $p->set_var('lang_firstname',$phpgw->nextmatchs->show_sort_order($sort,'account_firstname',$order,'accounts.php',lang('first name')));
+  $p->set_var('lang_loginid',$phpgw->nextmatchs->show_sort_order($sort,'account_lid',$order,'admin/accounts.php',lang('LoginID')));
+  $p->set_var('lang_lastname',$phpgw->nextmatchs->show_sort_order($sort,'account_lastname',$order,'admin/accounts.php',lang('last name')));
+  $p->set_var('lang_firstname',$phpgw->nextmatchs->show_sort_order($sort,'account_firstname',$order,'admin/accounts.php',lang('first name')));
 
   $p->set_var('lang_edit',lang('Edit'));
   $p->set_var('lang_delete',lang('Delete'));
@@ -87,17 +87,17 @@
 				$p->set_var('row_delete','&nbsp;');
 			}
 	
-			$p->set_var('row_view','<a href="' . $phpgw->link("/admin/viewaccount.php", "account_id="
-	   				         . $account_id) . '"> ' . lang("View") . ' </a>');
+			$p->set_var('row_view','<a href="' . $phpgw->link('/admin/viewaccount.php','account_id='
+	   				         . $account_id) . '"> ' . lang('View') . ' </a>');
 	
 			$p->parse('rows','row',True);
 		}
 	}		// End else
 
-	$p->set_var('actionurl',$phpgw->link("/admin/newaccount.php"));
-	$p->set_var('accounts_url',$phpgw->link("/admin/accounts.php"));
-	$p->set_var('lang_add',lang("add"));
-	$p->set_var('lang_search',lang("search"));
+	$p->set_var('actionurl',$phpgw->link('/admin/newaccount.php'));
+	$p->set_var('accounts_url',$phpgw->link('/admin/accounts.php'));
+	$p->set_var('lang_add',lang('add'));
+	$p->set_var('lang_search',lang('search'));
 
 	$p->pparse('out','list');
 
