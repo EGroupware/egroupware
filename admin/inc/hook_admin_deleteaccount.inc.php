38dead09f4e30875a79406b307fba2d44c55073b
skeeter <skeeter@alumni.egroupware.org>
2001-03-30 03:55:25 +0000

    New hook for deleting user account.

---
+++ admin/inc/hook_admin_deleteaccount.inc.php
@@ -0,0 +1,21 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare                                                             *
+  * http://www.phpgroupware.org                                              *
+  * Written by Mark Peters <skeeter@phpgroupware.org>                        *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+	/* $Id$ */
+	
+	// Delete all records for a user
+	if (floor($PHP_VERSION ) == 4)
+	{
+		global $phpgw, $account_id;
+	}
+
+	$phpgw->accounts->delete($account_id);
+?>
