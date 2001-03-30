0224d6d9da1ac6aacb23aa8899c0ead69e44573f
skeeter <skeeter@alumni.egroupware.org>
2001-03-30 04:42:23 +0000

    Misnamed hook file.  This will now either delete all records for the user or change ownership to another user when deleting a user.

---
+++ addressbook/inc/hook_addressbook_deleteaccount.inc.php
@@ -0,0 +1,29 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare                                                             *
+  * http://www.phpgroupware.org                                              *
+  * Written by Joseph Engo <jengo@phpgroupware.org>                          *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+	/* $Id$ */
+	// Delete all records for a user
+	if (floor($PHP_VERSION ) == 4)
+	{
+		global $account_id, $new_owner;
+	}
+
+	$contacts = CreateObject('phpgwapi.contacts');
+
+	if($new_owner==0)
+	{
+		$contacts->delete_all($account_id);
+	}
+	else
+	{
+		$contacts->change_owner($account_id,$new_owner);
+	}
+?>
---
+++ calendar/inc/hook_calendar_deleteaccount.inc.php
@@ -0,0 +1,31 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare                                                             *
+  * http://www.phpgroupware.org                                              *
+  * Written by Mark Peters <skeeter@phpgroupware.org>                        *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+	/* $Id$ */
+	
+	// Delete all records for a user
+	if (floor($PHP_VERSION ) == 4)
+	{
+		global $account_id, $new_owner;
+	}
+
+	$calendar = CreateObject('calendar.calendar');
+	$cal_stream = $calendar->open('INBOX',$account_id,'');
+	
+	if($new_owner==0)
+	{
+		$calendar->delete_calendar($cal_stream,$account_id);
+	}
+	else
+	{
+		$calendar->change_owner($account_id,$new_owner);
+	}
+?>
