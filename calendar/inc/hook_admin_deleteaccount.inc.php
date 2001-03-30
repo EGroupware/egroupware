69ef94cd170bb80d976389e61c92c0dcabd5e10b
skeeter <skeeter@alumni.egroupware.org>
2001-03-30 02:16:23 +0000

    New hook for deleting ALL records for a user when the user is deleted from the system.

---
+++ calendar/inc/hook_admin_deleteaccount.inc.php
@@ -0,0 +1,23 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare                                                             *
+  * http://www.phpgroupware.org                                              *
+  * Written by Mark Peters <skeeter@phpgroupware.org>                        *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+	/* $Id$ */
+	
+	// Delete all records for a user
+	if (floor($PHP_VERSION ) == 4)
+	{
+		global $accountid;
+	}
+
+	$calendar = CreateObject('calendar.calendar');
+	$cal_stream = $calendar->open('INBOX',$accountid,'');
+	$calendar->delete_calendar($cal_stream,$accountid);
+?>
