2811576045f73a1440c18a07a1e981c00a049d27
Miles Lott <milosch@alumni.egroupware.org>
2001-04-07 13:55:34 +0000

    Add cvs descr line

--- phpgwapi/inc/browser.inc.php
+++ phpgwapi/inc/browser.inc.php
@@ -22,6 +22,7 @@
   * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA            *
   \**************************************************************************/
 
+  /* $Id$ */
 
 	// Where do we need to put this? - Milosch
 
