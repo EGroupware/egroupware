b872330d12418ab2e4e49b63392c326518fa3097
Miles Lott <milosch@alumni.egroupware.org>
2001-04-08 07:50:20 +0000

    More formatting

--- about.php
+++ about.php
@@ -11,35 +11,44 @@
 
   /* $Id$ */
 
-  $phpgw_info["flags"]["currentapp"] = "about";
-  include("header.inc.php");
+	$phpgw_info["flags"]["currentapp"] = "about";
+	include("header.inc.php");
 
-  if ($app) {
-     $included = $phpgw->common->hook_single("about",$app);
-  } else {
-     $api_only = True;
-  }
-  
-  $tpl = CreateObject('phpgwapi.Template',$phpgw->common->get_tpl_dir('phpgwapi'));
-  $tpl->set_file(array("phpgw_about"         => "about.tpl",
-                       "phpgw_about_unknown" => "about_unknown.tpl"                                 
-                      ));
+	if ($app)
+	{
+		$included = $phpgw->common->hook_single("about",$app);
+	}
+	else
+	{
+		$api_only = True;
+	}
 
-  $tpl->set_var("webserver_url",$phpgw->common->get_image_path('phpgwapi'));
-  $tpl->set_var("phpgw_version","phpGroupWare API version " . $phpgw_info["server"]["versions"]["phpgwapi"]);
-  if ($included) {
-     $tpl->set_var("phpgw_app_about",about_app("",""));
-     //about_app($tpl,"phpgw_app_about");
-  } else {
-     if ($api_only) {
-        $tpl->set_var("phpgw_app_about","");
-     } else {
-        $tpl->set_var("app_header",$app);
-        $tpl->parse("phpgw_app_about","phpgw_about_unknown");
-     }
-  }
+	$tpl = CreateObject('phpgwapi.Template',$phpgw->common->get_tpl_dir('phpgwapi'));
+	$tpl->set_file(array(
+		"phpgw_about"         => "about.tpl",
+		"phpgw_about_unknown" => "about_unknown.tpl"
+	));
 
-  $tpl->pparse("out","phpgw_about");
-  $phpgw->common->phpgw_footer();
+	$tpl->set_var("webserver_url",$phpgw->common->get_image_path('phpgwapi'));
+	$tpl->set_var("phpgw_version","phpGroupWare API version " . $phpgw_info["server"]["versions"]["phpgwapi"]);
+	if ($included)
+	{
+		$tpl->set_var("phpgw_app_about",about_app("",""));
+		//about_app($tpl,"phpgw_app_about");
+	}
+	else
+	{
+		if ($api_only)
+		{
+			$tpl->set_var("phpgw_app_about","");
+		}
+		else
+		{
+			$tpl->set_var("app_header",$app);
+			$tpl->parse("phpgw_app_about","phpgw_about_unknown");
+		}
+	}
 
+	$tpl->pparse("out","phpgw_about");
+	$phpgw->common->phpgw_footer();
 ?>
