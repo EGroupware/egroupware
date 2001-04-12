b4c49a5ea695096bdd2cb56aaaba3f53d5a411f0
Miles Lott <milosch@alumni.egroupware.org>
2001-04-12 05:12:27 +0000

    Add setup info files

---
+++ addressbook/setup/setup_info.inc.php
@@ -0,0 +1,3 @@
+<?php
+$setup_info["addressbook"] = array("name" => "Addressbook", "app_order" => 4, "version" => $phpgw_info["server"]["versions"]["phpgwapi"]);
+?>
---
+++ calendar/setup/setup_info.inc.php
@@ -0,0 +1,3 @@
+<?php
+$setup_info["calendar"] = array("name" => "Calendar", "app_order" => 3, "version" => $phpgw_info["server"]["versions"]["phpgwapi"]);
+?>
