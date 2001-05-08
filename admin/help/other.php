43949f2fa9d3ea19314352a6e37aa26dbf1090d7
skeeter <skeeter@alumni.egroupware.org>
2001-05-08 23:51:02 +0000

    New manual system

---
+++ admin/help/other.php
@@ -0,0 +1,34 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare - User manual                                               *
+  * http://www.phpgroupware.org                                              *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+
+	/* $Id$ */
+
+	$phpgw_flags = Array(
+		'currentapp'	=> 'manual',
+		'enable_utilities_class'	=> True
+	);
+	$phpgw_info['flags'] = $phpgw_flags;
+	include('../../header.inc.php');
+?>
+<img src="<?php echo $phpgw->common->image('calendar','navbar.gif'); ?>" border=0> 
+<font face="<?php echo $phpgw_info['theme']['font']; ?>" size="2">
+<p>
+This function is usually only available to the systems administrator of the system.
+Administration of all applications, user and group accounts, session logging and control.
+<ul>
+<li><b>Network news:</b>
+<br>Manual update for news groups.
+<p>
+<li><b>Server information:</b>
+<br>At present phpinfo of the server that is running.
+<p>
+</ul>
+<?php $phpgw->common->phpgw_footer(); ?>
