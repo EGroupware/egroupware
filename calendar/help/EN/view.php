ab7e9f0da4395f033d9ee7d85c6bd3917a822002
skeeter <skeeter@alumni.egroupware.org>
2001-05-11 03:00:00 +0000

    Missed updating this one.

--- calendar/help/EN/view.php
+++ calendar/help/EN/view.php
@@ -12,29 +12,23 @@
 	/* $Id$ */
 
 	$phpgw_flags = Array(
-		'currentapp'	=> 'manual',
-		'enable_utilities_class'	=> True
+		'currentapp'	=> 'manual'
 	);
 	$phpgw_info['flags'] = $phpgw_flags;
-	include('../../header.inc.php');
+	include('../../../header.inc.php');
 ?>
-<img src="<?php echo $phpgw->common->image('calendar','navbar.gif'); ?>" border=0> 
-<font face="<?php echo $phpgw_info['theme']['font']; ?>" size="2">
-<p>
-A searchable daily,weekly,monthly calendar/scheduling application with alerts for high priority events.
-<br> For viewing in either hourly/day,current week or monthly option, click on the relevant 
-icons at the top left hand corner.
-<br>
-<ul>
-<li><b>Viewing:</b><img src="<?php echo $phpgw->common->image('calendar','today.gif'); ?>">Day <img src="<?php echo $phpgw->common->image('calendar','week.gif'); ?>">Week <img src="<?php echo $phpgw->common->image('calendar','month.gif'); ?>">Month <img src="<?php echo $phpgw->common->image('calendar','year.gif'); ?>">Year
-<p><i>Day:</i>
-<br>Current day is displayed, broken down in hour slots. Start and end time of the day
-can be set in the preferences.
-<p><i>Week:</i>
-<br>Current week is displayed. Start day of week can be set in preferences.
-<p><i>Month:</i>
-<br>Default entry is to the current month, viewed in monthly option, with both prior and future
-months easily accessible with one click.
-<p>
-</ul>
+<img src="<?php echo $phpgw->common->image('calendar','navbar.gif'); ?>" border="0">
+<font face="<?php echo $phpgw_info['theme']['font']; ?>" size="2"><p/>
+A searchable daily,weekly,monthly calendar/scheduling application with alerts for high priority events.<br/>
+For viewing in either hourly/day,current week or monthly option, click on the relevant 
+icons at the top left hand corner.<br/>
+<ul><li><b>Viewing:</b><img src="<?php echo $phpgw->common->image('calendar','today.gif'); ?>">Day <img src="<?php echo $phpgw->common->image('calendar','week.gif'); ?>">Week <img src="<?php echo $phpgw->common->image('calendar','month.gif'); ?>">Month <img src="<?php echo $phpgw->common->image('calendar','year.gif'); ?>">Year<p/>
+<i>Day:</i><br/>
+Current day is displayed, broken down in hour slots. Start and end time of the day
+can be set in the preferences.<p/>
+<i>Week:</i><br/>
+Current week is displayed. Start day of week can be set in preferences.<p/>
+<i>Month:</i><br/>
+Default entry is to the current month, viewed in monthly option, with both prior and future
+months easily accessible with one click.<p/></li></ul></font>
 <?php $phpgw->common->phpgw_footer(); ?>
