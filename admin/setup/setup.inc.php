575642501dc19d4af028ad7e359f50c0a70f6ab3
Miles Lott <milosch@alumni.egroupware.org>
2001-05-19 23:58:48 +0000

    This app has no tables

--- admin/setup/setup.inc.php
+++ admin/setup/setup.inc.php
@@ -1,8 +1,9 @@
 <?php
-	$setup_info['admin']['name'] = 'Administration';
-	$setup_info['admin']['version'] = '0.9.11';
+	$setup_info['admin']['name']      = 'admin';
+	$setup_info['admin']['title']     = 'Administration';
+	$setup_info['admin']['version']   = '0.9.11';
 	$setup_info['admin']['app_order'] = 1;
-	$setup_info['admin']['tables'] = "";
+	$setup_info['admin']['tables']    = '';
 	$hooks = Array();
 	$hooks_string = implode (',', $hooks);
 	$setup_info['admin']['hooks'] = $hooks_string;
