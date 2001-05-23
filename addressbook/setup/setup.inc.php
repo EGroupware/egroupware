413577a163fa94686b7625b026d221421166e739
seek3r <seek3r@alumni.egroupware.org>
2001-05-23 16:57:21 +0000

    updating to new format that supports depends

--- addressbook/setup/setup.inc.php
+++ addressbook/setup/setup.inc.php
@@ -1,10 +1,17 @@
 <?php
+	/* Basic information about this app */
 	$setup_info['addressbook']['name']      = 'addressbook';
 	$setup_info['addressbook']['title']     = 'Addressbook';
 	$setup_info['addressbook']['version']   = '0.9.11';
 	$setup_info['addressbook']['app_order'] = 4;
 	$setup_info['addressbook']['tables']    = '';
-	$hooks = Array();
-	$hooks_string = implode (',', $hooks);
-	$setup_info['addressbook']['hooks'] = $hooks_string;
+
+	/* The hooks this app includes, needed for hooks registration */
+	$setup_info['addressbook']['hooks'][] = 'preferences';
+
+	/* Dependacies for this app to work */
+	$setup_info['addressbook']['depends'][] = array(
+			 'appname' => 'phpgwapi',
+			 'versions' => Array('0.9.10', '0.9.11' , '0.9.12')
+		);
 ?>
