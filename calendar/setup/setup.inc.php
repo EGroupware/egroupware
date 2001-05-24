ba54e7d972a4f61b85ca0f7b0ec48abf0c06932e
Miles Lott <milosch@alumni.egroupware.org>
2001-05-24 08:18:41 +0000

    Add correct table names

--- calendar/setup/setup.inc.php
+++ calendar/setup/setup.inc.php
@@ -2,7 +2,7 @@
 	$setup_info['calendar']['name'] = 'Calendar';
 	$setup_info['calendar']['version'] = '0.9.11';
 	$setup_info['calendar']['app_order'] = 3;
-	$setup_info['calendar']['tables'] = "";
+	$setup_info['calendar']['tables'] = array('phpgw_cal','phpgw_cal_holidays','phpgw_cal_repeats','phpgw_cal_user');
 	$hooks = Array();
 	$hooks_string = implode (',', $hooks);
 	$setup_info['calendar']['hooks'] = $hooks_string;
