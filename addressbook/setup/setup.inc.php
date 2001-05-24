b0b883cc20a68716f24481f4fc633c444d22dc5f
Miles Lott <milosch@alumni.egroupware.org>
2001-05-24 20:33:37 +0000

    Adjust table names array

--- addressbook/setup/setup.inc.php
+++ addressbook/setup/setup.inc.php
@@ -4,17 +4,19 @@
 	$setup_info['addressbook']['title']     = 'Addressbook';
 	$setup_info['addressbook']['version']   = '0.9.11';
 	$setup_info['addressbook']['app_order'] = 4;
-	$setup_info['addressbook']['tables']    = '';
 
 	/* The tables this app creates */
-	$setup_info['addressbook']['tables']    = Array('skel');
+	$setup_info['addressbook']['tables']    = array(
+		'phpgw_addressbook',
+		'phpgw_addressbook_extra'
+	);
 
 	/* The hooks this app includes, needed for hooks registration */
 	$setup_info['addressbook']['hooks'][] = 'preferences';
 
 	/* Dependacies for this app to work */
 	$setup_info['addressbook']['depends'][] = array(
-			 'appname' => 'phpgwapi',
-			 'versions' => Array('0.9.10', '0.9.11' , '0.9.12')
-		);
+		 'appname' => 'phpgwapi',
+		 'versions' => Array('0.9.10', '0.9.11' , '0.9.12')
+	);
 ?>
