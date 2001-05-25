df1a10d4bd7299b7efaa0ef088a96bc8f20262a8
zone <zone@alumni.egroupware.org>
2001-05-25 02:36:55 +0000

    All preferences are now set; not working?

--- filemanager/inc/hook_phpwebhosting_add_def_pref.inc.php
+++ filemanager/inc/hook_phpwebhosting_add_def_pref.inc.php
@@ -1,11 +1,18 @@
 <?php
-  global $pref;
-  $pref->change("phpwebhosting","filename","True");
-  $pref->change("phpwebhosting","mime_type","False");
-  $pref->change("phpwebhosting","size","True");
-  $pref->change("phpwebhosting","created","True");
-  $pref->change("phpwebhosting","modified","True");
-  $pref->change("phpwebhosting","createdby","True");
-  $pref->change("phpwebhosting","modifiedby","True");
-  $pref->change("phpwebhosting","comment","True");
+
+global $pref;
+
+$pref->change ("phpwebhosting", "filename", "True");
+$pref->change ("phpwebhosting", "mime_type", "True");
+$pref->change ("phpwebhosting", "size", "True");
+$pref->change ("phpwebhosting", "created", "True");
+$pref->change ("phpwebhosting", "modified", "True");
+$pref->change ("phpwebhosting", "owner", "False");
+$pref->change ("phpwebhosting", "createdby", "True");
+$pref->change ("phpwebhosting", "modifiedby", "True");
+$pref->change ("phpwebhosting", "app", "False");
+$pref->change ("phpwebhosting", "comment", "True");
+$pref->change ("phpwebhosting", "dotdot", "False");
+$pref->change ("phpwebhosting", "dotfiles", "True");
+
 ?>
