329a51bf2bb832440eea7b2cf12d2a98dd0d7245
Miles Lott <milosch@alumni.egroupware.org>
2001-05-25 03:28:42 +0000

    Add tables_current, update setup.inc.php

--- calendar/setup/setup.inc.php
+++ calendar/setup/setup.inc.php
@@ -1,8 +1,13 @@
 <?php
-	$setup_info['calendar']['name'] = 'Calendar';
+	$setup_info['calendar']['name']    = 'calendar';
+	$setup_info['calendar']['title']   = 'Calendar';
 	$setup_info['calendar']['version'] = '0.9.11';
 	$setup_info['calendar']['app_order'] = 3;
-	$setup_info['calendar']['tables'] = array('phpgw_cal','phpgw_cal_holidays','phpgw_cal_repeats','phpgw_cal_user');
+
+	$setup_info['calendar']['tables'][] = 'phpgw_cal';
+	$setup_info['calendar']['tables'][] = 'phpgw_cal_holidays';
+	$setup_info['calendar']['tables'][] = 'phpgw_cal_repeats';
+	$setup_info['calendar']['tables'][] = 'phpgw_cal_user';
 	$hooks = Array();
 	$hooks_string = implode (',', $hooks);
 	$setup_info['calendar']['hooks'] = $hooks_string;
---
+++ calendar/setup/tables_current.inc.php
@@ -0,0 +1,77 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare                                                             *
+  * http://www.phpgroupware.org                                              *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+
+  /* $Id$ */
+
+	$phpgw_baseline = array(
+		'phpgw_cal' => array(
+			'fd' => array(
+				'cal_id' => array('type' => 'auto','nullable' => False),
+				'owner' => array('type' => 'int', 'precision' => 11,'nullable' => False),
+				'category' => array('type' => 'int', 'precision' => 11,'nullable' => False),
+				'groups' => array('type' => 'varchar', 'precision' => 255,'nullable' => True),
+				'datetime' => array('type' => 'int', 'precision' => 11,'nullable' => True),
+				'mdatetime' => array('type' => 'int', 'precision' => 11,'nullable' => True),
+				'edatetime' => array('type' => 'int', 'precision' => 11,'nullable' => True),
+				'priority' => array('type' => 'int', 'precision' => 11,'nullable' => False,'default' => '2'),
+				'cal_type' => array('type' => 'varchar', 'precision' => 10,'nullable' => True,'default' => '2'),
+				'is_public' => array('type' => 'int', 'precision' => 11,'nullable' => False,'default' => '1'),
+				'title' => array('type' => 'varchar', 'precision' => 80,'nullable' => False,'default' => '1'),
+				'description' => array('type' => 'text','nullable' => True,'default' => '1')
+			),
+			'pk' => array('cal_id'),
+			'fk' => array(''),
+			'ix' => array(''),
+			'uc' => array('')
+		),
+		'phpgw_cal_holidays' => array(
+			'fd' => array(
+				'hol_id' => array('type' => 'auto','nullable' => False),
+				'locale' => array('type' => 'char', 'precision' => 2,'nullable' => False),
+				'name' => array('type' => 'varchar', 'precision' => 50,'nullable' => False),
+				'mday' => array('type' => 'int', 'precision' => 11,'nullable' => False),
+				'month_num' => array('type' => 'int', 'precision' => 11,'nullable' => False),
+				'occurence' => array('type' => 'int', 'precision' => 11,'nullable' => False),
+				'dow' => array('type' => 'int', 'precision' => 11,'nullable' => False),
+				'observance_rule' => array('type' => 'int', 'precision' => 11,'nullable' => False)
+			),
+			'pk' => array('hol_id'),
+			'fk' => array(''),
+			'ix' => array(''),
+			'uc' => array('')
+		),
+		'phpgw_cal_repeats' => array(
+			'fd' => array(
+				'cal_id' => array('type' => 'int', 'precision' => 11,'nullable' => False),
+				'recur_type' => array('type' => 'int', 'precision' => 11,'nullable' => False),
+				'recur_use_end' => array('type' => 'int', 'precision' => 11,'nullable' => True),
+				'recur_enddate' => array('type' => 'int', 'precision' => 11,'nullable' => True),
+				'recur_interval' => array('type' => 'int', 'precision' => 11,'nullable' => True,'default' => '1'),
+				'recur_data' => array('type' => 'int', 'precision' => 11,'nullable' => True,'default' => '1')
+			),
+			'pk' => array(''),
+			'fk' => array(''),
+			'ix' => array(''),
+			'uc' => array('')
+		),
+		'phpgw_cal_user' => array(
+			'fd' => array(
+				'cal_id' => array('type' => 'int', 'precision' => 11,'nullable' => False),
+				'cal_login' => array('type' => 'int', 'precision' => 11,'nullable' => False),
+				'cal_status' => array('type' => 'char', 'precision' => 1,'nullable' => True,'default' => 'A')
+			),
+			'pk' => array('cal_id','cal_login'),
+			'fk' => array(''),
+			'ix' => array(''),
+			'uc' => array('')
+		)
+	);
+?>
