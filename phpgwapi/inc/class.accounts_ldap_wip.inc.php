4988ab7bb0eabf8a3891a74bce2d5658a6f8ae2a
Miles Lott <milosch@alumni.egroupware.org>
2001-05-25 20:30:25 +0000

    Add nscd to the exclusion list

--- phpgwapi/inc/class.accounts_ldap_wip.inc.php
+++ phpgwapi/inc/class.accounts_ldap_wip.inc.php
@@ -40,7 +40,7 @@
 		'alias'    => True, 'web'      => True, 'sweep'    => True,
 		'cvs'      => True, 'qmaild'   => True, 'qmaill'   => True,
 		'qmaillog' => True, 'qmailp'   => True, 'qmailq'   => True,
-		'qmailr'   => True, 'qmails'   => True
+		'qmailr'   => True, 'qmails'   => True, 'nscd'     => True
 	);
 
 	$phpgw_info["server"]["global_denied_groups"] = array(
