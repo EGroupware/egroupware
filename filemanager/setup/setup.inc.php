5ebb7a3d632cb0b34ffdf1ffb300cfb148c709ba
Miles Lott <milosch@alumni.egroupware.org>
2001-05-31 21:50:16 +0000

    Updated file

--- filemanager/setup/setup.inc.php
+++ filemanager/setup/setup.inc.php
@@ -1,4 +1,16 @@
 <?php
+	/**************************************************************************\
+	* phpGroupWare - PHP Webhosting                                            *
+	* http://www.phpgroupware.org                                              *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
 	$setup_info['phpwebhosting']['name']    = 'phpwebhosting';
 	$setup_info['phpwebhosting']['title']   = 'PHPWebHosting';
 	$setup_info['phpwebhosting']['version'] = '0.9.13.001';
@@ -10,7 +22,7 @@
 	/* Dependencies for this app to work */
 	$setup_info['phpwebhosting']['depends'][] = array(
 		 'appname' => 'phpgwapi',
-		 'versions' => Array('0.9.10', '0.9.11' , '0.9.12')
+		 'versions' => array('0.9.10', '0.9.11' , '0.9.12', '0.9.13')
 	);
 
 ?>
