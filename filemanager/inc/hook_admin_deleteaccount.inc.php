983f6e987b3fe2048b946381df016f1a1ce3a4c0
zone <zone@alumni.egroupware.org>
2001-06-03 22:49:02 +0000

    Initial Revision/Weak start

---
+++ filemanager/inc/hook_admin_deleteaccount.inc.php
@@ -0,0 +1,28 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare                                                             *
+  * http://www.phpgroupware.org                                              *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+	/* $Id$ */
+	
+	global $phpgw;
+	global $account_id;
+
+	echo "HERE!!!";
+
+	/* NOTE: This is untested */
+	/* WIP: it should get all files owned by $account_id, not just in /home/account_id */
+	/* Should also be capable of transfering files to another user */
+
+	$phpgw->vfs->working_id = $account_id;
+	$ls_array = $phpgw->vfs->ls ($phpgw->vfs->fakebase . "/" . $account_id, array (RELATIVE_NONE));
+	while (list ($num, $entry) = each ($ls_array))
+	{
+		$phpgw->vfs->rm ($entry["dir"] . "/" . $entry["name"], array (RELATIVE_NONE));
+	}
+?>
