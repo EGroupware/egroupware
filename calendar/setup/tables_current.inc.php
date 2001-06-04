8eacdb5cbbd6b0a87880bddd69f78025c5c745b0
Miles Lott <milosch@alumni.egroupware.org>
2001-06-04 14:37:40 +0000

    Remove spaces in = >, remove '' from int default values (not sure that is req.)

--- calendar/setup/tables_current.inc.php
+++ calendar/setup/tables_current.inc.php
@@ -21,9 +21,9 @@
 				'datetime' => array('type' => 'int', 'precision' => 8,'nullable' => True),
 				'mdatetime' => array('type' => 'int', 'precision' => 8,'nullable' => True),
 				'edatetime' => array('type' => 'int', 'precision' => 8,'nullable' => True),
-				'priority' => array('type' => 'int', 'precision' => 8,'nullable' => False,'default' => '2'),
+				'priority' => array('type' => 'int', 'precision' => 8,'nullable' => False,'default' => 2),
 				'cal_type' => array('type' => 'varchar', 'precision' => 10,'nullable' => True),
-				'is_public' => array('type' => 'int', 'precision' => 8,'nullable' => False,'default' => '1'),
+				'is_public' => array('type' => 'int', 'precision' => 8,'nullable' => False,'default' => 1),
 				'title' => array('type' => 'varchar', 'precision' => 80,'nullable' => False,'default' => '1'),
 				'description' => array('type' => 'text','nullable' => True)
 			),
@@ -37,11 +37,11 @@
 				'hol_id' => array('type' => 'auto','nullable' => False),
 				'locale' => array('type' => 'char', 'precision' => 2, 'nullable' => False),
 				'name' => array('type' => 'varchar', 'precision' => 50, 'nullable' => False),
-				'mday' => array('type' => 'int', 'precision' => 8, 'nullable' => False, 'default' => '0'),
-				'month_num' => array('type' => 'int', 'precision' => 8, 'nullable' => False, 'default' => '0'),
-				'occurence' => array('type' => 'int', 'precision' => 8, 'nullable' => False, 'default' => '0'),
-				'dow' => array('type' => 'int', 'precision' => 8, 'nullable' => False, 'default' => '0'),
-				'observance_rule' => array('type' => 'int', 'precision' => 8, 'nullable' => False, 'default' => '0')
+				'mday' => array('type' => 'int', 'precision' => 8, 'nullable' => False, 'default' => 0),
+				'month_num' => array('type' => 'int', 'precision' => 8, 'nullable' => False, 'default' => 0),
+				'occurence' => array('type' => 'int', 'precision' => 8, 'nullable' => False, 'default' => 0),
+				'dow' => array('type' => 'int', 'precision' => 8, 'nullable' => False, 'default' => 0),
+				'observance_rule' => array('type' => 'int', 'precision' => 8, 'nullable' => False, 'default' => 0)
 			),
 			'pk' => array('hol_id'),
 			'fk' => array(),
@@ -52,10 +52,10 @@
 			'fd' => array(
 				'cal_id' => array('type' => 'int', 'precision' => 8, 'nullable' => False),
 				'recur_type' => array('type' => 'int', 'precision' => 8, 'nullable' => False),
-				'recur_use_end' => array('type' => 'int', 'precision' => 8, 'nullable' => True, 'default' => '0'),
+				'recur_use_end' => array('type' => 'int', 'precision' => 8, 'nullable' => True, 'default' => 0),
 				'recur_enddate' => array('type' => 'int', 'precision' => 8, 'nullable' => True),
-				'recur_interval' => array('type' => 'int', 'precision' => 8, 'nullable' => True, 'default' => '1'),
-				'recur_data' => array('type' => 'int', 'precision' => 8, 'nullable' => True, 'default' => '1')
+				'recur_interval' => array('type' => 'int', 'precision' => 8, 'nullable' => True, 'default' => 1),
+				'recur_data' => array('type' => 'int', 'precision' => 8, 'nullable' => True, 'default' => 1)
 			),
 			'pk' => array(),
 			'fk' => array(),
@@ -64,8 +64,8 @@
 		),
 		'phpgw_cal_user' => array(
 			'fd' => array(
-				'cal_id' => array('type' => 'int', 'precision' => 8, 'nullable' => False, 'default' = > '0'),
-				'cal_login' => array('type' => 'int', 'precision' => 8, 'nullable' => False, 'default' = > '0'),
+				'cal_id' => array('type' => 'int', 'precision' => 8, 'nullable' => False, 'default' => 0),
+				'cal_login' => array('type' => 'int', 'precision' => 8, 'nullable' => False, 'default' => 0),
 				'cal_status' => array('type' => 'char', 'precision' => 1, 'nullable' => True, 'default' => 'A')
 			),
 			'pk' => array('cal_id','cal_login'),
