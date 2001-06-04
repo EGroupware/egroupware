7457a46f27824c19dcb092081d80d8380543deb9
Miles Lott <milosch@alumni.egroupware.org>
2001-06-04 15:19:31 +0000

    add header and chmod -x

--- phpgwapi/setup/setup.inc.php
+++ phpgwapi/setup/setup.inc.php
@@ -1,4 +1,16 @@
 <?php
+	/**************************************************************************\
+	* phpGroupWare - phpgwapi setup                                            *
+	* http://www.phpgroupware.org                                              *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
 	/* Basic information about this app */
 	$setup_info['phpgwapi']['name']      = 'phpgwapi';
 	$setup_info['phpgwapi']['title']     = 'phpgwapi';
