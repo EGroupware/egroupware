6f72175fb0abe3cfc6550677a15b1ce77ab13fe1
Miles Lott <milosch@alumni.egroupware.org>
2001-06-06 20:42:42 +0000

    Don't overwrite hostname after the setting was saved once (!= '')

--- phpgwapi/setup/config.inc.php
+++ phpgwapi/setup/config.inc.php
@@ -5,17 +5,17 @@
    </tr>
    <tr bgcolor="e6e6e6">
     <td>Enter the full path for temporary files.<br>Examples: /tmp, C:\TEMP</td>
-    <td><input name="newsettings[temp_dir]" value="<?php echo $current_config["temp_dir"]; ?>" size="40"></td>
+    <td><input name="newsettings[temp_dir]" value="<?php echo $current_config['temp_dir']; ?>" size="40"></td>
    </tr>
 
    <tr bgcolor="e6e6e6">
     <td>Enter the full path for users and group files.<br>Examples: /files, E:\FILES</td>
-    <td><input name="newsettings[files_dir]" value="<?php echo $current_config["files_dir"]; ?>" size="40"></td>
+    <td><input name="newsettings[files_dir]" value="<?php echo $current_config['files_dir']; ?>" size="40"></td>
    </tr>
    
    <tr bgcolor="e6e6e6">
     <td>Enter the location of phpGroupWare's URL.<br>Example: http://www.domain.com/phpgroupware &nbsp; or &nbsp; /phpgroupware<br><b>No trailing slash</b></td>
-    <td><input name="newsettings[webserver_url]" value="<?php echo $current_config["webserver_url"]; ?>" size="40"></td>
+    <td><input name="newsettings[webserver_url]" value="<?php echo $current_config['webserver_url']; ?>" size="40"></td>
    </tr>
 
    <tr bgcolor="FFFFFF">
@@ -27,26 +27,29 @@
    </tr>
 
    <tr bgcolor="e6e6e6">
+	<?php if ($current_config['hostname']) { $thishostname = $current_config['hostname']; }
+		else { $thishostname = $SERVER_NAME; }
+	?>
     <td>Enter the hostname of the machine on which this server is running.</td>
-    <td><input name="newsettings[hostname]" value="<?php echo $SERVER_NAME; ?>"></td>
+    <td><input name="newsettings[hostname]" value="<?php echo $thishostname; ?>"></td>
    </tr>
 
    <tr bgcolor="e6e6e6">
     <td>Enter your default FTP server.</td>
-    <td><input name="newsettings[default_ftp_server]" value="<?php echo $current_config["default_ftp_server"]; ?>"></td>
+    <td><input name="newsettings[default_ftp_server]" value="<?php echo $current_config['default_ftp_server']; ?>"></td>
    </tr>
 
    <tr bgcolor="e6e6e6">
     <td>Attempt to use correct mimetype for FTP instead of default 'application/octet-stream'.</td>
-    <td><input type="checkbox" name="newsettings[ftp_use_mime]" value="True"<?php echo ($current_config["ftp_use_mime"]?" checked":""); ?>></td>
+    <td><input type="checkbox" name="newsettings[ftp_use_mime]" value="True"<?php echo ($current_config['ftp_use_mime']?' checked':''); ?>></td>
    </tr>
 
    <tr bgcolor="e6e6e6">
     <td>Enter your HTTP proxy server.</td>
-    <td><input name="newsettings[httpproxy_server]" value="<?php echo $current_config["httpproxy_server"]; ?>"></td>
+    <td><input name="newsettings[httpproxy_server]" value="<?php echo $current_config['httpproxy_server']; ?>"></td>
    </tr>
 
    <tr bgcolor="e6e6e6">
     <td>Enter your HTTP proxy server port.</td>
-    <td><input name="newsettings[httpproxy_port]" value="<?php echo $current_config["httpproxy_port"]; ?>"></td>
+    <td><input name="newsettings[httpproxy_port]" value="<?php echo $current_config['httpproxy_port']; ?>"></td>
    </tr>
