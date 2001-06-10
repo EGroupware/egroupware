5098bae2eaf0c8f37cb9c3325275cac61c9cab5b
skeeter <skeeter@alumni.egroupware.org>
2001-06-10 21:07:48 +0000

    Fix for sending email updates on new events/appts.

--- calendar/inc/class.calendar__.inc.php
+++ calendar/inc/class.calendar__.inc.php
@@ -79,10 +79,13 @@ class calendar__
 
 		$tz_offset = ((60 * 60) * intval($temp_tz_offset));
 
-		$t_old_start_time = mktime($old_event->start->hour,$old_event->start->min,$old_event->start->sec,$old_event->start->month,$old_event->start->mday,$old_event->start->year);
-		if($t_old_start_time < (time() - 86400))
+		if($old_event != False)
 		{
-			return False;
+			$t_old_start_time = mktime($old_event->start->hour,$old_event->start->min,$old_event->start->sec,$old_event->start->month,$old_event->start->mday,$old_event->start->year);
+			if($t_old_start_time < (time() - 86400))
+			{
+				return False;
+			}
 		}
 
 		$temp_user = $phpgw_info['user'];
