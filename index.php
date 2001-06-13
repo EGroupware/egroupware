30148312bc2c31ad759e2fbbf2d5b5b28e6bb2a4
Miles Lott <milosch@alumni.egroupware.org>
2001-06-13 07:48:20 +0000

    Make open notify window a lang call

--- index.php
+++ index.php
@@ -185,7 +185,7 @@
 <?php
 	echo '<p><table border="0" width="100%" align="center">';
 	//Uncomment the next line to enable the notify window.  It will not work until a notifywindow app is added.
-	echo '<a href="javascript:opennotifywindow()">Open notify window</a>';
+	echo '<a href="javascript:opennotifywindow()">' . lang('Open notify window') . '</a>';
 
 	$phpgw->common->hook('home',array('email','calendar','news','addressbook'));
 
