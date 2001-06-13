1548a531b7e686e3f703a1865d9fc88126640997
Miles Lott <milosch@alumni.egroupware.org>
2001-06-13 06:24:07 +0000

    change from dbl to single ticks

--- phpgwapi/inc/class.db.inc.php
+++ phpgwapi/inc/class.db.inc.php
@@ -1,4 +1,4 @@
 <?php 
-  if (empty($phpgw_info["server"]["db_type"])){$phpgw_info["server"]["db_type"] = "mysql";}
-  include(PHPGW_API_INC."/class.db_".$phpgw_info["server"]["db_type"].".inc.php"); 
+  if (empty($phpgw_info['server']['db_type'])){$phpgw_info['server']['db_type'] = 'mysql';}
+  include(PHPGW_API_INC.'/class.db_'.$phpgw_info['server']['db_type'].'.inc.php'); 
 ?>
