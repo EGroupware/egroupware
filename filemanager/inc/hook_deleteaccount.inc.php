678b3e68ace82a6233a7573ed53776253ead0d2f
zone <zone@alumni.egroupware.org>
2001-07-02 21:18:03 +0000

    Renamed deleteaccount

--- filemanager/inc/hook_deleteaccount.inc.php
+++ filemanager/inc/hook_deleteaccount.inc.php
@@ -13,16 +13,16 @@
 	global $phpgw;
 	global $account_id;
 
-	echo "HERE!!!";
-
 	/* NOTE: This is untested */
 	/* WIP: it should get all files owned by $account_id, not just in /home/account_id */
 	/* Should also be capable of transfering files to another user */
 
+/*
 	$phpgw->vfs->working_id = $account_id;
 	$ls_array = $phpgw->vfs->ls ($phpgw->vfs->fakebase . "/" . $account_id, array (RELATIVE_NONE));
 	while (list ($num, $entry) = each ($ls_array))
 	{
 		$phpgw->vfs->rm ($entry["dir"] . "/" . $entry["name"], array (RELATIVE_NONE));
 	}
+*/
 ?>
