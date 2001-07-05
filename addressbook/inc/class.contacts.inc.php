a6be13d37ae6b681cf6b200f20a237454d490f9a
Miles Lott <milosch@alumni.egroupware.org>
2001-07-05 07:41:08 +0000

    This should complete the remerge of contacts class to api

--- addressbook/inc/class.contacts.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-	if (!$phpgw_info["server"]["contact_repository"]) { $phpgw_info["server"]["contact_repository"] = "sql"; }
-	include(PHPGW_INCLUDE_ROOT."/addressbook/inc/class.contacts_".$phpgw_info["server"]["contact_repository"].".inc.php");
-	include(PHPGW_INCLUDE_ROOT."/addressbook/inc/class.contacts_shared.inc.php");
-?>
