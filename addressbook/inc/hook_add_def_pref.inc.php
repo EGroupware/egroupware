6e05eaab28627d6a899ab9c4931901db0ff16ff8
Miles Lott <milosch@alumni.egroupware.org>
2001-07-05 05:12:30 +0000

    Renamed

---
+++ addressbook/inc/hook_add_def_pref.inc.php
@@ -0,0 +1,7 @@
+<?php
+	global $pref;
+	$pref->change('addressbook','company','addressbook_True');
+	$pref->change('addressbook','lastname','addressbook_True');
+	$pref->change('addressbook','firstname','addressbook_True');
+	$pref->change('addressbook','default_category','all');
+?>
--- addressbook/inc/hook_addressbook_add_def_pref.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-	global $pref;
-	$pref->change("addressbook","company","addressbook_True");
-	$pref->change("addressbook","lastname","addressbook_True");
-	$pref->change("addressbook","firstname","addressbook_True");
-	$pref->change("addressbook","default_category","all");
-?>
