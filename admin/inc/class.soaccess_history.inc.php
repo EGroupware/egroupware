9b6cc18eb8674312199a58365b7ac0ccc27f712c
jengo <jengo@alumni.egroupware.org>
2001-07-13 07:05:19 +0000

    - Added banners - Re-added the file I just deleted on mistake (currentusers.php) - accesslog.php is no longer needed

--- admin/inc/class.soaccess_history.inc.php
+++ admin/inc/class.soaccess_history.inc.php
@@ -1,4 +1,15 @@
 <?php
+	/**************************************************************************\
+	* phpGroupWare - Administration                                            *
+	* http://www.phpgroupware.org                                              *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
 
 	class soaccess_history
 	{
