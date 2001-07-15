7e4189916ea3368bac720ac6120fa8466f04df2a
Miles Lott <milosch@alumni.egroupware.org>
2001-07-15 13:49:37 +0000

    Rollback changes pending further tests

--- phpgwapi/inc/functions.inc.php
+++ phpgwapi/inc/functions.inc.php
@@ -40,7 +40,8 @@
 	@param $classname name of class
 	@param $p1-$p16 class parameters (all optional)
 	*/
-	function CreateObject($classname, $p1='',$p2='',$p3='',$p4='',$p5='',$p6='',$p7='',$p8='',$p9='',$p10='',$p11='',$p12='',$p13='',$p14='',$p15='',$p16='')
+	function CreateObject($classname, $p1='')
+/*	function CreateObject($classname, $p1='',$p2='',$p3='',$p4='',$p5='',$p6='',$p7='',$p8='',$p9='',$p10='',$p11='',$p12='',$p13='',$p14='',$p15='',$p16='')*/
 	{
 		global $phpgw, $phpgw_info, $phpgw_domain;
 		$classpart = explode (".", $classname);
@@ -58,7 +59,8 @@
 		}
 		else
 		{
-			$obj = new $classname($p1,$p2,$p3,$p4,$p5,$p6,$p7,$p8,$p9,$p10,$p11,$p12,$p13,$p14,$p15,$p16);
+			/* $obj = new $classname($p1,$p2,$p3,$p4,$p5,$p6,$p7,$p8,$p9,$p10,$p11,$p12,$p13,$p14,$p15,$p16); */
+			$obj = new $classname($p1);
 		}
 		return $obj;
 	}
