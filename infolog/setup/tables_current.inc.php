89e392d9df6f47e59d488c87bfb80eed268260d1
Ralf Becker <ralfbecker@outdoor-training.de>
2001-07-25 22:39:14 +0000

    some more defaults to get pgsql work

--- infolog/setup/tables_current.inc.php
+++ infolog/setup/tables_current.inc.php
@@ -14,32 +14,33 @@
 	$phpgw_baseline = array(
 		'phpgw_infolog' => array(
 			'fd' => array(
-				'info_id' => array('type' => 'auto', 'nullable' => false),
-				'info_type' => array('type' => 'varchar', 'precision' => 10), 
-				'info_addr_id' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
-				'info_proj_id' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
-				'info_from' => array('type' => 'varchar', 'precision' => 64),
-				'info_addr' => array('type' => 'varchar', 'precision' => 64),
-				'info_subject' => array('type' => 'varchar', 'precision' => 64, 'nullable' => false),
-				'info_des' => array('type' => 'text'),
-				'info_owner' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
-				'info_responsible' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
-				'info_access' => array('type' => 'varchar', 'precision' => 10),
-				'info_cat' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
-				'info_datecreated' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
-				'info_startdate' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
-				'info_enddate' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
-				'info_id_parent' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
-				'info_pri' => array('type' => 'varchar', 'precision' => 10),
-				'info_time' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
-				'info_bill_cat' => array('type' => 'int', 'precision' => 4, 'nullable' => false),
-				'info_status' => array('type' => 'varchar', 'precision' => 10),
-				'info_confirm' => array('type' => 'varchar', 'precision' => 10)
+				'info_id' => array('type' => 'auto','nullable' => False),
+				'info_type' => array('type' => 'varchar', 'precision' => 255,'nullable' => False,'default' => 'task'),
+				'info_addr_id' => array('type' => 'int', 'precision' => 4,'nullable' => False, 'default' => 0),
+				'info_proj_id' => array('type' => 'int', 'precision' => 4,'nullable' => False, 'default' => 0),
+				'info_from' => array('type' => 'varchar', 'precision' => 64,'nullable' => True),
+				'info_addr' => array('type' => 'varchar', 'precision' => 64,'nullable' => True),
+				'info_subject' => array('type' => 'varchar', 'precision' => 64,'nullable' => True),
+				'info_des' => array('type' => 'text','nullable' => True),
+				'info_owner' => array('type' => 'int', 'precision' => 4,'nullable' => False),
+				'info_responsible' => array('type' => 'int', 'precision' => 4,'nullable' => False, 'default' => 0),
+				'info_access' => array('type' => 'varchar', 'precision' => 10,'nullable' => True,'default' => 'public'),
+				'info_cat' => array('type' => 'int', 'precision' => 4,'nullable' => False, 'default' => 0),
+				'info_datecreated' => array('type' => 'int', 'precision' => 4,'nullable' => False),
+				'info_startdate' => array('type' => 'int', 'precision' => 4,'nullable' => False, 'default' => 0),
+				'info_enddate' => array('type' => 'int', 'precision' => 4,'nullable' => False, 'default' => 0),
+				'info_id_parent' => array('type' => 'int', 'precision' => 4,'nullable' => False,'default' => 0),
+				'info_pri' => array('type' => 'varchar', 'precision' => 255,'nullable' => True,'default' => 'normal'),
+				'info_time' => array('type' => 'int', 'precision' => 4,'nullable' => False, 'default' => 0),
+				'info_bill_cat' => array('type' => 'int', 'precision' => 4,'nullable' => False, 'default' => 0),
+				'info_status' => array('type' => 'varchar', 'precision' => 255,'nullable' => True,'default' => 'done'),
+				'info_confirm' => array('type' => 'varchar', 'precision' => 255,'nullable' => True,'default' => 'not')
 			),
 			'pk' => array('info_id'),
 			'fk' => array(),
 			'ix' => array(),
 			'uc' => array()
-		)
+		),
 	);
+
 ?>
