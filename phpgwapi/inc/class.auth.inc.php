9b6465af7a3f26c125e276858dbd05cf6c211c4a
Miles Lott <milosch@alumni.egroupware.org>
2001-08-30 19:40:44 +0000

    Using GLOBALS

--- phpgwapi/inc/class.auth.inc.php
+++ phpgwapi/inc/class.auth.inc.php
@@ -1,4 +1,7 @@
 <?php 
-	if (empty($phpgw_info['server']['auth_type'])){$phpgw_info['server']['auth_type'] = 'sql';}
-	include(PHPGW_API_INC.'/class.auth_'.$phpgw_info['server']['auth_type'].'.inc.php');
+	if (empty($GLOBALS['phpgw_info']['server']['auth_type']))
+	{
+		$GLOBALS['phpgw_info']['server']['auth_type'] = 'sql';
+	}
+	include(PHPGW_API_INC.'/class.auth_'.$GLOBALS['phpgw_info']['server']['auth_type'].'.inc.php');
 ?>
