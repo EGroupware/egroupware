2a7159831a80af6f39db5727e92b0b2b9876e102
Miles Lott <milosch@alumni.egroupware.org>
2001-09-05 10:46:39 +0000

    Add service name wrapper classes for some basic functions

---
+++ phpgwapi/inc/class.service.inc.php
@@ -0,0 +1,63 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare API - Services Abstraction Class                            *
+  * This file written by Miles Lott <milosch@phpgroupware.org>               *
+  * Copyright (C) 2001 Miles Lott                                            *
+  * -------------------------------------------------------------------------*
+  * This library is part of the phpGroupWare API                             *
+  * http://www.phpgroupware.org/api                                          * 
+  * ------------------------------------------------------------------------ *
+  * This library is free software; you can redistribute it and/or modify it  *
+  * under the terms of the GNU Lesser General Public License as published by *
+  * the Free Software Foundation; either version 2.1 of the License,         *
+  * or any later version.                                                    *
+  * This library is distributed in the hope that it will be useful, but      *
+  * WITHOUT ANY WARRANTY; without even the implied warranty of               *
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+  * See the GNU Lesser General Public License for more details.              *
+  * You should have received a copy of the GNU Lesser General Public License *
+  * along with this library; if not, write to the Free Software Foundation,  *
+  * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA            *
+  \**************************************************************************/
+
+  /* $Id$ */
+
+	class service
+	{
+		var $provider = '';
+		var $svc      = '';
+		var $type     = '';
+		var $function_map = array();
+
+		function exec($service)
+		{
+			if(is_array($service))
+			{
+				$data     = $service[2];
+				$function = $service[1];
+				$service  = $service[0];
+			}
+			switch ($service)
+			{
+				case 'schedule':
+				case 'contacts':
+				case 'notes':
+				case 'todo':
+					$this = CreateObject('phpgwapi.service_' . $service);
+					break;
+				default:
+					$this = CreateObject($service);
+					break;
+			}
+			if($function)
+			{
+				return $this->$function($data);
+			}
+		}
+
+		function list_methods()
+		{
+			return $this->function_map;
+		}
+	}
+?>
---
+++ phpgwapi/inc/class.service_contacts.inc.php
@@ -0,0 +1,60 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare API - Services Abstraction Class                            *
+  * This file written by Miles Lott <milosch@phpgroupware.org>               *
+  * Copyright (C) 2001 Miles Lott                                            *
+  * -------------------------------------------------------------------------*
+  * This library is part of the phpGroupWare API                             *
+  * http://www.phpgroupware.org/api                                          * 
+  * ------------------------------------------------------------------------ *
+  * This library is free software; you can redistribute it and/or modify it  *
+  * under the terms of the GNU Lesser General Public License as published by *
+  * the Free Software Foundation; either version 2.1 of the License,         *
+  * or any later version.                                                    *
+  * This library is distributed in the hope that it will be useful, but      *
+  * WITHOUT ANY WARRANTY; without even the implied warranty of               *
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+  * See the GNU Lesser General Public License for more details.              *
+  * You should have received a copy of the GNU Lesser General Public License *
+  * along with this library; if not, write to the Free Software Foundation,  *
+  * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA            *
+  \**************************************************************************/
+
+  /* $Id$ */
+
+	class service_contacts extends service
+	{
+		function service_contacts()
+		{
+			$this->provider = $GLOBALS['phpgw_info']['contact_service'] ? $GLOBALS['phpgw_info']['contact_service'] : 'addressbook';
+			$this->svc = $this->provider . '.bo' . $this->provider;
+			$type = $this->type ? $this->type : 'xmlrpc';
+			$this->function_map = ExecMethod($this->svc . '.list_methods',$type);
+		}
+
+		function read($data)
+		{
+			return ExecMethod($this->svc . '.' . $this->function_map['read']['function'],$data);
+		}
+
+		function read_list($data)
+		{
+			return ExecMethod($this->svc . '.' . $this->function_map['read_list']['function'],$data);
+		}
+
+		function save($data)
+		{
+			return ExecMethod($this->svc . '.' . $this->function_map['save']['function'],$data);
+		}
+
+		function add($data)
+		{
+			return ExecMethod($this->svc . '.' . $this->function_map['add']['function'],$data);
+		}
+
+		function delete($data)
+		{
+			return ExecMethod($this->svc . '.' . $this->function_map['delete']['function'],$data);
+		}
+	}
+?>
