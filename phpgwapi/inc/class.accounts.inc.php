121e9072fc3b51f765564bd1fbe3d46eefbda5ad
Miles Lott <milosch@alumni.egroupware.org>
2001-09-23 19:08:44 +0000

    GLOBALS...

--- phpgwapi/inc/class.accounts.inc.php
+++ phpgwapi/inc/class.accounts.inc.php
@@ -1,15 +1,15 @@
 <?php
-	if (empty($phpgw_info['server']['account_repository']))
+	if (empty($GLOBALS['phpgw_info']['server']['account_repository']))
 	{
-		if (!empty($phpgw_info['server']['auth_type']))
+		if (!empty($GLOBALS['phpgw_info']['server']['auth_type']))
 		{
-			$phpgw_info['server']['account_repository'] = $phpgw_info['server']['auth_type'];
+			$GLOBALS['phpgw_info']['server']['account_repository'] = $GLOBALS['phpgw_info']['server']['auth_type'];
 		}
 		else
 		{
-			$phpgw_info['server']['account_repository'] = 'sql';
+			$GLOBALS['phpgw_info']['server']['account_repository'] = 'sql';
 		}
 	}
-	include(PHPGW_API_INC . '/class.accounts_' . $phpgw_info['server']['account_repository'] . '.inc.php');
+	include(PHPGW_API_INC . '/class.accounts_' . $GLOBALS['phpgw_info']['server']['account_repository'] . '.inc.php');
 	include(PHPGW_API_INC . '/class.accounts_shared.inc.php');
 ?>
