7fc7a192c55ee0ceee9ffd949c8fcd548cf4cb81
Miles Lott <milosch@alumni.egroupware.org>
2001-09-24 16:43:55 +0000

    Possible fix for bug #461589

--- addressbook/inc/hook_add_def_pref.inc.php
+++ addressbook/inc/hook_add_def_pref.inc.php
@@ -2,5 +2,5 @@
 	$GLOBALS['pref']->change('addressbook','company','addressbook_True');
 	$GLOBALS['pref']->change('addressbook','lastname','addressbook_True');
 	$GLOBALS['pref']->change('addressbook','firstname','addressbook_True');
-	$GLOBALS['pref']->change('addressbook','default_category','all');
+	$GLOBALS['pref']->change('addressbook','default_category','');
 ?>
