c4446115a9b8f4db8e710a35f7ad6a9579f5b106
Miles Lott <milosch@alumni.egroupware.org>
2001-09-25 07:19:16 +0000

    add header

--- doc/inlinedocparser.php
+++ doc/inlinedocparser.php
@@ -1,4 +1,17 @@
 <?php
+	/**************************************************************************\
+	* phpGroupWare                                                             *
+	* http://www.phpgroupware.org                                              *
+	* The file written by Miles Lott <milosch@phpgroupware.org>                *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
     $types = array('abstract','param','example','syntax','result','description','discussion','author','copyright','package','access');
 
     if ($fn)
