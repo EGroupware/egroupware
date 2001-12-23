17dd3eb457a08fe1f585369f8ab65507f05710ac
Miles Lott <milosch@alumni.egroupware.org>
2001-12-23 16:20:10 +0000

    Add more printed comments; change to use www.phpgroupware.org

--- phpgwapi/doc/xmlrpc/test.py
+++ phpgwapi/doc/xmlrpc/test.py
@@ -8,28 +8,30 @@ import sys
 server = Server("http://www.phpgroupware.org/cvsdemo/xmlrpc.php");
 
 try:
-    print "Listing methods:"
+    print "Listing methods:\n"
     r = server.system.listMethods();
     print r
 
-    print "Trying to login:"
+    print "\nTrying to login:\n"
     up = {'domain': 'default', 'username': 'demo', 'password': 'guest'}
     l = server.system.login(up);
     print l
 
+    print "\nTrying name/age struct/array test:\n"
     # name/age example. this exercises structs and arrays
     a = [ {'name': 'Dave', 'age': 35}, {'name': 'Edd', 'age': 45 },
           {'name': 'Fred', 'age': 23}, {'name': 'Barney', 'age': 36 }]
     r = server.examples.sortByAge(a)
     print r
 
+    print "\nTrying base 64 test:\n"
     # test base 64
     b = Binary("Mary had a little lamb She tied it to a pylon")
     b.encode(sys.stdout)
     r = server.examples.decode64(b)
     print r
     
-    print "Trying to logout:"
+    print "\nTrying to logout:\n"
     sk = {'sessionid': l['sessionid'], 'kp3': l['kp3']}
     r = server.system.logout(sk);
     print r
