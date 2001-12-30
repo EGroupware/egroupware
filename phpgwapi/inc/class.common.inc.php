e46451e26a39f1535654aca4e09989690caee80a
seek3r <seek3r@alumni.egroupware.org>
2001-12-30 09:42:55 +0000

    trying to fix problem on phpgroupware.org cvs demo

--- phpgwapi/inc/class.common.inc.php
+++ phpgwapi/inc/class.common.inc.php
@@ -673,11 +673,11 @@
 			$tpldir         = PHPGW_SERVER_ROOT . '/' . $appname . '/templates/' . $GLOBALS['phpgw_info']['server']['template_set'];
 			$tpldir_default = PHPGW_SERVER_ROOT . '/' . $appname . '/templates/default';
 
-			if (is_dir($tpldir))
+			if (@is_dir($tpldir))
 			{
 				return $tpldir;
 			}
-			elseif (is_dir($tpldir_default))
+			elseif (@is_dir($tpldir_default))
 			{
 				return $tpldir_default;
 			}
