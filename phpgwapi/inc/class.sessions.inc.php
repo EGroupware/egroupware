10077bcb4c30b9cad589e262c4b75ab70aceaebf
skeeter <skeeter@alumni.egroupware.org>
2002-01-05 02:24:10 +0000

    I think this is nothing more than a formatting change.

--- phpgwapi/inc/class.sessions.inc.php
+++ phpgwapi/inc/class.sessions.inc.php
@@ -3,5 +3,5 @@
 	{
 		$GLOBALS['phpgw_info']['server']['sessions_type'] = 'db';
 	}
-	include(PHPGW_API_INC.'/class.sessions_'.$GLOBALS['phpgw_info']['server']['sessions_type'].'.inc.php'); 
+	include(PHPGW_API_INC.'/class.sessions_'.$GLOBALS['phpgw_info']['server']['sessions_type'].'.inc.php');
 ?>
