5cff4754a0ed9154a88b4a34af837cf67b03afbb
Miles Lott <milosch@alumni.egroupware.org>
2002-01-11 02:01:24 +0000

    fix banner

--- phpgwapi/inc/class.preferences.inc.php
+++ phpgwapi/inc/class.preferences.inc.php
@@ -1,29 +1,29 @@
 <?php
 	/**************************************************************************\
-	* phpGroupWare API - Preferences							*
-	* This file written by Joseph Engo <jengo@phpgroupware.org>			*
-	* and Mark Peters <skeeter@phpgroupware.org>					*
-	* Manages user preferences								*
-	* Copyright (C) 2000, 2001 Joseph Engo						*
-	* -------------------------------------------------------------------------			*
-	* This library is part of the phpGroupWare API					*
-	* http://www.phpgroupware.org/api							* 
-	* ------------------------------------------------------------------------ 			*
-	* This library is free software; you can redistribute it and/or modify it 		*
-	* under the terms of the GNU Lesser General Public License as published by	*
-	* the Free Software Foundation; either version 2.1 of the License,			*
-	* or any later version.								*
-	* This library is distributed in the hope that it will be useful, but			*
-	* WITHOUT ANY WARRANTY; without even the implied warranty of		*
-	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	*
-	* See the GNU Lesser General Public License for more details.			*
-	* You should have received a copy of the GNU Lesser General Public License 	*
-	* along with this library; if not, write to the Free Software Foundation, 		*
-	* Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA			*
+	* phpGroupWare API - Preferences                                           *
+	* This file written by Joseph Engo <jengo@phpgroupware.org>                *
+	* and Mark Peters <skeeter@phpgroupware.org>                               *
+	* Manages user preferences                                                 *
+	* Copyright (C) 2000, 2001 Joseph Engo                                     *
+	* -------------------------------------------------------------------------*
+	* This library is part of the phpGroupWare API                             *
+	* http://www.phpgroupware.org/api                                          *
+	* ------------------------------------------------------------------------ *
+	* This library is free software; you can redistribute it and/or modify it  *
+	* under the terms of the GNU Lesser General Public License as published by *
+	* the Free Software Foundation; either version 2.1 of the License,         *
+	* or any later version.                                                    *
+	* This library is distributed in the hope that it will be useful, but      *
+	* WITHOUT ANY WARRANTY; without even the implied warranty of               *
+	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+	* See the GNU Lesser General Public License for more details.              *
+	* You should have received a copy of the GNU Lesser General Public License *
+	* along with this library; if not, write to the Free Software Foundation,  *
+	* Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA            *
 	\**************************************************************************/
 
 	/* $Id$ */
-	
+
 	/*!
 	@class preferences 
 	@abstract preferences class used for setting application preferences
@@ -505,8 +505,7 @@
 				$prefs = $this->data;
 			}
 			// are we dealing with the default email account or an extra email account?
-			if (!(isset($acctnum))
-			|| ((string)$acctnum == ''))
+			if (!(isset($acctnum)) || ((string)$acctnum == ''))
 			{
 				// account 0 is the default email account
 				$acctnum = 0;
