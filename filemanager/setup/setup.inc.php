26edf4dccbe11e821f3f2863ac5ea3b55368504d
ceb <ceb@alumni.egroupware.org>
2002-01-13 20:37:05 +0000

    added api version to deps

--- filemanager/setup/setup.inc.php
+++ filemanager/setup/setup.inc.php
@@ -26,6 +26,6 @@
 	/* Dependencies for this app to work */
 	$setup_info['phpwebhosting']['depends'][] = array(
 		 'appname' => 'phpgwapi',
-		 'versions' => array('0.9.10', '0.9.11' , '0.9.12', '0.9.13', '0.9.14')
+		 'versions' => array('0.9.13', '0.9.14','0.9.15')
 	);
 ?>
--- preferences/setup/setup.inc.php
+++ preferences/setup/setup.inc.php
@@ -28,6 +28,6 @@
 	/* Dependacies for this app to work */
 	$setup_info['preferences']['depends'][] = array(
 		 'appname' => 'phpgwapi',
-		 'versions' => Array('0.9.10', '0.9.11' , '0.9.12', '0.9.13', '0.9.14')
+		 'versions' => Array('0.9.13', '0.9.14','0.9.15')
 	);
 ?>
