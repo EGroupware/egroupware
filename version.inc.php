dc3a4f82ece390870dd6a7755e2bf39980c0a3a7
Miles Lott <milosch@alumni.egroupware.org>
2002-01-23 02:42:25 +0000

    added to aid in updates from old header versions

---
+++ version.inc.php
@@ -0,0 +1,20 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare                                                             *
+	* http://www.phpgroupware.org                                              *
+	* Written by Dan Kuykendall <seek3r@phpgroupware.org>                      *
+	*            Joseph Engo    <jengo@phpgroupware.org>                       *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
+	/*
+	 This is here only to aid in the header.inc.php update from .12 and earlier.
+	 After this you can delete it.
+	*/
+?>
