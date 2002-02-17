63b0e05ffe61aac61f0055848f42589fa1117836
Miles Lott <milosch@alumni.egroupware.org>
2002-02-17 20:20:09 +0000

    adjust banners to credit seek3r, angles, and some other dude

--- setup/inc/class.setup_html.inc.php
+++ setup/inc/class.setup_html.inc.php
@@ -3,6 +3,9 @@
   * phpGroupWare - Setup                                                     *
   * http://www.phpgroupware.org                                              *
   * --------------------------------------------                             *
+  * This file written by Tony Puglisi (Angles) <angles@phpgroupware.org>     *
+  *  and Miles Lott<milosch@phpgroupware.org>                                *
+  * --------------------------------------------                             *
   *  This program is free software; you can redistribute it and/or modify it *
   *  under the terms of the GNU General Public License as published by the   *
   *  Free Software Foundation; either version 2 of the License, or (at your  *
--- setup/inc/class.setup_process.inc.php
+++ setup/inc/class.setup_process.inc.php
@@ -3,6 +3,8 @@
   * phpGroupWare - Setup                                                     *
   * http://www.phpgroupware.org                                              *
   * --------------------------------------------                             *
+  * This file written by Miles Lott<milosch@phpgroupware.org>                *
+  * --------------------------------------------                             *
   *  This program is free software; you can redistribute it and/or modify it *
   *  under the terms of the GNU General Public License as published by the   *
   *  Free Software Foundation; either version 2 of the License, or (at your  *
