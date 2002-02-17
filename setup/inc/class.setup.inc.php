92c0e20c12a67f86697bd3462490baf638c2f011
Miles Lott <milosch@alumni.egroupware.org>
2002-02-17 20:23:26 +0000

    Adjust banners to credit just about all of the core team...

--- setup/inc/class.setup.inc.php
+++ setup/inc/class.setup.inc.php
@@ -3,6 +3,11 @@
   * phpGroupWare - Setup                                                     *
   * http://www.phpgroupware.org                                              *
   * --------------------------------------------                             *
+  * This file written by Joseph Engo<jengo@phpgroupware.org>                 *
+  *  and Dan Kuykendall<seek3r@phpgroupware.org>                             *
+  *  and Mark Peters<skeeter@phpgroupware.org>                               *
+  *  and Miles Lott<milosch@phpgroupware.org>                                *
+  * --------------------------------------------                             *
   *  This program is free software; you can redistribute it and/or modify it *
   *  under the terms of the GNU General Public License as published by the   *
   *  Free Software Foundation; either version 2 of the License, or (at your  *
