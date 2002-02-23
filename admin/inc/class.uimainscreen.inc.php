25378a7f88b0f7628e33bdf60f07b1b32ae50b3a
Miles Lott <milosch@alumni.egroupware.org>
2002-02-23 17:17:02 +0000

    adjust sql for new table names

--- admin/inc/class.uimainscreen.inc.php
+++ admin/inc/class.uimainscreen.inc.php
@@ -60,9 +60,9 @@
 				$GLOBALS['phpgw']->template->set_var('tr_color',$tr_color);
 
 				$select_lang = '<select name="select_lang">';
-				$GLOBALS['phpgw']->db->query("SELECT lang,languages.lang_name,languages.lang_id FROM phpgw_lang,phpgw_languages WHERE "
-					. "lang.lang=languages.lang_id GROUP BY lang,languages.lang_name,"
-					. "languages.lang_id ORDER BY lang");
+				$GLOBALS['phpgw']->db->query("SELECT lang,phpgw_languages.lang_name,phpgw_languages.lang_id FROM phpgw_lang,phpgw_languages WHERE "
+					. "phpgw_lang.lang=phpgw_languages.lang_id GROUP BY lang,phpgw_languages.lang_name,"
+					. "phpgw_languages.lang_id ORDER BY lang");
 				while ($GLOBALS['phpgw']->db->next_record())
 				{
 					$select_lang .= '<option value="' . $GLOBALS['phpgw']->db->f('lang') . '">' . $GLOBALS['phpgw']->db->f('lang_id')
