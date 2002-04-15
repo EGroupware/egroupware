44fd07fde4cc4a14df926830683280e0678b5be5
Miles Lott <milosch@alumni.egroupware.org>
2002-04-15 12:39:24 +0000

    update banner

--- xmlrpc.php
+++ xmlrpc.php
@@ -2,7 +2,7 @@
   /**************************************************************************\
   * phpGroupWare xmlrpc server                                               *
   * http://www.phpgroupware.org                                              *
-  * Written by Dan Kuykendall <dan@kuykendall.org>                           *
+  * Written by Miles Lott <milosch@phpgroupware.org>                         *
   * --------------------------------------------                             *
   *  This program is free software; you can redistribute it and/or modify it *
   *  under the terms of the GNU General Public License as published by the   *
