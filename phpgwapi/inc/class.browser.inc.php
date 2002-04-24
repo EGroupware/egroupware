23ec34bd8854d596e2cedc349a098689880c46e8
Miles Lott <milosch@alumni.egroupware.org>
2002-04-24 13:27:46 +0000

    OK, maybe this fixes IE downloads (part of GNU patch #136)

--- phpgwapi/inc/class.browser.inc.php
+++ phpgwapi/inc/class.browser.inc.php
@@ -284,7 +284,9 @@
 				if($nocache)
 				{
 					header('Pragma: no-cache');
+					header('Pragma: public');
 					header('Expires: 0');
+					header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 				}
 			}
 		}
