2a6ff0ffd47fa77d4c61264fb80f1b8ca7b12385
Miles Lott <milosch@alumni.egroupware.org>
2002-05-26 20:14:02 +0000

    format, fix banner

--- phpgwapi/inc/class.msg.inc.php
+++ phpgwapi/inc/class.msg.inc.php
@@ -1,28 +1,29 @@
 <?php
 	/**************************************************************************\
-	* phpGroupWare API - php IMAP SO access object constructor			*
-	* This file written by Mark Peters <skeeter@phpgroupware.org>			*
-	* and Angelo Tony Puglisi (Angles) <angles@phpgroupware.org>		*
-	* Handles initializing the appropriate class dcom object				*
-	* Copyright (C) 2001 Mark Peters							*
-	* -------------------------------------------------------------------------			*
-	* This library is part of the phpGroupWare API					*
-	* http://www.phpgroupware.org/api						* 
-	* ------------------------------------------------------------------------ 			*
-	* This library is free software; you can redistribute it and/or modify it		*
-	* under the terms of the GNU Lesser General Public License as published by	*
-	* the Free Software Foundation; either version 2.1 of the License,		*
-	* or any later version.								*
-	* This library is distributed in the hope that it will be useful, but		*
-	* WITHOUT ANY WARRANTY; without even the implied warranty of		*
-	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	*
-	* See the GNU Lesser General Public License for more details.			*
-	* You should have received a copy of the GNU Lesser General Public License 	*
-	* along with this library; if not, write to the Free Software Foundation,  	*
-	* Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA		*
+	* phpGroupWare API - php IMAP SO access object constructor                 *
+	* ------------------------------------------------------------------------ *
+	* This file written by Mark Peters <skeeter@phpgroupware.org>              *
+	* and Angelo Tony Puglisi (Angles) <angles@phpgroupware.org>               *
+	* Handles initializing the appropriate class dcom object                   *
+	* Copyright (C) 2001 Mark Peters                                           *
+	* ------------------------------------------------------------------------ *
+	* This library is part of the phpGroupWare API                             *
+	* http://www.phpgroupware.org/api                                          * 
+	* ------------------------------------------------------------------------ *
+	* This library is free software; you can redistribute it and/or modify it  *
+	* under the terms of the GNU Lesser General Public License as published by *
+	* the Free Software Foundation; either version 2.1 of the License,         *
+	* or any later version.                                                    *
+	* This library is distributed in the hope that it will be useful, but      *
+	* WITHOUT ANY WARRANTY; without even the implied warranty of               *
+	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+	* See the GNU Lesser General Public License for more details.              *
+	* You should have received a copy of the GNU Lesser General Public License *
+	* along with this library; if not, write to the Free Software Foundation,  *
+	* Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA            *
 	\**************************************************************************/
 
-	if (isset($p1)	&& ($p1) && ( (stristr($p1, 'imap') || stristr($p1, 'pop') || stristr($p1, 'nntp'))))
+	if(isset($p1) && ($p1) && ((stristr($p1, 'imap') || stristr($p1, 'pop') || stristr($p1, 'nntp'))))
 	{
 		$msg_server_type = $p1;
 	}
@@ -56,11 +57,11 @@
 	//CreateObject('phpgwapi.msg_base'.$sock_fname);
 	include(PHPGW_INCLUDE_ROOT.'/phpgwapi/inc/class.msg_base'.$sock_fname.'.inc.php');
 
-	if (($msg_server_type == 'imap')	|| ($msg_server_type == 'imaps'))
-  {
+	if (($msg_server_type == 'imap') || ($msg_server_type == 'imaps'))
+	{
 		include(PHPGW_INCLUDE_ROOT.'/phpgwapi/inc/class.msg_imap'.$sock_fname.'.inc.php');
 	}
-	elseif (($msg_server_type == 'pop3')	|| ($msg_server_type == 'pop3s'))
+	elseif (($msg_server_type == 'pop3') || ($msg_server_type == 'pop3s'))
 	{
 		include(PHPGW_INCLUDE_ROOT.'/phpgwapi/inc/class.msg_pop3'.$sock_fname.'.inc.php');
 	}
@@ -68,11 +69,11 @@
 	{
 		include(PHPGW_INCLUDE_ROOT.'/phpgwapi/inc/class.msg_nntp'.$sock_fname.'.inc.php');
 	}
-	elseif ((isset($msg_server_type))	&& ($msg_server_type != ''))
+	elseif ((isset($msg_server_type)) && ($msg_server_type != ''))
 	{
 		// educated guess based on info being available:
 		include(PHPGW_INCLUDE_ROOT.'/phpgwapi/inc/class.msg_'.$GLOBALS['phpgw_info']['user']['preferences']['email']['msg_server_type'].$sock_fname.'.inc.php');
-  	}
+	}
 	else
 	{
 		// DEFAULT FALL BACK:
--- phpgwapi/inc/class.msg_imap.inc.php
+++ phpgwapi/inc/class.msg_imap.inc.php
@@ -1,24 +1,24 @@
 <?php
   /**************************************************************************\
-  * phpGroupWare Email - IMAP abstraction				*
-  * http://www.phpgroupware.org/api					*
-  * This file written by Itzchak Rehberg <izzy@phpgroupware.org>	*
-  * and Joseph Engo <jengo@phpgroupware.org>				*
-  * Mail function abstraction for IMAP servers				*
-  * Copyright (C) 2000, 2001 Itzchak Rehberg				*
-  * -------------------------------------------------------------------------		*
+  * phpGroupWare Email - IMAP abstraction                                    *
+  * http://www.phpgroupware.org/api                                          *
+  * This file written by Itzchak Rehberg <izzy@phpgroupware.org>             *
+  * and Joseph Engo <jengo@phpgroupware.org>                                 *
+  * Mail function abstraction for IMAP servers                               *
+  * Copyright (C) 2000, 2001 Itzchak Rehberg                                 *
+  * ------------------------------------------------------------------------ *
   * This library is part of phpGroupWare (http://www.phpgroupware.org)       * 
   * This library is free software; you can redistribute it and/or modify it  *
   * under the terms of the GNU Lesser General Public License as published by *
-  * the Free Software Foundation; either version 2.1 of the License,	*
-  * or any later version.							*
-  * This library is distributed in the hope that it will be useful, but	*
-  * WITHOUT ANY WARRANTY; without even the implied warranty of	*
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	*
-  * See the GNU Lesser General Public License for more details.		*
+  * the Free Software Foundation; either version 2.1 of the License,         *
+  * or any later version.                                                    *
+  * This library is distributed in the hope that it will be useful, but      *
+  * WITHOUT ANY WARRANTY; without even the implied warranty of               *
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+  * See the GNU Lesser General Public License for more details.              *
   * You should have received a copy of the GNU Lesser General Public License *
   * along with this library; if not, write to the Free Software Foundation,  *
-  * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA	*
+  * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA            *
   \**************************************************************************/
 
   /* $Id$ */
@@ -243,7 +243,7 @@
 			}
 			return imap_search($stream,$criteria,$flags);
 		}
-		
+
 		function sort($stream,$criteria,$reverse='',$flags=0)
 		{
 			// do we force use of msg UID's 
@@ -323,7 +323,7 @@
 				{
 					$fieldFound = false;
 				}
-		
+
 				if ($fieldFound && $fieldCount == $field_no)
 				{
 					// We now need to see if the next lines belong to this  message. 
