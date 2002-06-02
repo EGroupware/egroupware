1a8bab5e440404f025e4a9d7b0b82fd7d6cdcd43
Ralf Becker <ralfbecker@outdoor-training.de>
2002-06-02 22:07:47 +0000

    changed for new standard of index.php file

--- etemplate/doc/et_media/index.php
+++ etemplate/doc/et_media/index.php
@@ -1,4 +1,17 @@
 <?php
+	/**************************************************************************\
+	* phpGroupWare - eTemplates - Example App et_media                         *
+	* http://www.phpgroupware.org                                              *
+	* Written by Ralf Becker <RalfBecker@outdoor-training.de>                  *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
 	$GLOBALS['phpgw_info']['flags'] = array(
 		'currentapp'	=> 'et_media',
 		'noheader'	=> True,
@@ -6,8 +19,5 @@
 	);
 	include('../header.inc.php');
 
-	$et_media = CreateObject('et_media.et_media');
-
-	$et_media->edit();
-
-	$GLOBALS['phpgw']->common->phpgw_footer();
+	header('Location: '.$GLOBALS['phpgw']->link('/index.php','menuaction=et_media.et_media.edit'));
+	$GLOBALS['phpgw']->common->phpgw_exit();
