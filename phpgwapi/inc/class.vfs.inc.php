241a0bbf0948af01c7874b84c5fd061eab0ae212
Zone <Zone@alumni.egroupware.org>
2003-02-17 19:35:52 +0000

    Load vfs_shared class

--- phpgwapi/inc/class.vfs.inc.php
+++ phpgwapi/inc/class.vfs.inc.php
@@ -4,5 +4,6 @@
 		$GLOBALS['phpgw_info']['server']['file_repository'] = 'sql';
 	}
 
+	include (PHPGW_API_INC . '/class.vfs_shared.inc.php');
 	include (PHPGW_API_INC . '/class.vfs_' . $GLOBALS['phpgw_info']['server']['file_repository'] . '.inc.php');
 ?>
