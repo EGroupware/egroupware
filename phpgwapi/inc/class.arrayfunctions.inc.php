6b683518ef4b5d7bc281433a60bc461d127450d0
ceb <ceb@alumni.egroupware.org>
2003-07-03 19:29:26 +0000

    update

--- phpgwapi/inc/class.arrayfunctions.inc.php
+++ phpgwapi/inc/class.arrayfunctions.inc.php
@@ -1,23 +1,25 @@
 <?php
-	/**************************************************************************\
-	* phpGroupWare API - Arrayfunctions                                        *
-	* This file written by Lars Kneschke <lkneschke@phpgw.de>                  *
-	* Copyright (C) 2002 Lars Kneschke                                         *
-	* ------------------------------------------------------------------------ *
-	* This library is part of the phpGroupWare API                             *
-	* ------------------------------------------------------------------------ *
-	* This library is free software; you can redistribute it and/or modify it  *
-	* under the terms of the GNU Lesser General Public License as published by *
-	* the Free Software Foundation; either version 2.1 of the License,         *
-	* or any later version.                                                    *
-	* This library is distributed in the hope that it will be useful, but      *
-	* WITHOUT ANY WARRANTY; without even the implied warranty of               *
-	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
-	* See the GNU Lesser General Public License for more details.              *
-	* You should have received a copy of the GNU Lesser General Public License *
-	* along with this library; if not, write to the Free Software Foundation,  *
-	* Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA            *
-	\**************************************************************************/
+	/*******************************************************************\
+	* phpGroupWare API - Arrayfunctions                                 *
+	* Written by Lars Kneschke <lkneschke@phpgw.de>                     *
+	* Copyright (C) 2002, 2003 Lars Kneschke                            *
+	* ----------------------------------------------------------------- *
+	* This library is part of the phpGroupWare API                      *
+	* ----------------------------------------------------------------- *
+	* This program is free software; you can redistribute it and/or     *
+	* modify it under the terms of the GNU General Public License as    *
+	* published by the Free Software Foundation; either version 2 of    *
+	* the License, or (at your option) any later version.               *
+	*                                                                   *
+	* This program is distributed in the hope that it will be useful,   *
+	* but WITHOUT ANY WARRANTY; without even the implied warranty of    *
+	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU  *
+	* General Public License for more details.                          *
+	*                                                                   *
+	* You should have received a copy of the GNU General Public License *
+	* along with this program; if not, write to the Free Software       *
+	* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.         *
+	\*******************************************************************/
 	/* $Id$ */
 
 	class arrayfunctions
