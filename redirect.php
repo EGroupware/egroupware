6550ed8594db63b4b2a96f41f2718ed2bc87b856
Lars Kneschke <lkeschke@alumni.egroupware.org>
2003-09-11 05:35:51 +0000

    updated documentation and make it working correctly

--- redirect.php
+++ redirect.php
@@ -1,9 +1,9 @@
 <?php
 	/**************************************************************************\
-	* phpGroupWare - save redirect script                                      *
+	* eGroupWare - save redirect script                                      *
 	* idea by: Jason Wies <jason@xc.net>                                       *
 	* doing and adding to cvs: Lars Kneschke <lkneschke@linux-at-work.de>      *
-	* http://www.phpgroupware.org                                              *
+	* http://www.egroupware.org                                              *
 	* --------------------------------------------                             *
 	*  This program is free software; you can redistribute it and/or modify it *
 	*  under the terms of the GNU General Public License as published by the   *
@@ -13,9 +13,18 @@
 	
 	/* $Id$ */
 
-	if ($GLOBALS['go'])
+	/*
+	  Use this script when you want to link to a external url.
+	  This way you don't send sonething like sessionid's as referer
+	  
+	  Use this in your app:
+	  
+	  "<a href=\"$webserverURL/redirect.php?go=".htmlentities(urlencode('http://www.egroupware.org')).'">'
+	*/
+
+	if ($_GET['go'])
 	{
-	        Header('Location: ' . $GLOBALS['go']);
+	        Header('Location: ' . html_entity_decode(urldecode($_GET['go'])));
 	        exit;
 	}
 	else
