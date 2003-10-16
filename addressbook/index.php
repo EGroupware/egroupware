8b188b06f9b875b6506e76660e1136ffce5eb7ce
Miles Lott <milosch@alumni.egroupware.org>
2003-10-16 07:10:21 +0000

    This header was wrong for a long time

--- addressbook/index.php
+++ addressbook/index.php
@@ -1,10 +1,7 @@
 <?php
 	/**************************************************************************\
-	* phpGroupWare - Messenger                                                 *
+	* phpGroupWare - Addressbook                                               *
 	* http://www.phpgroupware.org                                              *
-	* This application written by Joseph Engo <jengo@phpgroupware.org>         *
-	* --------------------------------------------                             *
-	* Funding for this program was provided by http://www.checkwithmom.com     *
 	* --------------------------------------------                             *
 	*  This program is free software; you can redistribute it and/or modify it *
 	*  under the terms of the GNU General Public License as published by the   *
