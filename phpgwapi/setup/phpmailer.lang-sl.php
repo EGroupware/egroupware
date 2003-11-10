e9a4a7e47337ec152c09fb15099c0a9baa62709d
dragob <dragob@alumni.egroupware.org>
2003-11-10 17:33:43 +0000

    Some cosmetics in translations.

---
+++ phpgwapi/setup/phpmailer.lang-sl.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * PHPMailer language file.
+ * English Version
+ */
+
+$PHPMAILER_LANG = array();
+
+$PHPMAILER_LANG["provide_address"] = 'Vnesti morate enaslov vsaj enega naslovnika.';
+$PHPMAILER_LANG["mailer_not_supported"] = ' epošta ni podprta.';
+$PHPMAILER_LANG["execute"] = 'Ne morem izvesti: ';
+$PHPMAILER_LANG["instantiate"] = 'Ne morem naložiti poštne funkcije.';
+$PHPMAILER_LANG["authenticate"] = 'SMTP napaka: Ne morem preveriti pristnosti.';
+$PHPMAILER_LANG["from_failed"] = 'Naslednji naslov pošiljatelja je napaèen: ';
+$PHPMAILER_LANG["recipients_failed"] = 'SMTP napaka: Naslednji naslovniki niso pravi: ';
+$PHPMAILER_LANG["data_not_accepted"] = 'SMTP napaka: Podatki niso bili sprejeti.';
+$PHPMAILER_LANG["connect_host"] = 'SMTP napaka: Ne morem se povezati s strežnikom SMTP.';
+$PHPMAILER_LANG["file_access"] = 'Ne morem dostopati do datoteke: ';
+$PHPMAILER_LANG["file_open"] = 'Napaka z datoteko: Ne morem odpreti datoteke: ';
+$PHPMAILER_LANG["encoding"] = 'Neznano kodiranje: ';
+?>
