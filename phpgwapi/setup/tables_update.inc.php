6b6d8f250e231881eefab441318bc3faa7cfae2b
shrykedude <shrykedude@alumni.egroupware.org>
2004-01-25 06:15:02 +0000

    increase size of last_mod fields, per bug 869442

--- phpgwapi/setup/tables_update.inc.php
+++ phpgwapi/setup/tables_update.inc.php
@@ -987,4 +987,23 @@
 		return $GLOBALS['setup_info']['phpgwapi']['currentver'];
 	}
 
+
+
+	$test[] = '0.9.99.011';
+	function phpgwapi_upgrade0_9_99_011()
+	{
+		$GLOBALS['phpgw_setup']->oProc->AlterColumn('phpgw_categories','last_mod',array(
+			'type' => 'int',
+			'precision' => '8',
+			'nullable' => False
+		));
+		$GLOBALS['phpgw_setup']->oProc->AlterColumn('phpgw_addressbook','last_mod',array(
+			'type' => 'int',
+			'precision' => '8',
+			'nullable' => False
+		));
+
+		$GLOBALS['setup_info']['phpgwapi']['currentver'] = '0.9.99.012';
+		return $GLOBALS['setup_info']['phpgwapi']['currentver'];
+	}
 ?>
