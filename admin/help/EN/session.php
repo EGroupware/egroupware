e64fd1dea6dd02f2728829c537917653c0f28f05
reinerj <reinerj@alumni.egroupware.org>
2004-01-26 23:06:52 +0000

    changes from phpgw to egw dev group

--- admin/help/EN/session.php
+++ admin/help/EN/session.php
@@ -1,7 +1,7 @@
 <?php
   /**************************************************************************\
-  * phpGroupWare - User manual                                               *
-  * http://www.phpgroupware.org                                              *
+  * eGroupWare - User manual                                                 *
+  * http://www.egroupware.org                                                *
   * --------------------------------------------                             *
   *  This program is free software; you can redistribute it and/or modify it *
   *  under the terms of the GNU General Public License as published by the   *
