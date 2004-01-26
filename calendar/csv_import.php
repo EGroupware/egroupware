6b292326c1d476c8cb9a47440528ed9134af4c5a
reinerj <reinerj@alumni.egroupware.org>
2004-01-26 23:53:02 +0000

    changes from phpgw to egw dev group

--- calendar/csv_import.php
+++ calendar/csv_import.php
@@ -1,7 +1,7 @@
 <?php
   /**************************************************************************\
-  * phpGroupWare - Calendar: CSV - Import                                    *
-  * http://www.phpgroupware.org                                              *
+  * eGroupWare - Calendar: CSV - Import                                      *
+  * http://www.egroupware.org                                                *
   * Written by Ralf Becker <RalfBecker@outdoor-training.de>                  *
   * --------------------------------------------                             *
   *  This program is free software; you can redistribute it and/or modify it *
