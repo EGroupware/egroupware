2d3fdcbed21f39098f0b91efa24388e2f186adf9
reinerj <reinerj@alumni.egroupware.org>
2004-01-27 15:32:32 +0000

    change files from phpgw to egw

--- doc/cvs_full_checkout.php
+++ doc/cvs_full_checkout.php
@@ -1,8 +1,8 @@
 #!/usr/bin/php
 <?php
 	/**************************************************************************\
-	* phpGroupWare                                                             *
-	* http://www.phpgroupware.org                                              *
+	* eGroupWare                                                               *
+	* http://www.egroupware.org                                                *
 	* The file written by Dan Kuykendall <seek3r@phpgroupware.org>             *
 	*                     Joseph Engo    <jengo@phpgroupware.org>              *
 	* --------------------------------------------                             *
@@ -19,7 +19,7 @@
 	\****************************************************************************/
 	// Temp paths that can be read and written to
 	$tmp_dir       = '/tmp';
-	// Path of where you want the phpgroupware directory to go.  NO trailing /
+	// Path of where you want the egroupware directory to go.  NO trailing /
 	$co_dir        = '/var/www/html';
 	// If you do not have developer access to cvs, change to True
 	$cvs_anonymous = True;
@@ -27,65 +27,33 @@
 	$cvs_login     = '';
 
 
-	// Modules you want to checkout, do NOT add the phpgroupware module
+	// Modules you want to checkout, do NOT add the egroupware module
 	$co_modules[] = 'addressbook';
 	$co_modules[] = 'admin';
 	$co_modules[] = 'backup';
-	$co_modules[] = 'bookkeeping';
 	$co_modules[] = 'bookmarks';
-	$co_modules[] = 'brewer';
 	$co_modules[] = 'calendar';
-	$co_modules[] = 'cart';
-	$co_modules[] = 'ccs';
-	$co_modules[] = 'cdb';
-	$co_modules[] = 'chat';
-	$co_modules[] = 'chora';
 	$co_modules[] = 'comic';
-	$co_modules[] = 'cron';
 	$co_modules[] = 'developer_tools';
-	$co_modules[] = 'dj';
-	$co_modules[] = 'eldaptir';
 	$co_modules[] = 'email';
 	$co_modules[] = 'filemanager';
 	$co_modules[] = 'forum';
 	$co_modules[] = 'ftp';
 	$co_modules[] = 'headlines';
-	$co_modules[] = 'hr';
 	$co_modules[] = 'infolog';
-	$co_modules[] = 'inv';
 	$co_modules[] = 'manual';
-	$co_modules[] = 'mediadb';
-	$co_modules[] = 'meerkat';
 	$co_modules[] = 'messenger';
-	$co_modules[] = 'napster';
-	$co_modules[] = 'netsaint';
 	$co_modules[] = 'news_admin';
-	$co_modules[] = 'nntp';
-	$co_modules[] = 'notes';
-	$co_modules[] = 'phonelog';
-	$co_modules[] = 'phpGWShell_Win32_VB';
 	$co_modules[] = 'phpgwapi';
-	$co_modules[] = 'phpgwnetsaint';
 	$co_modules[] = 'phpsysinfo';
-	$co_modules[] = 'phpwebhosting';
 	$co_modules[] = 'polls';
 	$co_modules[] = 'preferences';
 	$co_modules[] = 'projects';
-	$co_modules[] = 'qmailldap';
-	$co_modules[] = 'rbs';
 	$co_modules[] = 'setup';
 	$co_modules[] = 'skel';
 	$co_modules[] = 'soap';
-	$co_modules[] = 'squirrelmail';
 	$co_modules[] = 'stocks';
-	$co_modules[] = 'syncml-server';
-	$co_modules[] = 'timetrack';
-	$co_modules[] = 'todo';
-	$co_modules[] = 'transy';
 	$co_modules[] = 'tts';
-	$co_modules[] = 'wap';
-	$co_modules[] = 'wcm';
-	$co_modules[] = 'weather';
 	$co_modules[] = 'xmlrpc';
 
    // -- End config section
--- doc/inlinedocparser.php
+++ doc/inlinedocparser.php
@@ -1,8 +1,8 @@
 <?php
 	/**************************************************************************\
-	* phpGroupWare                                                             *
-	* http://www.phpgroupware.org                                              *
-	* The file written by Miles Lott <milosch@phpgroupware.org>                *
+	* eGroupWare                                                               *
+	* http://www.egroupware.org                                                *
+	* The file written by Miles Lott <milosch@groupwhere.org>                  *
 	* --------------------------------------------                             *
 	*  This program is free software; you can redistribute it and/or modify it *
 	*  under the terms of the GNU General Public License as published by the   *
