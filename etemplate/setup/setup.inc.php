2deefe45f71d9bdc2f2fb532599185c971b7d616
reinerj <reinerj@alumni.egroupware.org>
2004-03-28 12:46:09 +0000

    update to the next version from egw

--- etemplate/setup/setup.inc.php
+++ etemplate/setup/setup.inc.php
@@ -14,7 +14,7 @@
 
 	$setup_info['etemplate']['name']      = 'etemplate';
 	$setup_info['etemplate']['version']   = '0.9.15.002';
-	$setup_info['etemplate']['app_order'] = 8;	// just behind the developers-tools
+	$setup_info['etemplate']['app_order'] = 60;	// just behind the developers-tools
 	$setup_info['etemplate']['tables']    = array('phpgw_etemplate');
 	$setup_info['etemplate']['enable']    = 1;
 
@@ -39,6 +39,6 @@
 	/* Dependencies for this app to work */
 	$setup_info['etemplate']['depends'][] = array(
 		 'appname' => 'phpgwapi',
-		 'versions' => Array('0.9.13','0.9.14','0.9.15','0.9.16')
+		 'versions' => Array('0.9.14','0.9.15','0.9.16','1.0.0')
 	);
 ?>
