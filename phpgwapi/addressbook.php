3034363c136d2b3911fbf1ceffbc0a91853bb5e9
Lars Kneschke <lkeschke@alumni.egroupware.org>
2004-06-16 06:39:18 +0000

    add header for content type

--- phpgwapi/addressbook.php
+++ phpgwapi/addressbook.php
@@ -18,9 +18,12 @@
 		'currentapp' => 'home',	// resolves to phpgwapi, which is not allowed itself
 		'enable_nextmatchs_class' => True
 	);
-
+	
 	include('../header.inc.php');
-
+	
+	// add a content-type header to overwrite an existing default charset in apache (AddDefaultCharset directiv)
+	header('Content-type: text/html; charset='.$GLOBALS['phpgw']->translation->charset());
+	
 	$GLOBALS['phpgw_info']['flags']['currentapp'] = 'addressbook';
 	include('templates/'.$GLOBALS['phpgw_info']['user']['preferences']['common']['template_set'].'/head.inc.php');
 
