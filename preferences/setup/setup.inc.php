18bc2d28758ee196804a0a65b18bcdae6a1400bd
Ralf Becker <ralfbecker@outdoor-training.de>
2004-07-02 22:42:04 +0000

    pushing all versions to 1.0.0

--- preferences/setup/setup.inc.php
+++ preferences/setup/setup.inc.php
@@ -13,7 +13,7 @@
 
 	$setup_info['preferences']['name']      = 'preferences';
 	$setup_info['preferences']['title']     = 'Preferences';
-	$setup_info['preferences']['version']   = '0.9.13.002';
+	$setup_info['preferences']['version']   = '1.0.0';
 	$setup_info['preferences']['app_order'] = 1;
 	$setup_info['preferences']['tables']    = '';
 	$setup_info['preferences']['enable']    = 2;
@@ -21,13 +21,12 @@
 	/* The hooks this app includes, needed for hooks registration */
 	$setup_info['preferences']['hooks'][] = 'deleteaccount';
 	$setup_info['preferences']['hooks'][] = 'config';
-	$setup_info['preferences']['hooks'][] = 'manual';
 	$setup_info['preferences']['hooks'][] = 'preferences';
 	$setup_info['preferences']['hooks'][] = 'settings';
 
 	/* Dependencies for this app to work */
 	$setup_info['preferences']['depends'][] = array(
 		'appname' => 'phpgwapi',
-		'versions' => Array('0.9.10', '0.9.11', '0.9.12', '0.9.13', '0.9.14')
+		'versions' => Array('0.9.14','1.0.0')
 	);
 ?>
