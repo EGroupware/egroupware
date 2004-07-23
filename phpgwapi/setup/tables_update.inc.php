b23989981e502b8297ff4ec5739571f0726cdbce
Ralf Becker <ralfbecker@outdoor-training.de>
2004-07-23 00:47:30 +0000

    added 'async_' prefix to column-names of phpgw_async, as 'id' is a reserved word in sapdb

--- phpgwapi/setup/tables_update.inc.php
+++ phpgwapi/setup/tables_update.inc.php
@@ -28,20 +28,6 @@
 		$GLOBALS['phpgw_setup']->oProc->RenameColumn('phpgw_async','method','async_method');
 		$GLOBALS['phpgw_setup']->oProc->RenameColumn('phpgw_async','data','async_data');
 		$GLOBALS['phpgw_setup']->oProc->RenameColumn('phpgw_async','account_id','async_account_id');
-		$GLOBALS['phpgw_setup']->oProc->RefreshTable('phpgw_async',array(
-			'fd' => array(
-				'async_id' => array('type' => 'varchar','precision' => '255','nullable' => False),
-				'async_next' => array('type' => 'int','precision' => '4','nullable' => False),
-				'async_times' => array('type' => 'varchar','precision' => '255','nullable' => False),
-				'async_method' => array('type' => 'varchar','precision' => '80','nullable' => False),
-				'async_data' => array('type' => 'text','nullable' => False),
-				'async_account_id' => array('type' => 'int','precision' => '4','nullable' => False,'default' => '0')
-			),
-			'pk' => array('async_id'),
-			'fk' => array(),
-			'ix' => array(),
-			'uc' => array()
-		));
 
 		$GLOBALS['setup_info']['phpgwapi']['currentver'] = '1.0.1.001';
 		return $GLOBALS['setup_info']['phpgwapi']['currentver'];
