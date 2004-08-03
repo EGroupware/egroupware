ee1eaa2130cc9791f15965bf66c28d10aa7589f5
Ralf Becker <ralfbecker@outdoor-training.de>
2004-08-03 08:03:04 +0000

    added 1.0.1 dependency for the api

--- etemplate/doc/et_media/setup/setup.inc.php
+++ etemplate/doc/et_media/setup/setup.inc.php
@@ -14,7 +14,7 @@
 
 	$setup_info['et_media']['name']      = 'et_media';
 	$setup_info['et_media']['title']     = 'eT-Media';
-	$setup_info['et_media']['version']   = '0.9.15.001';
+	$setup_info['et_media']['version']   = '1.0.0';
 	$setup_info['et_media']['app_order'] = 100;     // at the end
 	$setup_info['et_media']['tables']    = array('phpgw_et_media');
 	$setup_info['et_media']['enable']    = 1;
@@ -34,9 +34,9 @@
 	/* Dependencies for this app to work */
 	$setup_info['et_media']['depends'][] = array(
 				'appname' => 'phpgwapi',
-				'versions' => Array('0.9.13','0.9.14','0.9.15')
+				'versions' => Array('0.9.13','0.9.14','0.9.15','1.0.0')
 	);
 	$setup_info['et_media']['depends'][] = array(   // this is only necessary as long the etemplate-class is not in the api
 				'appname' => 'etemplate',
-				'versions' => Array('0.9.13','0.9.14','0.9.15')
+				'versions' => Array('0.9.13','0.9.14','1.0.0','1.0.1')
 	);
