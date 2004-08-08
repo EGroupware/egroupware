ae5eb29bfe7a8c227dbb3247aed4a35a09c78f02
reinerj <reinerj@alumni.egroupware.org>
2004-08-08 21:29:14 +0000

    replace the crap

--- admin/inc/class.uilog.inc.php
+++ admin/inc/class.uilog.inc.php
@@ -1,7 +1,7 @@
 <?php
 	/***************************************************************************\
-	* phpGroupWare - uilog                                                      *
-	* http://www.phpgroupware.org                                               *
+	* eGroupWare - uilog                                                        *
+	* http://www.egroupware.org                                                 *
 	* Written by : jerry westrick [jerry@westrick.com]                          *
 	* -------------------------------------------------                         *
 	* This program is free software; you can redistribute it and/or modify it   *
