796c0fd53e5d4d85a2ee4c77256375341947ddbd
reinerj <reinerj@alumni.egroupware.org>
2004-08-09 10:07:33 +0000

    replace the crap

---
+++ doc/cvs_full_checkout.php
@@ -0,0 +1,115 @@
+#!/usr/bin/php
+<?php
+	/**************************************************************************\
+	* eGroupWare                                                               *
+	* http://www.egroupware.org                                                *
+	* The file written by Dan Kuykendall <seek3r@phpgroupware.org>             *
+	*                     Joseph Engo    <jengo@phpgroupware.org>              *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
+	/****************************************************************************\
+	* Config section                                                             *
+	\****************************************************************************/
+	// Temp paths that can be read and written to
+	$tmp_dir       = '/tmp';
+	// Path of where you want the egroupware directory to go.  NO trailing /
+	$co_dir        = '/var/www/html';
+	// If you do not have developer access to cvs, change to True
+	$cvs_anonymous = True;
+	// Only needed if you have developers cvs access
+	$cvs_login     = '';
+
+
+	// Modules you want to checkout, do NOT add the egroupware module
+	$co_modules[] = 'addressbook';
+	$co_modules[] = 'admin';
+	$co_modules[] = 'backup';
+	$co_modules[] = 'bookmarks';
+	$co_modules[] = 'calendar';
+	$co_modules[] = 'comic';
+	$co_modules[] = 'developer_tools';
+	$co_modules[] = 'email';
+	$co_modules[] = 'filemanager';
+	$co_modules[] = 'forum';
+	$co_modules[] = 'ftp';
+	$co_modules[] = 'headlines';
+	$co_modules[] = 'infolog';
+	$co_modules[] = 'manual';
+	$co_modules[] = 'messenger';
+	$co_modules[] = 'news_admin';
+	$co_modules[] = 'phpgwapi';
+	$co_modules[] = 'phpsysinfo';
+	$co_modules[] = 'polls';
+	$co_modules[] = 'preferences';
+	$co_modules[] = 'projects';
+	$co_modules[] = 'setup';
+	$co_modules[] = 'skel';
+	$co_modules[] = 'soap';
+	$co_modules[] = 'stocks';
+	$co_modules[] = 'tts';
+	$co_modules[] = 'xmlrpc';
+
+   // -- End config section
+
+	function docvscommand($command, $anonymous_login = False)
+	{
+		global $tmp_dir, $cvs_anonymous;
+
+		$fp = fopen($tmp_dir . '/createrelease.exp','w');
+		$contents = "#!/usr/bin/expect -f\n";
+		$contents .= "send -- \"export CVS_RSH=ssh\"\n";
+		$contents .= "set force_conservative 0\n";
+		$contents .= "if {\$force_conservative} {\n";
+		$contents .= "      set send_slow {1 .1}\n";
+		$contents .= "      proc send {ignore arg} {\n";
+		$contents .= "              sleep .1\n";
+		$contents .= "              exp_send -s -- \$arg\n";
+		$contents .= "      }\n";
+		$contents .= "}\n";
+		$contents .= "set timeout -1\n";
+		$contents .= "spawn $command\n";
+		$contents .= "match_max 100000\n";
+
+		if ($cvs_anonymous && $anonymous_login)
+		{
+			$contents .= "expect \"CVS password:\"\n";
+			$contents .= "send -- \"\\r\"\n";
+		}
+
+		$contents .= "expect eof\n";
+		fputs($fp, $contents, strlen($contents));
+		fclose($fp);
+		system('/usr/bin/expect ' . $tmp_dir . '/createrelease.exp');
+		unlink($tmp_dir . '/createrelease.exp');
+	}
+
+	chdir($co_dir);
+	if ($cvs_anonymous)
+	{
+		docvscommand('cvs -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/egroupware login',True);
+		docvscommand('cvs -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/egroupware co egroupware',True);
+	}
+	else
+	{
+		docvscommand('cvs -d' . $cvs_login . '@cvs.sourceforge.net:/cvsroot/egroupware co egroupware');
+	}
+
+	chdir($co_dir . '/egroupware');
+
+	if ($cvs_anonymous)
+	{
+		docvscommand('cvs -z3 -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/egroupware co ' . implode(' ',$co_modules));
+	}
+	else
+	{
+		docvscommand('cvs -d' . $cvs_login . '@cvs.sourceforge.net:/cvsroot/egroupware co ' . implode(' ',$co_modules));
+	}
+
+	docvscommand('cvs update -dP');
---
+++ doc/inlinedocparser_html.tpl
@@ -0,0 +1,69 @@
+<!-- BEGIN submit_button -->
+	<input tabindex="{button_tabindex}" type="submit" value="{button_value}" name="{button_name}" class="blacktext">&nbsp;
+<!-- END submit_button -->
+
+<!-- BEGIN border_top -->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<HTML>
+	<HEAD>
+		<META http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<META name="AUTHOR" content="eGroupWare inline documentation parser http://www.egroupware.org">
+		<META NAME="description" CONTENT="eGroupWare documentation">
+		<META NAME="keywords" CONTENT="eGroupWare documentation">
+		<TITLE>Local setup - Login</TITLE>
+	</HEAD>
+
+	<BODY bgcolor="#FFFFFF">
+<!-- END border_top -->
+
+<!-- BEGIN group -->
+		<H1>{group_name}</H1>
+		{group_contents}
+		<P>
+<!-- END group -->
+
+<!-- BEGIN object -->
+		<H2><A href="{PHP_SELF}?object={object_id}">{object_name}</A></H2>
+		{object_contents}
+<!-- END object -->
+
+<!-- BEGIN abstract -->
+	<B>Abstract:</B> {abstract}<BR>
+<!-- END abstract -->
+
+<!-- BEGIN generic -->
+	<B>{generic_name}:</B> {generic_value}<BR>
+<!-- END generic -->
+
+<!-- BEGIN generic_para -->
+	<P><B>{generic_name}:</B> {generic_value}</P>
+<!-- END generic_para -->
+
+<!-- BEGIN generic_pre -->
+		<B>{generic_name}:</B>
+		<PRE>
+		{generic_value}
+		</PRE>
+<!-- END generic_pre -->
+
+<!-- BEGIN params -->
+	<TABLE border="1">
+		<TR>
+			<TD>Name</TD>
+			<TD>Details</TD>
+		</TR>
+		{param_entry}
+	</TABLE>
+<!-- END params -->
+
+<!-- BEGIN param_entry -->
+		<tr>
+			<td>{name}</td>
+			<td>{details}</td>
+		</tr>
+<!-- END param_entry -->
+
+<!-- BEGIN border_bottom -->
+	</BODY>
+</HTML>
+<!-- END border_bottom -->
