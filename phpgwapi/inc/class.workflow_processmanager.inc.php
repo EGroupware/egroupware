36a38b9b56fa2bb85d1a71eae91dfd13eaa7affe
alpeb <alpeb@alumni.egroupware.org>
2004-08-10 14:08:56 +0000

    The ProcessManager class is dependant on the RoleManager class (when creating new minors/majors)

--- phpgwapi/inc/class.workflow_processmanager.inc.php
+++ phpgwapi/inc/class.workflow_processmanager.inc.php
@@ -2,6 +2,7 @@
 	// include galaxia's configuration tailored to egroupware
 	require_once(PHPGW_API_INC . SEP . 'galaxia_workflow/config.egw.inc.php');
 
+	require_once(GALAXIA_LIBRARY . SEP . 'src' . SEP . 'ProcessManager' . SEP . 'RoleManager.php');
 	require_once(GALAXIA_LIBRARY . SEP . 'src' . SEP . 'ProcessManager' . SEP . 'ProcessManager.php');
 	require_once(GALAXIA_LIBRARY . SEP . 'src' . SEP . 'ProcessManager' . SEP . 'ActivityManager.php');
 
