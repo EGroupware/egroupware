4583477a6095cc0194cd071dbc20dab2f695bdcc
reinerj <reinerj@alumni.egroupware.org>
2004-08-21 15:39:30 +0000

    change the wrong pg name

--- set_box.php
+++ set_box.php
@@ -1,7 +1,7 @@
 <?php
   /**************************************************************************\
-  * phpGroupWare                                                             *
-  * http://www.phpgroupware.org                                              *
+  * eGroupWare                                                               *
+  * http://www.egroupware.org                                                *
   * The file written by Joseph Engo <jengo@phpgroupware.org>                 *
   * --------------------------------------------                             *
   *  This program is free software; you can redistribute it and/or modify it *
