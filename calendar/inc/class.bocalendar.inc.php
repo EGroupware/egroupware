b775e8ca4ce76e4906b94feb04a3dfef6138cb94
Ralf Becker <ralfbecker@outdoor-training.de>
2004-09-08 09:42:21 +0000

    fix for bug [ 1022185 ] Last day of event shows 00:00:00 00:00:00

--- calendar/inc/class.bocalendar.inc.php
+++ calendar/inc/class.bocalendar.inc.php
@@ -2108,7 +2108,8 @@
 						continue;	// fetch recuring events only in 2. loop
 					}
 					$startdate = (int)(date('Ymd',$this->maketime($event['start'])));
-					$enddate = (int)(date('Ymd',$this->maketime($event['end'])));
+					// maketime($event['end'][)-1 to allow events to end on a full hour/day without the need to enter it as minute=59
+					$enddate = (int)(date('Ymd',$this->maketime($event['end'])-1));
 					$this->cached_events[$startdate][] = $event;
 					if($startdate != $enddate)
 					{
