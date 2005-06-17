b75dfc816d424cfa8d4f3af29fbd7015de9d21c2
Ralf Becker <ralfbecker@outdoor-training.de>
2005-06-17 07:17:54 +0000

    fixed index file: new version requires the contacts_and_resources branch

--- calendar/index.php
+++ calendar/index.php
@@ -1,25 +1,45 @@
 <?php
-/**************************************************************************\
-* eGroupWare - Calendar                                                    *
-* http://www.egroupware.org                                                *
-* Written and (c) 2005 by Ralf Becker <RalfBecker@outdoor-training.de>     *
-* --------------------------------------------                             *
-*  This program is free software; you can redistribute it and/or modify it *
-*  under the terms of the GNU General Public License as published by the   *
-*  Free Software Foundation; either version 2 of the License, or (at your  *
-*  option) any later version.                                              *
-\**************************************************************************/
+  /**************************************************************************\
+  * eGroupWare - Calendar                                                    *
+  * http://www.egroupware.org                                                *
+  * Based on Webcalendar by Craig Knudsen <cknudsen@radix.net>               *
+  *          http://www.radix.net/~cknudsen                                  *
+  * Modified by Mark Peters <skeeter@phpgroupware.org>                       *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
 
-/* $Id$ */
+	/* $Id$ */
 
-$GLOBALS['egw_info'] = array('flags' => array(
-	'currentapp'=>	'calendar',
-	'noheader'	=>	True,
-	'nonavbar'	=>	True,
-));
+	$phpgw_flags = Array(
+		'currentapp'	=>	'calendar',
+		'noheader'	=>	True,
+		'nonavbar'	=>	True,
+		'noappheader'	=>	True,
+		'noappfooter'	=>	True,
+		'nofooter'	=>	True
+	);
 
-include('../header.inc.php');
+	$GLOBALS['phpgw_info']['flags'] = $phpgw_flags;
 
-ExecMethod('calendar.uiviews.index');
+	include('../header.inc.php');
+	if(!is_object($GLOBALS['phpgw']->datetime))
+	{
+		$GLOBALS['phpgw']->datetime = CreateObject('phpgwapi.datetime');
+	}
 
-$GLOBALS['egw']->common->egw_footer();
+	$parms = Array(
+#		'menuaction'=> 'calendar.uicalendar.index',
+		'date'		=> date('Ymd',$GLOBALS['phpgw']->datetime->users_localtime)
+	);
+
+	//echo 'Local DateTime: '.date('Ymd H:i:s',$GLOBALS['phpgw']->datetime->users_localtime).'<br>'."\n";
+
+#	$GLOBALS['phpgw']->redirect_link('/index.php',$parms);
+	ExecMethod('calendar.uicalendar.index',$parms);
+	$GLOBALS['phpgw']->common->phpgw_exit();
+
+?>
