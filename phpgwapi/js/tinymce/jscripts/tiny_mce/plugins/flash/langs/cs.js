26c5ed537a5512fc15bed0f022ab59bf1e562a17
Pim Snel <mipmip@alumni.egroupware.org>
2005-06-21 18:45:19 +0000

    Add TineMCE, the future htmlArea Replacement. A clean WYSIWYG editor see tinymce.moxiecode.com/ for more info

---
+++ phpgwapi/js/tinymce/jscripts/tiny_mce/plugins/flash/langs/cs.js
@@ -0,0 +1,6 @@
+// UK lang variables
+
+tinyMCELang['lang_insert_flash']      = 'Vložit/editovat Flash Movie';
+tinyMCELang['lang_insert_flash_file'] = 'Flash soubor (.swf)';
+tinyMCELang['lang_insert_flash_size'] = 'Velikost';
+tinyMCELang['lang_flash_props'] = 'Flash properties';
---
+++ phpgwapi/js/tinymce/jscripts/tiny_mce/plugins/flash/langs/fr_ca.js
@@ -0,0 +1,6 @@
+// CA_FR lang variables
+
+tinyMCELang['lang_insert_flash']      = 'Insérer / Modifier une animation Flash';
+tinyMCELang['lang_insert_flash_file'] = 'Fichier Flash (.swf)';
+tinyMCELang['lang_insert_flash_size'] = 'Dimension';
+tinyMCELang['lang_flash_props'] = 'Flash properties';
