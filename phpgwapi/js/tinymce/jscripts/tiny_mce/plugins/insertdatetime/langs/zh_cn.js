19bb84b8828e3195dddfeef14697fa5b77d0197f
Pim Snel <mipmip@alumni.egroupware.org>
2005-06-21 18:55:36 +0000

    Add TineMCE, the future htmlArea Replacement. A clean WYSIWYG editor see tinymce.moxiecode.com/ for more info

---
+++ phpgwapi/js/tinymce/jscripts/tiny_mce/plugins/insertdatetime/langs/zh_cn.js
@@ -0,0 +1,8 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@etang.com)
+
+tinyMCELang['lang_insertdate_desc'] = '插入当前日期';
+tinyMCELang['lang_inserttime_desc'] = '插入当前时间';
+tinyMCELang['lang_inserttime_months_long'] = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+tinyMCELang['lang_inserttime_months_short'] = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+tinyMCELang['lang_inserttime_day_long'] = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+tinyMCELang['lang_inserttime_day_short'] = new Array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun");
