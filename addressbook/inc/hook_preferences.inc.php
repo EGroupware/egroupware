4250e7b6a7e430c12d8d1580ffbbaa3e14965e0c
Lars Kneschke <lkeschke@alumni.egroupware.org>
2005-09-15 08:09:17 +0000

    reverted to $GLOBALS['phpgw']

--- addressbook/inc/hook_preferences.inc.php
+++ addressbook/inc/hook_preferences.inc.php
@@ -15,10 +15,10 @@
 	// Only Modify the $file and $title variables.....
 	$title = $appname;
 	$file = array();
-	$file['Preferences']		= $GLOBALS['egw']->link('/index.php','menuaction=addressbook.uiaddressbook.preferences');
-	if(!$GLOBALS['egw_info']['server']['deny_user_grants_access'])
-		$file['Grant Access']	= $GLOBALS['egw']->link('/index.php','menuaction=preferences.uiaclprefs.index&acl_app='.$appname);
-	$file['Edit Categories']	= $GLOBALS['egw']->link('/index.php','menuaction=preferences.uicategories.index&cats_app='.$appname . '&cats_level=True&global_cats=True');
+	$file['Preferences']		= $GLOBALS['phpgw']->link('/index.php','menuaction=addressbook.uiaddressbook.preferences');
+	if(!$GLOBALS['phpgw_info']['server']['deny_user_grants_access'])
+		$file['Grant Access']	= $GLOBALS['phpgw']->link('/index.php','menuaction=preferences.uiaclprefs.index&acl_app='.$appname);
+	$file['Edit Categories']	= $GLOBALS['phpgw']->link('/index.php','menuaction=preferences.uicategories.index&cats_app='.$appname . '&cats_level=True&global_cats=True');
 
 	//Do not modify below this line
 	display_section($appname,$title,$file);
