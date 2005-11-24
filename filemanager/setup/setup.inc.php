8d5b6eef7a9212075c41f80c920ca783e4119382
Ralf Becker <ralfbecker@outdoor-training.de>
2005-11-24 19:53:15 +0000

    adding 1.2 dependcy for the api

--- filemanager/setup/setup.inc.php
+++ filemanager/setup/setup.inc.php
@@ -13,7 +13,7 @@
 
 	$setup_info['filemanager']['name']    = 'filemanager';
 	$setup_info['filemanager']['title']   = 'Filemanager';
-	$setup_info['filemanager']['version'] = '1.0.0';
+	$setup_info['filemanager']['version'] = '1.2';
 	$setup_info['filemanager']['app_order'] = 6;
 	$setup_info['filemanager']['enable']  = 1;
 
@@ -28,6 +28,6 @@
 	/* Dependencies for this app to work */
 	$setup_info['filemanager']['depends'][] = array(
 		'appname' => 'phpgwapi',
-		'versions' => array('0.9.14','0.9.16','1.0.0','1.0.1')
+		'versions' => array('1.0.0','1.0.1','1.2')
 	);
 ?>
