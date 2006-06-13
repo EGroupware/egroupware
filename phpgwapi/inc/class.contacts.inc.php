b2a00816965afaabbae230ba766625399eb10467
Ralf Becker <ralfbecker@outdoor-training.de>
2006-06-13 04:16:38 +0000

    phpDoc compatible header

--- phpgwapi/inc/class.contacts.inc.php
+++ phpgwapi/inc/class.contacts.inc.php
@@ -1,16 +1,14 @@
 <?php
-/**************************************************************************\
-* eGroupWare - contacts service provided by the addressbook application    *
-* http://www.egroupware.org                                                *
-* Written and (c) 2006 Ralf Becker <RalfBecker-AT-outdoor-training.de>     *
-* ------------------------------------------------------------------------ *
-*  This program is free software; you can redistribute it and/or modify it *
-*  under the terms of the GNU General Public License as published by the   *
-*  Free Software Foundation; either version 2 of the License, or (at your  *
-*  option) any later version.                                              *
-\**************************************************************************/
-
-/* $Id$ */
+/**
+ * API - contacts service provided by the addressbook application
+ *
+ * @link http://www.egroupware.org
+ * @author Ralf Becker <RalfBecker-AT-outdoor-training.de>
+ * @package api
+ * @copyright (c) 2006 by Ralf Becker <RalfBecker-AT-outdoor-training.de>
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @version $Id$ 
+ */
 
 require_once(EGW_INCLUDE_ROOT.'/addressbook/inc/class.bocontacts.inc.php');
 
