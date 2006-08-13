93b71ce63d8ca58874957ce9cd4b93a6de8b9179
Ralf Becker <ralfbecker@outdoor-training.de>
2006-08-13 05:11:24 +0000

    fixed "to many warnings in error_log", which caused acording to Finjon Kiang to crash apache

--- setup/check_install.php
+++ setup/check_install.php
@@ -1,19 +1,18 @@
 <?php
-  /**************************************************************************\
-  * eGroupWare - Setup Check Installation                                    *
-  * http://www.eGroupWare.org                                                *
-  * --------------------------------------------                             *
-  *  This program is free software; you can redistribute it and/or modify it *
-  *  under the terms of the GNU General Public License as published by the   *
-  *  Free Software Foundation; either version 2 of the License, or (at your  *
-  *  option) any later version.                                              *
-  \**************************************************************************/
-
-  /* $Id$ */
+	/**************************************************************************\
+	* eGroupWare - Setup Check Installation                                    *
+	* http://www.eGroupWare.org                                                *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+	
+	/* $Id$ */
 
 	$run_by_webserver = !!$_SERVER['PHP_SELF'];
 	$is_windows = strtoupper(substr(PHP_OS,0,3)) == 'WIN';
-
 
 	if ($run_by_webserver)
 	{
@@ -363,7 +362,7 @@
 		$checks = implode(', ',$checks);
 
 		$icon = $passed_icon;
-		if (($msg = $args['msg']))
+		if (isset($args['msg']) && ($msg = $args['msg']))
 		{
 			$msg .= ': '.$perms."<br />\n";
 		}
@@ -371,7 +370,7 @@
 		{
 			$msg = lang('Checking file-permissions of %1 for %2 %3: %4',$rel_name,$check_not,$checks,$perms)."<br />\n";
 		}
-		if ($args['error'])
+		if (isset($args['error']) && $args['error'])
 		{
 			$extra_error_msg = "<br />\n".$args['error'];
 		}
