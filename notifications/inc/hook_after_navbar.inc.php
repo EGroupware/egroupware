c57dadd8f7c56f4433737ebfb30eb47594a8f414
Cornelius Weiß <nelius_weiss@alumni.egroupware.org>
2006-09-07 07:04:15 +0000

    new notification module

---
+++ notifications/inc/hook_after_navbar.inc.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * eGroupWare - Notifications
+ * serves the hook "after_navbar" to create the notificationwindow
+ * 
+ * @abstract notificatonwindow is an empty and non displayed 1px div which gets rezised 
+ * and populated if a notification is about to be displayed.
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package notifications
+ * @subpackage ajaxpoup
+ * @link http://www.egroupware.org
+ * @author Cornelius Weiss <nelius@cwtech.de>
+ * @version $Id:  $
+ * @todo check if user wants notifications via egw popup!
+ */
+$GLOBALS['egw']->translation->add_app('notifications');
+if (!$GLOBALS['egw_info']['user']['preferences']['notifications']['disable_ajaxpopup']) {
+	echo '<script src="'. $GLOBALS['egw_info']['server']['webserver_url']. SEP. 'notifications'. SEP. 'js'. SEP. 'notificationajaxpopup.js'. '" type="text/javascript"></script>';
+	echo '<script type="text/javascript">notificationwindow_init()</script>';
+	echo '
+		<div id="notificationwindow" style="display: none; z-index: 999;">
+			<div id="divAppboxHeader">'. lang('Notification'). '</div>
+				<div id="divAppbox">
+				<div id="notificationwindow_message"</div>
+				<center><input type="submit" value="'. lang('ok'). '" onClick="notificationwindow_button_ok();"></center>
+			</div>
+		</div>
+	';
+}
---
+++ notifications/js/notificationajaxpopup.js
@@ -0,0 +1,41 @@
+/**
+ * eGroupWare - Notifications
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package notifications
+ * @subpackage ajaxpoup
+ * @link http://www.egroupware.org
+ * @author Cornelius Weiss <nelius@cwtech.de>
+ * @version $Id:  $
+ */
+
+function notificationwindow_init() {
+	window.setTimeout("notificationwindow_refresh();", 1000);
+}
+
+function notificationwindow_setTimeout() {
+	window.setTimeout("notificationwindow_refresh();", 10000);
+}
+function notificationwindow_refresh() {
+	xajax_doXMLHTTP("notifications.notification_popup.ajax_get_notifications");
+	notificationwindow_setTimeout();
+}
+
+function notificationwindow_display() {
+	var notificationwindow;
+	notificationwindow = document.getElementById("notificationwindow");
+	notificationwindow.style.display = "inline";
+	notificationwindow.style.position = "absolute";
+	notificationwindow.style.width = "500px";
+	notificationwindow.style.left = screen.availWidth/2 - 250 + "px";
+	notificationwindow.style.top = screen.availHeight/4 + "px";
+	notificationwindow.style.height = "100%";
+}
+
+function notificationwindow_button_ok() {
+	var notificationwindow;
+	var notificationwindow_message;
+	notificationwindow = document.getElementById("notificationwindow");
+	notificationwindow_message = document.getElementById("notificationwindow_message");
+	notificationwindow.style.display = "none";
+	notificationwindow_message.innerHTML = "";
+}
