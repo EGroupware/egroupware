cf1dad6d524f25d6d1e569e5a3cdeadec99531b6
Lars Kneschke <lkeschke@alumni.egroupware.org>
2006-10-25 08:18:13 +0000

    when synchronizing using syncml, we already have php based session started and we are (currently) not allowed to change the session_id later to solve this problem, we simply return the current session_id in new_session_id()

--- phpgwapi/inc/class.sessions.inc.php
+++ phpgwapi/inc/class.sessions.inc.php
@@ -1326,6 +1326,13 @@
 		*/
 		function new_session_id()
 		{
+			# when synchronizing using syncml, we already have php4 based session started
+			# and we are currently not allowed to change the sessionid later
+			# to solve this problem, we simply return the current session_id
+			if(basename($_SERVER["REQUEST_URI"]) == 'rpc.php' && session_id() != '') {
+				return session_id();
+			}
+			
 			return md5($GLOBALS['egw']->common->randomstring(15));
 		}
 
