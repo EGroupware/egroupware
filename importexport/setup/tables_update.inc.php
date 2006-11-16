a9b1257487d30a804c68d6b445340e46b56b3892
Cornelius Weiß <nelius_weiss@alumni.egroupware.org>
2006-11-16 11:05:42 +0000

    the mappings from old export framework can be importet  now

---
+++ importexport/setup/tables_update.inc.php
@@ -0,0 +1,32 @@
+<?php
+
+	/**
+	 * eGroupWare - Setup
+	 * http://www.egroupware.org 
+	 * Created by eTemplates DB-Tools written by ralfbecker@outdoor-training.de
+	 *
+	 * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+	 * @package importexport
+	 * @subpackage setup
+	 * @version $Id: class.db_tools.inc.php 21408 2006-04-21 10:31:06Z nelius_weiss $
+	 */
+
+	$test[] = '0.002';
+	function importexport_upgrade0_002()
+	{
+		$GLOBALS['egw_setup']->oProc->AddColumn('egw_importexport_definitions','description',array(
+			'type' => 'varchar',
+			'precision' => '255'
+		));
+
+		return $GLOBALS['setup_info']['importexport']['currentver'] = '0.003';
+	}
+
+
+	$test[] = '0.003';
+	function importexport_upgrade0_003()
+	{
+
+		return $GLOBALS['setup_info']['importexport']['currentver'] = '0.003';
+	}
+?>
