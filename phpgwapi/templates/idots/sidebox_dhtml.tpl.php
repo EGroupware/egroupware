db40bf64709e1c61e53a3ca65d719f87cedf5501
Christian Binder <christian@jaytraxx.de>
2007-01-05 21:49:36 +0000

    workaround to get resizeable sidebox and calendar dragdrop working together at the moment - we can remove this if we moved with sidebox to the dragdrop class

--- phpgwapi/templates/idots/sidebox_dhtml.tpl.php
+++ phpgwapi/templates/idots/sidebox_dhtml.tpl.php
@@ -1,4 +1,9 @@
 <!-- Savant template - sidebox dhtml -->
+<?php
+// workaround to get rid of the "too much recursion" timeout if other app uses wz_dragdrop too
+// if we moved the DHTML here to the dragdrop class we can remove this workaround
+$GLOBALS['egw_info']['flags']['wz_dragdrop_runonce_SET_DHTML'] = true;
+?>
 <script language="JavaScript" type="text/javascript">SET_DHTML("thesideboxcolumn"+NO_DRAG)</script>
 <script language="JavaScript" type="text/javascript">ADD_DHTML("sideresize"+CURSOR_W_RESIZE+MAXOFFBOTTOM+0+MAXOFFTOP+0+MAXOFFLEFT+1000+MAXOFFRIGHT+1000)</script>
 <script language="JavaScript" type="text/javascript">
