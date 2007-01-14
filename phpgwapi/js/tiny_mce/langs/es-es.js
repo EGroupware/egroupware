28fa81a805847b9a3e75fee09044da5be5bc94f7
Oscar Manuel Gómez Senovilla <omgs@escomposlinux.org>
2007-01-14 20:51:18 +0000

    spanish tinymce translation

---
+++ phpgwapi/js/tiny_mce/langs/es-es.js
@@ -0,0 +1,53 @@
+/**
+ * ES lang variables
+ * 
+ * Authors : Alvaro Velasco,
+ *           Adolfo Sanz De Diego (asanzdiego) <asanzdiego@yahoo.es>,
+ *           Carlos C Soto (eclipxe) <csoto@sia-solutions.com>
+ *           Eneko Castresana Vara
+ * Last Updated : July 14, 2006
+ * TinyMCE Version : 2.0.6.1
+ */
+
+
+tinyMCE.addToLang('',{
+bold_desc : 'Negrita (Ctrl+B)',
+bold_img : "bold_es.gif",
+bullist_desc : 'Lista sin ordenar',
+cancel : 'Cancelar',
+cleanup_desc : 'Limpiar codigo',
+clipboard_msg : 'No se pueden utilizar los comandos Copiar / Cortar / Pegar en Mozilla y en Firefox.\r\nUse el teclado Copiar (Ctrl+C) / Cortar (Ctrl+X) / Pegar (Ctrl+V)\r\n&#191;Quieres obtener m&aacute;s informaci&oacute;n?',
+edit_confirm : '¿Quieres usar el modo WYSIWYG (What You See Is What You Get - "lo que ves es lo que obtienes") para esta area de texto?',
+edit_confirm : 'Do you want to use the WYSIWYG mode for this textarea?',
+focus_alert : 'A editor instance must be focused before using this command.',
+focus_alert : 'Una instancia del editor debe ser enfocada antes de usar este comando.',
+help_desc : 'Ayuda',
+image_desc : 'Insertar imagen',
+indent_desc : 'Aumentar sangr&iacute;a',
+insert_image_alt : 'Descripci&oacute;n de la imagen',
+insert_image_src : 'Direcci&oacute;n de la imagen',
+insert_image_title : 'Insertar/editar imagen',
+insert : 'Insertar',
+insert_link_target_blank : 'Abrir enlace en una ventana nueva',
+insert_link_target : 'Destino',
+insert_link_target_same : 'Abrir enlace en la misma ventana',
+insert_link_title : 'Insertar/editar enlace',
+insert_link_url : 'Direcci&oacute;n del enlace',
+italic_desc : 'Cursiva (Ctrl+I)',
+italic_img : "italic_es.gif",
+justifycenter_desc : 'Alinear al centro',
+justifyfull_desc : 'Alinear justificado',
+justifyleft_desc : 'Alinear a la izquierda',
+justifyright_desc : 'Alinear a la derecha',
+link_desc : 'Insertar/editar enlace',
+numlist_desc : 'Lista ordenada',
+outdent_desc : 'Disminuir sangr&iacute;a',
+popup_blocked : 'Lo siento, pero tu bloqueador de popups ha desactivado una ventana que ha lanzado la aplicaci&oacute;n. Debes desactivar tu bloqueador de popups para tener toda la funcionalad de la aplicaci&oacute;n.',
+redo_desc : 'Rehacer',
+striketrough_desc : 'Tachado',
+underline_desc : 'Subrayado (Ctrl+U)',
+underline_img : "underline_es.gif",
+undo_desc : 'Deshacer',
+unlink_desc : 'Quitar enlace',
+update : 'Actualizar'
+});
