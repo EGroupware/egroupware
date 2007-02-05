59a5ebe73a5c87c261355f2a1da2b775006e275c
Christian Binder <christian@jaytraxx.de>
2007-02-05 20:22:01 +0000

    added real Minimum and Maximum values to the resizeable sidebar

--- phpgwapi/templates/idots/sidebox_dhtml.tpl.php
+++ phpgwapi/templates/idots/sidebox_dhtml.tpl.php
@@ -4,18 +4,19 @@ require_once(EGW_INCLUDE_ROOT . '/phpgwapi/inc/class.dragdrop.inc.php');
 if($GLOBALS['egw_info']['user']['preferences']['common']['enable_dragdrop'])
 {
 	$dragdrop = new dragdrop();
+	$maxOffLeft = $this->sideboxwidth - 200;
+	$maxOffRight = 500 - $this->sideboxwidth;
+	$maxOffLeft < 0 && $maxOffLeft = 0;
+	$maxOffRight < 0 && $maxOffRight = 0;
 
 	$dragdrop->addCustom(
 		'thesideboxcolumn',
-		array('NO_DRAG'),
-		false,
-		false,
-		false
+		array('NO_DRAG')
 	);
 
 	$dragdrop->addCustom(
 		'sideresize',
-		array('CURSOR_W_RESIZE','MAXOFFBOTTOM+0','MAXOFFTOP+0','MAXOFFLEFT+1000','MAXOFFRIGHT+1000'),
+		array('CURSOR_W_RESIZE','HORIZONTAL','MAXOFFLEFT+'.$maxOffLeft,'MAXOFFRIGHT+'.$maxOffRight),
 		array('sideboxwidth'=>$this->sideboxwidth),
 		'phpgwapi.dragDropFunctions.dragSidebar',
 		'phpgwapi.dragDropFunctions.dropSidebar'
