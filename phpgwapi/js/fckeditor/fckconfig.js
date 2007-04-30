eb739714537191b7dffa5024e33fd37801d1418c
Lars Kneschke <lkeschke@alumni.egroupware.org>
2007-04-30 07:55:01 +0000

    enter creates linebreak, shift-enter creates paragraph in fckeditor now. decided by ralf and me

--- phpgwapi/js/fckeditor/fckconfig.js
+++ phpgwapi/js/fckeditor/fckconfig.js
@@ -150,7 +150,7 @@ FCKConfig.ToolbarSets["egw_advanced"] = [
 ] ;
 
 FCKConfig.EnterMode = 'br' ;			// p | div | br
-FCKConfig.ShiftEnterMode = 'br' ;	// p | div | br
+FCKConfig.ShiftEnterMode = 'p' ;	// p | div | br
 
 FCKConfig.Keystrokes = [
 	[ CTRL + 65 /*A*/, true ],
