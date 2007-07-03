bd1a7729c56b1b4a2f8f6c97f36bb126731a1736
Ralf Becker <ralfbecker@outdoor-training.de>
2007-07-03 04:37:56 +0000

    fixed type of info_responsible, it's varchar, in the query, gives error in MaxDB

--- infolog/inc/class.soinfolog.inc.php
+++ infolog/inc/class.soinfolog.inc.php
@@ -252,7 +252,7 @@ class soinfolog 				// DB-Layer
 	
 			if ($filter == 'user' && $f_user > 0)
 			{
-				$filtermethod .= " AND (info_owner=$f_user AND info_responsible=0 OR ".$this->responsible_filter($f_user).')';
+				$filtermethod .= " AND (info_owner=$f_user AND info_responsible='0' OR ".$this->responsible_filter($f_user).')';
 			}
 		}
 		//echo "<p>aclFilter(filter='$filter_was',user='$user') = '$filtermethod', privat_user_list=".print_r($privat_user_list,True).", public_user_list=".print_r($public_user_list,True)."</p>\n";
