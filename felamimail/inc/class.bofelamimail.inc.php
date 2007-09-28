2bce586215dc0ae6fe262d0737dc0d34be245ee8
Klaus Leithoff <kl@stylite.de>
2007-09-28 07:15:02 +0000

    added the MULTIPART/RELATED Fix to the handling of the Structure with partID 1 in function _getSubStructure

--- felamimail/inc/class.bofelamimail.inc.php
+++ felamimail/inc/class.bofelamimail.inc.php
@@ -615,7 +615,9 @@
 					if($structure->subParts[$tempID]->type == 'MESSAGE' && $structure->subParts[$tempID]->subType == 'RFC822' &&
 					   count($structure->subParts[$tempID]->subParts) == 1 &&
 					   $structure->subParts[$tempID]->subParts[$tempID]->type == 'MULTIPART' &&
-					   ($structure->subParts[$tempID]->subParts[$tempID]->subType == 'MIXED' || $structure->subParts[$tempID]->subParts[$tempID]->subType == 'REPORT')) {
+					   ($structure->subParts[$tempID]->subParts[$tempID]->subType == 'MIXED' || 
+					    $structure->subParts[$tempID]->subParts[$tempID]->subType == 'ALTERNATIVE' ||
+					    $structure->subParts[$tempID]->subParts[$tempID]->subType == 'REPORT')) {
 						$structure = $structure->subParts[$tempID]->subParts[$tempID];
 					} else {
 						$structure = $structure->subParts[$tempID];
