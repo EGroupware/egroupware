EGroupware/egroupware

commit c09fefda34dc6f5fff917ee2f502f99bb2621ce9

"Patch #1149 from JF B: get_egwId could not deal with dashes in the Id, which can happen with LDAP"
git-svn-id: http://svn.egroupware.org/egroupware/trunk@24522 923e3c2e-ae13-0410-ba18-ced7211e33de

phpgwapi/inc/class.common.inc.php
@@ -169,8 +169,10 @@
 			if(empty($_globalUid)) return false;
 			
 			$globalUidParts = explode('-',$_globalUid);
+			array_shift($globalUidParts);	// remove the app name
+			array_pop($globalUidParts);		// remove the install_id
 			
-			return $globalUidParts[1];
+			return explode('-',$globalUidParts);	// return the rest, allowing to have dashs in the id, can happen with LDAP!
 		}
 
 		// This is used for searching the access fields

commit 015180cd0674c1b736330b15392d6bdb2935eca3

"fixed bug introduced by my commit r24522: egw could not deal with LDAP Ids"

phpgwapi/inc/class.common.inc.php
@@ -172,7 +172,7 @@
 			array_shift($globalUidParts);	// remove the app name
 			array_pop($globalUidParts);		// remove the install_id
 			
-			return explode('-',$globalUidParts);	// return the rest, allowing to have dashs in the id, can happen with LDAP!
+			return implode('-',$globalUidParts);	// return the rest, allowing to have dashs in the id, can happen with LDAP!
 		}
 
 		// This is used for searching the access fields