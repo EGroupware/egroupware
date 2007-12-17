181085c4a7728fecfd6c3f799e8fd41e28c9c93a
José Luis Gordo Romero <jgordor@alumni.egroupware.org>
2007-12-17 17:58:19 +0000

    Adding Fckeditor 2.5 to trunk

---
+++ phpgwapi/js/fckeditor/fckeditor.egwconfig.js
@@ -0,0 +1,36 @@
+FCKConfig.ToolbarSets["egw_simple"] = [
+	['Bold','Italic','Underline'],
+	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+	['UnorderedList','OrderedList','Outdent','Indent','Undo','Redo'],
+	['FitWindow'],
+	'/',
+	['FontFormat','FontName','FontSize'],
+	['TextColor','BGColor']
+] ;
+
+FCKConfig.ToolbarSets["egw_extended"] = [
+	['Bold','Italic','Underline'],
+	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+	['UnorderedList','OrderedList','Outdent','Indent','Undo','Redo'],
+	['Link','Unlink','Anchor'],
+	['Find','Replace'],
+	['FitWindow','Image','Table'],
+	'/',
+	['FontFormat','FontName','FontSize'],
+	['TextColor','BGColor']
+] ;
+
+FCKConfig.ToolbarSets["egw_advanced"] = [
+	['Source','DocProps','-','Save','NewPage','Preview','-','Templates'],
+	['Cut','Copy','Paste','PasteText','PasteWord','-','Print'/*,'SpellCheck'*/],
+	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+	'/',
+	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
+	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+	['UnorderedList','OrderedList','-','Outdent','Indent'],
+	['Link','Unlink','Anchor'],
+	['FitWindow','Image',/*'Flash',*/'Table','Rule',/*'Smiley',*/'SpecialChar','PageBreak'], //,'UniversalKey'
+	'/',
+	['Style','FontFormat','FontName','FontSize'],
+	['TextColor','BGColor']
+] ;
