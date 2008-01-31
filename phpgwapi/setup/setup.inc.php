e1214a3896be129c37e01380a66ada384b81dd1b
Klaus Leithoff <kl@stylite.de>
2008-01-31 12:35:15 +0000

    increment version, because of table field alterations in egw_addressbook

--- phpgwapi/setup/setup.inc.php
+++ phpgwapi/setup/setup.inc.php
@@ -14,7 +14,7 @@
 	/* Basic information about this app */
 	$setup_info['phpgwapi']['name']      = 'phpgwapi';
 	$setup_info['phpgwapi']['title']     = 'eGroupWare API';
-	$setup_info['phpgwapi']['version']   = '1.5.001';
+	$setup_info['phpgwapi']['version']   = '1.5.002';
 	$setup_info['phpgwapi']['versions']['current_header'] = '1.28';
 	$setup_info['phpgwapi']['enable']    = 3;
 	$setup_info['phpgwapi']['app_order'] = 1;
