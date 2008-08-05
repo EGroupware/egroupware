4f799dad542f240efe84097ce2a070e580dcbd1f
Ralf Becker <ralfbecker@outdoor-training.de>
2008-08-05 06:06:09 +0000

    moved PEAR & HTTP_WebDAV_Server checks into the API and set version to 1.6

--- filemanager/setup/setup.inc.php
+++ filemanager/setup/setup.inc.php
@@ -10,7 +10,7 @@
 
 $setup_info['filemanager']['name']    = 'filemanager';
 $setup_info['filemanager']['title']   = 'Filemanager';
-$setup_info['filemanager']['version'] = '1.5';
+$setup_info['filemanager']['version'] = '1.6';
 $setup_info['filemanager']['app_order'] = 6;
 $setup_info['filemanager']['enable']  = 1;
 
@@ -20,17 +20,5 @@ $setup_info['filemanager']['enable']  = 1;
 /* Dependencies for this app to work */
 $setup_info['filemanager']['depends'][] = array(
 	'appname' => 'phpgwapi',
-	'versions' => array('1.3','1.4','1.5')
-);
-
-// installation checks for filemanager
-$setup_info['filemanager']['check_install'] = array(
-	'' => array(
-		'func' => 'pear_check',
-		'from' => 'Filemanager',
-	),
-	'HTTP_WebDAV_Server' => array(
-		'func' => 'pear_check',
-		'from' => 'Filemanager',
-	),
+	'versions' => array('1.5','1.6','1.7')
 );
