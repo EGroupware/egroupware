aceef3f54880efd0a94cd6df37e66b317f170773
Ralf Becker <ralfbecker@outdoor-training.de>
2008-10-07 12:25:28 +0000

    "header update"

--- etemplate/process_exec.php
+++ etemplate/process_exec.php
@@ -1,26 +1,24 @@
 <?php
-	/**************************************************************************\
-	* eGroupWare - eTemplates - process_exec                                   *
-	* http://www.egroupware.org                                                *
-	* Written by Ralf Becker <RalfBecker@outdoor-training.de>                  *
-	* --------------------------------------------                             *
-	*  This program is free software; you can redistribute it and/or modify it *
-	*  under the terms of the GNU General Public License as published by the   *
-	*  Free Software Foundation; either version 2 of the License, or (at your  *
-	*  option) any later version.                                              *
-	\**************************************************************************/
+/**
+ * eGroupWare - eTemplates - Editor
+ *
+ * @link http://www.egroupware.org
+ * @author Ralf Becker <RalfBecker-AT-outdoor-training.de>
+ * @package etemplate
+ * @copyright (c) 2002-8 by Ralf Becker <RalfBecker-AT-outdoor-training.de>
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @version $Id$
+ */
 
-	/* $Id$ */
+list($app) = explode('.',$_GET['menuaction']);
 
-	list($app) = explode('.',$_GET['menuaction']);
+$GLOBALS['egw_info'] = array(
+	'flags' => array(
+		'currentapp'	=> $app,
+		'noheader'		=> True,
+		'nonavbar'		=> True,
+	),
+);
+include('../header.inc.php');
 
-	$GLOBALS['egw_info'] = array(
-		'flags' => array(
-			'currentapp'	=> $app,
-			'noheader'		=> True,
-			'nonavbar'		=> True,
-		),
-	);
-	include('../header.inc.php');
-
-	ExecMethod('etemplate.etemplate.process_exec');
+ExecMethod('etemplate.etemplate.process_exec');
