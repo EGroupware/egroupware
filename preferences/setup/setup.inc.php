3931b25010eb325b9d02f10f674a6fd4a03f843a
Ralf Becker <ralfbecker@outdoor-training.de>
2008-10-10 11:51:29 +0000

    "version and dependencies for 1.6"

--- preferences/setup/setup.inc.php
+++ preferences/setup/setup.inc.php
@@ -1,33 +1,30 @@
 <?php
-	/**************************************************************************\
-	* eGroupWare - Preferences                                                 *
-	* http://www.egroupware.org                                                *
-	* --------------------------------------------                             *
-	*  This program is free software; you can redistribute it and/or modify it *
-	*  under the terms of the GNU General Public License as published by the   *
-	*  Free Software Foundation; either version 2 of the License, or (at your  *
-	*  option) any later version.                                              *
-	\**************************************************************************/
+/**
+ * eGroupWare - Preferences
+ *
+ * @link http://www.egroupware.org
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package preferences
+ * @subpackage setup
+ * @version $Id$
+ */
 
-	/* $Id$ */
+$setup_info['preferences']['name']      = 'preferences';
+$setup_info['preferences']['title']     = 'Preferences';
+$setup_info['preferences']['version']   = '1.6';
+$setup_info['preferences']['app_order'] = 1;
+$setup_info['preferences']['tables']    = '';
+$setup_info['preferences']['enable']    = 2;
 
-	$setup_info['preferences']['name']      = 'preferences';
-	$setup_info['preferences']['title']     = 'Preferences';
-	$setup_info['preferences']['version']   = '1.4';
-	$setup_info['preferences']['app_order'] = 1;
-	$setup_info['preferences']['tables']    = '';
-	$setup_info['preferences']['enable']    = 2;
+/* The hooks this app includes, needed for hooks registration */
+$setup_info['preferences']['hooks'][] = 'deleteaccount';
+$setup_info['preferences']['hooks'][] = 'config';
+$setup_info['preferences']['hooks'][] = 'preferences';
+$setup_info['preferences']['hooks'][] = 'settings';
+$setup_info['preferences']['hooks']['edit_user']    = 'preferences.uisettings.edit_user';
 
-	/* The hooks this app includes, needed for hooks registration */
-	$setup_info['preferences']['hooks'][] = 'deleteaccount';
-	$setup_info['preferences']['hooks'][] = 'config';
-	$setup_info['preferences']['hooks'][] = 'preferences';
-	$setup_info['preferences']['hooks'][] = 'settings';
-	$setup_info['preferences']['hooks']['edit_user']    = 'preferences.uisettings.edit_user';
-
-	/* Dependencies for this app to work */
-	$setup_info['preferences']['depends'][] = array(
-		'appname' => 'phpgwapi',
-		'versions' => Array('1.2','1.3','1.4','1.5')
-	);
-?>
+/* Dependencies for this app to work */
+$setup_info['preferences']['depends'][] = array(
+	'appname' => 'phpgwapi',
+	'versions' => Array('1.2','1.3','1.4','1.5','1.6','1.7')
+);
