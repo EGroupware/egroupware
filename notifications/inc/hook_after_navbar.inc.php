21338a83277215ae51f3d3f350531cad975b00da
Ralf Becker <ralfbecker@outdoor-training.de>
2009-03-24 09:33:28 +0000

    "added filemtime to force browser to reload javascript file after a modification"

--- notifications/inc/hook_after_navbar.inc.php
+++ notifications/inc/hook_after_navbar.inc.php
@@ -15,13 +15,14 @@
 $notification_config = config::read('notifications');
 if ($notification_config['popup_enable']) {
 	$GLOBALS['egw']->translation->add_app('notifications');
-	echo '<script src="'. $GLOBALS['egw_info']['server']['webserver_url']. '/notifications/js/notificationajaxpopup.js'. '" type="text/javascript"></script>';
+	echo '<script src="'. $GLOBALS['egw_info']['server']['webserver_url']. '/notifications/js/notificationajaxpopup.js?'.
+		filemtime(EGW_SERVER_ROOT.'/notifications/js/notificationajaxpopup.js'). '" type="text/javascript"></script>';
 	echo '<script type="text/javascript">egwpopup_init();</script>';
 	echo '
 		<div id="egwpopup" style="display: none; z-index: 999;">
 			<div id="egwpopup_header">'.lang('Notification').'</div>
 			<div id="egwpopup_message"></div>
-			<div id="egwpopup_footer">	
+			<div id="egwpopup_footer">
 				<input id="egwpopup_ok_button" type="submit" value="'. lang('ok'). '" onClick="egwpopup_button_ok();">
 			</div>
 		</div>
