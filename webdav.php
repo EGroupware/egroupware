90fa0dc0e20deffd57d661ae7bf6cb883d901e20
Ralf Becker <ralfbecker@outdoor-training.de>
2009-06-25 10:25:35 +0000

    "reverting my inition comit to switch output buffering off in webdav.php, as it makes problems on system with do automatic output compression, eg. debian5 --> modified header template to not switch it on in first place, for webdav.php"

--- webdav.php
+++ webdav.php
@@ -89,10 +89,6 @@ catch (egw_exception_no_permission_app $e)
 }
 $headertime = microtime(true);
 
-// switch of output buffering (switched on in header), as otherwise we run against memory_limit, for big files
-// only do it if no output_handler is set in php.ini, as at least with zlib.output_handler it stalls ...
-if (!ini_get('output_handler')) ob_end_flush();
-
 $webdav_server = new vfs_webdav_server();
 $webdav_server->ServeRequest();
 //error_log(sprintf("WebDAV %s request took %5.3f s (header include took %5.3f s)",$_SERVER['REQUEST_METHOD'],microtime(true)-$starttime,$headertime-$starttime));
