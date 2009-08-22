19e6159a4acd38f2a8a7decf1cf532fbb333f8a8
Ralf Becker <ralfbecker@outdoor-training.de>
2009-08-22 06:58:38 +0000

    - phpDocumentor headers and improved docu about class methods - coding stylite cleanup

--- phpgwapi/inc/class.ldap.inc.php
+++ phpgwapi/inc/class.ldap.inc.php
@@ -1,255 +1,268 @@
 <?php
-	/**************************************************************************\
-	* eGroupWare API - Accounts manager for LDAP                               *
-	* This file written by Lars Kneschke <l.kneschke@metaways.de>              *
-	* View and manipulate contact records using LDAP                           *
-	* ------------------------------------------------------------------------ *
-	* This library is part of the eGroupWare API                               *
-	* http://www.egroupware.org/api                                            *
-	* --------------------------------------------                             *
-	*  This program is free software; you can redistribute it and/or modify it *
-	*  under the terms of the GNU General Public License as published by the   *
-	*  Free Software Foundation; version 2 of the License.                     *
-	\**************************************************************************/
-	
-	/* $Id$ */
-	
-	/*!
-	 @class contacts
-	 @abstract Contact List System
-	 @discussion Author: jengo/Milosch <br>
-	 This class provides a contact database scheme. <br>
-	 It attempts to be based on the vcard 2.1 standard, with mods as needed to make for more reasonable sql storage. <br>
-	 The LDAP schema used here may require installation of schema files available in the phpgwapi/doc/ldap dir.
-	 Please see the README file there.
-	 Syntax: CreateObject('phpgwapi.contacts'); <br>
-	 Example1: $contacts = CreateObject('phpgwapi.contacts');
+/**
+ * API - LDAP connection handling
+ *
+ * @link http://www.egroupware.org
+ * @author Lars Kneschke <l.kneschke@metaways.de>
+ * @author Ralf Becker <RalfBecker-AT-outdoor-training.de>
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package api
+ * @subpackage ldap
+ * @version $Id$
+ */
+
+/**
+ * LDAP connection handling
+ */
+class ldap
+{
+	/**
+	* @var resource $ds holds the LDAP link identifier
 	*/
-	class ldap
+	var $ds;
+
+	/**
+	* @var array $ldapServerInfo holds the detected information about the different ldap servers
+	*/
+	var $ldapServerInfo;
+
+	/**
+	 * the constructor for this class
+	 */
+	function __construct()
 	{
-		/**
-		* @var resource $ds holds the LDAP link identifier
-		*/
-		var $ds;
-		
-		/**
-		* @var array $ldapServerInfo holds the detected information about the different ldap servers
-		*/
-		var $ldapServerInfo;
-		
-		/**
-		 * the constructor for this class
-		 */
-		function ldap() {
-			$this->restoreSessionData();
-		}
-		
-		/**
-		 * escapes a string for use in searchfilters meant for ldap_search.
-		 *
-		 * Escaped Characters are: '*', '(', ')', ' ', '\', NUL
-		 * It's actually a PHP-Bug, that we have to escape space.
-		 * For all other Characters, refer to RFC2254.
-		 * @param $string either a string to be escaped, or an array of values to be escaped
-		 */
-		function getLDAPServerInfo($_host)
-		{
-			if(is_a($this->ldapServerInfo[$_host], 'ldapserverinfo')) {
-				return $this->ldapServerInfo[$_host];
-			}
-			
-			return false;
-		}
+		$this->restoreSessionData();
+	}
 
-		/**
-		 * escapes a string for use in searchfilters meant for ldap_search.
-		 *
-		 * Escaped Characters are: '*', '(', ')', ' ', '\', NUL
-		 * It's actually a PHP-Bug, that we have to escape space.
-		 * For all other Characters, refer to RFC2254.
-		 * 
-		 * @param string/array $string either a string to be escaped, or an array of values to be escaped
-		 */
-		function quote($string)
+	/**
+	 * escapes a string for use in searchfilters meant for ldap_search.
+	 *
+	 * Escaped Characters are: '*', '(', ')', ' ', '\', NUL
+	 * It's actually a PHP-Bug, that we have to escape space.
+	 * For all other Characters, refer to RFC2254.
+	 * @param $string either a string to be escaped, or an array of values to be escaped
+	 * @return ldapserverinfo|boolean
+	 */
+	function getLDAPServerInfo($_host)
+	{
+		if(is_a($this->ldapServerInfo[$_host], 'ldapserverinfo'))
 		{
-			return str_replace(array('\\','*','(',')','\0',' '),array('\\\\','\*','\(','\)','\\0','\20'),$string);
+			return $this->ldapServerInfo[$_host];
 		}
+		return false;
+	}
 
-		/**
-		 * connect to the ldap server and return a handle
-		 *
-		 * @param $host ldap host
-		 * @param $dn ldap dn
-		 * @param $passwd ldap pw
-		 */
-		function ldapConnect($host='', $dn='', $passwd='')
+	/**
+	 * escapes a string for use in searchfilters meant for ldap_search.
+	 *
+	 * Escaped Characters are: '*', '(', ')', ' ', '\', NUL
+	 * It's actually a PHP-Bug, that we have to escape space.
+	 * For all other Characters, refer to RFC2254.
+	 *
+	 * @param string|array $string either a string to be escaped, or an array of values to be escaped
+	 * @return string
+	 */
+	static function quote($string)
+	{
+		return str_replace(array('\\','*','(',')','\0',' '),array('\\\\','\*','\(','\)','\\0','\20'),$string);
+	}
+
+	/**
+	 * connect to the ldap server and return a handle
+	 *
+	 * @param $host ldap host
+	 * @param $dn ldap dn
+	 * @param $passwd ldap pw
+	 * @return resource|boolean resource from ldap_connect() or false on error
+	 */
+	function ldapConnect($host='', $dn='', $passwd='')
+	{
+		if(!function_exists('ldap_connect'))
 		{
-			if(!function_exists('ldap_connect'))
+			/* log does not exist in setup(, yet) */
+			if(isset($GLOBALS['egw']->log))
 			{
-				/* log does not exist in setup(, yet) */
-				if(isset($GLOBALS['egw']->log))
+				$GLOBALS['egw']->log->message('F-Abort, LDAP support unavailable');
+				$GLOBALS['egw']->log->commit();
+			}
+
+			printf('<b>Error: LDAP support unavailable</b><br>',$host);
+			return False;
+		}
+		if(!$host)
+		{
+			$host = $GLOBALS['egw_info']['server']['ldap_host'];
+		}
+
+		if(!$dn)
+		{
+			$dn = $GLOBALS['egw_info']['server']['ldap_root_dn'];
+		}
+
+		if(!$passwd)
+		{
+			$passwd = $GLOBALS['egw_info']['server']['ldap_root_pw'];
+		}
+
+		// connects to ldap server
+		if(!$this->ds = ldap_connect($host))
+		{
+			/* log does not exist in setup(, yet) */
+			if(isset($GLOBALS['egw']->log))
+			{
+				$GLOBALS['egw']->log->message('F-Abort, Failed connecting to LDAP server');
+				$GLOBALS['egw']->log->commit();
+			}
+
+			printf("<b>Error: Can't connect to LDAP server %s!</b><br>",$host);
+			echo function_backtrace(1);
+			return False;
+		}
+
+		if(ldap_set_option($this->ds, LDAP_OPT_PROTOCOL_VERSION, 3))
+		{
+			$supportedLDAPVersion = 3;
+		}
+		else
+		{
+			$supportedLDAPVersion = 2;
+		}
+
+		if(!isset($this->ldapServerInfo[$host]))
+		{
+			//error_log("no ldap server info found");
+			$ldapbind = @ldap_bind($this->ds, $GLOBALS['egw_info']['server']['ldap_root_dn'], $GLOBALS['egw_info']['server']['ldap_root_pw']);
+
+			$filter='(objectclass=*)';
+			$justthese = array('structuralObjectClass','namingContexts','supportedLDAPVersion','subschemaSubentry');
+
+			if(($sr = @ldap_read($this->ds, '', $filter, $justthese)))
+			{
+				if($info = ldap_get_entries($this->ds, $sr))
 				{
-					$GLOBALS['egw']->log->message('F-Abort, LDAP support unavailable');
-					$GLOBALS['egw']->log->commit();
-				}
+					$ldapServerInfo = new ldapserverinfo();
 
-				printf('<b>Error: LDAP support unavailable</b><br>',$host);
-				return False;
-			}
-			if(!$host)
-			{
-				$host = $GLOBALS['egw_info']['server']['ldap_host'];
-			}
+					$ldapServerInfo->setVersion($supportedLDAPVersion);
 
-			if(!$dn)
-			{
-				$dn = $GLOBALS['egw_info']['server']['ldap_root_dn'];
-			}
-
-			if(!$passwd)
-			{
-				$passwd = $GLOBALS['egw_info']['server']['ldap_root_pw'];
-			}
-
-			// connects to ldap server
-			if(!$this->ds = ldap_connect($host))
-			{
-				/* log does not exist in setup(, yet) */
-				if(isset($GLOBALS['egw']->log))
-				{
-					$GLOBALS['egw']->log->message('F-Abort, Failed connecting to LDAP server');
-					$GLOBALS['egw']->log->commit();
-				}
-
-				printf("<b>Error: Can't connect to LDAP server %s!</b><br>",$host);
-				echo function_backtrace(1);
-				return False;
-			}
-
-			if(ldap_set_option($this->ds, LDAP_OPT_PROTOCOL_VERSION, 3)) {
-				$supportedLDAPVersion = 3;
-			} else {
-				$supportedLDAPVersion = 2;
-			}
-
-			if(!isset($this->ldapServerInfo[$host])) {
-				//error_log("no ldap server info found");
-				$ldapbind = @ldap_bind($this->ds, $GLOBALS['egw_info']['server']['ldap_root_dn'], $GLOBALS['egw_info']['server']['ldap_root_pw']);
-
-				$filter='(objectclass=*)';
-				$justthese = array('structuralObjectClass','namingContexts','supportedLDAPVersion','subschemaSubentry');
-				
-				if(($sr = @ldap_read($this->ds, '', $filter, $justthese))) {
-					if($info = ldap_get_entries($this->ds, $sr)) {
-						$ldapServerInfo = new ldapserverinfo();
-		
-						$ldapServerInfo->setVersion($supportedLDAPVersion);
-						
-						// check for naming contexts
-						if($info[0]['namingcontexts']) {
-							for($i=0; $i<$info[0]['namingcontexts']['count']; $i++) {
-								$namingcontexts[] = $info[0]['namingcontexts'][$i];
-							}
-							$ldapServerInfo->setNamingContexts($namingcontexts);
+					// check for naming contexts
+					if($info[0]['namingcontexts'])
+					{
+						for($i=0; $i<$info[0]['namingcontexts']['count']; $i++)
+						{
+							$namingcontexts[] = $info[0]['namingcontexts'][$i];
 						}
+						$ldapServerInfo->setNamingContexts($namingcontexts);
+					}
 
-						// check for ldap server type
-						if($info[0]['structuralobjectclass']) {
-							switch($info[0]['structuralobjectclass'][0]) {
-								case 'OpenLDAProotDSE':
-									$ldapServerType = OPENLDAP_LDAPSERVER;
-									break;
-								default:
-									$ldapServerType = UNKNOWN_LDAPSERVER;
-									break;
-							}
-							$ldapServerInfo->setServerType($ldapServerType);
+					// check for ldap server type
+					if($info[0]['structuralobjectclass'])
+					{
+						switch($info[0]['structuralobjectclass'][0])
+						{
+							case 'OpenLDAProotDSE':
+								$ldapServerType = OPENLDAP_LDAPSERVER;
+								break;
+							default:
+								$ldapServerType = UNKNOWN_LDAPSERVER;
+								break;
 						}
-						
-						// check for subschema entry dn
-						if($info[0]['subschemasubentry']) {
-							$subschemasubentry = $info[0]['subschemasubentry'][0];
-							$ldapServerInfo->setSubSchemaEntry($subschemasubentry);
-						}
+						$ldapServerInfo->setServerType($ldapServerType);
+					}
 
-						// create list of supported objetclasses
-						if(!empty($subschemasubentry)) {
-							$filter='(objectclass=*)';
-							$justthese = array('objectClasses');
-							
-							if($sr=ldap_read($this->ds, $subschemasubentry, $filter, $justthese)) {
-								if($info = ldap_get_entries($this->ds, $sr)) {
-									if($info[0]['objectclasses']) {
-										for($i=0; $i<$info[0]['objectclasses']['count']; $i++) {
-											$pattern = '/^\( (.*) NAME \'(\w*)\' /';
-											if(preg_match($pattern, $info[0]['objectclasses'][$i], $matches)) {
-												#_debug_array($matches);
-												if(count($matches) == 3) {
-													$supportedObjectClasses[$matches[1]] = strtolower($matches[2]);
-												}
+					// check for subschema entry dn
+					if($info[0]['subschemasubentry'])
+					{
+						$subschemasubentry = $info[0]['subschemasubentry'][0];
+						$ldapServerInfo->setSubSchemaEntry($subschemasubentry);
+					}
+
+					// create list of supported objetclasses
+					if(!empty($subschemasubentry))
+					{
+						$filter='(objectclass=*)';
+						$justthese = array('objectClasses');
+
+						if($sr=ldap_read($this->ds, $subschemasubentry, $filter, $justthese))
+						{
+							if($info = ldap_get_entries($this->ds, $sr))
+							{
+								if($info[0]['objectclasses']) {
+									for($i=0; $i<$info[0]['objectclasses']['count']; $i++)
+									{
+										$pattern = '/^\( (.*) NAME \'(\w*)\' /';
+										if(preg_match($pattern, $info[0]['objectclasses'][$i], $matches))
+										{
+											#_debug_array($matches);
+											if(count($matches) == 3)
+											{
+												$supportedObjectClasses[$matches[1]] = strtolower($matches[2]);
 											}
 										}
-
-										$ldapServerInfo->setSupportedObjectClasses($supportedObjectClasses);
 									}
+									$ldapServerInfo->setSupportedObjectClasses($supportedObjectClasses);
 								}
 							}
 						}
-						
-						$this->ldapServerInfo[$host] = $ldapServerInfo;
 					}
-				} else {
-					$this->ldapServerInfo[$host] = false;
+					$this->ldapServerInfo[$host] = $ldapServerInfo;
 				}
-				$this->saveSessionData();
-			} else {
-				$ldapServerInfo = $this->ldapServerInfo[$host];
 			}
-
-			if(!@ldap_bind($this->ds, $dn, $passwd)) {
-				if(isset($GLOBALS['egw']->log)) {
-					$GLOBALS['egw']->log->message('F-Abort, Failed binding to LDAP server');
-					$GLOBALS['egw']->log->commit();
-				}
-
-				printf("<b>Error: Can't bind to LDAP server: %s!</b> %s<br />",$dn,function_backtrace(1));
-				return False;
-			}
-
-			return $this->ds;
-		}
-
-		/**
-		 * disconnect from the ldap server
-		 */
-		function ldapDisconnect() 
-		{
-			if(is_resource($this->ds)) 
+			else
 			{
-				ldap_unbind($this->ds);
+				$this->ldapServerInfo[$host] = false;
 			}
+			$this->saveSessionData();
 		}
-		
-		/**
-		 * restore the session data
-		 */
-		function restoreSessionData() 
+		else
 		{
-			if (isset($GLOBALS['egw']->session))	// no availible in setup
-			{
-				$this->ldapServerInfo = (array) unserialize($GLOBALS['egw']->session->appsession('ldapServerInfo'));
-			}
+			$ldapServerInfo = $this->ldapServerInfo[$host];
 		}
-		/**
-		 * save the session data
-		 */
-		function saveSessionData() 
+
+		if(!@ldap_bind($this->ds, $dn, $passwd))
 		{
-			if (isset($GLOBALS['egw']->session))	// no availible in setup
+			if(isset($GLOBALS['egw']->log))
 			{
-				$GLOBALS['egw']->session->appsession('ldapServerInfo','',serialize($this->ldapServerInfo));
+				$GLOBALS['egw']->log->message('F-Abort, Failed binding to LDAP server');
+				$GLOBALS['egw']->log->commit();
 			}
+
+			printf("<b>Error: Can't bind to LDAP server: %s!</b> %s<br />",$dn,function_backtrace(1));
+			return False;
 		}
-		                                                        
+
+		return $this->ds;
 	}
-?>
+
+	/**
+	 * disconnect from the ldap server
+	 */
+	function ldapDisconnect()
+	{
+		if(is_resource($this->ds))
+		{
+			ldap_unbind($this->ds);
+		}
+	}
+
+	/**
+	 * restore the session data
+	 */
+	function restoreSessionData()
+	{
+		if (isset($GLOBALS['egw']->session))	// no availible in setup
+		{
+			$this->ldapServerInfo = (array) unserialize($GLOBALS['egw']->session->appsession('ldapServerInfo'));
+		}
+	}
+	/**
+	 * save the session data
+	 */
+	function saveSessionData()
+	{
+		if (isset($GLOBALS['egw']->session))	// no availible in setup
+		{
+			$GLOBALS['egw']->session->appsession('ldapServerInfo','',serialize($this->ldapServerInfo));
+		}
+	}
+}
--- phpgwapi/inc/class.ldapserverinfo.inc.php
+++ phpgwapi/inc/class.ldapserverinfo.inc.php
@@ -1,137 +1,126 @@
 <?php
-	/**************************************************************************\
-	* eGroupWare API - Accounts manager for LDAP                               *
-	* This file written by Lars Kneschke <l.kneschke@metaways.de>              *
-	* View and manipulate contact records using LDAP                           *
-	* ------------------------------------------------------------------------ *
-	* This library is part of the eGroupWare API                               *
-	* http://www.egroupware.org/api                                            *
-	* --------------------------------------------                             *
-	*  This program is free software; you can redistribute it and/or modify it *
-	*  under the terms of the GNU General Public License as published by the   *
-	*  Free Software Foundation; version 2 of the License.                     *
-	\**************************************************************************/
-	
-	/* $Id$ */
+/**
+ * API - LDAP server information
+ *
+ * @link http://www.egroupware.org
+ * @author Lars Kneschke <l.kneschke@metaways.de>
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package api
+ * @subpackage ldap
+ * @version $Id$
+ */
 
-	define('UNKNOWN_LDAPSERVER',0);
-	define('OPENLDAP_LDAPSERVER',1);
+define('UNKNOWN_LDAPSERVER',0);
+define('OPENLDAP_LDAPSERVER',1);
 
-	/*!
-	 @class contacts
-	 @abstract Contact List System
-	 @discussion Author: jengo/Milosch <br>
-	 This class provides a contact database scheme. <br>
-	 It attempts to be based on the vcard 2.1 standard, with mods as needed to make for more reasonable sql storage. <br>
-	 The LDAP schema used here may require installation of schema files available in the phpgwapi/doc/ldap dir.
-	 Please see the README file there.
-	 Syntax: CreateObject('phpgwapi.contacts'); <br>
-	 Example1: $contacts = CreateObject('phpgwapi.contacts');
+/**
+ * Class to store and retrieve information (eg. supported object classes) of a connected ldap server
+ */
+class ldapserverinfo
+{
+	/**
+	* @var array $namingContext holds the supported namingcontexts
 	*/
-	class ldapserverinfo
+	var $namingContext = array();
+
+	/**
+	* @var string $version holds the LDAP server version
+	*/
+	var $version = 2;
+
+	/**
+	* @var integer $serverType holds the type of LDAP server(OpenLDAP, ADS, NDS, ...)
+	*/
+	var $serverType = 0;
+
+	/**
+	* @var string $_subSchemaEntry the subschema entry DN
+	*/
+	var $subSchemaEntry = '';
+
+	/**
+	* @var array $supportedObjectClasses the supported objectclasses
+	*/
+	var $supportedObjectClasses = array();
+
+	/**
+	* @var array $supportedOIDs the supported OIDs
+	*/
+	var $supportedOIDs = array();
+
+	/**
+	* gets the version
+	*
+	* @return integer the supported ldap version
+	*/
+	function getVersion()
 	{
-		/**
-		* @var array $namingContext holds the supported namingcontexts
-		*/
-		var $namingContext = array();
-		
-		/**
-		* @var string $version holds the LDAP server version
-		*/
-		var $version = 2;
-		
-		/**
-		* @var integer $serverType holds the type of LDAP server(OpenLDAP, ADS, NDS, ...)
-		*/
-		var $serverType = 0;
-
-		/**
-		* @var string $_subSchemaEntry the subschema entry DN
-		*/
-		var $subSchemaEntry = '';
-
-		/**
-		* @var array $supportedObjectClasses the supported objectclasses
-		*/
-		var $supportedObjectClasses = array();
-		
-		/**
-		* @var array $supportedOIDs the supported OIDs
-		*/
-		var $supportedOIDs = array();
-		
-		/**
-		* the constructor for this class
-		*/
-		/*function ldapserverinfo() {
-		}*/
-		
-		/**
-		* gets the version
-		*
-		* @return integer the supported ldap version
-		*/
-		function getVersion() {
-			return $this->version;
-		}
-
-		/**
-		* sets the namingcontexts
-		*
-		* @param array $_namingContext the supported namingcontexts
-		*/
-		function setNamingContexts($_namingContext) {
-			$this->namingContext = $_namingContext;
-		}
-
-		/**
-		* sets the type of the ldap server(OpenLDAP, ADS, NDS, ...)
-		*
-		* @param integer $_serverType the type of ldap server
-		*/
-		function setServerType($_serverType) {
-			$this->serverType = $_serverType;
-		}
-
-		/**
-		* sets the DN for the subschema entry
-		*
-		* @param string $_subSchemaEntry the subschema entry DN
-		*/
-		function setSubSchemaEntry($_subSchemaEntry) {
-			$this->subSchemaEntry = $_subSchemaEntry;
-		}
-
-		/**
-		* sets the supported objectclasses
-		*
-		* @param array $_supportedObjectClasses the supported objectclasses
-		*/
-		function setSupportedObjectClasses($_supportedObjectClasses) {
-			$this->supportedOIDs = $_supportedObjectClasses;
-			$this->supportedObjectClasses = array_flip($_supportedObjectClasses);
-		}
-
-		/**
-		* sets the version
-		*
-		* @param integer $_version the supported ldap version
-		*/
-		function setVersion($_version) {
-			$this->version = $_version;
-		}
-		
-		/**
-		* checks for supported objectclasses
-		*
-		* @return bool returns true if the ldap server supports this objectclass
-		*/
-		function supportsObjectClass($_objectClass) {
-			if($this->supportedObjectClasses[strtolower($_objectClass)]) {
-				return true;
-			} else {
-				return false;
-			}
-		}
+		return $this->version;
 	}
-?>
+
+	/**
+	* sets the namingcontexts
+	*
+	* @param array $_namingContext the supported namingcontexts
+	*/
+	function setNamingContexts($_namingContext)
+	{
+		$this->namingContext = $_namingContext;
+	}
+
+	/**
+	* sets the type of the ldap server(OpenLDAP, ADS, NDS, ...)
+	*
+	* @param integer $_serverType the type of ldap server
+	*/
+	function setServerType($_serverType)
+	{
+		$this->serverType = $_serverType;
+	}
+
+	/**
+	* sets the DN for the subschema entry
+	*
+	* @param string $_subSchemaEntry the subschema entry DN
+	*/
+	function setSubSchemaEntry($_subSchemaEntry)
+	{
+		$this->subSchemaEntry = $_subSchemaEntry;
+	}
+
+	/**
+	* sets the supported objectclasses
+	*
+	* @param array $_supportedObjectClasses the supported objectclasses
+	*/
+	function setSupportedObjectClasses($_supportedObjectClasses)
+	{
+		$this->supportedOIDs = $_supportedObjectClasses;
+		$this->supportedObjectClasses = array_flip($_supportedObjectClasses);
+	}
+
+	/**
+	* sets the version
+	*
+	* @param integer $_version the supported ldap version
+	*/
+	function setVersion($_version)
+	{
+		$this->version = $_version;
+	}
+
+	/**
+	* checks for supported objectclasses
+	*
+	* @return bool returns true if the ldap server supports this objectclass
+	*/
+	function supportsObjectClass($_objectClass)
+	{
+		if($this->supportedObjectClasses[strtolower($_objectClass)])
+		{
+			return true;
+		}
+		return false;
+	}
+}
