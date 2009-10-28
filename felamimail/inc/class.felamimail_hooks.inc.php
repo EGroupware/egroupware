3dd56d81dcc173f66dca20f23b5e26a353ddea44
Ralf Becker <ralfbecker@outdoor-training.de>
2009-10-28 21:21:16 +0000

    "installation default pref to show mails on home is no good idea, as mail is usually not yet configured"

--- felamimail/inc/class.felamimail_hooks.inc.php
+++ felamimail/inc/class.felamimail_hooks.inc.php
@@ -253,7 +253,6 @@ class felamimail_hooks
 				'values' => $selectOptions,
 				'xmlrpc' => True,
 				'admin'  => False,
-				'default'=> '2',
 			),
 			'mainscreen_showfolders' => array(
 				'type'   => 'multiselect',
