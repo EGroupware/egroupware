7574857f8c7e5db75402031050d7f7b7e82949cc
Ralf Becker <ralfbecker@outdoor-training.de>
2010-01-24 00:06:25 +0000

    fixed not displayed custom field type "link-entry"

--- etemplate/inc/class.link_widget.inc.php
+++ etemplate/inc/class.link_widget.inc.php
@@ -406,7 +406,7 @@ class link_widget
 			$value = array(
 				'app'        => is_array($value) && isset($value['default_sel']) ? $value['default_sel'] : $app,
 				'no_app_sel' => !!$extension_data['app'],
-				'id'         => is_array($value) ? $value['current'] : $value,
+				'id'         => is_array($value) ? $value['current'] : $id,
 				'query'      => is_array($value) ? $value['query'] : '',
 				'blur'       => count($options) == 1 ? lang($app) : lang('Search'),
 				'extra'      => $cell['onchange'] ? ','.self::AJAX_NEED_ONCHANGE : null,	// store flang for ajax_search, to display extra_line required by onchange
