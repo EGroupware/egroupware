ffa48c7c40fcfece24c2ac8a49bdd1b572650eb7
Andreas Stöckel <andreas.stoeckel@googlemail.com>
2010-05-21 11:51:40 +0000

    Committed CKEditor 3.2.1 stock version

---
+++ phpgwapi/js/ckeditor3/ckeditor.egwconfig.js
@@ -0,0 +1,88 @@
+CKEDITOR.editorConfig = function( config )
+{
+
+	config.toolbar_egw_simple = [
+		['Bold','Italic','Underline'],
+		['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+		['BulletList','NumberedList','Outdent','Indent','Undo','Redo'],
+		['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print'],
+		['Maximize'],
+		'/',
+		['Format','Font','FontSize'],
+		['TextColor','BGColor'],
+		['ShowBlocks','-','About']
+	] ;
+
+	config.toolbar_egw_simple_spellcheck = [
+		['Bold','Italic','Underline'],
+		['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+		['BulletList','NumberedList','Outdent','Indent','Undo','Redo'],
+		['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print'],
+		['Maximize','SpellCheck'],
+		'/',
+		['Format','Font','FontSize'],
+		['TextColor','BGColor'],
+		['ShowBlocks','-','About']
+	] ;
+
+	config.toolbar_egw_extended = [
+		['Bold','Italic','Underline'],
+		['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+		['BulletList','NumberedList','Outdent','Indent','Undo','Redo'],
+		['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print'],
+		['Link','Unlink','Anchor'],
+		['Find','Replace'],
+		['Maximize','Image','Table'],
+		'/',
+		['Format','Font','FontSize'],
+		['TextColor','BGColor'],
+		['ShowBlocks','-','About']
+	] ;
+
+	config.toolbar_egw_extended_spellcheck = [
+		['Bold','Italic','Underline'],
+		['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+		['BulletList','NumberedList','Outdent','Indent','Undo','Redo'],
+		['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print'],
+		['Link','Unlink','Anchor'],
+		['Find','Replace'],
+		['Maximize','SpellCheck','Image','Table'],
+		'/',
+		['Format','Font','FontSize'],
+		['TextColor','BGColor'],
+		['ShowBlocks','-','About']
+	] ;
+
+	config.toolbar_egw_advanced = [
+		['Source','DocProps','-','Save','NewPage','Preview','-','Templates'],
+		['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print'],
+		['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+		'/',
+		['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+		['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+		['BulletList','NumberedList','-','Outdent','Indent'],
+		['Link','Unlink','Anchor'],
+		['Maximize','Image',/*'Flash',*/'Table','HorizontalRule',/*'Smiley',*/'SpecialChar','PageBreak'], //,'UniversalKey'
+		'/',
+		['Style','Format','Font','FontSize'],
+		['TextColor','BGColor'],
+		['ShowBlocks','-','About']
+	] ;
+
+	config.toolbar_egw_advanced_spellcheck = [
+		['Source','DocProps','-','Save','NewPage','Preview','-','Templates'],
+		['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print','SpellCheck'],
+		['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+		'/',
+		['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+		['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+		['BulletList','NumberedList','-','Outdent','Indent'],
+		['Link','Unlink','Anchor'],
+		['Maximize','Image',/*'Flash',*/'Table','HorizontalRule',/*'Smiley',*/'SpecialChar','PageBreak'], //,'UniversalKey'
+		'/',
+		['Style','Format','Font','FontSize'],
+		['TextColor','BGColor'],
+		['ShowBlocks','-','About']
+	] ;
+
+}
