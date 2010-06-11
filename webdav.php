f05e8181124e655c96f3abedfab2134d4ec38a95
Jörg Lehrke <jlehrke@noc.de>
2010-06-11 09:05:35 +0000

    Fix WebDAV support for Bitkinex client (EGw.org#2650)

--- webdav.php
+++ webdav.php
@@ -98,9 +98,10 @@ $GLOBALS['egw']->session->commit_session();
 $webdav_server = new vfs_webdav_server();
 $user_agent = strtolower($_SERVER['HTTP_USER_AGENT']);
 if (strstr($user_agent, 'microsoft-webdav') !== false ||
-	strstr($user_agent, 'neon') !== false)
+	strstr($user_agent, 'neon') !== false ||
+	strstr($user_agent, 'bitkinex') !== false)
 {
-	// neon & Windows 7 special treatment
+	// Windows 7 et.al. special treatment
 	$webdav_server->cnrnd = true;
 }
 $webdav_server->ServeRequest();
