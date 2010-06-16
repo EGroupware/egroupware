230d666597d3dfdb89fe86ef4622cbccb34486df
Jörg Lehrke <jlehrke@noc.de>
2010-06-16 08:37:45 +0000

    Fix config::save_value static call issue

--- phpgwapi/inc/class.config.inc.php
+++ phpgwapi/inc/class.config.inc.php
@@ -126,9 +126,8 @@ class config
 			throw new egw_exception_assertion_failed('$app parameter required for static call of config::save_value($name,$value,$app)!');
 		}
 		//echo "<p>config::save_value('$name','".print_r($value,True)."','$app')</p>\n";
-		if (!$app || isset($this) && $app == $this->appname)
+		if (!$app || isset($this) && is_a($this,'config') && $app == $this->appname)
 		{
-			$app = $this->appname;
 			$this->config_data[$name] = $value;
 		}
 		//echo "<p>config::save_value('$name','".print_r($value,True)."','$app')</p>\n";
@@ -223,7 +222,7 @@ class config
 					// manually retrieve the string lengths of the serialized array if unserialize failed
 					$test = @unserialize(preg_replace('!s:(\d+):"(.*?)";!se', "'s:'.mb_strlen('$2','8bit').':\"$2\";'", $value));
 				}
-
+ 
 				$config[$name] = is_array($test) ? $test : $value;
 			}
 		}
