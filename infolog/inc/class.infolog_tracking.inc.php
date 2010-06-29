2bf4f56dfdd81e7baeca68f29c397f28526d9ed2
Klaus Leithoff <kl@stylite.de>
2010-06-29 12:05:40 +0000

    notify on infolog creation as well (was not, as condition was not met for changes=true)

--- infolog/inc/class.infolog_tracking.inc.php
+++ infolog/inc/class.infolog_tracking.inc.php
@@ -284,6 +284,7 @@ class infolog_tracking extends bo_tracking
 		if(is_null($changed_fields))
 		{
 			$changed_fields = $this->changed_fields($data, $old);
+			$changes = count($changed_fields); // we need that since TRUE evaluates to 1
 		}
 		if(is_array($changed_fields) && $changes == 1 && in_array('info_datemodified', $changed_fields))
 		{
