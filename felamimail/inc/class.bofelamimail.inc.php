21e83f292843e28ebf3ee2295bc3d6fa67a32a25
Klaus Leithoff <kl@stylite.de>
2010-07-08 08:44:02 +0000

    fix typo

--- felamimail/inc/class.bofelamimail.inc.php
+++ felamimail/inc/class.bofelamimail.inc.php
@@ -1156,7 +1156,7 @@
 			$attachments = $this->getMessageAttachments($_uid, $_part);
 			foreach($attachments as $attachment) {
 				#error_log(print_r($attachment,true));
-				if(strpos($attachment['cid'], $_cid) !== false || strpos($cid, $attachment['cid']) !== false) {
+				if(strpos($attachment['cid'], $_cid) !== false || strpos($_cid, $attachment['cid']) !== false) {
 					$partID = $attachment['partID'];
 					break;
 				}
