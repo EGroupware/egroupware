e95f4e6ebab9c138d462b3630899f4eba4d15168
Klaus Leithoff <kl@stylite.de>
2010-09-13 08:14:03 +0000

    fix for expected parameter for data must be an array in set_timer, when starting async testjob; handling array as adr in send when creating messages

--- admin/inc/class.uiasyncservice.inc.php
+++ admin/inc/class.uiasyncservice.inc.php
@@ -59,7 +59,7 @@ class uiasyncservice
 				{
 					echo '<p><b>'.lang("You have no email address for your user set !!!")."</b></p>\n";
 				}
-				elseif (!$async->set_timer($times,'test','admin.uiasyncservice.test',$GLOBALS['egw_info']['user']['email']))
+				elseif (!$async->set_timer($times,'test','admin.uiasyncservice.test',(array)$GLOBALS['egw_info']['user']['email']))
 				{
 					echo '<p><b>'.lang("Error setting timer, wrong syntax or maybe there's one already running !!!")."</b></p>\n";
 				}
