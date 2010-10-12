8aa40841e0be8fe13e4969e07163a5a78f6ee974
Ralf Becker <ralfbecker@outdoor-training.de>
2010-10-12 14:33:19 +0000

    fix for mystylite bug #818: planner by user only with read rights (should work with freebusy rights too)

--- calendar/inc/class.calendar_uiviews.inc.php
+++ calendar/inc/class.calendar_uiviews.inc.php
@@ -2091,7 +2091,7 @@ function open_edit(series)
 				foreach((array) $GLOBALS['egw']->accounts->member($user) as $data)
 				{
 					$user = $data['account_id'];
-					if ($this->bo->check_perms(EGW_ACL_READ,0,$user))
+					if ($this->bo->check_perms(EGW_ACL_READ | EGW_ACL_FREEBUSY,0,$user))
 					{
 						$users[$user] = $this->bo->participant_name($user);
 					}
