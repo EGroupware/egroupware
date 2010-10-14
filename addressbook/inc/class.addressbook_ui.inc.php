9f315b6187dea54c47e54f6fbfb0c9035a1e4234
Nathan Gray <nathangray.bsc@gmail.com>
2010-10-14 15:53:09 +0000

    * Fix for Addressbook History NOT showing old or new value for some custom fields (Stylite #8230)

--- addressbook/inc/class.addressbook_ui.inc.php
+++ addressbook/inc/class.addressbook_ui.inc.php
@@ -2247,7 +2247,7 @@ class addressbook_ui extends addressbook_bo
 					$content['history']['status-widgets']['#'.$name] = $settings['type'];
 				} elseif($settings['values']['@']) {
 					$content['history']['status-widgets']['#'.$name] = customfields_widget::_get_options_from_file($settings['values']['@']);
-				} else {
+				} elseif(count($settings['values'])) {
 					$content['history']['status-widgets']['#'.$name] = $settings['values'];
 				}
 			}
