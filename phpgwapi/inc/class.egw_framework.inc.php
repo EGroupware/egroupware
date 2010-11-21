594efde2f97e8ac2b9bf4ab477ab719867a62c2b
Ralf Becker <ralfbecker@outdoor-training.de>
2010-11-21 13:37:09 +0000

    removed wrong app parameter from about url and disabled error_log for not found js file

--- phpgwapi/inc/class.egw_framework.inc.php
+++ phpgwapi/inc/class.egw_framework.inc.php
@@ -650,7 +650,7 @@ abstract class egw_framework
 		// We handle this here becuase its special
 		$apps['about']['title'] = lang('About %1','EGroupware');
 
-		$apps['about']['url']   = egw::link('/about.php','app='.$app);
+		$apps['about']['url']   = egw::link('/about.php');
 		$apps['about']['icon']  = common::image('phpgwapi',Array('about','nonav'));
 		$apps['about']['icon_hover']  = common::image_on('phpgwapi',Array('about','nonav'),'-over');
 		$apps['about']['name'] = 'about';
@@ -1119,7 +1119,7 @@ abstract class egw_framework
 			}
 			return True;
 		}
-		error_log(__METHOD__."($package,$file,$app) $path NOT found!");
+		//error_log(__METHOD__."($package,$file,$app) $path NOT found!");
 		return False;
 	}
 
