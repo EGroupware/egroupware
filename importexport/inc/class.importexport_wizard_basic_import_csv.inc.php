21f71990c12a2204b5120118ab1a8b16e40e1228
Klaus Leithoff <kl@stylite.de>
2010-11-24 10:10:01 +0000

    option to choose how to handle categories -> update or replace; backport of rev32605 of Nathan Gray

--- importexport/inc/class.importexport_wizard_basic_import_csv.inc.php
+++ importexport/inc/class.importexport_wizard_basic_import_csv.inc.php
@@ -195,6 +195,14 @@ class importexport_wizard_basic_import_csv
 			$sel_options['charset'] = $GLOBALS['egw']->translation->get_installed_charsets()+
 				array('utf-8' => 'utf-8 (Unicode)');
 			$preserv = $content;
+			if($this->mapping_fields['cat_id']) {
+				$sel_options['update_cats'] = array(
+					'add'   => lang('Add'),
+					'replace'=> lang('Replace')
+				);
+			} else {
+				$content['no_cats'] = true;
+			}
 			unset ($preserv['button']);
 			return $this->step_templates[$content['step']];
 		}
