0efb958fa1d386e7c29c77c5a8978bb978c52a6f
Nathan Gray <nathangray.bsc@gmail.com>
2011-01-12 17:02:43 +0000

    Load the translations for the app for headings

--- importexport/inc/class.importexport_wizard_basic_export_csv.inc.php
+++ importexport/inc/class.importexport_wizard_basic_export_csv.inc.php
@@ -59,6 +59,9 @@ class importexport_wizard_basic_export_csv
 			'wizard_step30' => lang('Choose fields to export'),
 			'wizard_step40' => lang('Choose seperator and charset'),
 		);
+		list($appname, $part2) = explode('_', get_class($this));
+		if(!$GLOBALS['egw_info']['apps'][$appname]) $appname .= $part2; // Handle apps with _ in the name
+		translation::add_app($appname);
 	}
 
 	/**
