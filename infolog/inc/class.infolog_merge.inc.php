509306fcf8ec45a6e63a144cf45185a2d92446da
Christian Binder <christian@jaytraxx.de>
2011-02-07 10:44:08 +0000

    exclude field "info_link" for merge which contains an array and cannot be handled this way by bo_merge

--- infolog/inc/class.infolog_merge.inc.php
+++ infolog/inc/class.infolog_merge.inc.php
@@ -101,6 +101,7 @@ class infolog_merge extends bo_merge
 		// Add markers
 		foreach($array as $key => &$value)
 		{
+			if($key == 'info_link') continue; // not used for merge
 			if(!$value) $value = '';
 			$info['$$'.($prefix ? $prefix.'/':'').$key.'$$'] = $value;
 		}
