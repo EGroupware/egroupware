19ede9a2cc1cdd21397b1a653ca4ea65fd594809
Nathan Gray <nathangray.bsc@gmail.com>
2011-02-22 19:56:49 +0000

    Get list of extensions from parent class

--- addressbook/inc/class.addressbook_hooks.inc.php
+++ addressbook/inc/class.addressbook_hooks.inc.php
@@ -257,7 +257,7 @@ class addressbook_hooks
 				'name'   => 'default_document',
 				'help'   => lang('If you specify a document (full vfs path) here, addressbook displays an extra document icon for each address. That icon allows to download the specified document with the contact data inserted.').' '.
 					lang('The document can contain placeholder like {{n_fn}}, to be replaced with the contact data (%1full list of placeholder names%2).','<a href="'.$link.'" target="_blank">','</a>').' '.
-					lang('At the moment the following document-types are supported:').'*.rtf, *.txt',
+					lang('At the moment the following document-types are supported:'). implode(',',bo_merge::get_file_extensions()),
 				'run_lang' => false,
 				'xmlrpc' => True,
 				'admin'  => False,
@@ -269,7 +269,7 @@ class addressbook_hooks
 				'name'   => 'document_dir',
 				'help'   => lang('If you specify a directory (full vfs path) here, addressbook displays an action for each document. That action allows to download the specified document with the contact data inserted.').' '.
 					lang('The document can contain placeholder like {{n_fn}}, to be replaced with the contact data (%1full list of placeholder names%2).','<a href="'.$link.'" target="_blank">','</a>').' '.
-					lang('At the moment the following document-types are supported:').'*.rtf, *.txt',
+					lang('At the moment the following document-types are supported:'). implode(',',bo_merge::get_file_extensions()),
 				'run_lang' => false,
 				'xmlrpc' => True,
 				'admin'  => False,
