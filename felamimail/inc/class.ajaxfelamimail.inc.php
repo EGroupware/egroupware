f14704947ec9cd58a9395aee8feeb64c6ff0bf68
Klaus Leithoff <kl@stylite.de>
2011-04-13 12:11:55 +0000

    * eMail: fix a problem regarding the doubleencoding of text while toggling compose from html->text

--- felamimail/inc/class.ajaxfelamimail.inc.php
+++ felamimail/inc/class.ajaxfelamimail.inc.php
@@ -263,9 +263,10 @@ class ajaxfelamimail
 					$_content = implode('',$contentArr);
 				}
 				$_content = $bocompose->_getCleanHTML($_content);
-				$_content = $bocompose->convertHTMLToText($_content);
+				//$_content = $bocompose->convertHTMLToText($_content);
+				$_content = translation::convertHTMLToText($_content,'ISO-8859-1',$stripcrl=false,$stripalltags=true);
 			}
-
+			if($this->_debug); error_log(__METHOD__.__LINE__.$_content);
 			$this->saveSessionData();
 			
 			$response = new xajaxResponse();
