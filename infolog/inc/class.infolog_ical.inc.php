6269714dc8101442428e3459427f2afcaf2e7eb2
Jörg Lehrke <jlehrke@noc.de>
2011-06-02 18:17:52 +0000

    fix for egroupware.org bug #2824: SyncML ingnored multiline descriptions in infolog

--- infolog/inc/class.infolog_ical.inc.php
+++ infolog/inc/class.infolog_ical.inc.php
@@ -864,7 +864,7 @@ class infolog_ical extends infolog_bo
 				$txt = $GLOBALS['egw']->translation->convert($_data, $charset);
 				$txt = str_replace("\r\n", "\n", $txt);
 
-				if (preg_match('/([^\n]+)\n\n(.*)/m', $txt, $match))
+				if (preg_match('/([^\n]+)\n\n(.*)/ms', $txt, $match))
 				{
 					$note['info_subject'] = $match[1];
 					$note['info_des'] = $match[2];
