504db7e3aec0eea4c59b5aa42f90e7af8e234161
Ralf Becker <ralfbecker@outdoor-training.de>
2011-09-04 08:37:39 +0000

    validation for date-widgets, needs more testing

--- etemplate/inc/class.etemplate_widget_date.inc.php
+++ etemplate/inc/class.etemplate_widget_date.inc.php
@@ -6,6 +6,8 @@
  * @package etemplate
  * @subpackage api
  * @link http://www.egroupware.org
+ * @author Ralf Becker <RalfBecker@outdoor-training.de>
+ * @copyright 2002-11 by RalfBecker@outdoor-training.de
  * @author Nathan Gray
  * @copyright 2011 Nathan Gray
  * @version $Id$
@@ -13,13 +15,70 @@
 
 /**
  * eTemplate date widget
+ *
  * Deals with date and time.  Overridden to handle date-houronly as a transform
+ *
+ * Supported attributes: dataformat[,mode]
+ *  dataformat: '' = timestamps or automatic conversation, or eg. 'Y-m-d H:i:s' for 2002-12-31 23:59:59
+ *  mode: &1 = year is int-input not selectbox, &2 = show a [Today] button, (html-UI always uses jscal and dont care for &1+&2)
+ *           &4 = 1min steps for time (default is 5min, with fallback to 1min if value is not in 5min-steps),
+ *           &8 = dont show time for readonly and type date-time if time is 0:00,
+ *           &16 = prefix r/o display with dow
+ *           &32 = prefix r/o display with week-number
+ *			 &64 = prefix r/o display with weeknumber and dow
+ *           &128 = no icon to trigger popup, click into input trigers it, also removing the separators to save space
+ *
+ * @todo validation of date-duration
  */
 class etemplate_widget_date extends etemplate_widget_transformer
 {
 	protected static $transformation = array(
 		'type' => array('date-houronly' => 'select-hour')
 	);
+
+	/**
+	 * (Array of) comma-separated list of legacy options to automatically replace when parsing with set_attrs
+	 *
+	 * @var string|array
+	 */
+	protected $legacy_options = 'dataformat,mode';
+
+	/**
+	 * Validate input
+	 *
+	 * @param string $cname current namespace
+	 * @param array $content
+	 * @param array &$validated=array() validated content
+	 * @return boolean true if no validation error, false otherwise
+	 */
+	public function validate($cname, array $content, &$validated=array())
+	{
+		if (!$this->is_readonly($cname) && $this->type != 'date-since')	// date-since is always readonly
+		{
+			$form_name = self::form_name($cname, $this->id);
+
+			$value = self::get_array($content, $form_name);
+			$valid =& self::get_array($validated, $form_name, true);
+
+			if ((string)$value === '' && $this->attrs['needed'])
+			{
+				self::set_validation_error($form_name,lang('Field must not be empty !!!'));
+			}
+			elseif (empty($this->attrs['dataformat']))	// integer timestamp
+			{
+				$valid = (int)$value;
+			}
+			// string with formatting letters like for php's date() method
+			elseif (($obs = DateTime::createFromFormat($this->attrs['dataformat'], $value)))
+			{
+				$valid = $obj->format($this->attrs['dataformat']);
+			}
+			else
+			{
+				// this is not really a user error, but one of the clientside engine
+				self::set_validation_error($name,lang("'%1' is not a valid date !!!", $value).' '.$this->dataformat);
+			}
+		}
+	}
 }
 new jscalendar();
-etemplate_widget::registerWidget('etemplate_widget_date', array('date-houronly'));
