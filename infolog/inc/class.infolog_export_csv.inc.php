eb95a1b1dbb6bafa0eb5d69d8120581defe51cec
Nathan Gray <nathangray.bsc@gmail.com>
2011-11-29 00:21:15 +0000

    Search & process records in batches of 500, pre-load info_link_id in one query

--- infolog/inc/class.infolog_export_csv.inc.php
+++ infolog/inc/class.infolog_export_csv.inc.php
@@ -28,30 +28,75 @@ class infolog_export_csv implements importexport_iface_export_plugin {
 		$bo = new infolog_bo();
 		$selection = array();
 		$query = array();
+		$cf_links = array();
 
 		$export_object = new importexport_export_csv($_stream, (array)$options);
 		$export_object->set_mapping($options['mapping']);
 
 		// do we need to query the cf's
 		foreach($options['mapping'] as $field => $map) {
-			if($field[0] == '#') $query['custom_fields'][] = $field;
+			if($field[0] == '#') {
+				$query['custom_fields'][] = $field;
+
+				if($GLOBALS['egw_info']['user']['apps'][$bo->customfields[substr($field,1)]['type']])
+				{
+					$cf_links[$field] = $bo->customfields[substr($field,1)]['type'];
+				}
+			}
 		}
 
 		$ids = array();
-		if ($options['selection'] == 'search') {
-			$query = array_merge($GLOBALS['egw']->session->appsession('session_data','infolog'), $query);
-			$query['num_rows'] = -1;	// all
-			$selection = $bo->search($query);
-			$ids = array_keys($selection);
-		}
-		elseif ( $options['selection'] == 'all' ) {
-			$query['num_rows'] = -1;
-			$selection = $bo->search($query);
-			$ids = array_keys($selection);
-		} else {
-			$ids = $selection = explode(',',$options['selection']);
-		}
+		switch($options['selection'])
+		{
+			case 'search':
+				$query = array_merge($GLOBALS['egw']->session->appsession('session_data','infolog'), $query);
+				// Fall through
+			case 'all':
+				$query['num_rows'] = 500;
+				$query['start'] = 0;
+				do {
+					$selection = $bo->search($query);
+					$ids = array_keys($selection);
 
+					// Pre-load any cfs that are links
+					$cf_preload = array();
+					foreach($cf_links as $field => $app) {
+						foreach($selection as &$row) {
+							if($row[$field]) $cf_preload[$app][] = $row[$field];
+						}
+						if($cf_preload[$app]){
+							 $selects[$field] = egw_link::titles($app, $cf_preload[$app]);
+							error_log('Preload ' . $field . '['.$app . ']: ' . implode(',',$cf_preload[$app]));
+						}
+					}
+
+					$this->export_records($export_object, $options, $selection, $ids);
+					$query['start'] += $query['num_rows'];
+				} while($query['start'] < $query['total']);
+
+				return $export_object;
+				break;
+			default:
+				$ids = $selection = explode(',',$options['selection']);
+				$this->export_records($export_object, $options, $selection, $ids);
+				break;
+		}
+		return $export_object;
+	}
+
+	protected function export_records(&$export_object, $options, &$selection, $ids = array())
+	{
+		// Pre-load links all at once
+		if($ids && $options['mapping']['info_link_id'])
+		{
+			$links = egw_link::get_links_multiple('infolog', $ids, true);
+			foreach($links as $id => $link) {
+				if(!is_array($selection[$id])) break;
+				$selection[$id]['info_link_id'] = $link;
+				if($options['convert']) $selection[$id]['info_link_id'] = egw_link::title($link['app'], $link['id']);
+			}
+		}
+		// If exporting PM fields, pre-load them all at once
 		if($ids && ($options['mapping']['pm_id'] || $options['mapping']['project']))
 		{
 			$projects = egw_link::get_links_multiple('infolog', $ids, true, 'projectmanager');
@@ -66,7 +111,7 @@ class infolog_export_csv implements importexport_iface_export_plugin {
 		foreach ($selection as $_identifier) {
 			if(!is_array($_identifier)) {
 				$record = new infolog_egw_record($_identifier);
-			
+				if($link = $links[$record->info_id]) $record->info_link_id = $options['convert'] ? egw_link::title($link['app'], $link['id']) : $link;
 				if($project = $projects[$record->info_id])
 				{
 					$record->pm_id = current($project);
@@ -89,7 +134,6 @@ class infolog_export_csv implements importexport_iface_export_plugin {
 			$export_object->export_record($record);
 			unset($record);
 		}
-		return $export_object;
 	}
 
 	/**
