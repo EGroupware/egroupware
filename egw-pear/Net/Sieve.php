ae0c6da1664bb5ac18ef5e42df44ab02fce4d96c
Klaus Leithoff <kl@stylite.de>
2011-12-28 08:57:12 +0000

    fixing typo in error handling of authCRAM_MD5 and authDigest_MD5

--- egw-pear/Net/Sieve.php
+++ egw-pear/Net/Sieve.php
@@ -505,8 +505,8 @@ class Net_Sieve
     function _authCRAM_MD5($uid, $pwd, $euser)
     {
         if ( PEAR::isError( $challenge = $this->_doCmd( 'AUTHENTICATE "CRAM-MD5"' ) ) ) {
-            $this->_error=challenge ;
-            return challenge ;
+            $this->_error=$challenge ;
+            return $challenge ;
         }
         $challenge=trim($challenge);
         $challenge = base64_decode( trim($challenge) );
@@ -538,8 +538,8 @@ class Net_Sieve
     function _authDigest_MD5($uid, $pwd, $euser)
     {
         if ( PEAR::isError( $challenge = $this->_doCmd('AUTHENTICATE "DIGEST-MD5"') ) ) {
-            $this->_error=challenge ;
-            return challenge ;
+            $this->_error=$challenge ;
+            return $challenge ;
         }
         $challenge = base64_decode( $challenge );
         $digest = &Auth_SASL::factory('digestmd5');
