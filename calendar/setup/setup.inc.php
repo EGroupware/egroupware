cec9e395828585bb952006761ecf07adfbc3d4e1
Ralf Becker <ralfbecker@outdoor-training.de>
2012-01-04 21:46:09 +0000

    * Calendar/CalDAV: Add index for cal_modified and cal_user_modified to improve performance of ctag and etag generation on big installtions Running this update is NOT required, calendar and CalDAV will continue working without, therefore I backported it to 11.1

--- calendar/setup/setup.inc.php
+++ calendar/setup/setup.inc.php
@@ -10,7 +10,7 @@
  */
 
 $setup_info['calendar']['name']    = 'calendar';
-$setup_info['calendar']['version'] = '1.9.003';
+$setup_info['calendar']['version'] = '1.9.004';
 $setup_info['calendar']['app_order'] = 3;
 $setup_info['calendar']['enable']  = 1;
 $setup_info['calendar']['index']   = 'calendar.calendar_uiviews.index';
@@ -68,3 +68,5 @@ $setup_info['calendar']['check_install'] = array(
 		'from' => 'Calendar',
 	),
 );
+
+
