b849d7fc80ace05dc6467f64f31d08527da88380
Klaus Leithoff <kl@stylite.de>
2012-01-13 09:19:55 +0000

    adapt html purifiers URIScheme cid to new purifiers class requirements; adapt URIScheme data to accept allowed types in whatever (upper/lower/mixed) case

--- phpgwapi/inc/htmlpurifier/library/HTMLPurifier/URIScheme/cid.php
+++ phpgwapi/inc/htmlpurifier/library/HTMLPurifier/URIScheme/cid.php
@@ -8,8 +8,11 @@
 class HTMLPurifier_URIScheme_cid extends HTMLPurifier_URIScheme {
 
     public $browsable = true;
+    // this is actually irrelevant since we only write out the path
+    // component
+    public $may_omit_host = true;
 
-    public function validate(&$uri, $config, $context) {
+    public function doValidate(&$uri, $config, $context) {
         //error_log(__METHOD__." calledi with:".print_r($uri,true));
         //parent::validate($uri, $config, $context);
         $uri->userinfo = null;
