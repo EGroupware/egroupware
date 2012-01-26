a701566edafb74e24ac886263a6f5bf931573594
Klaus Leithoff <kl@stylite.de>
2012-01-26 11:57:17 +0000

    * API/Login: using tabindex to control tab succession on/in loginscreen.

--- phpgwapi/templates/idots/class.idots_framework.inc.php
+++ phpgwapi/templates/idots/class.idots_framework.inc.php
@@ -315,7 +315,7 @@ egw.set_user('.$GLOBALS['egw']->accounts->json($GLOBALS['egw_info']['user']['acc
 			}
 			$tmpl->set_var(array(
 				'lang_domain'   => lang('domain'),
-				'select_domain' => html::select('logindomain',$_COOKIE['last_domain'],$domains,true),
+				'select_domain' => html::select('logindomain',$_COOKIE['last_domain'],$domains,true,'tabindex="2"'),
 			));
 		}
 		else
@@ -405,7 +405,7 @@ egw.set_user('.$GLOBALS['egw']->accounts->json($GLOBALS['egw_info']['user']['acc
 			$tmpl->set_var(array(
 				'lang_language' => lang('Language'),
 				'select_language' => html::select('lang',$GLOBALS['egw_info']['user']['preferences']['common']['lang'],
-				translation::get_installed_langs(),true),
+				translation::get_installed_langs(),true,'tabindex="1"'),
 			));
 		}
 		else
@@ -430,7 +430,7 @@ egw.set_user('.$GLOBALS['egw']->accounts->json($GLOBALS['egw_info']['user']['acc
 				'1week'=> lang('1 Week'),
 				'1month' => lang('1 Month'),
 				'forever' => lang('Forever'),
-			),true));
+			),true,'tabindex="3"'));
 		}
 		else
 		{
