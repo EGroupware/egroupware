59b277ec5f119b3684af2f3888b28b646ad6b753
Nathan Gray <nathangray.bsc@gmail.com>
2012-02-11 00:47:54 +0000

    A new sitemgr module for public reservation of one specified resource.  Uses an emailed confirmation link to confirm the reservation

---
+++ resources/inc/class.resources_reserve.inc.php
@@ -0,0 +1,289 @@
+<?php
+/**
+ * eGroupWare - resources
+ * Book a single resource through sitemgr module, not the normal method
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package resources
+ * @link http://www.egroupware.org
+ * @author Nathan Gray
+ * @version $Id$
+ */
+
+class resources_reserve {
+
+	public function __construct() {
+		$this->tmpl = new etemplate();
+		$this->bo = new resources_bo();
+	}
+
+	/**
+	 * Book a resource
+	 */
+	public function book($content = array(), $arguments = array())
+	{
+		$data = array();
+		$readonlys = array();
+		$display_days = $_GET['planner_days'] ? $_GET['planner_days'] : 3;
+		$planner_date = $_GET['date'] ? $_GET['date'] : strtotime('yesterday',$content['date'] ? $content['date'] : time());
+
+		if($_GET['confirm']) {
+			$register_code = ($_GET['confirm'] && preg_match('/^[0-9a-f]{32}$/',$_GET['confirm'])) ? $_GET['confirm'] : false;
+			if($register_code && $registration = registration_bo::confirm($register_code)) {
+				// Get calendar through link
+				$links = egw_link::get_links('registration', $registration['reg_id'],'calendar');
+				$bo = new calendar_boupdate();
+				$data = $bo->read(current($links));
+
+				$addressbook = new addressbook_bo();
+				$data += $addressbook->read(key($data['participant_types']['c']));
+
+				// Show date of reservation, so they can see it blocked off
+				$display_days = 1;
+				$planner_date = mktime(0,0,0,date('m',$data['start']),date('d',$data['start']),date('Y',$data['start']));
+				$readonlys['__ALL__'] = true;
+				$content = array(
+					'resource' => key($data['participant_types']['r']),
+					'date' => $data['start'],
+					'time' => $data['start'] - mktime(0,0,0,date('m',$data['start']),date('d',$data['start']),date('Y',$data['start']))
+				);
+				$data['msg']= '<div class="confirm">'.lang('Registration confirmed %1', egw_time::to($data['start'])) .'</div>';
+			} else {
+				$data['msg']= '<div class="confirm">'.lang('Unable to process confirmation.').'</div>';
+			}
+                }
+
+		$this->tmpl->read('resources.sitemgr_book');
+		
+		if(!$arguments['resource'] && !($arguments['resource'] = $content['resource'])) return false;
+		$data += $this->bo->read($arguments['resource']);
+
+		$data['quantity'] = $content['quantity'] ? $content['quantity'] : 2;
+
+		$data['date'] = $arguments['date'] ? $arguments['date'] : $content['date'];
+		$data['time'] = $content['time'];
+
+		$preserve = array(
+			'resource' => $data['res_id'],
+		);
+		$preserve['sitemgr_version'] = $arguments['sitemgr_version'] ? $arguments['sitemgr_version'] : $content['sitemgr_version'];
+		$preserve['confirmation'] = $arguments['confirmation'] || $content['confirmation'];
+		$preserve['contact_form'] = $arguments['contact_form'] ? $arguments['contact_form'] : $content['contact_form'];
+		$preserve['link'] = $arguments['link'] ? $arguments['link'] : $content['link'];
+		$preserve['email_message'] = $arguments['email_message'] ? $arguments['email_message'] : $content['email_message'];
+
+		// Check for valid time
+		if($data['date'] && $content['book'])
+		{
+			$start_time = $data['date'] + $data['time'];
+			$duration = ($GLOBALS['egw_info']['user']['preferences']['calendar']['defaultlength'] * 60);
+			$end_time = $start_time + $duration;
+
+			$config = config::read('registration');
+
+			// Not a user, need contact
+			if($GLOBALS['egw_info']['user']['account_lid'] == $GLOBALS['sitemgr_info']['anonymous_user'] && !$content['contact_id']) 
+			{
+				if ($config['pending_addressbook'])   // save the contact in the addressbook
+				{
+					$content['owner'] = $config['pending_addressbook'];
+					$content['private'] = 0;        // in case default_private is set
+				}
+
+				$addressbook = new addressbook_bo();
+				$contact_fields = $addressbook->contact_fields;
+				unset($contact_fields['email']); // Always present
+				unset($contact_fields['id']); // Address already there
+				if(array_intersect_key($contact_fields,$content)) {
+					$result = $addressbook->save($content);
+					if(!$result) {
+						throw new egw_exception_no_permission($addressbook->error);
+						return False;
+					}
+		
+
+					$contact_id = $result;
+					$contact = 'c'.$contact_id;
+				}
+			}
+			else
+			{
+				$contact = $GLOBALS['egw_info']['user']['account_id'];
+				$contact_id = $GLOBALS['egw_info']['user']['person_id'];
+			}
+
+			if($contact) {
+				// Make event
+				$event = array(
+					'title' =>	egw_link::title('addressbook', $contact_id),
+					'start' =>	$start_time,
+					'end' =>	$end_time,
+					'participants' => array(
+						'r'.$data['res_id'] => calendar_so::combine_status('U', $data['quantity'], 'REQ-PARTICIPANT'),
+						$contact => calendar_so::combine_status('U', $data['quantity'], 'REQ-PARTICIPANT')
+					)
+				);
+				$cal_bo = new calendar_boupdate();
+				$result = $cal_bo->update($event);
+
+				// Show date of reservation, so they can see it blocked off
+				$display_days = 1;
+				$planner_date = mktime(0,0,0,date('m',$event['start']),date('d',$event['start']),date('Y',$event['start']));
+
+				// Free time search
+				if(!is_numeric($result))
+				{
+					$form = new calendar_uiforms();
+					$freetime = $form->freetime('r'.$data['res_id'], 
+						strtotime($GLOBALS['egw_info']['user']['preferences']['calendar']['workdaystarts'] . ':00',$start_time), 
+						strtotime($GLOBALS['egw_info']['user']['preferences']['calendar']['workdayends'] . ':00 ',$end_time),
+						$duration
+					);
+					$data += $content;
+					$data['msg'] = lang('Please choose a different time:')."\n";
+					foreach($freetime as $slot) {
+						$data['msg'] .= egw_time::to($slot['start']) . ' - ' . egw_time::to($slot['end'])."<br />\n";
+					}
+				}
+				elseif ($preserve['confirmation'] && $contact_id && $result)
+				{
+					// Confirmation required - send registration email
+					$reg_id = registration_bo::save(array(
+						'contact_id' => $contact_id,
+						'timestamp' => time() + ($config['expiry'] * 3600),
+						'post_confirm_hook' => 'resources_reserve::confirm'
+					), false);
+					if(is_numeric($reg_id))
+					{
+						// Link to event
+						egw_link::link('registration', $reg_id, 'calendar', $result);
+						$reg = registration_bo::read($reg_id);
+
+						// Send email
+						$email_info = $config + array(
+							'title' => $data['name'],
+							'subject' => $data['name'] . ' ' . egw_time::to($start_time),
+							'link' => $preserve['link'],
+						);
+						if($preserve['email_message'])
+						{
+							$email_info['message'] = lang($preserve['email_message'], 
+								egw_time::to($start_time), 
+								$preserve['link'].'&confirm='.$reg['register_code'],
+								egw_time::to($reg['timestamp']) 
+							);
+						}
+						$data['msg'] .= registration_bo::send_confirmation($email_info, $reg);
+					}
+				}
+			}
+		}
+
+		$data['picture'] = $this->bo->get_picture($data);
+
+		// Not a user, need contact
+		if($GLOBALS['egw_info']['user']['account_lid'] == $GLOBALS['sitemgr_info']['anonymous_user'] && !$content['contact_id']) 
+		{
+			$data['contact_form'] = $preserve['contact_form'];
+			$data['show_contact'] = !$preserve['contact_form'];
+		}
+
+		$cal_user = array('r'.$arguments['resource']);
+		foreach($this->bo->get_acc_list($data['res_id']) as $acc_id => $name)
+		{
+			$cal_user[] = 'r'.$acc_id;
+		}
+
+		if(!$data['date']) $data['date'] = strtotime('tomorrow');
+		if(!$data['time']) $data['time'] = $GLOBALS['egw_info']['user']['preferences']['calendar']['workdaystarts'] * 60 * 60;
+		if(!$GLOBALS['egw_info']['user']['apps']['resources'] || !$this->bo->get_calendar_info($data['res_id']) || // Needed for booking
+			!$GLOBALS['egw_info']['user']['apps']['calendar'] // Needed for updating schedule
+		)
+		{
+			$data['planner'] = 'Permission error - site not configured properly.  Need ' . lang('resources') . ' & ' . lang('calendar');
+		}
+		else 
+		{
+			$data['planner'] = $this->get_planner(
+				$arguments['resource'],
+				$planner_date ? $planner_date : strtotime('yesterday',$date), // Show day before, too
+				$display_days,
+				$preserve['link']
+			);
+		}
+
+		return $this->tmpl->exec('resources.resources_reserve.book',$data,$sel_options,$readonlys,$preserve);
+	}
+
+	public function ajax_update_planner($resource_id, $date) {
+		$response = egw_json_response::get();
+		$response->assign('exec[planner_div]','innerHTML',$this->get_planner(
+			$resource_id,
+			strtotime('yesterday',$date),
+			3,
+			$_SERVER['HTTP_REFERER']
+		));
+	}
+
+	/**
+	 * Get HTML for the planner to show when its busy
+	 *
+	 * @param resource_id 
+	 *
+	 * @return String - HTML
+	 */
+	protected function get_planner($resource_id, $date, $days = 3, $link)
+	{
+//echo "get_planner($resource_id, $date, $days, $link)";
+		$calendar = new calendar_uiviews(array(
+			'owner'		=> 'r'.$resource_id,
+			//'owner'		=> implode(',',$cal_user),
+			'planner_days'	=> $days,
+			'view'		=> 'date_advance',
+			'date'		=> $date
+		));
+		// Need to add this in explicitly, or it may be lost if use clicks a day
+		$calendar->search_params['users'][] = 'r'.$resource_id;
+		$calendar->search_params['users'][] = $GLOBALS['egw_info']['user']['account_id'];
+		$calendar->allowEdit = false;    // switches off all edit popups
+		
+		$planner = '<style type="text/css">'."\n".
+			'@import url('.$GLOBALS['egw_info']['server']['webserver_url'].'/calendar/templates/default/app.css'.");\n".
+			'</style>'."\n";
+
+		// replacing egw-urls with sitemgr ones, allows to use navigation links
+		$planner .= str_replace($GLOBALS['egw_info']['server']['webserver_url'].'/index.php?',
+			$link.'&',
+			$calendar->planner(true));
+
+		return $planner;
+	}
+
+	/**
+	 * Confirm a registration done through sitemgr
+	 *
+	 * @param registration - registration information
+	 */
+	public function confirm($registration)
+	{
+		// Get calendar through link
+		$links = egw_link::get_links('registration', $registration['reg_id'],'calendar');
+		$bo = new calendar_boupdate();
+		$event = $bo->read(current($links));
+		if($registration['status'] == registration_bo::CONFIRMED)
+		{
+			$bo->set_status(current($links), 'c'.$registration['contact_id'], 'A');
+		}
+		else
+		{
+			$bo->delete(current($links));
+			// Remove contact ID, or registration will try to purge the account
+			if($registration['account_id'])
+			{
+				unset($registration['contact_id']);
+			}
+		}
+		return $registration;
+	}
+}
+
---
+++ resources/sitemgr/class.module_resources_reservation.inc.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Module for quick & easy resource reservation
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package resources
+ * @author Nathan Gray
+ * @copyright (c) 2011 Nathan Gray
+ * @version $Id$
+*/
+
+/**
+ * Needs permission to resources to read the resource, calendar app to properly push everything through
+ */
+class module_resources_reservation extends sitemgr_module
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->title = lang('Reserve');
+		$this->description = lang('Simple reservation of a single item');
+		$this->etemplate_method = 'resources.resources_reserve.book';
+		
+		$categories = new categories('', 'resources');
+		$cat_list = $categories->return_sorted_array();
+		$cat_options = array();
+		foreach($cat_list as $category)
+		{
+			$cat_options[$category['id']] = $category['name'];
+		}
+		$this->arguments = array(
+			'category' => array(
+				'type'	=> 'select',
+				'label' => lang('Category'),
+				'options' => $cat_options
+			),
+			'resource' => array(
+				'type'	=> 'select',
+				'label' => lang('Resource'),
+				'options' => array(
+				)
+			),
+			'contact_form' => array(
+				'type' => 'textfield',
+				'label' => lang('Custom eTemplate for the contactform'),
+				'params' => array('size' => 40),
+			),
+			'confirmation' => array(
+				'type'	=> 'checkbox',
+				'label'	=> lang('Require confirmation'),
+			),
+			'email_message' => array(
+				'type' => 'textarea',
+				'large' => true,
+				'label' => lang('Confirmation email text').'<br />%1 = ' . lang('Event start').'<br/>%2 = link<br />%3 = '.lang('expiry'),
+				'params' => array(
+					'rows'	=> 8,
+					'cols'	=> 110
+				)
+			)
+		);
+	}
+
+	public function get_user_interface()
+	{
+		$query = array(
+			// Resources uses filter, not cat_id
+			'filter' => $this->block->arguments['category'],
+			'bookable' => true
+		);
+
+		// Add resources from selected category
+		$bo = new resources_bo();
+		$bo->get_rows($query, $list, $readonlys);
+		foreach($list as $resource) 
+		{
+			$this->arguments['resource']['options'][$resource['res_id']] = $resource['name'];
+		}
+		return parent::get_user_interface();
+	}
+
+	/**
+         * generate the module content AND process submitted forms
+	 * Overridden from parent to pass arguments
+         *
+         * @param array &$arguments $arguments['arg1']-$arguments['arg3'] will be passed for non-submitted forms (first call)
+         * @param array $properties
+         * @return string the html content
+         */
+        function get_content(&$arguments,$properties)
+        {
+                list($app) = explode('.',$this->etemplate_method);
+                $GLOBALS['egw']->translation->add_app($app);
+
+                $extra = "<style type=\"text/css\">\n<!--\n@import url(".$GLOBALS['egw_info']['server']['webserver_url'].
+                        "/etemplate/templates/default/app.css);\n";
+
+                if ($app != 'etemplate' && file_exists(EGW_SERVER_ROOT.'/'.$app.'/templates/default/app.css'))
+                {
+                        $extra .= "@import url(".$GLOBALS['egw_info']['server']['webserver_url'].
+                                '/'.$app."/templates/default/app.css);\n";
+                }
+                $extra .= "-->\n</style>\n";
+                $extra .= '<script src="'.$GLOBALS['egw_info']['server']['webserver_url'].'/etemplate/js/etemplate.js" type="text/javascript"></script>'."\n";
+                $ret = false;
+                if($_POST['etemplate_exec_id'])
+                {
+                        $ret = ExecMethod('etemplate.etemplate.process_exec');
+                }
+		if($_GET['date']) $arguments['date'] = strtotime($_GET['date']);
+		$arguments['link'] = $this->link();
+		$arguments['sitemgr_version'] = $this->block->version;
+                return $extra.($ret ? $ret : ExecMethod2($this->etemplate_method,null,$arguments));
+        }
+
+}
