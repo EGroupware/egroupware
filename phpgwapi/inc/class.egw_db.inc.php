ea079a68032aa55501f0264c3e993e35bafe7f53
Ralf Becker <ralfbecker@outdoor-training.de>
2012-03-08 06:20:21 +0000

    fixing wiki rate code / mixed case column names for PostgreSQL

--- phpgwapi/inc/class.egw_db.inc.php
+++ phpgwapi/inc/class.egw_db.inc.php
@@ -1385,7 +1385,8 @@ class egw_db
 
 		// if name contains special characters, quote it
 		// always quote for postgreSQL, as this is the only way to support mixed case names
-		if ( preg_match('/\W/', $name) || $this->Type == 'ppsql') {
+		if (preg_match('/\W/', $name) || $this->Type == 'pgsql' && preg_match('/[A-Z]+/', $name))
+		{
 			return $quote . $name . $quote;
 		}
 
