1f54b34ddef1f5ade649dfe6d600944f7e9047d5
Ralf Becker <ralfbecker@outdoor-training.de>
2012-04-06 12:50:01 +0000

    * Resources: SiteMgr module uses old class-name ui_resources instead of resources_ui

---
+++ resources/sitemgr/class.module_resources.inc.php
@@ -0,0 +1,25 @@
+<?php
+	/**
+	 * eGroupWare - resources
+	 * http://www.egroupware.org
+	 *
+	 * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+	 * @package resources
+	 * @author Cornelius Weiss <egw@von-und-zu-weiss.de>
+	 * @author Lukas Weiss <wnz_gh05t@users.sourceforge.net>
+	 * @version $Id$
+	 */
+
+	require_once (EGW_INCLUDE_ROOT.'/etemplate/inc/class.sitemgr_module.inc.php');
+
+	class module_resources extends sitemgr_module
+	{
+		function module_resources()
+		{
+			$this->arguments = array();
+			$this->properties = array();
+			$this->title = lang('Resources');
+			$this->description = lang('This module displays the resources app');
+			$this->etemplate_method = 'resources.resources_ui.index';
+		}
+	}
