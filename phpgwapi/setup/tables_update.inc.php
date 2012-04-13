025c3d3b44655241c5c67feff700a9813bbda34f
Ralf Becker <ralfbecker@outdoor-training.de>
2012-04-13 14:35:55 +0000

    check if categories are already updated to global == cat_owner=0, has to run as API update, before apps (tracker) creates new global cats with cat_owner=0 stopping the update

--- phpgwapi/setup/tables_update.inc.php
+++ phpgwapi/setup/tables_update.inc.php
@@ -331,6 +331,11 @@ function phpgwapi_upgrade1_9_010()
 	$prefs->save_repository(false, 'default');
 	egw_vfs::$is_root = false;
 
+	// check if categories are already updated to global == cat_owner=0
+	// has to run as API update, before apps (tracker) creates new global cats with cat_owner=0 stopping the update
+	categories::invalidate_cache();
+	categories::init_cache();	// updates the cats
+
 	return $GLOBALS['setup_info']['phpgwapi']['currentver'] = '1.9.011';
 }
 
