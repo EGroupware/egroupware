254fc0e56574677ffa19919bac8b44f91e66f1ba
Klaus Leithoff <kl@stylite.de>
2012-04-25 10:08:53 +0000

    fix issue for changed default param for emailadmin_bo contructor; always use setDefaultProfile if there is NO profile available at all; fix issue regarding missing control of group ownership on edit.general

--- setup/setup-cli.php
+++ setup/setup-cli.php
@@ -209,7 +209,7 @@ function do_emailadmin()
 	}
 	$config['smtpAuth'] = $config['smtp_auth_user'] ? 'yes' : null;
 
-	$emailadmin = new emailadmin_bo(-1,false);	// false=no session stuff
+	$emailadmin = new emailadmin_bo(false,false);	// false=no session stuff
 	$emailadmin->setDefaultProfile($config);
 
 	echo "\n".lang('EMailAdmin profile updated:')."\n";
