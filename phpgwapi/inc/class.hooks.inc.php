5b46ac167736d2db34ae7e9387ed4b34a87b6c25
Ralf Becker <ralfbecker@outdoor-training.de>
2012-07-18 10:35:09 +0000

    * Admin/Setup: fixing high memory usage in registering all hooks

--- phpgwapi/inc/class.hooks.inc.php
+++ phpgwapi/inc/class.hooks.inc.php
@@ -271,6 +271,7 @@ class hooks
 			$setup_info = array($appname => array());
 			if(@file_exists($f)) include($f);
 			// some apps have setup_info for more then themselfs (eg. phpgwapi for groupdav)
+			$hdata = array();
 			foreach($setup_info as $appname => $data)
 			{
 				if ($data['hooks']) $this->register_hooks($appname,$data['hooks']);
