6049fb09981da7a197c6994e36c7221f8da93ffe
Nathan Gray <nathangray.bsc@gmail.com>
2012-07-18 16:47:49 +0000

    Not needed anymore, framework loads the theme CSS

--- etemplate/inc/class.etemplate_widget_link.inc.php
+++ etemplate/inc/class.etemplate_widget_link.inc.php
@@ -28,9 +28,6 @@ class etemplate_widget_link extends etemplate_widget
 	{
 		if($xml) {
 			parent::__construct($xml);
-
-			// TODO: probably a better way to do this
-			egw_framework::includeCSS('/phpgwapi/js/jquery/jquery-ui/smoothness/jquery-ui-1.8.16.custom.css');
 		}
 		$this->setElementAttribute($this->id.'_file', 'max_file_size', egw_vfs::int_size(ini_get('upload_max_filesize')));
 	}
