a0a3cfb4fcb7eef0ac9b3d2fc433297419ab1809
Nathan Gray <nathangray.bsc@gmail.com>
2012-10-09 15:55:57 +0000

    Still check original file columns

--- importexport/inc/class.importexport_import_ui.inc.php
+++ importexport/inc/class.importexport_import_ui.inc.php
@@ -294,7 +294,7 @@
 			$data = translation::convert($data,$options['charset']);
 
 			$ok = true;
-			if(count($data) != count($options['field_mapping']))
+			if(count($data) != count($options['field_mapping']) && max(array_keys($data)) != max(array_keys($options['csv_fields'])))
 			{
 				$message[] = lang("Column mismatch.  Expected %1 columns, your file has %2.",
 					count($options['field_mapping']),
