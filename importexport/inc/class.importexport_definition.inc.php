3a1c4fb7fb67fb9435a7f409d2ba58af4b941851
Nathan Gray <nathangray.bsc@gmail.com>
2013-01-03 15:53:31 +0000

    Try to catch some errors

--- importexport/inc/class.importexport_definition.inc.php
+++ importexport/inc/class.importexport_definition.inc.php
@@ -78,10 +78,17 @@ class importexport_definition implements importexport_iface_egw_record {
 			if ( !( importexport_definitions_bo::is_permitted($this->get_record_array()) || $this->is_admin)) {
 				throw new Exception('Error: User "'.$this->user.'" is not permitted to get definition with identifier "'.$_identifier.'"!');
 			}
-			$options_data = importexport_arrayxml::xml2array( $this->definition['plugin_options'] );
-			$this->definition['plugin_options'] = $options_data['root'];
-			if($this->definition['filter']) $filter = importexport_arrayxml::xml2array( $this->definition['filter']  );
-			$this->definition['filter'] = $filter['root'];
+			try
+			{
+				$options_data = importexport_arrayxml::xml2array( $this->definition['plugin_options'] );
+				$this->definition['plugin_options'] = $options_data['root'];
+				if($this->definition['filter']) $filter = importexport_arrayxml::xml2array( $this->definition['filter']  );
+				$this->definition['filter'] = $filter['root'];
+			}
+			catch (Exception $e)
+			{
+				error_log($e->getMessage());
+			}
 		}
 	}
 
