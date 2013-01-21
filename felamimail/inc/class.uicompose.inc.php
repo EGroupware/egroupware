4617c2cbcdc4424f8a83be85c312816c4acc19b6
Klaus Leithoff <kl@stylite.de>
2013-01-21 10:40:15 +0000

    increase timeframe for not sending messages with identical composeids (when composeid was used in previous send action)

--- felamimail/inc/class.uicompose.inc.php
+++ felamimail/inc/class.uicompose.inc.php
@@ -181,8 +181,8 @@
 					return;
 				}
 			} else {
-				$cachedComposeID = egw_cache::getCache(egw_cache::INSTANCE,'email','composeIdCache'.trim($GLOBALS['egw_info']['user']['account_id']),$callback=null,$callback_params=array(),$expiration=10);
-				egw_cache::setCache(egw_cache::INSTANCE,'email','composeIdCache'.trim($GLOBALS['egw_info']['user']['account_id']),$this->composeID,$expiration=10);
+				$cachedComposeID = egw_cache::getCache(egw_cache::INSTANCE,'email','composeIdCache'.trim($GLOBALS['egw_info']['user']['account_id']),$callback=null,$callback_params=array(),$expiration=60);
+				egw_cache::setCache(egw_cache::INSTANCE,'email','composeIdCache'.trim($GLOBALS['egw_info']['user']['account_id']),$this->composeID,$expiration=60);
 				//error_log(__METHOD__.__LINE__.' '.$formData['subject'].' '.$cachedComposeID.'<->'.$this->composeID);
 				if (!empty($cachedComposeID) && $cachedComposeID != $this->composeID) return;
 				if(!$this->bocompose->send($formData)) {
