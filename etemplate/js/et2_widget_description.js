6949040f86711827874f32226a913e8a456798c4
Nathan Gray <nathangray.bsc@gmail.com>
2013-02-13 09:49:43 +0000

    Avoid errors if _value is null / undefined

--- etemplate/js/et2_widget_description.js
+++ etemplate/js/et2_widget_description.js
@@ -112,6 +112,7 @@ var et2_description = et2_baseWidget.extend([et2_IDetachedDOM], {
 	},
 
 	set_value: function(_value) {
+		if(!_value) _value = "";
 		et2_insertLinkText(this._parseText(_value),
 			this.span[0],
 			this.options.extra_link_target
