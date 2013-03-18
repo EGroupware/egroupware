6204999b19832f73933338cfe511b77a22d83c0e
Klaus Leithoff <kl@stylite.de>
2013-03-18 15:36:27 +0000

    allow better config options for spell checking in ckeditor (add BrowserBased only)

--- phpgwapi/inc/class.egw_ckeditor_config.inc.php
+++ phpgwapi/inc/class.egw_ckeditor_config.inc.php
@@ -238,28 +238,31 @@ class egw_ckeditor_config
 	private static function add_spellchecker_options(&$config, &$spellchecker_button, &$scayt_button)
 	{
 		//error_log(__METHOD__.__LINE__.' Spellcheck:'.$GLOBALS['egw_info']['server']['enabled_spellcheck']);
-		if (isset($GLOBALS['egw_info']['server']['enabled_spellcheck']))
+		if (isset($GLOBALS['egw_info']['server']['enabled_spellcheck']) && $GLOBALS['egw_info']['server']['enabled_spellcheck'])
 		{
 			// enable browsers native spellchecker as default, if e.g.: aspell fails
 			// to use browsers native spellchecker, you have to hold CMD/CTRL button on rightclick to
 			// access the browsers spell correction options
-			$config['disableNativeSpellChecker'] = false;
+			if ($GLOBALS['egw_info']['server']['enabled_spellcheck']!='YesNoSCAYT') $config['disableNativeSpellChecker'] = false;
 			$spellchecker_button = 'SpellChecker';
 			if (!empty($GLOBALS['egw_info']['server']['aspell_path']) &&
 				is_executable($GLOBALS['egw_info']['server']['aspell_path']) &&
-				!($GLOBALS['egw_info']['server']['enabled_spellcheck']=='YesUseWebSpellCheck')
+				($GLOBALS['egw_info']['server']['enabled_spellcheck']!='YesUseWebSpellCheck' &&
+				 $GLOBALS['egw_info']['server']['enabled_spellcheck']!='YesBrowserBased')
 			)
 			{
 				$spellchecker_button = 'SpellCheck';
 				$config['extraPlugins'] = "aspell";
 			}
-			if (!($GLOBALS['egw_info']['server']['enabled_spellcheck']=='YesNoSCAYT'))
+			if ($GLOBALS['egw_info']['server']['enabled_spellcheck']!='YesNoSCAYT' &&
+				$GLOBALS['egw_info']['server']['enabled_spellcheck']!='YesBrowserBased'
+			)
 			{
 				$scayt_button='Scayt';
 				$config['scayt_autoStartup'] = true;
 				$config['scayt_sLang'] = self::get_lang().'_'.self::get_country();
+				$config['disableNativeSpellChecker'] = true; // only one spell as you type
 			}
-
 		}
 		else
 		{
