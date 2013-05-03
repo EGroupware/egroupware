bb17f1ae3d88bc5e90ba507b520439a927033179
Ralf Becker <ralfbecker@outdoor-training.de>
2013-05-03 15:29:34 +0000

    using just etemplate

--- preferences/inc/class.preferences_settings.inc.php
+++ preferences/inc/class.preferences_settings.inc.php
@@ -43,7 +43,7 @@ class preferences_settings
 	 */
 	function index(array $content=null, $msg='')
 	{
-		$tpl = new etemplate_old('preferences.settings');
+		$tpl = new etemplate('preferences.settings');
 		if (!is_array($content))
 		{
 			$appname = isset($_GET['appname']) && $_GET['appname'] != 'preferences' ? $_GET['appname'] : 'common';
