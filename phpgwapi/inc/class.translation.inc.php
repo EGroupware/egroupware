00234be893c59cef4fd93581f0003810137d5ecd
Klaus Leithoff <kl@stylite.de>
2013-05-08 09:58:30 +0000

    * Tracker: fix bug regarding the save of replys that could be regarded as empty (only span with style, no content)

--- phpgwapi/inc/class.translation.inc.php
+++ phpgwapi/inc/class.translation.inc.php
@@ -1079,6 +1079,7 @@ class translation
 			'@&(trade|#8482);@i',             //   trade
 			'@&#39;@i',                       //   singleQuote
 			'@(\xc2\xa0)@',                   //   nbsp or tab (encoded windows-style)
+			'@(\xe2\x80\x8b)@',               //   ZERO WIDTH SPACE
 		);
 		$Replace = array ('',
 			'"',
@@ -1094,6 +1095,7 @@ class translation
 			'(TM)',// trade
 			"'",
 			' ',
+			'',
 		);
 		$_html = preg_replace($Rules, $Replace, $_html);
 
