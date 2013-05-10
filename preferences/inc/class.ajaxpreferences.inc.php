4ae5eb452bf34b38cf02b1366c4eed5409a0c223
Ralf Becker <ralfbecker@outdoor-training.de>
2013-05-10 17:21:14 +0000

    docu update

--- preferences/inc/class.ajaxpreferences.inc.php
+++ preferences/inc/class.ajaxpreferences.inc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * ajaxpreferences
+ * EGroupware - set presonal preferences via ajax
  *
  * @package preferences
  * @copyright Lingewoud B.V.
@@ -18,19 +18,19 @@ class ajaxpreferences
 	/**
 	 * storeEGWPref
 	 *
-	 * @param mixed $repository egroupware preferences repository
-	 * @param mixed $key key to preference
+	 * @param mixed $appname appname
+	 * @param mixed $key name of preference
 	 * @param mixed $value new value
 	 * @access public
 	 * @return mixed returns null when no erro, else return error message.
 	 */
-	function storeEGWPref($repository,$key,$value)
+	function storeEGWPref($appname,$key,$value)
 	{
 		$response = new xajaxResponse();
 		$GLOBALS['egw']->preferences->read_repository();
-		$GLOBALS['egw']->preferences->change($repository,$key,$value);
+		$GLOBALS['egw']->preferences->add($appname,$key,$value);
 		$GLOBALS['egw']->preferences->save_repository(True);
-		//$response->addAlert(__METHOD__."('$repository','$key','$value')");
+		//$response->addAlert(__METHOD__."('$appname','$key','$value')");
 		return $response->getXML();
 	}
 }
