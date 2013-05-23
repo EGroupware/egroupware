38f7a1341c3a0cacd7bea63996d8e217cbbe6651
Ralf Becker <ralfbecker@outdoor-training.de>
2013-05-23 13:46:16 +0000

    backported account-migration improvments from Trunk: - r40487 allow to create an instance specific admin, user- or group-context - r40903 changes account_repository and auth_type to migration type (no need to store that type first) - r40907 warning if account-migration to SQL of certain groups will fail, because they have identical IDs as existing users

--- setup/account_migration.php
+++ setup/account_migration.php
@@ -42,12 +42,12 @@ foreach($GLOBALS['egw_setup']->db->select($GLOBALS['egw_setup']->config_table,'c
 {
 	$GLOBALS['egw_info']['server'][$row['config_name']] = $row['config_value'];
 }
-$to = $GLOBALS['egw_info']['server']['account_repository'];
-if (!$to && !($to = $GLOBALS['egw_info']['server']['auth_type']))
+$from = $GLOBALS['egw_info']['server']['account_repository'];
+if (!$from && !($from = $GLOBALS['egw_info']['server']['auth_type']))
 {
-	$to = 'sql';
+	$from = 'sql';
 }
-$from = $to == 'sql' ? 'ldap' : 'sql';
+$to = $from == 'sql' ? 'ldap' : 'sql';
 $direction = strtoupper($from).' --> '.strtoupper($to);
 
 $GLOBALS['egw_setup']->html->show_header($direction,False,'config',$GLOBALS['egw_setup']->ConfigDomain .
@@ -60,9 +60,9 @@ array_shift($base_parts);
 $cmd = new setup_cmd_ldap(array(
 	'domain' => $GLOBALS['egw_setup']->ConfigDomain,
 	'sub_command' => 'migrate_to_'.$to,
-	// in regular setup we only support one ldap root user, setting him as admin user too
-	'ldap_admin' => $GLOBALS['egw_info']['server']['ldap_root_dn'],
-	'ldap_admin_pw' => $GLOBALS['egw_info']['server']['ldap_root_pw'],
+	// allow to set ldap root DN (ldap_admin) to create instance specific admin DN and structure
+	'ldap_admin' => !empty($_POST['ldap_admin']) ? $_POST['ldap_admin'] : $GLOBALS['egw_info']['server']['ldap_root_dn'],
+	'ldap_admin_pw' => !empty($_POST['ldap_admin']) ? $_POST['ldap_admin_pw'] : $GLOBALS['egw_info']['server']['ldap_root_pw'],
 	'ldap_base' => implode(',',$base_parts),
 )+$GLOBALS['egw_info']['server']);
 
@@ -74,6 +74,7 @@ if (!$_POST['migrate'])
 	$setup_tpl->set_block('migration','header','header');
 	$setup_tpl->set_block('migration','user_list','user_list');
 	$setup_tpl->set_block('migration','group_list','group_list');
+	$setup_tpl->set_block('migration','ldap_admin','ldap_admin');
 	$setup_tpl->set_block('migration','submit','submit');
 	$setup_tpl->set_block('migration','footer','footer');
 
@@ -81,6 +82,10 @@ if (!$_POST['migrate'])
 	{
 		if ($account['account_type'] == 'g')
 		{
+			if (isset($accounts[abs($account_id)]))
+			{
+				$identical_account_id_warning .= lang('Group %1 uses identical ID %2 as user %3!', $account['account_lid'], abs($account_id), $accounts[abs($account_id)]['account_lid']);
+			}
 			$group_list .= '<option value="' . $account_id . '" selected="1">'. $account['account_lid'] . "</option>\n";
 		}
 		else
@@ -97,7 +102,12 @@ if (!$_POST['migrate'])
 	$setup_tpl->set_var('description',lang('Migration between eGroupWare account repositories').': '.$direction);
 	$setup_tpl->set_var('select_users',lang('Select which user(s) will be exported'));
 	$setup_tpl->set_var('select_groups',lang('Select which group(s) will be exported'));
-	$setup_tpl->set_var('memberships',lang('Group memberships will be migrated too.'));
+	$setup_tpl->set_var('memberships',$identical_account_id_warning ?
+		'<p style="color: red">'.$identical_account_id_warning.'<br/>'.lang('Migration of group will fail, as SQL does NOT allow identical IDs.').'</p>' :
+		lang('Group memberships will be migrated too.'));
+	$setup_tpl->set_var('ldap_admin_message', lang('Give LDAP root DN and password, if you need to create an instance specific admin user, user- or group-context'));
+	$setup_tpl->set_var('ldap_admin_label', lang('Root DN'));
+	$setup_tpl->set_var('ldap_admin_pw_label', lang('Root DN password'));
 	$setup_tpl->set_var('migrate',$direction);
 	$setup_tpl->set_var('cancel',lang('Cancel'));
 
@@ -110,6 +120,10 @@ if (!$_POST['migrate'])
 	{
 		$setup_tpl->pfp('out','group_list');
 	}
+	if ($to == 'ldap')
+	{
+		$setup_tpl->pfp('out','ldap_admin');
+	}
 	$setup_tpl->pfp('out','submit');
 	$setup_tpl->pfp('out','footer');
 }
@@ -118,6 +132,12 @@ else	// do the migration
 	$cmd->only = array_merge((array)$_POST['users'],(array)$_POST['groups']);
 	$cmd->verbose = true;
 	echo '<p align="center">'.str_replace("\n","</p>\n<p align='center'>",$cmd->run())."</p>\n";
+	// store new repostory (and auth_type), as we are migrated now
+	config::save_value('account_repository', $GLOBALS['egw_info']['server']['account_repository']=$to, 'phpgwapi');
+	if (empty($GLOBALS['egw_info']['server']['auth_type']) || $GLOBALS['egw_info']['server']['auth_type'] == $from)
+	{
+		config::save_value('auth_type', $GLOBALS['egw_info']['server']['auth_type']=$to, 'phpgwapi');
+	}
 	echo '<p align="center">'.lang('Click <a href="index.php">here</a> to return to setup.')."</p>\n";
 }
 
---
+++ setup/templates/default/account_migration.tpl
@@ -0,0 +1,64 @@
+<!-- BEGIN header -->
+ <form action="{action_url}" method="post">
+  <table border="0" align="center" width="70%">
+   <tr bgcolor="#486591">
+     <td colspan="2" align="center"><b><font color="#fefefe">{description}</font></b></td>
+   </tr>
+   <tr bgcolor="#e6e6e6">
+<!-- END header -->
+
+<!-- BEGIN user_list -->
+    <td align="center" valign="top">
+     &nbsp;{select_users}<br />
+     <select name="users[]" multiple size="20">
+{users}
+     </select>
+    </td>
+<!-- END user_list -->
+
+<!-- BEGIN group_list -->
+    <td align="center" valign="top">
+     &nbsp;{select_groups}<br />
+     <select name="groups[]" multiple size="20">
+{groups}
+     </select>
+    </td>
+<!-- END group_list -->
+
+<!-- BEGIN ldap_admin -->
+   <tr bgcolor="#e6e6e6">
+    <td colspan="2" align="center">{ldap_admin_message}:</td>
+   </tr>
+   <tr bgcolor="#e6e6e6">
+    <td align="right">{ldap_admin_label}: </td><td><input name="ldap_admin" value="" /></td>
+   </tr>
+   <tr bgcolor="#e6e6e6">
+    <td align="right">{ldap_admin_pw_label}: </td><td><input type="password" name="ldap_admin_pw" value="" /></td>
+   </tr>
+<!-- END ldap_admin -->
+
+<!-- BEGIN submit -->
+   </tr>
+   <tr bgcolor="#e6e6e6">
+    <td colspan="2" align="center">{memberships}</td>
+   </tr>
+   <tr bgcolor="#e6e6e6">
+    <td colspan="2" align="center">
+     <input type="submit" name="migrate" value="{migrate}" />
+     <input type="submit" name="cancel" value="{cancel}" />
+    </td>
+   </tr>
+<!-- END submit -->
+
+<!-- BEGIN cancel_only -->
+   <tr bgcolor="#e6e6e6">
+    <td colspan="2" align="center">
+      <input type="submit" name="cancel" value="{cancel}" />
+    </td>
+   </tr>
+<!-- END cancel_only -->
+
+<!-- BEGIN footer -->
+  </table>
+ </form>
+<!-- END footer -->
