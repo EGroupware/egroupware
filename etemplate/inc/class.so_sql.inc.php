EGroupware/egroupware

commit 145cf37cd9bf1054225f755d82ab497854026841

* All apps/custom fields: fixed multiple identical rows, if custom fields where set (introduced in last package)
git-svn-id: http://svn.egroupware.org/egroupware/trunk@43149 923e3c2e-ae13-0410-ba18-ced7211e33de

etemplate/inc/class.so_sql_cf.inc.php
@@ -500,6 +500,11 @@ class so_sql_cf extends so_sql
 					$col = $this->table_name .'.'.array_search($col, $this->db_cols).' AS '.$col;
 				}
 			}
+			$only_keys[0] = 'DISTINCT '.$only_keys[0];
+		}
+		else
+		{
+			$only_keys = 'DISTINCT '.$only_keys;
 		}
 		// check if we order by a custom field --> join cf table for given cf and order by it's value
 		if (strpos($order_by,self::CF_PREFIX) !== false)

commit 0bca64517cb65032e6af9be47e367679f8870c67

fixed SQL error introduced by r43149: multiple identical rows

etemplate/inc/class.so_sql.inc.php
@@ -823,6 +823,7 @@ class so_sql
 	 */
 	function &search($criteria,$only_keys=True,$order_by='',$extra_cols='',$wildcard='',$empty=False,$op='AND',$start=false,$filter=null,$join='',$need_full_no_count=false)
 	{
+		//error_log(__METHOD__.'('.array2string(array_combine(array_slice(array('criteria','only_keys','order_by','extra_cols','wildcard','empty','op','start','filter','join','need_full_no_count'), 0, count(func_get_args())), func_get_args())).')');
 		if ((int) $this->debug >= 4) echo "<p>so_sql::search(".print_r($criteria,true).",'$only_keys','$order_by',".print_r($extra_cols,true).",'$wildcard','$empty','$op','$start',".print_r($filter,true).",'$join')</p>\n";
 
 		// if extending class or instanciator set columns to search, convert string criteria to array

etemplate/inc/class.so_sql_cf.inc.php
@@ -414,6 +414,7 @@ class so_sql_cf extends so_sql
 	 */
 	function &search($criteria,$only_keys=True,$order_by='',$extra_cols='',$wildcard='',$empty=False,$op='AND',$start=false,$filter=null,$join='',$need_full_no_count=false)
 	{
+		//error_log(__METHOD__.'('.array2string(array_combine(array_slice(array('criteria','only_keys','order_by','extra_cols','wildcard','empty','op','start','filter','join','need_full_no_count'), 0, count(func_get_args())), func_get_args())).')');
 		if ($only_keys === false)
 		{
 			$only_keys = $this->table_name.'.*';
@@ -500,6 +501,8 @@ class so_sql_cf extends so_sql
 					$col = $this->table_name .'.'.array_search($col, $this->db_cols).' AS '.$col;
 				}
 			}
+			// add DISTINCT as by joining custom fields for search a row can be returned multiple times
+			$only_keys = array_values($only_keys);
 			$only_keys[0] = 'DISTINCT '.$only_keys[0];
 		}
 		else