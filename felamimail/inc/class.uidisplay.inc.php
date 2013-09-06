7fe91e4e77630c2e39db6080521c33e6edb9a5bb
Klaus Leithoff <kl@stylite.de>
2013-09-06 12:00:41 +0000

    * eMail: fix possible problem when mail-message-body (text or html part) is empty

--- felamimail/inc/class.uidisplay.inc.php
+++ felamimail/inc/class.uidisplay.inc.php
@@ -1494,6 +1494,12 @@ blockquote[type=cite] {
 					$body .= $singleBodyPart['body'];
 					continue;
 				}
+				$bodyPartIsSet = strlen(trim($singleBodyPart['body']));
+				if (!$bodyPartIsSet)
+				{
+					$body .= '';
+					continue;
+				}
 				if(!empty($body)) {
 					$body .= '<hr style="border:dotted 1px silver;">';
 				}
