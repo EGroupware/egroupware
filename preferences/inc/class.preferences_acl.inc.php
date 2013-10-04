f8cb87e5a472b9dc8b68ca57e1483a2a4356396f
Ralf Becker <ralfbecker@outdoor-training.de>
2013-10-04 08:42:47 +0000

    missing preferences_acl class uses for user-level ACL access

---
+++ preferences/inc/class.preferences_acl.inc.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * EGroupware: Preferences ACL
+*
+* @link http://www.egroupware.org
+* @author Ralf Becker <rb@stylite.de>
+* @package admin
+* @copyright (c) 2013 by Ralf Becker <rb@stylite.de>
+* @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+* @version $Id$
+*/
+
+/**
+ * Preferences ACL
+ *
+ * Uses admin_acl, which already does necessary access-control for non-admin users!
+ */
+class preferences_acl extends admin_acl
+{
+	function __construct()
+	{
+		translation::add_app('admin');
+		egw_framework::includeCSS('admin', 'app');
+	}
+}
