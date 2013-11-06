e53f781610c3529fe81636c0740d4971ce4a0f10
Ralf Becker <ralfbecker@outdoor-training.de>
2013-11-06 10:30:18 +0000

    adding caption too

--- etemplate/inc/class.etemplate_widget.inc.php
+++ etemplate/inc/class.etemplate_widget.inc.php
@@ -302,7 +302,10 @@ class etemplate_widget
 					// Fall back to widget class, we can not ignore it, as the widget may contain other widgets
 					$class_name = 'etemplate_widget';
 					// Don't warn about these known missing ones
-					if (!in_array($type, array('styles','menulist','tabs','tab','tabpanels','html','split','iframe','toolbar')))
+					if (!in_array($type, array(
+						'styles', 'menulist', 'tabs', 'tab', 'tabpanels', 'html',
+						'split', 'iframe', 'toolbar', 'caption',
+					)))
 					{
 						trigger_error("Could not find a class for $type, using $class_name", E_USER_NOTICE);
 					}
