6985c3d61fdc04fbc59fbc7c0dabd86dcd3e076c
Nathan Gray <nathangray.bsc@gmail.com>
2013-11-21 19:09:17 +0000

    Resize popups according to their content

--- phpgwapi/js/jsapi/egw.js
+++ phpgwapi/js/jsapi/egw.js
@@ -178,6 +178,13 @@
 			}
 			else
 			{
+				if(popup || window.opener)
+				{
+					// Resize popup when et2 load is done
+					jQuery(node).one("load",function() {
+						window.resizeTo(jQuery(document).width()+10,jQuery(document).height()+60);
+					});
+				}
 				var et2 = new etemplate2(node, window.egw_appName+".etemplate_new.ajax_process_content.etemplate");
 				et2.load(data.name,data.url,data.data,callback);
 			}
