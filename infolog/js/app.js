5ae4da09cdbcf2fff21e741a408fe13516b423ce
Nathan Gray <nathangray.bsc@gmail.com>
2014-01-10 14:25:41 +0000

    Make sure change callback doesn't run during template rendering

--- infolog/js/app.js
+++ infolog/js/app.js
@@ -220,6 +220,9 @@ app.classes.infolog = AppJS.extend(
 	*/
 	status_changed: function(changed_id, status_id, percent_id, datecompleted_id)
 	{
+		// Make sure this doesn't get executed while template is loading
+		if(this.et2 == null) return;
+		
 		var status = document.getElementById(status_id);
 		var percent = document.getElementById(percent_id);
 		var datecompleted = document.getElementById(datecompleted_id+'[str]');
