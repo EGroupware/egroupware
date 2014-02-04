ad464d2824161e99a48b58fb845f3bae6b819bfe
Ralf Becker <ralfbecker@outdoor-training.de>
2014-02-04 09:24:17 +0000

    show backtrace in error_log of validation-errors

--- etemplate/inc/class.etemplate_widget.inc.php
+++ etemplate/inc/class.etemplate_widget.inc.php
@@ -782,7 +782,7 @@ class etemplate_widget
 	public static function set_validation_error($name,$error,$cname=null)
 	{
 		// not yet used: if (is_null($cname)) $cname = self::$name_vars;
-		error_log(__METHOD__."('$name','$error','$cname')");
+		error_log(__METHOD__."('$name','$error','$cname') ".function_backtrace());
 
 		if ($cname) $name = self::form_name($cname,$name);
 
