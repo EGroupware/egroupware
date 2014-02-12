85e844fd9bd08f72516223ba02abaab521e3732d
Klaus Leithoff <kl@stylite.de>
2014-02-12 08:10:58 +0000

    fix deprecated warning returnValue use preventDefault()

--- phpgwapi/js/jquery/jquery.js
+++ phpgwapi/js/jquery/jquery.js
@@ -5371,7 +5371,7 @@ jQuery.Event = function( src, props ) {
 
 		// Events bubbling up the document may have been marked as prevented
 		// by a handler lower down the tree; reflect the correct value.
-		this.isDefaultPrevented = ( src.defaultPrevented || src.returnValue === false ||
+		this.isDefaultPrevented = ( src.defaultPrevented || src.preventDefault() === false ||
 			src.getPreventDefault && src.getPreventDefault() ) ? returnTrue : returnFalse;
 
 	// Event type
