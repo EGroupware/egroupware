3ba14a12911dd1c9e84302d27811fec4540b9884
Ralf Becker <ralfbecker@outdoor-training.de>
2014-02-17 11:18:59 +0000

    always stop event propagation for click-handler executing on javascript urls in navigation

--- phpgwapi/templates/idots/js/idots.js
+++ phpgwapi/templates/idots/js/idots.js
@@ -63,7 +63,8 @@ egw_LAB.wait(function() {
 
 		// allowing javascript urls in topmenu and sidebox only under CSP by binding click handlers to them
 		var href_regexp = /^javascript:([^\(]+)\((.*)?\);?$/;
-		jQuery('#topmenu_items,#thesideboxcolumn').on('click','a[href^="javascript:"]',function(){
+		jQuery('#topmenu_items,#thesideboxcolumn').on('click','a[href^="javascript:"]',function(ev){
+			ev.stopPropagation();	// do NOT execute regular event, as it will violate CSP, when handler does NOT return false
 			var matches = this.href.match(href_regexp);
 			var args = [];
 			if (matches.length > 1 && matches[2] !== undefined)
