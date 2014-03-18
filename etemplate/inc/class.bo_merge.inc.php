92e29ff0c4ecc35852c06cf4d41bb6674ade06d4
Nathan Gray <nathangray.bsc@gmail.com>
2014-03-18 14:13:57 +0000

    Fix merge print to work with both old & new ODF spreadsheets - adds support for calcext:value-type cell attribute

--- etemplate/inc/class.bo_merge.inc.php
+++ etemplate/inc/class.bo_merge.inc.php
@@ -1084,7 +1084,7 @@ abstract class bo_merge
 		switch($mimetype)
 		{
 			case 'application/vnd.oasis.opendocument.spreadsheet':		// open office calc
-				$format = '/<table:table-cell([^>]+?)office:value-type="[^"]+"([^>]*?)(?:calcext:value-type="[^"]+")>.?<([a-z].*?)[^>]*>('.implode('|',$names).')<\/\3>.?<\/table:table-cell>/s';
+				$format = '/<table:table-cell([^>]+?)office:value-type="[^"]+"([^>]*?)(?:calcext:value-type="[^"]+")?>.?<([a-z].*?)[^>]*>('.implode('|',$names).')<\/\3>.?<\/table:table-cell>/s';
 				$replacement = '<table:table-cell$1office:value-type="float" office:value="$4"$2 calcext:value-type="float"><$3>$4</$3></table:table-cell>';
 				break;
 			case 'application/vnd.oasis.opendocument.text':		// tables in open office writer
