c3b70189167a3c7e01b188ff855598de350bc40a
Ralf Becker <ralfbecker@outdoor-training.de>
2014-05-16 09:09:38 +0000

    logging for exceptions now also request-method and user-agent

--- phpgwapi/inc/common_functions.inc.php
+++ phpgwapi/inc/common_functions.inc.php
@@ -1610,8 +1610,9 @@ function _egw_log_exception(Exception $e,&$headline=null)
 		{
 			error_log($line);
 		}
-		error_log('# Instance='.$GLOBALS['egw_info']['user']['domain'].', User='.$GLOBALS['egw_info']['user']['account_lid'].', URL='.
-			($_SERVER['HTTPS']?'https://':'http://').$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+		error_log('# Instance='.$GLOBALS['egw_info']['user']['domain'].', User='.$GLOBALS['egw_info']['user']['account_lid'].
+			', Request='.$_SERVER['REQUEST_METHOD'].' '.($_SERVER['HTTPS']?'https://':'http://').$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].
+			', User-agent='.$_SERVER['HTTP_USER_AGENT']);
 	}
 }
 
