d1c25c7c3835a018932c00d6e4b0d7a6b0e3ad98
Ralf Becker <ralfbecker@outdoor-training.de>
2014-05-27 12:46:18 +0000

    * fixed SQL error "Field last_mod doesnt have a default value" on update for newer MySQL versions or PostgreSQL

--- phpgwapi/inc/class.categories.inc.php
+++ phpgwapi/inc/class.categories.inc.php
@@ -956,6 +956,7 @@ class categories
 					'cat_appname' => '*update*',
 					'cat_name'    => 'global=0',
 					'cat_description' => 'global=0',
+					'last_mod'    => time(),
 				),false,__LINE__,__FILE__);
 			}
 			self::$cache = array();
