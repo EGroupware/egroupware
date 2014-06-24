545b35de5bcc1ae1d540cf1002eec720f65e348e
Hadi Nategh <hn@stylite.de>
2014-06-24 12:40:25 +0000

    Declare a non declared variable which was messing with app global object

--- etemplate/js/et2_extension_nextmatch.js
+++ etemplate/js/et2_extension_nextmatch.js
@@ -815,6 +815,7 @@ var et2_nextmatch = et2_DOMWidget.extend([et2_IResizeable, et2_IInput],
 	 */
 	_updateUserPreferences: function() {
 		var colMgr = this.dataview.getColumnMgr();
+		var app = "";
 		if(!this.options.settings.columnselection_pref) {
 			this.options.settings.columnselection_pref = this.options.template;
 		}
