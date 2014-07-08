EGroupware/egroupware

commit 419eb67c75c0b37cc4176e81deed378688aeeb39

Use select_all flag to export all entries, not just visible selected ones.
git-svn-id: http://svn.egroupware.org/egroupware/trunk@47527 923e3c2e-ae13-0410-ba18-ced7211e33de

importexport/inc/class.importexport_export_ui.inc.php
@@ -52,6 +52,8 @@ class importexport_export_ui {
 		$_appname = $_content['appname'] ? $_content['appname'] : $_GET['appname'];
 		$_definition = $_content['definition'] ? $_content['definition'] : $_GET['definition'];
 		$_plugin = $_content['plugin'] ? $_content['plugin'] : $_GET['plugin'];
+		// Select all from context menu, means use all search results, not just selected
+		if($_GET['select_all']) $_GET['selection'] = 'search';
 		$_selection = $_content['selection'] ? $_content['selection'] : $_GET['selection'];
 		if($_GET['selection'] || $_content['selection_passed']) $content['selection_passed'] = $preserv['selection_passed'] = true;
 

addressbook/inc/class.addressbook_ui.inc.php
@@ -644,7 +644,7 @@ class addressbook_ui extends addressbook_bo
 					'csv'    => array(
 						'caption' => 'Export as CSV',
 						'allowOnMultiple' => true,
-						'url' => 'menuaction=importexport.importexport_export_ui.export_dialog&appname=addressbook&plugin=addressbook_export_contacts_csv&selection=$id',
+						'url' => 'menuaction=importexport.importexport_export_ui.export_dialog&appname=addressbook&plugin=addressbook_export_contacts_csv&selection=$id&select_all=$select_all',
 						'popup' => '850x440'
 					),
 					'vcard'  => array(

commit 15926a315fdc2e5f7e97d13542a59bc6f9bbb2fa

Fix selecting only certain records, broken by r47527

importexport/inc/class.importexport_export_ui.inc.php
@@ -53,7 +53,7 @@ class importexport_export_ui {
 		$_definition = $_content['definition'] ? $_content['definition'] : $_GET['definition'];
 		$_plugin = $_content['plugin'] ? $_content['plugin'] : $_GET['plugin'];
 		// Select all from context menu, means use all search results, not just selected
-		if($_GET['select_all']) $_GET['selection'] = 'search';
+		if($_GET['select_all'] == 'true') $_GET['selection'] = 'search';
 		$_selection = $_content['selection'] ? $_content['selection'] : $_GET['selection'];
 		if($_GET['selection'] || $_content['selection_passed']) $content['selection_passed'] = $preserv['selection_passed'] = true;
 