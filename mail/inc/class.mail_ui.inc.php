24606be5821a81359a9d17ab8022e4007b1d7ec2
Klaus Leithoff <kl@stylite.de>
2014-07-17 09:59:14 +0000

    handle all text string for move and delete action success message

--- mail/inc/class.mail_ui.inc.php
+++ mail/inc/class.mail_ui.inc.php
@@ -4229,6 +4229,7 @@ class mail_ui
 	{
 		if(mail_bo::$debug) error_log(__METHOD__."->".print_r($_messageList,true).' Method:'.$_forceDeleteMethod);
 		$error = null;
+		$filtered =  false;
 		if ($_messageList=='all' || !empty($_messageList['msg']))
 		{
 			if (isset($_messageList['all']) && $_messageList['all'])
@@ -4247,6 +4248,7 @@ class mail_ui
 							emailadmin_imapbase::$supportsORinQuery = egw_cache::getCache(egw_cache::INSTANCE,'email','supportsORinQuery'.trim($GLOBALS['egw_info']['user']['account_id']), null, array(), 60*60*10);
 							if (!isset(emailadmin_imapbase::$supportsORinQuery[$this->mail_bo->profileID])) emailadmin_imapbase::$supportsORinQuery[$this->mail_bo->profileID]=true;
 						}
+						$filtered =  true;
 						$filter = array('filterName' => (emailadmin_imapbase::$supportsORinQuery[$this->mail_bo->profileID]?lang('quicksearch'):lang('subject')),'type' => ($query['filter2']?$query['filter2']:(emailadmin_imapbase::$supportsORinQuery[$this->mail_bo->profileID]?'quick':'subject')),'string' => $query['search'],'status' => (!empty($query['filter'])?$query['filter']:'any'));
 					}
 					else
@@ -4303,7 +4305,7 @@ class mail_ui
 			$response = egw_json_response::get();
 			if (empty($error))
 			{
-				$response->call('app.mail.mail_deleteMessagesShowResult',array('egw_message'=>lang('deleted %1 messages in %2',($messageList=='all'||$_messageList['all']?lang('all'):count($_messageList['msg'])),$folder),'msg'=>$_messageList['msg']));
+				$response->call('app.mail.mail_deleteMessagesShowResult',array('egw_message'=>lang('deleted %1 messages in %2',($messageList=='all'||$_messageList['all']?($filtered?lang('all filtered'):lang('all')):count($_messageList['msg'])),$folder),'msg'=>$_messageList['msg']));
 			}
 			else
 			{
@@ -4350,7 +4352,7 @@ class mail_ui
 			$lastFoldersUsedForMoveCont[$targetProfileID][$targetFolder]=$_folderName;
 			$changeFolderActions = true;
 		}
-
+		$filtered = false;
 		if ($_messageList=='all' || !empty($_messageList['msg']))
 		{
 			$error=false;
@@ -4371,6 +4373,7 @@ class mail_ui
 							emailadmin_imapbase::$supportsORinQuery = egw_cache::getCache(egw_cache::INSTANCE,'email','supportsORinQuery'.trim($GLOBALS['egw_info']['user']['account_id']), null, array(), 60*60*10);
 							if (!isset(emailadmin_imapbase::$supportsORinQuery[$this->mail_bo->profileID])) emailadmin_imapbase::$supportsORinQuery[$this->mail_bo->profileID]=true;
 						}
+						$filtered = true;
 						$filter = array('filterName' => (emailadmin_imapbase::$supportsORinQuery[$this->mail_bo->profileID]?lang('quicksearch'):lang('subject')),'type' => ($query['filter2']?$query['filter2']:(emailadmin_imapbase::$supportsORinQuery[$this->mail_bo->profileID]?'quick':'subject')),'string' => $query['search'],'status' => (!empty($query['filter'])?$query['filter']:'any'));
 					}
 					else
@@ -4454,11 +4457,11 @@ class mail_ui
 			{
 				if ($_copyOrMove=='copy')
 				{
-					$response->call('egw.message',lang('copied %1 message(s) from %2 to %3',count($messageList),$folder,$targetFolder));
+					$response->call('egw.message',lang('copied %1 message(s) from %2 to %3',($messageList=='all'||$_messageList['all']?($filtered?lang('all filtered'):lang('all')):count($messageList)),$folder,$targetFolder));
 				}
 				else
 				{
-					$response->call('egw.refresh',lang('moved %1 message(s) from %2 to %3',count($messageList),$folder,$targetFolder),'mail',$messageListForRefresh,'delete');
+					$response->call('egw.refresh',lang('moved %1 message(s) from %2 to %3',($messageList=='all'||$_messageList['all']?($filtered?lang('all filtered'):lang('all')):count($messageList)),$folder,$targetFolder),'mail',$messageListForRefresh,'delete');
 				}
 			}
 			if ($changeFolderActions == true)
