34c3e1bd9a1d131a681384e93687897684088e55
Hadi Nategh <hn@stylite.de>
2014-08-14 07:39:16 +0000

    Revert the change on ajax_refreshVacationNotice from commit 48039. Fix not keeping other vacations of an user in the cache

--- mail/inc/class.mail_ui.inc.php
+++ mail/inc/class.mail_ui.inc.php
@@ -3925,7 +3925,7 @@ class mail_ui
 			if (empty($icServerID)) $icServerID = $mail->mail_bo->profileID;
 			if ($icServerID != $mail->mail_bo->profileID) return;
 
-			$vacation = $mail->gatherVacation();
+			$vacation = $mail->gatherVacation($cachedVacations);
 		}
 
 		if($vacation) {
