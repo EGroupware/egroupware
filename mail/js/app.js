491e0002ca449b572448124fbf4340d885a05208
Hadi Nategh <hn@stylite.de>
2014-09-09 13:55:43 +0000

    * Mail/Chrome: Fix not closing print dialog in compose after print or cancel in Chrome

--- mail/js/app.js
+++ mail/js/app.js
@@ -3741,7 +3741,10 @@ app.classes.mail = AppJS.extend(
 		
 		if (!window.onafterprint)
 		{
-			window.onfocus = afterprint;
+			// For browsers which does not support onafterprint event, eg. Chrome
+			setTimeout(function() {
+				window.close();
+			}, 2000);
 		}
 		else
 		{
