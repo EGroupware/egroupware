812518f72fa9598ba4488aba792f426ea4535bd0
Nathan Gray <nathangray.bsc@gmail.com>
2014-09-24 16:15:24 +0000

    Fix unwanted apps showing in link-app

--- etemplate/js/et2_widget_link.js
+++ etemplate/js/et2_widget_link.js
@@ -1803,7 +1803,8 @@ var et2_link_add = et2_inputWidget.extend(
 		}
 		this.app_select = et2_createWidget("link-apps", jQuery.extend({},this.options,{
 			'id': this.options.id + 'app',
-			value: this.options.application ? this.options.application : this.options.value && this.options.value.add_app ? this.options.value.add_app : null
+			value: this.options.application ? this.options.application : this.options.value && this.options.value.add_app ? this.options.value.add_app : null,
+			application_list: this.options.application ? this.options.application : null
 		}) ,this);
 		this.div.append(this.app_select.getDOMNode());
 		this.button = et2_createWidget("button", {id:this.options.id+"_add",label: this.egw().lang("add")}, this);
