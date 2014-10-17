c1cea7ce7396e097fef82b65e0b927a1b1970fde
Nathan Gray <nathangray.bsc@gmail.com>
2014-10-17 17:27:19 +0000

    Add timesheet time (info_sum_timesheets) placeholder

--- infolog/inc/class.infolog_merge.inc.php
+++ infolog/inc/class.infolog_merge.inc.php
@@ -130,6 +130,15 @@ class infolog_merge extends bo_merge
 		// Links
 		$array += $this->get_all_links('infolog', $id, $prefix, $content);
 
+		// Timesheet time
+		if(strpos($content, 'info_sum_timesheets'))
+		{
+			$timesheets = array();
+			$links = egw_link::get_links('infolog',$id,'timesheet');
+			$sum = ExecMethod('timesheet.timesheet_bo.sum',$links);
+			$info['$$info_sum_timesheets$$'] = $sum['duration'];
+		}
+
 		// Check for linked project ID
 		$links = egw_link::get_links('infolog', $id, 'projectmanager');
 		foreach($links as $link_id => $app_id) {
@@ -168,7 +177,7 @@ class infolog_merge extends bo_merge
 
 		$n = 0;
 		$tracking = new infolog_tracking($this->bo);
-		$fields = array('info_id' => lang('Infolog ID'), 'pm_id' => lang('Project ID'), 'project' => lang('Project name')) + $tracking->field2label;
+		$fields = array('info_id' => lang('Infolog ID'), 'pm_id' => lang('Project ID'), 'project' => lang('Project name')) + $tracking->field2label + array('info_sum_timesheets' => lang('Used time'));
 		translation::add_app('projectmanager');
 		foreach($fields as $name => $label)
 		{
