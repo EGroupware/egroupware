f9e573f5cc716d666b9085231a22da235afeeb4d
Ralf Becker <ralfbecker@outdoor-training.de>
2014-10-17 12:51:49 +0000

    * Addressbook: with double-click preference set to edit, CRM-view did not open when selected in menu

--- addressbook/js/app.js
+++ addressbook/js/app.js
@@ -492,6 +492,8 @@ app.classes.addressbook = AppJS.extend(
 
 	/**
 	 * Apply advanced search filters to index nextmatch
+	 *
+	 * @param {object} filters
 	 */
 	adv_search: function(filters)
 	{
@@ -568,7 +570,7 @@ app.classes.addressbook = AppJS.extend(
 	 */
 	addEmail: function(action, selected)
 	{
-		// Check for all selected.  
+		// Check for all selected.
 		var nm = this.et2.getWidgetById('nm');
 		if(fetchAll(selected, nm, jQuery.proxy(function(ids) {
 			// fetchAll() returns just the ID, no prefix, so map it to match normal selected
