7cf323b3be95704a0c8902d77166c94d76384103
Ralf Becker <ralfbecker@outdoor-training.de>
2014-10-30 13:20:36 +0000

    remove upper limit of number of characters at last domain-part to allow new domain-names like .hamburg

--- etemplate/inc/class.etemplate_widget_url.inc.php
+++ etemplate/inc/class.etemplate_widget_url.inc.php
@@ -33,7 +33,7 @@ class etemplate_widget_url extends etemplate_widget
 	 *
 	 * Same preg is in et2_widget_url Javascript class, but no \x00 allowed and /u modifier for utf8!
 	 */
-	const EMAIL_PREG = "/^(([^\042',<][^,<]+|\042[^\042]+\042|\'[^\']+\'|)\s?<)?[^\x01-\x20()<>@,;:\042\[\]]+@([a-z0-9ÄÖÜäöüß](|[a-z0-9ÄÖÜäöüß_-]*[a-z0-9ÄÖÜäöüß])\.)+[a-z]{2,6}>?$/iu";
+	const EMAIL_PREG = "/^(([^\042',<][^,<]+|\042[^\042]+\042|\'[^\']+\'|)\s?<)?[^\x01-\x20()<>@,;:\042\[\]]+@([a-z0-9ÄÖÜäöüß](|[a-z0-9ÄÖÜäöüß_-]*[a-z0-9ÄÖÜäöüß])\.)+[a-z]{2,}>?$/iu";
 
 	/**
 	 * Validate input
