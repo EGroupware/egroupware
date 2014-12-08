0fbdc3e5950771b783564a051343e5b29bc7147b
Nathan Gray <nathangray.bsc@gmail.com>
2014-12-08 17:23:57 +0000

    Explicitly apply state filters to UI

--- calendar/inc/class.calendar_favorite_portlet.inc.php
+++ calendar/inc/class.calendar_favorite_portlet.inc.php
@@ -64,6 +64,12 @@ class calendar_favorite_portlet extends home_favorite_portlet
 		else
 		{
 			$ui = new calendar_uiviews();
+			if($this->favorite['state']['start']) $ui->search_params['start'] = $this->favorite['state']['start'];
+			if($this->favorite['state']['cat_id']) $ui->search_params['cat_id'] = $this->favorite['state']['cat_id'];
+			if($this->favorite['state']['owner']) $ui->search_params['users'] = explode(',',$this->favorite['state']['owner']);
+			if($this->favorite['state']['filter']) $ui->search_params['filter'] = $this->favorite['state']['filter'];
+			if($this->favorite['state']['sortby']) $ui->search_params['sortby'] = $this->favorite['state']['sortby'];
+			
 			$etemplate->read('home.legacy');
 
 			$etemplate->set_dom_id($id);
