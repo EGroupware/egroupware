ee807027afa98114a79e9ecb8885cc2dc8921bce
Ralf Becker <ralfbecker@outdoor-training.de>
2014-12-13 09:54:14 +0000

    move defining of (deprecated) SEP constant to common functions, as there are cases where it gets used before defined

--- phpgwapi/inc/common_functions.inc.php
+++ phpgwapi/inc/common_functions.inc.php
@@ -1100,6 +1100,7 @@ function get_account_id($account_id = '',$default_id = '')
  *
  * This is completely unnecessary, as you can use forward slashes in php under every OS -- RalfBecker 2005/11/09
  *
+ * @deprecated just use forward slashes supported by PHP on all OS
  * @return file system separator
  */
 function filesystem_separator()
@@ -1113,6 +1114,11 @@ function filesystem_separator()
 		return '/';
 	}
 }
+
+/**
+ * @deprecated just use forward slashes supported by PHP on all OS
+ */
+define('SEP', filesystem_separator());
 
 /**
  * print an array or object as pre-formatted html
