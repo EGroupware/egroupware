9bd80cdfc4b465819b2d088ec16682f8657e86e9
Hadi Nategh <hn@stylite.de>
2015-01-21 11:15:59 +0000

    Fix url for expose view thumbnails

--- etemplate/js/et2_widget_vfs.js
+++ etemplate/js/et2_widget_vfs.js
@@ -337,7 +337,7 @@ var et2_vfsMime = expose(et2_valueWidget.extend([et2_IDetachedDOM],
 				title: _value.name,
 				href: base_url + _value.download_url,
 				type: _value.mime,
-				thumbnail: _value.path && _value.mime ? egw(window).window.location.origin + this.egw().mime_icon(_value['mime'], _value['path']) : this.image.attr('src')
+				thumbnail: _value.path && _value.mime ? this.egw().mime_icon(_value['mime'], _value['path']) : this.image.attr('src')
 			}];
 		}
 		return mediaContent;
