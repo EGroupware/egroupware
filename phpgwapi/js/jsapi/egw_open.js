bc5023f18c231350ff4c2cfca9ad20b983e7592b
Hadi Nategh <hn@stylite.de>
2015-05-29 15:33:00 +0000

    Keep the fix from previous commit only for IE

--- phpgwapi/js/jsapi/egw_open.js
+++ phpgwapi/js/jsapi/egw_open.js
@@ -66,14 +66,14 @@ egw.extend('open', egw.MODULE_WND_LOCAL, function(_egw, _wnd)
 			// Accoring to microsoft, IE 10/11 can only accept a url with 2083 caharacters
 			// therefore we need to send request to compose window with POST method
 			// instead of GET. We create a temporary <Form> and will post emails.
-			if (uri.length > 2083)
+			if (uri.length > 2083 && navigator.userAgent.match(/Trident|MSIE|Edge/,"g"))
 			{
 				popup = egw.open('','mail','add','','compose__','mail');
-				popup.onload = function(){
-					var emails = uri.replace('mailto:','');
+				popup.onload = new function(){
 					// Build a temp Form and submit right away
-					jQuery('<form  method="post" target="'+popup.name+'" action="'+popup.location.href+'">\n\
-							<input name="preset[mailto]" value="'+emails+'"></input></form>').submit();
+					var $tmpForm = jQuery('<form  method="post" target="'+popup.name+'" action="'+popup.location.href+'">\n\
+							<input name="preset[mailto]" type="text" value="'+uri+'"></input><input type="submit"></input></form>').appendTo('body').submit();
+					$tmpForm.remove();
 				};
 			}
 			else // simple GET request
