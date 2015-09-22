f8f586eeabab100d4438f13358648faaa700bdbf
Nathan Gray <nathangray.bsc@gmail.com>
2015-09-22 16:05:27 +0000

    Do not die if select custom field is not fully set up

--- etemplate/js/et2_extension_customfields.js
+++ etemplate/js/et2_extension_customfields.js
@@ -438,11 +438,14 @@ var et2_customfields_list = et2_valueWidget.extend([et2_IDetachedDOM, et2_IInput
 	},
 	_setup_ajax_select: function(field_name, field, attrs) {
 		var attributes = ['get_rows','get_title','id_field','template'];
-		for(var i = 0; i < attributes.length; i++)
+		if(field.values)
 		{
-			if(typeof field.values[attributes[i]] !== 'undefined')
+			for(var i = 0; i < attributes.length; i++)
 			{
-				attrs[attributes[i]] = field.values[attributes[i]];
+				if(typeof field.values[attributes[i]] !== 'undefined')
+				{
+					attrs[attributes[i]] = field.values[attributes[i]];
+				}
 			}
 		}
 		return true;
