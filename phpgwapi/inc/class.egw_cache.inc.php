9da4924f0097a40d369d61aa5e6f295518b09097
Ralf Becker <ralfbecker@outdoor-training.de>
2015-10-10 10:33:35 +0000

    allow to overide egw_server_root used as key in egw_cache for tree- AND instance-level

--- phpgwapi/inc/class.egw_cache.inc.php
+++ phpgwapi/inc/class.egw_cache.inc.php
@@ -90,6 +90,13 @@ class egw_cache
 	static $max_expiration;
 
 	/**
+	 * Used to determine keys for tree- and instance-level caches
+	 *
+	 * @var string
+	 */
+	static $egw_server_root = EGW_SERVER_ROOT;
+
+	/**
 	 * Add some data in the cache, only if the key does not yet exist
 	 *
 	 * @param string $level use egw_cache::(TREE|INSTANCE)
@@ -675,7 +682,7 @@ class egw_cache
 			case self::TREE:
 				if (!isset($tree_key))
 				{
-					$tree_key = $level.'-'.str_replace(array(':','/','\\'),'-',EGW_SERVER_ROOT);
+					$tree_key = $level.'-'.str_replace(array(':','/','\\'),'-', self::$egw_server_root);
 					// add charset to key, if not utf-8 (as everything we store depends on charset!)
 					if (($charset = self::get_system_config('system_charset',false)) && $charset != 'utf-8')
 					{
