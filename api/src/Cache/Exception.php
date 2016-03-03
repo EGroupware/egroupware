1f3c674ef65370bad4a07346e86ea97ea0fc0827
Ralf Becker <ralfbecker@outdoor-training.de>
2016-03-03 11:41:45 +0000

    adding an Exceptions for caching problems

---
+++ api/src/Cache/Exception.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * EGroupware API - Cache Exceptions
+ *
+ * @link http://www.egroupware.org
+ * @author Ralf Becker <RalfBecker-AT-outdoor-training.de>
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package api
+ * @subpackage cache
+ * @access public
+ * @version $Id$
+ */
+
+namespace EGroupware\Api\Cache;
+
+use EGroupware\Api;
+
+/**
+ * Exception thrown by Cache classes
+ */
+class Exception extends Api\Exception {}
