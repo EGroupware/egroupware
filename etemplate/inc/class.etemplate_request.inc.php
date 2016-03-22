0d1eb171ac198758b9f44f9840299f46dbef4e9b
Ralf Becker <ralfbecker@outdoor-training.de>
2016-03-22 10:48:48 +0000

    (re-)adding etemplate_request, it seems to be used in many places

---
+++ etemplate/inc/class.etemplate_request.inc.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * EGroupware - eTemplate request object
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package api
+ * @subpackage etemplate
+ * @link http://www.egroupware.org
+ * @author Ralf Becker <RalfBecker@outdoor-training.de>
+ * @copyright (c) 2007-16 by Ralf Becker <RalfBecker@outdoor-training.de>
+ * @version $Id$
+ */
+
+use EGroupware\Api\Etemplate\Request;
+
+/**
+ * Class to represent the persitent information of an eTemplate request
+ *
+ * @deprecated use Api\Etemplate\Request
+ */
+class etemplate_request extends Request {}
