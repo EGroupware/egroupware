4fe441085fe4438cb02cc2ddef882ccd09e22446
Ralf Becker <ralfbecker@outdoor-training.de>
2016-04-07 13:09:23 +0000

    cant include api/asyncwrapper.php, as #!/usr/bin/php gets echoed and therefore generated cron mails

--- phpgwapi/cron/asyncwrapper.php
+++ phpgwapi/cron/asyncwrapper.php
@@ -11,4 +11,14 @@
  * @version $Id$
  */
 
-include __DIR__.'/../../api/asyncwrapper.php';
+$path_to_egroupware = realpath(__DIR__.'/../..');	//  need to be adapted if this script is moved somewhere else
+$php = isset($_ENV['_']) ? $_ENV['_'] : $_SERVER['_'];
+
+foreach(file($path_to_egroupware. '/header.inc.php') as $line)
+{
+	if(preg_match("/GLOBALS\['egw_domain']\['(.*)']/", $line, $matches))
+	{
+		// -d memory_limit=-1 --> no memory limit
+		system($php. ' -q -d memory_limit=-1 '.$path_to_egroupware.'/api/asyncservices.php '. $matches[1]);
+	}
+}
