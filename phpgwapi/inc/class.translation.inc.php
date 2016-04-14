aa00789d4fb7720d2be96fd9d97169f40a4bdba9
Klaus Leithoff <kl@stylite.de>
2016-04-14 14:21:51 +0000

    try to prevent infinite loops when decoding mail-headers

--- phpgwapi/inc/class.translation.inc.php
+++ phpgwapi/inc/class.translation.inc.php
@@ -1147,7 +1147,8 @@ class translation
 				{
 					if( strtoupper($element->charset) != 'UTF-8') $element->text = preg_replace($sar,$rar,$element->text);
 					// check if there is a possible nested encoding; make sure that the inputstring and the decoded result are different to avoid loops
-					if(preg_match('/\?=.+=\?/', $element->text) && $element->text != $_string)
+					$openTags = substr_count($element->text,'?=');
+					if(preg_match('/\?=.+=\?/', $element->text) && $openTags>0 && $openTags==substr_count($element->text,'=?') && $element->text != $_string)
 					{
 						$element->text = self::decodeMailHeader($element->text, $element->charset);
 						$element->charset = $displayCharset;
