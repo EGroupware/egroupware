62ab3ce897ece41689c0f5e7112a77afaca3ae0b
Ralf Becker <ralfbecker@outdoor-training.de>
2016-04-25 07:23:36 +0000

    using new et2 site config

--- notifications/inc/hook_admin.inc.php
+++ notifications/inc/hook_admin.inc.php
@@ -1,17 +1,17 @@
 <?php
-	/**
-	 * eGroupWare - Notifications
-	 *
-	 * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
-	 * @package notifications
-	 * @link http://www.egroupware.org
-	 * @author Christian Binder <christian@jaytraxx.de>
-	 * @version $Id: hook_preferences.inc.php 22498 2006-09-25 10:20:46Z jaytraxx $
-	 */
-	
-	$file = Array(	'Site Configuration' => $GLOBALS['egw']->link('/index.php', array(
-		'menuaction'	=> 'admin.uiconfig.index',
-		'appname'		=> $appname,
-		)));
-	display_section($appname,$file);
-?>
+/**
+ * EGroupware - Notifications
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package notifications
+ * @link http://www.egroupware.org
+ * @author Christian Binder <christian@jaytraxx.de>
+ * @version $Id: hook_preferences.inc.php 22498 2006-09-25 10:20:46Z jaytraxx $
+ */
+
+$file = Array(	'Site Configuration' => egw::link('/index.php', array(
+	'menuaction'	=> 'admin.admin_config.index',
+	'appname'		=> $appname,
+	'ajax'          => 'true',
+)));
+display_section($appname,$file);
