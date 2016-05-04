b408d9fa1d4b67f64360a46d2070bf3510b63944
Nathan Gray <nathangray.bsc@gmail.com>
2016-05-04 16:45:42 +0000

    Stop day view resize while hidden - it gets the size wrong. Another attempt, preventing events from resizing incorrectly

--- calendar/js/et2_widget_daycol.js
+++ calendar/js/et2_widget_daycol.js
@@ -857,10 +857,6 @@ var et2_calendar_daycol = (function(){ "use strict"; return et2_valueWidget.exte
 	 */
 	position_event: function(event)
 	{
-		// Not visible?  Skip it for speed.
-		// Should be called again as part of resize when shown.
-		if(!this.div.is(':visible')) return;
-
 		// Sort events into minimally-overlapping columns
 		var columns = this._spread_events();
 
