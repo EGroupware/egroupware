aa0814f4d040f9df1ee468056b1ed386ad8a7851
nathangray <nathangray.bsc+github@gmail.com>
2016-05-24 15:01:20 -0600

    Do not set end date to current time, just leave it unset

--- timesheet/inc/class.timesheet_ui.inc.php
+++ timesheet/inc/class.timesheet_ui.inc.php
@@ -472,8 +472,7 @@ class timesheet_ui extends timesheet_bo
 	{
 		$this->show_sums = false;
 
-		$query_in['enddate'] = $query_in['enddate'] ? $query_in['enddate'] : time();
-		$date_filter = $this->date_filter($query_in['filter'],$query_in['startdate'],$query_in['enddate']);
+		$date_filter = $this->date_filter($query_in['filter'],$query_in['startdate'],$query_in['enddate'] ? $query_in['enddate'] : false);
 
 		if ($query_in['startdate'])
 		{
