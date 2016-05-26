2b2845fff77160d5dbfcb86e8a6215641d89f921
Ralf Becker <RalfBecker@outdoor-training.de>
2016-05-26 11:27:03 +0200

    fix PHP Deprecated:  Methods with the same name as their class will not be constructors in a future version of PHP

--- api/src/WebDAV/Tools/_parse_lockinfo.php
+++ api/src/WebDAV/Tools/_parse_lockinfo.php
@@ -36,12 +36,12 @@
 
 /**
  * helper class for parsing LOCK request bodies
- * 
+ *
  * @package HTTP_WebDAV_Server
  * @author Hartmut Holzgraefe <hholzgra@php.net>
  * @version @package-version@
  */
-class _parse_lockinfo 
+class _parse_lockinfo
 {
     /**
      * success state flag
@@ -82,21 +82,21 @@ class _parse_lockinfo
      * @access private
      */
     var $collect_owner = false;
-    
+
     /**
      * constructor
      *
      * @param  string  path of stream to read
      * @access public
      */
-    function _parse_lockinfo($path) 
+    function __construct($path)
     {
         // we assume success unless problems occur
         $this->success = true;
 
         // remember if any input was parsed
         $had_input = false;
-        
+
         // open stream
         $f_in = fopen($path, "r");
         if (!$f_in) {
@@ -125,7 +125,7 @@ class _parse_lockinfo
                 $had_input = true;
                 $this->success &= xml_parse($xml_parser, $line, false);
             }
-        } 
+        }
 
         // finish parsing
         if ($had_input) {
@@ -140,9 +140,9 @@ class _parse_lockinfo
         xml_parser_free($xml_parser);
 
         // close input stream
-        fclose($f_in);      
+        fclose($f_in);
     }
-    
+
 
     /**
      * tag start handler
@@ -153,7 +153,7 @@ class _parse_lockinfo
      * @return void
      * @access private
      */
-    function _startElement($parser, $name, $attrs) 
+    function _startElement($parser, $name, $attrs)
     {
         // namespace handling
         if (strstr($name, " ")) {
@@ -162,8 +162,8 @@ class _parse_lockinfo
             $ns  = "";
             $tag = $name;
         }
-        
-  
+
+
         if ($this->collect_owner) {
             // everything within the <owner> tag needs to be collected
             $ns_short = "";
@@ -192,7 +192,7 @@ class _parse_lockinfo
             }
         }
     }
-    
+
     /**
      * data handler
      *
@@ -201,7 +201,7 @@ class _parse_lockinfo
      * @return void
      * @access private
      */
-    function _data($parser, $data) 
+    function _data($parser, $data)
     {
         // only the <owner> tag has data content
         if ($this->collect_owner) {
@@ -217,7 +217,7 @@ class _parse_lockinfo
      * @return void
      * @access private
      */
-    function _endElement($parser, $name) 
+    function _endElement($parser, $name)
     {
         // namespace handling
         if (strstr($name, " ")) {
