EGroupware/egroupware

commit 9b99ce5339e4d8f0104feb9848ee8186a0f359b2

Fix mail tree throws xml error when tries to call wizard popup

mail/inc/class.mail_tree.inc.php
@@ -184,15 +184,9 @@ class mail_tree
 
 		try
 		{
-			// User defined folders based on account
-			$definedFolders = array(
-				'Trash'     => $this->ui->mail_bo->getTrashFolder(false),
-				'Templates' => $this->ui->mail_bo->getTemplateFolder(false),
-				'Drafts'    => $this->ui->mail_bo->getDraftFolder(false),
-				'Sent'      => $this->ui->mail_bo->getSentFolder(false),
-				'Junk'      => $this->ui->mail_bo->getJunkFolder(false),
-				'Outbox'    => $this->ui->mail_bo->getOutboxFolder(false),
-			);
+			// *** Note: Should not apply any imap transaction, because in case of exception it will stop the
+			// process of rendering root node
+
 			if ($_parent && !self::isAccountNode($_parent)) // Single node loader
 			{
 				$nodeInfo = Mail::pathToFolderData($_parent, $hDelimiter);
@@ -233,6 +227,16 @@ class mail_tree
 
 				//List of folders
 				$foldersList = $this->ui->mail_bo->getFolderArrays(null, true, $_allInOneGo?0:2,$_subscribedOnly, false);
+
+				// User defined folders based on account
+				$definedFolders = array(
+					'Trash'     => $this->ui->mail_bo->getTrashFolder(false),
+					'Templates' => $this->ui->mail_bo->getTemplateFolder(false),
+					'Drafts'    => $this->ui->mail_bo->getDraftFolder(false),
+					'Sent'      => $this->ui->mail_bo->getSentFolder(false),
+					'Junk'      => $this->ui->mail_bo->getJunkFolder(false),
+					'Outbox'    => $this->ui->mail_bo->getOutboxFolder(false),
+				);
 				foreach ($foldersList as &$folder)
 				{
 					$path = $parent = $parts = explode($folder['delimiter'], $folder['MAILBOX']);
@@ -505,7 +509,7 @@ class mail_tree
 		{
 			if ($account[Tree::ID] == $_profileID)
 			{
-				$account = $branches;
+				array_merge($account , $branches);
 			}
 		}
 		return $tree;

commit d5aab5ca50308c80a184e44d8f969fa2ffa4e8f2

Fix broken mail tree caused by commit 9b99ce5339e4d8f0104feb9848ee8186a0f359b2

mail/inc/class.mail_tree.inc.php
@@ -509,7 +509,7 @@ class mail_tree
 		{
 			if ($account[Tree::ID] == $_profileID)
 			{
-				array_merge($account , $branches);
+				$account = array_merge($account , $branches);
 			}
 		}
 		return $tree;