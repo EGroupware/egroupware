24532ec3d7aa4bd06ff6a45d061932f2408e0ef6
Ralf Becker <RalfBecker@outdoor-training.de>
2016-06-09 16:26:39 +0200

    close InfoLog edit window with "Permission denied!", if user has no read rights to an entry, happens eg. when clicking on elementlist in PM, which shows elements independent of read rights

--- infolog/inc/class.infolog_ui.inc.php
+++ infolog/inc/class.infolog_ui.inc.php
@@ -1903,7 +1903,10 @@ class infolog_ui
 			$print = (int) $_REQUEST['print'];
 			//echo "<p>infolog_ui::edit: info_id=$info_id,  action='$action', action_id='$action_id', type='$type', referer='$referer'</p>\n";
 
-			$content = $this->bo->read( $info_id || $action != 'sp' ? $info_id : $action_id );
+			if (!($content = $this->bo->read( $info_id || $action != 'sp' ? $info_id : $action_id )))
+			{
+				Framework::window_close(lang('Permission denied!'));
+			}
 			if (!(strpos($content['info_addr'],',')===false) && strpos($content['info_addr'],', ')===false) $content['info_addr'] = str_replace(',',', ',$content['info_addr']);
 			foreach(array('info_subject', 'info_des') as $key)
 			{
