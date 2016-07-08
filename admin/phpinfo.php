1687476928d79b4063b326fbf72ddaf3c2335281
Ralf Becker <RalfBecker@outdoor-training.de>
2016-07-08 12:09:37 +0200

    * Admin: add used caching provider and APCu stats to "PHP Information"

--- admin/phpinfo.php
+++ admin/phpinfo.php
@@ -8,6 +8,8 @@
  * @version $Id$
  */
 
+use EGroupware\Api;
+
 $GLOBALS['egw_info']['flags'] = array(
 	'noheader'   => True,
 	'nonavbar'   => True,
@@ -20,4 +22,22 @@ if ($GLOBALS['egw']->acl->check('info_access',1,'admin'))
 	$GLOBALS['egw']->redirect_link('/index.php');
 }
 
+$cache_provider = Api\Cache::getProvider();
+$cache_info = '<table><tbody><tr>';
+$cache_info .= '<td class="e">EGroupware caching provider</td><td class="v">'.Api\Cache::getProvider();
+if ($cache_provider == 'EGroupware\\Api\\Cache\\Apcu')
+{
+	$cache_info .= ' <a href="'.htmlspecialchars(Api\Egw::link('/admin/apcu.php')).'">View APCu stats</a>';
+}
+$cache_info .= '</td></tr></tbody></table>'."\n";
+
+ob_start();
 phpinfo();
+$phpinfo = ob_get_clean();
+
+$info = str_ireplace('<body><div class="center">', '<body><div class="center">'."\n".$cache_info, $phpinfo);
+if ($info == $phpinfo)
+{
+	echo $cache_info;
+}
+echo $info;
