5459267be2b5ea21601e894d3620b0a261e5301c
Hadi Nategh <hnategh@gmail.com>
2016-07-25 10:44:44 +0200

    * All apps: Fix not able to set date for empty date fields in mobile template

--- api/js/etemplate/et2_widget_date.js
+++ api/js/etemplate/et2_widget_date.js
@@ -467,7 +467,7 @@ String: A string in the user\'s date format, or a relative date. Relative dates 
 			_value = date('Y-m-d\\TH:i:s\\Z', _value);
 		}
 		// Check for full timestamp
-		if(typeof _value == 'string' && _value.match(/(\d{4})-(\d{2})-(\d{2})T(\d{2})\:(\d{2})\:(\d{2})(?:\.\d{3})?(?:Z|[+-](\d{2})\:(\d{2}))/))
+		if(typeof _value == 'string' && _value.match(/^(\d{4})-(\d{2})-(\d{2})T(\d{2})\:(\d{2})\:(\d{2})(?:\.\d{3})?(?:Z|[+-](\d{2})\:(\d{2})|)$/))
 		{
 			_value = new Date(_value);
 		}
