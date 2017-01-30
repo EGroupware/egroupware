a81fe4f82a9ee80d7783d4aa1ef797e865e67291
Ralf Becker <RalfBecker@outdoor-training.de>
2017-01-30 09:59:19 +0100

    partial revert of 768b6ae, as filter, filter2 and cat get lost when changing a column filter

--- api/js/etemplate/et2_extension_nextmatch.js
+++ api/js/etemplate/et2_extension_nextmatch.js
@@ -2715,6 +2715,12 @@ var et2_nextmatch_header_bar = (function(){ "use strict"; return et2_DOMWidget.e
 
 		if (this.nextmatch.options.settings[name+"_onchange"])
 		{
+			// Make sure to get the new value for filtering
+			input.change(this.nextmatch, function(event) {
+				var set = {};
+				set[name] = select.getValue();
+				event.data.applyFilters(set);
+			});
 			// Get the onchange function string
 			var onchange = this.nextmatch.options.settings[name+"_onchange"];
 
