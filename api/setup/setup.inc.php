b5ded9edc9f287033c9b8c64dcdd86239ceacf5c
Ralf Becker <rb@stylite.de>
2017-04-15 16:31:23 +0000

    Changelog for 16.1.20170415

--- api/setup/setup.inc.php
+++ api/setup/setup.inc.php
@@ -15,7 +15,7 @@ $setup_info['api']['title']     = 'EGroupware API';
 $setup_info['api']['version']   = '16.1.005';
 $setup_info['api']['versions']['current_header'] = '1.29';
 // maintenance release in sync with changelog in doc/rpm-build/debian.changes
-$setup_info['api']['versions']['maintenance_release'] = '16.1.20170315';
+$setup_info['api']['versions']['maintenance_release'] = '16.1.20170415';
 $setup_info['api']['enable']    = 3;
 $setup_info['api']['app_order'] = 1;
 $setup_info['api']['license'] = 'GPL';
