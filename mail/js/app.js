9eeb8e1199b8b0af116008e3ac954daaac9f9bfa
Hadi Nategh <hnategh@gmail.com>
2017-06-13 15:54:52 +0200

    * Mail: Fix contextmenu does not work on select all

--- mail/js/app.js
+++ mail/js/app.js
@@ -5528,7 +5528,7 @@ app.classes.mail = AppJS.extend(
 
 	spamTitan_setActionTitle: function (_action, _sender)
 	{
-		var id = _sender[0].id;
+		var id = _sender[0].id != 'nm'? _sender[0].id:_sender[1].id;
 		var data = egw.dataGetUIDdata(id);
 		var fromaddress = data.data.fromaddress.match(/<([^\'\" <>]+)>$/);
 		var email = _sender.length>1? this.egw.lang('emails'):(fromaddress && fromaddress[1]) ?fromaddress[1]:data.data.fromaddress;
