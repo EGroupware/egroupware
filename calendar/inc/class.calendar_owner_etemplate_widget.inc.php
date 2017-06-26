EGroupware/egroupware

commit 5995c63034aea011e514376448c1c68dc99dd600

Calendar - fix some bugs setting calendar owner via URL when calendar app is not yet loaded

calendar/inc/class.calendar_owner_etemplate_widget.inc.php
@@ -41,7 +41,10 @@ class calendar_owner_etemplate_widget extends Etemplate\Widget\Taglist
 		$form_name = self::form_name($cname, $this->id, $expand);
 
 		$value =& self::get_array(self::$request->content, $form_name);
-		if(!is_array($value)) $value = array();
+		if(!is_array($value))
+		{
+			$value = explode(',', $value);
+		}
 		if (!is_array(self::$request->sel_options[$form_name]))
 		{
 			self::$request->sel_options[$form_name] = array();

calendar/inc/class.calendar_ui.inc.php
@@ -559,6 +559,7 @@ class calendar_ui
 		if (!is_array($cont)) $cont = array();
 		$cont['view'] = $this->view ? $this->view : 'week';
 		$cont['date'] = $this->date ? $this->date : new Api\DateTime();
+		$cont['owner'] = $this->owner ? $this->owner : $cont['owner'];
 
 		$cont['year'] = (int)Api\DateTime::to($cont['date'],'Y');
 		$cont['holidays'] = $this->bo->read_holidays($cont['year']);

calendar/inc/class.calendar_uilist.inc.php
@@ -159,6 +159,10 @@ class calendar_uilist extends calendar_ui
 		{
 			$content['nm']['search'] = $_GET['search'];
 		}
+		if($this->owner)
+		{
+			$content['nm']['col_filter']['participant'] = is_array($this->owner) ? $this->owner : explode(',',$this->owner);
+		}
 		// search via jdots ajax_exec uses $_REQUEST['json_data'] instead of regular GET parameters
 		if (isset($_REQUEST['json_data']) && ($json_data = json_decode($_REQUEST['json_data'], true)) &&
 			!empty($json_data['request']['parameters'][0]))

calendar/inc/class.calendar_uiviews.inc.php
@@ -144,6 +144,7 @@ class calendar_uiviews extends calendar_ui
 			'start'   => $this->date,
 			'cat_id'  => $this->cat_id ? (is_array($this->cat_id)?$this->cat_id:explode(',',$this->cat_id)) : 0,
 			'users'   => explode(',',$this->owner),
+			'owner'   => explode(',',$this->owner),
 			'filter'  => $this->filter,
 			'daywise' => True,
 			'use_so_events' => $this->test === 'true',

commit 4a9ce8e82f97e38bf9596e444ba7b7cdbd12b599

Fix calendar participants validation error, caused by commit 5995c63034aea011e514376448c1c68dc99dd600

calendar/inc/class.calendar_owner_etemplate_widget.inc.php
@@ -41,10 +41,7 @@ class calendar_owner_etemplate_widget extends Etemplate\Widget\Taglist
 		$form_name = self::form_name($cname, $this->id, $expand);
 
 		$value =& self::get_array(self::$request->content, $form_name);
-		if(!is_array($value))
-		{
-			$value = explode(',', $value);
-		}
+
 		if (!is_array(self::$request->sel_options[$form_name]))
 		{
 			self::$request->sel_options[$form_name] = array();
@@ -76,7 +73,14 @@ class calendar_owner_etemplate_widget extends Etemplate\Widget\Taglist
 			array_keys($accounts), $accounts
 		);
 
-
+		if(!is_array($value))
+		{
+			// set value with an empty string only if sel options are not
+			// loaded, for example: setting calendar owner via URL when
+			// calendar app is not yet loaded.
+			$value = !empty($sel_options) ? array(): explode(',', $value);
+		}
+		
 		// Add external owners that a select account widget will not find
 		foreach($value as &$owner)
 		{