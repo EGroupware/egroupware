7d8eb1621258fb52bc57307e8c09eb160b949446
nathangray <nathangray.bsc+github@gmail.com>
2017-10-05 13:23:09 -0600

    Addressbook - better handling of missing contact type when importing. If it's missing and there's only the normal type, it'll quietly use that.

--- addressbook/inc/class.addressbook_import_contacts_csv.inc.php
+++ addressbook/inc/class.addressbook_import_contacts_csv.inc.php
@@ -64,6 +64,15 @@ class addressbook_import_contacts_csv extends importexport_basic_import_csv  {
 			$this->lookups['tid'][$tid] = $data['name'];
 		}
 
+		// Try and set a default type, for use if file does not specify
+		if(!$this->lookups['tid'][Api\Contacts\Storage::DELETED_TYPE] && count($this->lookups['tid']) == 1 ||
+			$this->lookups['tid'][Api\Contacts\Storage::DELETED_TYPE] && count($this->lookups['tid']) == 2)
+		{
+			reset($this->lookups['tid']);
+			$this->default_type = key($this->lookups['tid']);
+		}
+
+
 		// set contact owner
 		$contact_owner = isset( $_definition->plugin_options['contact_owner'] ) ?
 			$_definition->plugin_options['contact_owner'] : $this->user;
@@ -140,12 +149,12 @@ class addressbook_import_contacts_csv extends importexport_basic_import_csv  {
 		if(!$record->tid || !$this->lookups['tid'][$record->tid])
 		{
 			// Avoid lots of warnings about type (2 types are contact and deleted)
-			if(!$this->type_warned || count($this->lookups['tid']) == 2 )
+			if($record->tid && !$this->type_warned[$record->tid] && !$this->lookups['tid'][$record->tid] )
 			{
 				$this->warnings[$import_csv->get_current_position()] = lang('Unknown type %1, imported as %2',$record->tid,lang($this->lookups['tid']['n']));
-				$this->type_warned = true;
+				$this->type_warned[$record->tid] = true;
 			}
-			$record->tid = 'n';
+			$record->tid = $this->default_type;
 		}
 
 		// Also handle categories in their own field
