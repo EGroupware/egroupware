03771e0067e8813c7eb77a0187d533563baecf48
Ralf Becker <RalfBecker@outdoor-training.de>
2017-11-16 19:36:08 +0100

    fix permissions in files-directory, if eg. update creates new files, also make nginx.conf a configuration file

--- doc/rpm-build/post_install.php
+++ doc/rpm-build/post_install.php
@@ -590,7 +590,7 @@ function usage($error=null)
 }
 
 /**
- * fix egw_cache perms evtl. created by root, stoping webserver from accessing it
+ * fix egw_cache and files_dir perms evtl. created by root, stoping webserver from accessing it
  */
 function fix_perms()
 {
@@ -600,6 +600,11 @@ function fix_perms()
 	{
 		system('/bin/chown -R '.$config['webserver_user'].' /tmp/egw_cache');
 		system('/bin/chmod 700 /tmp/egw_cache');
+	}
+	// in case update changes something in filesystem
+	if (file_exists($config['data_dir']) && !empty($config['webserver_user']))
+	{
+		system('/bin/chown -R '.$config['webserver_user'].' '.$config['data_dir']);
 	}
 }
 
