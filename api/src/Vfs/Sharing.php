EGroupware/egroupware

commit 89a8e6f44836516ed1233993854c999b5e5de1d7

Place check on session first to avoid errors if it's missing

api/src/Vfs/Sharing.php
@@ -169,7 +169,7 @@ class Sharing
 			if (!isset($_SERVER['PHP_AUTH_PW'])) $_SERVER['PHP_AUTH_PW'] = '';
 
 			unset($GLOBALS['egw_info']['flags']['autocreate_session_callback']);
-			if ($GLOBALS['egw']->session->verify() && isset($GLOBALS['egw']->sharing) &&
+			if (isset($GLOBALS['egw']->sharing) && $GLOBALS['egw']->session->verify() &&
 				$GLOBALS['egw']->sharing->share['share_token'] === $token)
 			{
 				return $GLOBALS['egw']->session->sessionid;

commit dca72ebd9cba2c1a18c9a56c8234140aca9f8ff5

Fix broken sharing caused by commit 89a8e6f44836516ed1233993854c999b5e5de1d7

api/src/Vfs/Sharing.php
@@ -169,8 +169,8 @@ class Sharing
 			if (!isset($_SERVER['PHP_AUTH_PW'])) $_SERVER['PHP_AUTH_PW'] = '';
 
 			unset($GLOBALS['egw_info']['flags']['autocreate_session_callback']);
-			if (isset($GLOBALS['egw']->sharing) && $GLOBALS['egw']->session->verify() &&
-				$GLOBALS['egw']->sharing->share['share_token'] === $token)
+			if (isset($GLOBALS['egw']->session) && $GLOBALS['egw']->session->verify()
+				&& isset($GLOBALS['egw']->sharing) && $GLOBALS['egw']->sharing->share['share_token'] === $token)
 			{
 				return $GLOBALS['egw']->session->sessionid;
 			}