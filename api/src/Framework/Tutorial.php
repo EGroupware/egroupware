9ce0feaf9b1c8145ebdecc4dd859495c530930e7
Ralf Becker <RalfBecker@outdoor-training.de>
2018-03-12 11:10:02 +0100

    * Api: use proxy configuration when downloading available tutorial videos

--- api/src/Framework/Tutorial.php
+++ api/src/Framework/Tutorial.php
@@ -120,7 +120,8 @@ class Tutorial
 	{
 		if (!($json = Api\Cache::getCache(Api\Cache::TREE, __CLASS__, 'egw_tutorial_json')))
 		{
-			$json = file_get_contents('https://www.egroupware.de/videos/tutorials.json');
+			$json = file_get_contents('https://www.egroupware.de/videos/tutorials.json',
+				false, Api\Framework::proxy_context());
 			// Fallback tutorials.json
 			if (!$json) $json = file_get_contents('api/setup/tutorials.json');
 			// Cache the json object for two hours
