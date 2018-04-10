4323a40aad70345a4dff89e26e45d22f603fbdf8
Ralf Becker <RalfBecker@outdoor-training.de>
2018-04-10 14:02:38 +0200

    use "none" instead of "0" to allow default or forced pref

--- calendar/setup/tables_update.inc.php
+++ calendar/setup/tables_update.inc.php
@@ -2759,7 +2759,7 @@ function calendar_upgrade17_1()
 {
 	// Update birthdays as events preference from boolean
 	$change = function($attr, $old_value, $owner) {
-		return $old_value ? 'birthday' : '0';
+		return $old_value ? 'birthday' : 'none';
 	};
 	Api\Preferences::change_preference('calendar', 'birthdays_as_events', $change);
 	return $GLOBALS['setup_info']['calendar']['currentver'] = '17.1.001';
