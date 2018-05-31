7843da6e088e3f09e55dc02dc24ac1876398de74
nathangray <nathangray.bsc+github@gmail.com>
2018-05-31 09:19:39 -0600

    Change allowOnMultiple check to use count of selected objects, not count of actionLinks Allows enabled check functions to call other actions's enabled functions.

--- api/js/egw_action/egw_action.js
+++ api/js/egw_action/egw_action.js
@@ -2078,8 +2078,8 @@ egwActionObject.prototype._getLinks = function(_objs, _actionType)
 			(actionLinks[k].cnt >= testedSelected.length) &&
 			(
 				(actionLinks[k].actionObj.allowOnMultiple === true) ||
-				(actionLinks[k].actionObj.allowOnMultiple == "only" && actionLinks[k].cnt > 1) ||
-				(actionLinks[k].actionObj.allowOnMultiple == false && actionLinks[k].cnt == 1)
+				(actionLinks[k].actionObj.allowOnMultiple == "only" && _objs.length > 1) ||
+				(actionLinks[k].actionObj.allowOnMultiple == false && _objs.length === 1)
 			);
 		if (!egwIsMobile()) actionLinks[k].actionObj.hideOnMobile = false;
 		actionLinks[k].visible = actionLinks[k].visible && !actionLinks[k].actionObj.hideOnMobile &&
