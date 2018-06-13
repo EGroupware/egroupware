ad669ad3345980c3d28f286e4b50904ca53bb80b
nathangray <nathangray.bsc+github@gmail.com>
2018-06-13 14:44:12 -0600

    Keep current value if provided

--- api/js/etemplate/et2_widget_vfs.js
+++ api/js/etemplate/et2_widget_vfs.js
@@ -1190,6 +1190,10 @@ var et2_vfsSelect = (function(){ "use strict"; return et2_inputWidget.extend(
 		}
 	},
 
+	set_value: function(value) {
+		this.value = value;
+	},
+	
 	getValue: function() {
 		return this.value;
 	}
