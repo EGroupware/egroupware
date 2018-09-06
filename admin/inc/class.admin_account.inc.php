75e1a8bb3fada8d0def3bb192c1bd130e5bbdb7b
nathangray <nathangray.bsc+github@gmail.com>
2018-09-06 10:42:33 -0600

    Cast extra to array to avoid error if documentation tab is not there

--- admin/inc/class.admin_account.inc.php
+++ admin/inc/class.admin_account.inc.php
@@ -261,7 +261,7 @@ class admin_account
 		}
 		if ($content['delete'])
 		{
-			$cmd = new admin_cmd_delete_account($content['account_id'], $content['new_owner'], $content['account_id'] > 0, $content['admin_cmd']);
+			$cmd = new admin_cmd_delete_account($content['account_id'], $content['new_owner'], $content['account_id'] > 0, (array)$content['admin_cmd']);
 			$msg = $cmd->run();
 			if ($content['contact_id'])
 			{
