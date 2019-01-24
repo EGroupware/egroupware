EGroupware/egroupware

commit 9742956c67b6858e45906fb01c17e1342539e397

* Calendar - fix missing week numbers on multiweek view and wrong scrolling

calendar/js/et2_widget_timegrid.js
@@ -1603,14 +1603,14 @@ var et2_calendar_timegrid = (function(){ "use strict"; return et2_calendar_view.
 		this.div.removeClass('calendar_TimeGridNoLabel');
 
 		// Check to see if it's our own calendar, with just us showing
-		if(typeof _owner == 'object' && _owner.length == 1 && _owner[0] == this.egw().user('account_id'))
+		if(typeof _owner == 'object' && _owner.length == 1)
 		{
 			var rowCount = 0;
 			this._parent.iterateOver(function(widget) {
 				if(!widget.disabled) rowCount++;
 			},this, et2_calendar_timegrid);
 			// Just us, show week number
-			if(rowCount == 1) _owner = false;
+			if(rowCount == 1 || _owner.length == 1) _owner = false;
 		}
 
 		if(typeof _owner == 'string' && isNaN(_owner))

commit 72c21bffcfbd95efab4245240d18c72f37a42155

Calendar - fix week view display name of other user, broken by 9742956c

calendar/js/app.js
@@ -2488,18 +2488,23 @@ app.classes.calendar = (function(){ "use strict"; return AppJS.extend(
 				row_index = 0;
 
 				// Set rows that need it
+				var was_disabled = [];
 				grid.iterateOver(function(widget) {
-					var was_disabled = false;
+					was_disabled[row_index] = false;
 					if(row_index < value.length)
 					{
-						was_disabled = widget.options.disabled;
+						was_disabled[row_index] = widget.options.disabled;
 						widget.set_disabled(false);
 					}
 					else
 					{
 						widget.set_disabled(true);
-						return;
 					}
+					row_index++;
+				},this, et2_calendar_view);
+				row_index = 0;
+				grid.iterateOver(function(widget) {
+					if(row_index >= value.length) return;
 					if(widget.set_show_weekend)
 					{
 						widget.set_show_weekend(view.show_weekend(state.state));
@@ -2521,13 +2526,14 @@ app.classes.calendar = (function(){ "use strict"; return AppJS.extend(
 						window.setTimeout(jQuery.proxy(widget.set_header_classes, widget),0);
 
 						// If disabled while the daycols were loaded, they won't load their events
-						for(var day = 0; was_disabled && day < widget.day_widgets.length; day++)
+						for(var day = 0; was_disabled[row_index] && day < widget.day_widgets.length; day++)
 						{
 							egw.dataStoreUID(
 									widget.day_widgets[day].registeredUID,
 								egw.dataGetUIDdata(widget.day_widgets[day].registeredUID).data
 							);
 						}
+						widget.set_owner(value[row_index].owner);
 
 						// Hide loader
 						widget.loader.hide();

calendar/js/et2_widget_timegrid.js
@@ -1610,7 +1610,7 @@ var et2_calendar_timegrid = (function(){ "use strict"; return et2_calendar_view.
 				if(!widget.disabled) rowCount++;
 			},this, et2_calendar_timegrid);
 			// Just us, show week number
-			if(rowCount == 1 || _owner.length == 1) _owner = false;
+			if(rowCount == 1 && _owner.length == 1 && _owner[0] == egw.user('account_id') || rowCount != 1) _owner = false;
 		}
 
 		if(typeof _owner == 'string' && isNaN(_owner))