eb2cee5678d526bfe2ce8203b2a4306fd11c3f85
Hadi Nategh <hnategh@gmail.com>
2019-03-05 12:05:28 +0100

    Check for pixelegg theme and switch to SVG

--- api/src/Framework.php
+++ api/src/Framework.php
@@ -987,7 +987,7 @@ abstract class Framework extends Framework\Extra
 			self::includeJS('/api/images.php', array(
 				'template' => $GLOBALS['egw_info']['server']['template_set'],
 				'etag' => md5(json_encode(Image::map($GLOBALS['egw_info']['server']['template_set']))),
-				'svg' => Header\UserAgent::mobile() || $GLOBALS['egw_info']['user']['preferences']['common']['theme'] == 'modern',
+				'svg' => Header\UserAgent::mobile() || $GLOBALS['egw_info']['user']['preferences']['common']['theme'] == 'pixelegg',
 			));
 			self::includeJS('/api/user.php', array(
 				'user' => $GLOBALS['egw_info']['user']['account_lid'],
