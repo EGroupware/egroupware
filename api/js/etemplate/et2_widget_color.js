832f10598243ce36a310160b95b453157b1a0dc2
nathangray <nathangray.bsc+github@gmail.com>
2019-03-29 09:46:23 -0600

    Remove debug code Thanks, scrutinizer

--- api/js/etemplate/et2_widget_color.js
+++ api/js/etemplate/et2_widget_color.js
@@ -130,7 +130,6 @@ var et2_color = (function(){ "use strict"; return et2_inputWidget.extend(
 		jQuery.jPicker.List[list_id].id = this.id + "_jPicker";
 
 		// jPicker doesn't allow translation of this one
-		debugger;
 		jQuery('.Image', this.$node.parent()).attr('title', this.egw().lang('Click to open color picker'));
 
 		// Make it look better - plugin defers initialization, so we have to also
