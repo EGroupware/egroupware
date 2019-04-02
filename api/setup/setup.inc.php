dd2bbccf5c33d7af52b821a888e2144b2ab56ad0
Ralf Becker <rb@stylite.de>
2019-04-02 13:56:06 +0000

    copy 17.1 changelog to master to satisfy update checker

--- api/setup/setup.inc.php
+++ api/setup/setup.inc.php
@@ -14,7 +14,7 @@ $setup_info['api']['title']     = 'EGroupware API';
 $setup_info['api']['version']   = '17.9.002';
 $setup_info['api']['versions']['current_header'] = '1.29';
 // maintenance release in sync with changelog in doc/rpm-build/debian.changes
-$setup_info['api']['versions']['maintenance_release'] = '17.1.20190222';
+$setup_info['api']['versions']['maintenance_release'] = '17.1.20190402';
 $setup_info['api']['enable']    = 3;
 $setup_info['api']['app_order'] = 1;
 $setup_info['api']['license'] = 'GPL';
