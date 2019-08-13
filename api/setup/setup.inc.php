9af802cc9d9dcfca3044dd0613f8301a9e5dee16
Ralf Becker <RalfBecker@outdoor-training.de>
2019-08-13 09:22:44 +0200

    update master with 19.1 final release (19.1.20190813) changelog

--- api/setup/setup.inc.php
+++ api/setup/setup.inc.php
@@ -14,7 +14,7 @@ $setup_info['api']['title']     = 'EGroupware API';
 $setup_info['api']['version']   = '19.1';
 $setup_info['api']['versions']['current_header'] = '1.29';
 // maintenance release in sync with changelog in doc/rpm-build/debian.changes
-$setup_info['api']['versions']['maintenance_release'] = '19.1.20190806';
+$setup_info['api']['versions']['maintenance_release'] = '19.1.20190813';
 $setup_info['api']['enable']    = 3;
 $setup_info['api']['app_order'] = 1;
 $setup_info['api']['license'] = 'GPL';
