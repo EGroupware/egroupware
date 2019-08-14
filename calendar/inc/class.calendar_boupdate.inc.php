65437b4bd0423f6888c52320fe0f91cfbce6aa63
nathangray <nathangray.bsc+github@gmail.com>
2019-08-14 14:32:35 -0600

    Calendar - fix incorrectly moved preference read

--- calendar/inc/class.calendar_boupdate.inc.php
+++ calendar/inc/class.calendar_boupdate.inc.php
@@ -2938,8 +2938,6 @@ class calendar_boupdate extends calendar_bo
 
 		$status_reset = false;
 		$sameday = (date('Ymd', $old_event['start']) == date('Ymd', $event['start']));
-		$preferences = new Api\Preferences($uid);
-		$part_prefs = $preferences->read_repository();
 		foreach((array)$event['participants'] as $uid => $status)
 		{
 			$q = $r = null;
@@ -2952,6 +2950,8 @@ class calendar_boupdate extends calendar_bo
 			// Just user accounts
 			if (is_int($uid))
 			{
+				$preferences = new Api\Preferences($uid);
+				$part_prefs = $preferences->read_repository();
 				switch ($part_prefs['calendar']['reset_stati'])
 				{
 					case 'no':
