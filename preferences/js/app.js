d5950d51e0e2add3275f254442dbe34fc18f0e00
Ralf Becker <RalfBecker@outdoor-training.de>
2019-09-03 16:21:14 +0200

    empty app.js so apps hooking into Password & Security have one to "extend"

---
+++ preferences/js/app.js
@@ -0,0 +1,60 @@
+/**
+ * EGroupware Preferences
+ *
+ * @link https://www.egroupware.org
+ * @author Ralf Becker <rb-At-egroupware.org>
+ * @package preferences
+ */
+
+/**
+ * JavaScript for WebAuthn
+ *
+ * @augments AppJS
+ */
+app.classes.preferences = AppJS.extend(
+{
+	appname: 'preferences',
+
+	/**
+	 * et2 widget container
+	 */
+	et2: null,
+	/**
+	 * path widget
+	 */
+
+	/**
+	 * Constructor
+	 *
+	 * @memberOf app.timesheet
+	 */
+	init: function()
+	{
+		// call parent
+		this._super.apply(this, arguments);
+	},
+
+	/**
+	 * Destructor
+	 */
+	destroy: function()
+	{
+		delete this.et2;
+		// call parent
+		this._super.apply(this, arguments);
+	},
+
+	/**
+	 * This function is called when the etemplate2 object is loaded
+	 * and ready.  If you must store a reference to the et2 object,
+	 * make sure to clean it up in destroy().
+	 *
+	 * @param et2 etemplate2 Newly ready object
+	 */
+	et2_ready: function(et2)
+	{
+		// call parent
+		this._super.apply(this, arguments);
+
+	}
+});
