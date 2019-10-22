1d2062ffc46f8386ff8324ca1b59c58319e24e28
Ralf Becker <RalfBecker@outdoor-training.de>
2019-10-22 15:29:23 +0200

    permanent redirect requests to filemanager/webdav.php to webdav.php

--- filemanager/webdav.php
+++ filemanager/webdav.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * eGroupWare - WebDAV access
+ * EGroupware - old WebDAV access
  *
  * Just for backward compatibility of the url: better use the webdav.php in the root.
  *
@@ -8,8 +8,10 @@
  * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
  * @package filemanager
  * @author Ralf Becker <RalfBecker-AT-outdoor-training.de>
- * @copyright (c) 2008 by Ralf Becker <RalfBecker-AT-outdoor-training.de>
- * @version $Id$
+ * @copyright (c) 2008-19 by Ralf Becker <RalfBecker-AT-outdoor-training.de>
  */
 
-include('../webdav.php');
+// we permanent redirect to the correct URL instead of processing the request under the old url
+// (processing under misses certain special handling like not access_logging the request)
+http_response_code(301);
+header('Location: '.str_replace('/filemanager/webdav.php/', '/webdav.php/', $_SERVER['REQUEST_URI']));
