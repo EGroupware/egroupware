d8faef3503af79b8c7a1d94a94abf3188b88263a
nathangray <nathangray.bsc+github@gmail.com>
2019-11-07 13:46:14 -0700

    Admin: Extend Vfs::deleteAccount hook to all the files it can find, not just home dir.

--- admin/inc/class.admin_account.inc.php
+++ admin/inc/class.admin_account.inc.php
@@ -302,7 +302,10 @@ class admin_account
 			{
 				$entry = lang('Entries');
 			}
-			if($counts['total'] && Api\Hooks::exists('deleteaccount', $app))
+			if($counts['total'] && (
+					// Filemanager is a special case, since the hook is in API
+					Api\Hooks::exists('deleteaccount', $app) || in_array($app, array('filemanager'))
+			))
 			{
 				$content['delete_apps'][] = $app;
 				$sel_options['delete_apps'][] = array(
@@ -319,16 +322,7 @@ class admin_account
 				);
 			}
 		}
-		// Add filemanager home directory in as special case, hook is in the API
-		if(Api\Vfs::file_exists('/home/'.$GLOBALS['egw']->accounts->id2name($content['account_id'])))
-		{
-			$app = 'filemanager';
-			$sel_options['delete_apps'][] = array(
-				'value' => $app,
-				'label' => lang($app) . ': ' . lang('home directory')
-			);
-			$content['delete_apps'][] = $app;
-		}
+
 		$tpl = new Etemplate('admin.account.delete');
 		$tpl->exec('admin_account::delete', $content, $sel_options, array(), $preserve, 2);
 	}
