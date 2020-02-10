c7e734658470aa1474e747ae5e4a747dfb3f16b3
Hadi Nategh <hnategh@gmail.com>
2020-02-10 14:36:09 +0100

    Convert et2_widget_hrule to TS

---
+++ api/js/etemplate/et2_widget_hrule.ts
@@ -0,0 +1,41 @@
+/**
+ * EGroupware eTemplate2 - JS HRule object
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package etemplate
+ * @subpackage api
+ * @link http://www.egroupware.org
+ * @author Andreas Stöckel
+ */
+
+/*egw:uses
+	et2_core_baseWidget;
+*/
+
+import {et2_register_widget, WidgetConfig} from "./et2_core_widget";
+import {et2_baseWidget} from "./et2_core_baseWidget";
+import {ClassWithAttributes} from "./et2_core_inheritance";
+
+/**
+ * Class which implements the hrule tag
+ *
+ * @augments et2_baseWidget
+ */
+class et2_hrule extends et2_baseWidget
+{
+	/**
+	 * Constructor
+	 *
+	 * @memberOf et2_hrule
+	 */
+	constructor(_parent, _attrs? : WidgetConfig, _child? : object)
+	{
+		// Call the inherited constructor
+		super(_parent, _attrs, ClassWithAttributes.extendAttributes(et2_hrule._attributes, _child || {}));
+
+		this.setDOMNode(document.createElement("hr"));
+	}
+}
+et2_register_widget(et2_hrule, ["hrule"]);
+
+
