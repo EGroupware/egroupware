5d11d6f34b5731cc00bcd414bd01c42167eeed7b
Hadi Nategh <hnategh@gmail.com>
2020-02-13 15:45:23 +0100

    Convert et2_extension_itempicker_actions to TS

--- api/js/etemplate/et2_extension_itempicker_actions.js
+++ api/js/etemplate/et2_extension_itempicker_actions.js
@@ -10,19 +10,16 @@
  * @author Nathan Gray
  * @copyright 2012 Christian Binder
  * @copyright 2011 Nathan Gray
- * @version $Id: et2_widget_itempicker.js 38623 2012-03-26 23:27:53Z jaytraxx $
  */
-
-function itempickerDocumentAction(context, data)
-{
-	"use strict";
-
-	var formid = "itempicker_action_form";
-	var form = "<form id='" + formid + "' action='index.php?menuaction=" + data.app + "." + data.app + "_merge.download_by_request' method='POST'>"
-		+ "<input type='hidden' name='data_document_name' value='" + data.value.name + "' />"
-		+ "<input type='hidden' name='data_document_dir' value='" + data.value.dir + "' />"
-		+ "<input type='hidden' name='data_checked' value='" + data.checked.join(',') + "' />"
-		+ "</form>";
-	jQuery("body").append(form);
-	jQuery("#" + formid).submit().remove();
+function itempickerDocumentAction(context, data) {
+    "use strict";
+    var formid = "itempicker_action_form";
+    var form = "<form id='" + formid + "' action='index.php?menuaction=" + data.app + "." + data.app + "_merge.download_by_request' method='POST'>"
+        + "<input type='hidden' name='data_document_name' value='" + data.value.name + "' />"
+        + "<input type='hidden' name='data_document_dir' value='" + data.value.dir + "' />"
+        + "<input type='hidden' name='data_checked' value='" + data.checked.join(',') + "' />"
+        + "</form>";
+    jQuery("body").append(form);
+    jQuery("#" + formid).submit().remove();
 }
+//# sourceMappingURL=et2_extension_itempicker_actions.js.map
