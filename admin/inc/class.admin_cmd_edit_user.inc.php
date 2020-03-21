f0f4fd2ee3db957d49f244da5b58f9dcd1bb301c
Ralf Becker <RalfBecker@outdoor-training.de>
2020-03-21 14:38:32 +0100

    fix to allow setting "anonymous" flag for other accounts and try to fix Chrome autocomplete

--- admin/inc/class.admin_cmd_edit_user.inc.php
+++ admin/inc/class.admin_cmd_edit_user.inc.php
@@ -104,9 +104,9 @@ class admin_cmd_edit_user extends admin_cmd_change_pw
 		}
 
 		$data['changepassword'] = admin_cmd::parse_boolean($data['changepassword'],$this->account ? null : true);
-		$data['anonymous'] = admin_cmd::parse_boolean($data['anonymous'],$this->account ? null : false) ||
-			// automatic set anonymous flag for username "anonymous", to not allow to create anonymous user without it
-			($data['account_lid'] ?: admin_cmd::$accounts->id2name($this->account)) === 'anonymous';
+		// automatic set anonymous flag for username "anonymous", to not allow to create anonymous user without it
+		$data['anonymous'] = ($data['account_lid'] ?: admin_cmd::$accounts->id2name($this->account)) === 'anonymous' ?
+			true : admin_cmd::parse_boolean($data['anonymous'],$this->account ? null : false);
 
 		if ($data['mustchangepassword'] && $data['changepassword'])
 		{
