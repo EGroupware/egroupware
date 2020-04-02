1f118a41382d62b98d057cd04c22c6bbf49a3ba2
Jan Grünewald <gruenewald+github@cominto.de>
2020-04-02 07:41:22 +0200

    added autoloading to filemanager/cli.php

--- filemanager/cli.php
+++ filemanager/cli.php
@@ -16,6 +16,8 @@
 use EGroupware\Api;
 use EGroupware\Api\Vfs;
 
+require_once(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'autoload.php');
+
 chdir(dirname(__FILE__));	// to enable our relative pathes to work
 
 error_reporting(error_reporting() & ~E_NOTICE & ~E_DEPRECATED);
