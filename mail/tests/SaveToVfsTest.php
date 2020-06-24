4d67f98eeb63f81592f65f6096930c1301dba94d
nathangray <nathangray.bsc+github@gmail.com>
2020-06-24 13:43:18 -0600

    Fix incorrect class

--- mail/tests/SaveToVfsTest.php
+++ mail/tests/SaveToVfsTest.php
@@ -100,7 +100,7 @@ class SaveToVfsTest extends \PHPUnit\Framework\TestCase
 }
 
 
-class VfsTestMail extends Api\Mail
+class VfsTestMail extends \EGroupware\Api\Mail
 {
 	// Expose for testing
 	public static function clean_subject_for_filename($filename)
