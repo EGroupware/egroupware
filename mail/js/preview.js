e4c0661804f3ca658939a0715dac3a9c01fbcd7c
Ralf Becker <RalfBecker@outdoor-training.de>
2020-07-08 14:28:21 +0200

    * Mail: open notification email of own domain as popup

--- mail/js/preview.js
+++ mail/js/preview.js
@@ -1,12 +1,11 @@
 /**
- * mail - handle mailto and other links in preview
+ * EGroupware Mail - handle mailto and other links in preview
  *
  * @link http://www.egroupware.org
  * @author EGroupware GmbH [info@egroupware.org]
  * @copyright (c) 2014 by EGroupware GmbH <info-AT-egroupware.org>
  * @package mail
  * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
- * @version $Id$
  */
 
 /*egw:uses
@@ -23,6 +22,13 @@ jQuery(function()
 			egw(window).open(null, 'mail', 'add', {send_to: jQuery.base64Encode(this.href.substr(7).replace('%40', '@'))});
 
 			return false;	// cant do event.stopImediatePropagation() in on!
+		}
+		// open links with own orgin and "index.php?" as popup (not eg. share.php or *dav.php)
+		else if ((this.href[0] === '/' || this.href.match(new RegExp('^'+location.protocol+'//'+location.host+'/'))) &&
+			this.href.match(/\/index.php\?/))
+		{
+			top.egw.openPopup(this.href.replace(/([?&])no_popup=[^&]*/, '\1'), 800, 600, '_blank');
+			return false;
 		}
 		else	// add target=_blank to all other links, gives CSP error and would open in preview
 		{
