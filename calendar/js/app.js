3da73f95fba8113ebe0a1faebe3c220b216c1a3a
nathangray <nathangray.bsc+github@gmail.com>
2020-08-04 13:56:54 -0600

    * Calendar: Fix multiple all-day event rendering bug
    If you had multiple all day, non-blocking events and switched from month to week view, if your window was small enough, some of them would be hidden.

--- calendar/js/app.js
+++ calendar/js/app.js
@@ -2396,6 +2396,8 @@ var CalendarApp = /** @class */ (function (_super) {
                 grid.iterateOver(function (widget) {
                     if (row_index >= value.length)
                         return;
+                    // Clear height to make sure there's correct calculations
+                    widget.div.css("height", "");
                     if (widget.set_show_weekend) {
                         widget.set_show_weekend(view.show_weekend(state.state));
                     }
