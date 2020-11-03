f064ff824230096a6cbc7c84bd2d87d3de677dbf
Hadi Nategh <hnategh@gmail.com>
2020-11-03 11:47:26 +0100

    Make sure the params exists before addressing it

--- api/js/jsapi/egw_open.js
+++ api/js/jsapi/egw_open.js
@@ -224,7 +224,7 @@ egw.extend('open', egw.MODULE_WND_LOCAL, function(_egw, _wnd)
 				url = this.link(url, params);
 			}
 			if (target == '_tab') return {url: url};
-			if (type == 'view'  && params.target == 'tab') {
+			if (type == 'view'  && params && params.target == 'tab') {
 				return this.openTab(params[app_registry['view_id']], app, type, params, {
 					id: params[app_registry['view_id']] + '-' + this.appName,
 					icon: params['icon'],
