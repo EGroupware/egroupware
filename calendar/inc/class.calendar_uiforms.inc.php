dc4a2465ff1bc565fd51497ba3d575b079048db8
Hadi Nategh <hnategh@gmail.com>
2020-11-19 15:39:13 +0100

    Disable videoconference field in calendar edit dialog if it's not active

--- calendar/inc/class.calendar_uiforms.inc.php
+++ calendar/inc/class.calendar_uiforms.inc.php
@@ -2024,10 +2024,7 @@ class calendar_uiforms extends calendar_ui
 		if (!empty($preserved['lock_token'])) $content['lock_token'] = $preserved['lock_token'];
 
 		//Disable videoconference if the module is not enabled
-		if ($GLOBALS['egw_info']['user']['apps']['status'] && EGroupware\Status\Hooks::isVideoconferenceDisabled())
-		{
-			$readonlys['videoconference'] = true;
-		}
+		$etpl->disableElement('videoconference', $GLOBALS['egw_info']['user']['apps']['status'] && EGroupware\Status\Hooks::isVideoconferenceDisabled());
 
 		// non_interactive==true from $_GET calls immediate save action without displaying the edit form
 		if(isset($_GET['non_interactive']) && (bool)$_GET['non_interactive'] === true)
