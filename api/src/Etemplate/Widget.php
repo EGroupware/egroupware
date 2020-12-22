0a95588642ff828847165511b77196248dc1c3d7
nathangray <nathangray.bsc+github@gmail.com>
2020-12-22 11:54:31 -0700

    Preference needs this to not get changed here, otherwise validation doesn't find what it needs

--- api/src/Etemplate/Widget.php
+++ api/src/Etemplate/Widget.php
@@ -209,7 +209,7 @@ class Widget
 				// expand attributes values, otherwise eg. validation can not use attrs referencing to content
 				if ($value[0] == '@' || strpos($value, '$cont') !== false)
 				{
-					$template->attrs[$reader->name] = $value = self::expand_name($value, null, null, null, null,
+					$value = self::expand_name($value, null, null, null, null,
 						isset(self::$cont) ? self::$cont : self::$request->content);
 				}
 
