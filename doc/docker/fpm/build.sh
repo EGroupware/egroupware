d422c5ad62e6543a03015950bef455d42e3fcc9d
Ralf Becker <RalfBecker@outdoor-training.de>
2021-03-24 08:51:39 +0100

    docker files for 7.4 builds

--- doc/docker/fpm/build.sh
+++ doc/docker/fpm/build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -x
 
+cd $(dirname $0)
+
+DEFAULT_PHP_VERSION=7.4
+PHP_VERSION=$DEFAULT_PHP_VERSION
+
+if [[ $1 =~ ^[78]\.[0-9]$ ]]
+then
+  PHP_VERSION=$1
+  shift
+fi
+
 DEFAULT=$(git branch|grep ^*|cut -c3-)
 TAG=${1:-$DEFAULT}
 VERSION=$TAG
@@ -15,18 +26,21 @@ BRANCH=$(echo $VERSION|sed 's/\.[0-9]\{8\}$//')
 	}
 }
 
-cd $(dirname $0)
+# add PHP_VERSION to TAG, if not the default PHP version
+[ $PHP_VERSION != $DEFAULT_PHP_VERSION ] && TAG=$TAG-$PHP_VERSION
 
 docker pull ubuntu:20.04
-docker build --no-cache --build-arg "VERSION=$VERSION" -t egroupware/egroupware:$TAG . && {
+docker build --no-cache --build-arg "VERSION=$VERSION" --build-arg "PHP_VERSION=$PHP_VERSION" -t egroupware/egroupware:$TAG . && {
 	docker push egroupware/egroupware:$TAG
+	# further tags are only for the default PHP version
+	[ $PHP_VERSION != $DEFAULT_PHP_VERSION ] && exit
 	# tag only stable releases as latest
-	#[ $TAG != "master" ] && {
-	#	docker tag egroupware/egroupware:$TAG egroupware/egroupware:latest
-	#	docker push egroupware/egroupware:latest
-	#}
+	[ $TAG != "master" ] && {
+	  docker tag egroupware/egroupware:$TAG egroupware/egroupware:latest
+		docker push egroupware/egroupware:latest
+	}
 	[ "$BRANCH" != $VERSION -a "${BRANCH}.x-dev" != $VERSION ] && {
-		docker tag egroupware/egroupware:$VERSION egroupware/egroupware:$BRANCH
+		docker tag egroupware/egroupware:$TAG egroupware/egroupware:$BRANCH
 		docker push egroupware/egroupware:$BRANCH
 	}
 }
