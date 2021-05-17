EGroupware/egroupware

commit 45e477f0c01f825fea5d44281051f62292f443df

* Api: Sidebox accessibility improvements
- Using nav elements for sidebox categories
- Add keyboard support for expand/collapse menu categories
- Use lists for entries
- Added some roles for screenreaders

api/js/framework/fw_ui.js
@@ -79,13 +79,15 @@ var fw_ui_sidemenu_entry = (function(){ "use strict"; return Class.extend(
 
 		//close button on active header
 		this.closeButton = document.createElement('span');
-		this.closeButton.classList.add('close')
+		this.closeButton.classList.add('close');
 
 
 		//Create the content div
 		this.contentDiv = document.createElement("div");
 		this.contentDiv.id = _app+'_sidebox_content';
-		jQuery(this.contentDiv).addClass("egw_fw_ui_sidemenu_entry_content");
+		jQuery(this.contentDiv).addClass("egw_fw_ui_sidemenu_entry_content")
+			.attr("role","menu")
+			.attr("aria-label",this.entryName);
 		jQuery(this.contentDiv).hide();
 
 		//Add in invisible marker to store the original position of this element in the DOM tree
@@ -408,12 +410,13 @@ function egw_fw_ui_tab(_parent, _contHeaderDiv, _contDiv, _icon, _callback,
 			}
 			return true;
 
-		}.bind(this.closeButton))
+		}.bind(this.closeButton));
 	}
 
 	this.contentDiv = document.createElement("div");
-	jQuery(this.contentDiv).addClass("egw_fw_ui_tab_content");
-	jQuery(this.contentDiv).hide();
+	jQuery(this.contentDiv).addClass("egw_fw_ui_tab_content")
+		.attr("role","application")
+		.hide();
 
 	//Sort the element in at the given position
 	var _this = this;
@@ -846,17 +849,26 @@ function egw_fw_ui_category(_contDiv, _name, _title, _content, _callback, _anima
 	this.animationCallback = _animationCallback;
 	this.tag = _tag;
 
+	// Unique ID for accessibility
+	let uid = "sidebox_nav_"+egw.uid();
+
 	//Create the ui divs
-	this.headerDiv = document.createElement('div');
-	jQuery(this.headerDiv).addClass('egw_fw_ui_category');
+	this.headerDiv = document.createElement('nav');
+	jQuery(this.headerDiv).addClass('egw_fw_ui_category')
+		.attr("aria-haspopup",true)
+		.attr("aria-labelledby",uid)
+		.attr("role","section")
+		.attr("tabindex",0);
 
 	//Add the text
-	var entryH1 = document.createElement('h1');
-	jQuery(entryH1).append(_title);
-	jQuery(this.headerDiv).append(entryH1);
+	var entryH2 = document.createElement('h2');
+	jQuery(entryH2)
+		.attr("id",uid)
+		.append(_title);
+	jQuery(this.headerDiv).append(entryH2);
 
 	//Add the content
-	this.contentDiv = document.createElement('div');
+	this.contentDiv = document.createElement('ul');
 	this.contentDiv._parent = this;
 	jQuery(this.contentDiv).addClass('egw_fw_ui_category_content');
 	jQuery(this.contentDiv).append(_content);
@@ -864,8 +876,9 @@ function egw_fw_ui_category(_contDiv, _name, _title, _content, _callback, _anima
 
 	//Add content and header to the content div, add some magic jQuery code in order to make it foldable
 	this.headerDiv._parent = this;
-	jQuery(this.headerDiv).click(
-		function() {
+	jQuery(this.headerDiv).on("click keydown",
+		function(e) {
+			if([EGW_KEY_ENTER, EGW_KEY_SPACE].indexOf(e.which) == -1) return;
 			if (!jQuery(this).hasClass('egw_fw_ui_category_active'))
 			{
 				this._parent.open(false);
@@ -874,15 +887,17 @@ function egw_fw_ui_category(_contDiv, _name, _title, _content, _callback, _anima
 			{
 				this._parent.close(false);
 			}
+			e.stopPropagation();
 		});
 	jQuery(this.contDiv).append(this.headerDiv);
-	jQuery(this.contDiv).append(this.contentDiv);
+	jQuery(this.headerDiv).append(this.contentDiv);
 }
 
 egw_fw_ui_category.prototype.open = function(_instantly)
 {
 	this.callback.call(this, true);
-	jQuery(this.headerDiv).addClass('egw_fw_ui_category_active');
+	jQuery(this.headerDiv).addClass('egw_fw_ui_category_active')
+		.attr("aria-expanded",true);
 
 	if (_instantly)
 	{
@@ -895,12 +910,14 @@ egw_fw_ui_category.prototype.open = function(_instantly)
 			this._parent.animationCallback.call(this._parent);
 		});
 	}
+	jQuery("li:first-child", this.headerDiv).eq(0).focus();
 };
 
 egw_fw_ui_category.prototype.close = function(_instantly)
 {
 	this.callback.call(this, false);
-	jQuery(this.headerDiv).removeClass('egw_fw_ui_category_active');
+	jQuery(this.headerDiv).removeClass('egw_fw_ui_category_active')
+		.attr("aria-expanded", false);
 
 	if (_instantly)
 	{

api/js/framework/fw_base.js
@@ -303,7 +303,7 @@ var fw_base = (function(){ "use strict"; return Class.extend(
 						if (_data[i].entries[j].icon_or_star)
 						{
 							var disableIfNoEPL = _data[i].entries[j].disableIfNoEPL ? ' disableIfNoEPL" title="'+egw.lang("This feature is only available in EPL version.") : "";
-							catContent += '<div class="egw_fw_ui_sidemenu_listitem'+disableIfNoEPL+
+							catContent += '<li class="egw_fw_ui_sidemenu_listitem'+disableIfNoEPL+
 									'"><img class="egw_fw_ui_sidemenu_listitem_icon" src="' + _data[i].entries[j].icon_or_star + '" />';
 						}
 						if (_data[i].entries[j].item_link == '')

api/src/Framework/Favorites.php
@@ -83,7 +83,7 @@ class Favorites
 				continue;
 			}
 			$li = "<li data-id='$name' data-group='{$filter['group']}' class='ui-menu-item' role='menuitem'>\n";
-			$li .= '<a href="#" class="ui-corner-all" tabindex="-1">';
+			$li .= '<a href="#" class="ui-corner-all">';
 			$li .= "<div class='" . ((string)$name === (string)$default_filter ? 'ui-icon ui-icon-heart' : 'sideboxstar') . "'></div>".
 				$filter['name'];
 			$li .= ($filter['group'] != false && !$is_admin || $name === 'blank' ? "" :

api/templates/default/etemplate2.css
@@ -3264,7 +3264,7 @@ tr.disableIfNoEPL {
 .egw_fw_ui_sidemenu_entry_content hr {
     /* HR in sidebox */
     border-top: 1px solid silver;
-    margin: 3px 5px 2px 5px;
+    margin: 10px 5px 2px 5px;
 }
 .inactive_blur > * {opacity: 0.4; filter:blur(2px); pointer-events: none;}
 

pixelegg/css/mobile.less
@@ -474,7 +474,7 @@
 
 						&:active {.box_shadow_standard_light_inset;}
 
-						h1 {
+						h1,h2 {
 							margin: 6px 0 10px 0;
 							padding-top: 0.31em;
 							padding-left: 3em;
@@ -514,7 +514,7 @@
 
 					img {padding-left: 9px; padding-top: 6px; height: 18px;}
 
-					h1 {
+					h1,h2 {
 						text-transform: uppercase;
 						font-size: 12px;
 						.color_100_gray;
@@ -555,7 +555,7 @@
 				.egw_fw_ui_sidemenu_entry_content {
 
 
-					div.egw_fw_ui_category_content {
+					ul.egw_fw_ui_category_content {
 
 					}
 				}

commit a615b2dd9cac79843d3165c7445f46f9d5397e57

Api: Fix sidebox category expand/collapse on click, broken in 45e477f0c01f825fea5d44281051f62292f443df

api/js/framework/fw_ui.js
@@ -878,7 +878,7 @@ function egw_fw_ui_category(_contDiv, _name, _title, _content, _callback, _anima
 	this.headerDiv._parent = this;
 	jQuery(this.headerDiv).on("click keydown",
 		function(e) {
-			if([EGW_KEY_ENTER, EGW_KEY_SPACE].indexOf(e.which) == -1) return;
+			if(e.type == "keydown" && [EGW_KEY_ENTER, EGW_KEY_SPACE].indexOf(e.which) == -1) return;
 			if (!jQuery(this).hasClass('egw_fw_ui_category_active'))
 			{
 				this._parent.open(false);