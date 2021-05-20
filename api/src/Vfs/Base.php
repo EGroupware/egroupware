2c14226ee52b0b5c1f4136c06ed56c7ff52527d1
nathangray <nathangray.bsc+github@gmail.com>
2021-05-20 11:53:29 -0600

    Api: Respect do_symlink flag to keep links as links
    Fixes troubles with syncing

--- api/src/Vfs/Base.php
+++ api/src/Vfs/Base.php
@@ -311,7 +311,7 @@ class Base
 				// Make sure we don't cache anything with a link anywhere in the url, since it fails with eg: /apps/InfoLog/Open$/2021$.
 				// is_link() is not always right here
 				$is_link = is_link($url) || (self::symlinkCache_resolve(Vfs::parse_url($url,PHP_URL_PATH)) !== Vfs::parse_url($url,PHP_URL_PATH));
-				if($is_link)
+				if($is_link && $do_symlink)
 				{
 					$old_url = $url;
 					$_url = self::symlinkCache_resolve(Vfs::parse_url($url,PHP_URL_PATH));
