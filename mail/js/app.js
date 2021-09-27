d75c4146d2c462f670771ce0c88cdbf2c6c23c9c
Hadi Nategh <hnategh@gmail.com>
2021-09-27 10:50:44 +0200

    Catch miss encoding exception while checking mailbox for commit bec53dc57d0196a9a4256983a8d8398afa5e987f

--- mail/js/app.js
+++ mail/js/app.js
@@ -2443,12 +2443,18 @@ app.classes.mail = AppJS.extend(
 		var rvMain = false;
 		if ((obj_manager && _elems.length>1 && obj_manager.getAllSelected() && !_action.paste) || _action.id=='readall')
 		{
-			// Avoid possibly doing select all action on not desired mailbox e.g. INBOX
-			for (let n=0;n<_elems.length;n++)
+			try {
+				// Avoid possibly doing select all action on not desired mailbox e.g. INBOX
+				for (let n=0;n<_elems.length;n++)
+				{
+					// drop the action if there's a mixedup mailbox found in the selected messages
+					if (atob(_elems[n].id.split("::")[3]) != tree.getSelectedNode().id.split("::")[1]) return;
+				}
+			}catch(e)
 			{
-				// drop the action if there's a mixedup mailbox found in the selected messages
-				if (atob(_elems[n].id.split("::")[3]) != tree.getSelectedNode().id.split("::")[1]) return;
+				// continue
 			}
+
 
 			if (_confirm)
 			{
