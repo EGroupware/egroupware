89507d1f2b307cae3a143ec887576841b9d4a31d
Hadi Nategh <hnategh@gmail.com>
2021-12-06 10:14:37 +0100

    Add underline button into html editor's toolbar

--- api/js/etemplate/et2_widget_htmlarea.ts
+++ api/js/etemplate/et2_widget_htmlarea.ts
@@ -112,7 +112,7 @@ export class et2_htmlarea extends et2_editableWidget implements et2_IResizeable
 	 * arranged toolbars as simple mode
 	 * @constant
 	 */
-	public static readonly TOOLBAR_SIMPLE : string = "undo redo|formatselect fontselect fontsizeselect | bold italic removeformat forecolor backcolor | "+
+	public static readonly TOOLBAR_SIMPLE : string = "undo redo|formatselect fontselect fontsizeselect | bold italic underline removeformat forecolor backcolor | "+
 	"alignleft aligncenter alignright alignjustify | bullist "+
 	"numlist outdent indent| link image pastetext | table";
 
@@ -120,7 +120,7 @@ export class et2_htmlarea extends et2_editableWidget implements et2_IResizeable
 	 * arranged toolbars as extended mode
 	 * @constant
 	 */
-	public static readonly TOOLBAR_EXTENDED : string = "fontselect fontsizeselect | bold italic strikethrough forecolor backcolor | "+
+	public static readonly TOOLBAR_EXTENDED : string = "fontselect fontsizeselect | bold italic underline strikethrough forecolor backcolor | "+
 	"link | alignleft aligncenter alignright alignjustify  | numlist "+
 	"bullist outdent indent | removeformat | image | fullscreen | table";
 
@@ -128,7 +128,7 @@ export class et2_htmlarea extends et2_editableWidget implements et2_IResizeable
 	 * arranged toolbars as advanced mode
 	 * @constant
 	 */
-	public static readonly TOOLBAR_ADVANCED : string = "undo redo| formatselect | fontselect fontsizeselect | bold italic strikethrough forecolor backcolor | "+
+	public static readonly TOOLBAR_ADVANCED : string = "undo redo| formatselect | fontselect fontsizeselect | bold italic underline strikethrough forecolor backcolor | "+
 	"alignleft aligncenter alignright alignjustify | bullist "+
 	"numlist outdent indent ltr rtl | removeformat code| link image pastetext | searchreplace | fullscreen | table";
 
