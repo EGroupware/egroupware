84b81cb1f3cdcb59fa9960d3fbe019761b66e8d4
Hadi Nategh <hnategh@gmail.com>
2021-12-10 13:42:10 +0100

    Set From identity base on identLabel preference

--- mail/inc/class.mail_compose.inc.php
+++ mail/inc/class.mail_compose.inc.php
@@ -2463,7 +2463,7 @@ class mail_compose
 		$email_From =  $_identity['ident_email'] ? $_identity['ident_email'] : $activeMailProfile['ident_email'];
 		// Try to fix identity email with no domain part set
 		$_mailObject->setFrom(Mail::fixInvalidAliasAddress(Api\Accounts::id2name($_identity['account_id'], 'account_email'), $email_From),
-			Mail::generateIdentityString($_identity,false));
+			mail_tree::getIdentityName($_identity));
 
 		$_mailObject->addHeader('X-Priority', $_formData['priority']);
 		$_mailObject->addHeader('X-Mailer', 'EGroupware-Mail');
