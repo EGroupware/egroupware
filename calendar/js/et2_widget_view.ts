1de2a89eeb0a604d1aeda2b1f57a16591af859bf
nathan <nathangray.bsc+github@gmail.com>
2022-05-02 14:10:59 -0600

    Fix calendar events off by 1 minute

--- calendar/js/et2_widget_view.ts
+++ calendar/js/et2_widget_view.ts
@@ -341,6 +341,7 @@ export class et2_calendar_view extends et2_valueWidget
 			date.setUTCHours(0);
 			date.setUTCMinutes(0);
 			date.setUTCSeconds(0);
+			date.setUTCMilliseconds(0);
 		}
 		// Check for full timestamp
 		else if(typeof _value == 'string' && _value.match(/^(\d{4})-(\d{2})-(\d{2})T(\d{2})\:(\d{2})\:(\d{2})(?:\.\d{3})?(?:Z|[+-](\d{2})\:(\d{2})|)$/))
