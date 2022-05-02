949a13030678d2063dda7f77ce6c0da70c97e621
ralf <rb@egroupware.org>
2022-05-02 09:15:46 +0200

    fix displayed [] instead of [Login]

--- api/src/Framework.php
+++ api/src/Framework.php
@@ -499,7 +499,7 @@ abstract class Framework extends Framework\Extra
 		$app = $GLOBALS['egw_info']['flags']['currentapp'];
 		$app_title = $GLOBALS['egw_info']['apps'][$app]['title'] ?? lang($app);
 		$app_header = $GLOBALS['egw_info']['flags']['app_header'] ?? $app_title;
-		$site_title = strip_tags(($GLOBALS['egw_info']['server']['site_title']??'').' ['.$app_header.']');
+		$site_title = strip_tags(($GLOBALS['egw_info']['server']['site_title']??'').' ['.($app_header ?: $app_title).']');
 
 		// send appheader to clientside
 		$extra['app-header'] = $app_header;
