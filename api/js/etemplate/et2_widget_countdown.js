bc63d06f164dd9de9c6d84e31846d1f0c9460a90
Hadi Nategh <hnategh@gmail.com>
2022-05-23 15:53:09 +0200

    Reduce time complexity of alarms calculation

--- api/js/etemplate/et2_widget_countdown.js
+++ api/js/etemplate/et2_widget_countdown.js
@@ -86,11 +86,18 @@ var et2_countdown = /** @class */ (function (_super) {
         var distance = this.time.getTime() - now.getTime();
         if (distance < 0)
             return 0;
-        var alarms = Array.isArray(this.options.alarm) ? this.options.alarm : [this.options.alarm];
-        for (var i = 0; i <= alarms.length; i++) {
-            if (alarms[i] > 0 && alarms[i] == Math.floor(distance / 1000) && typeof this.onAlarm == 'function') {
-                this.onAlarm();
-            }
+        var alarms = [];
+        if (Array.isArray(this.options.alarm)) {
+            alarms = this.options.alarm;
+        }
+        else {
+            alarms[this.options.alarm] = this.options.alarm;
+        }
+        // alarm values should be set as array index to reduce its time complexity from O(n) to O(1)
+        // otherwise the execution time might be more than a second which would cause timer being delayed
+        if (alarms[Math.floor(distance / 1000)] && typeof this.onAlarm == 'function') {
+            console.log('alarm is called');
+            this.onAlarm();
         }
         var values = {
             days: Math.floor(distance / (1000 * 60 * 60 * 24)),
