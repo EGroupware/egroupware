5fa958de860a20a9fb2f8e755de929ee30902271
ralf <rb@egroupware.org>
2022-07-25 08:35:34 +0200

    trying to fix FF poping up install mailto-link message constantly, when changing content in an iframe / reading mail

--- api/js/jsapi/egw.js
+++ api/js/jsapi/egw.js
@@ -439,10 +439,13 @@
 					);
 				}
 
-				// register our mail as mailto protocol handler
-				let url = egw_webserverUrl;
-				if (url[0] === '/') url = document.location.protocol+'://'+document.location.hostname+(url !== '/' ? url : '');
-				navigator.registerProtocolHandler('mailto', url+'/index.php?menuaction=mail.mail_compose.compose&preset[mailto]=%s', 'Mail');
+				// register our mail as mailto protocol handler (only for main-page, FF seems to pop it up constantly, if we do so in an iframe)
+				if (location.href.match(/(\?|&)cd=yes(&|$)/))
+				{
+					let url = egw_webserverUrl;
+					if (url[0] === '/') url = document.location.protocol+'://'+document.location.hostname+(url !== '/' ? url : '');
+					navigator.registerProtocolHandler('mailto', url+'/index.php?menuaction=mail.mail_compose.compose&preset[mailto]=%s', 'Mail');
+				}
 			}
 			catch(e) {
 				// ignore SecurityError exception if top is different security context / cross-origin
