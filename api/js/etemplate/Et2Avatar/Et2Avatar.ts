8b4b66a17e8f09137ab292f6db88929f3e687103
Hadi Nategh <hnategh@gmail.com>
2022-07-28 15:09:19 +0200

    Make default avatar shape "rounded"

--- api/js/etemplate/Et2Avatar/Et2Avatar.ts
+++ api/js/etemplate/Et2Avatar/Et2Avatar.ts
@@ -100,6 +100,8 @@ export class Et2Avatar extends Et2Widget(SlotMixin(SlAvatar)) implements et2_IDe
 		this.editable = false;
 		this.crop = false;
 		this.size = "2.7em";
+		this.icon = "";
+		this.shape = "rounded";
 	}
 
 	/**
