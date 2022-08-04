de699325a6451a0512bbdc282f86c13251995dac
Hadi Nategh <hnategh@gmail.com>
2022-08-04 16:19:30 +0200

    Set a smaller font-size for tab's label

--- api/js/etemplate/Layout/Et2Tabs/Et2Tab.ts
+++ api/js/etemplate/Layout/Et2Tabs/Et2Tab.ts
@@ -1,8 +1,22 @@
 import {Et2Widget} from "../../Et2Widget/Et2Widget";
 import {SlTab} from "@shoelace-style/shoelace";
+import shoelace from "../../Styles/shoelace";
+import {css} from "@lion/core";
 
 export class Et2Tab extends Et2Widget(SlTab)
 {
+	static get styles()
+	{
+		return [
+			...super.styles,
+			...shoelace,
+			css`
+				.tab {
+					font-size: var(--sl-size-x-small);
+				}		
+			`
+		];
+	}
 
 	static get properties()
 	{
