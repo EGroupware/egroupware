1d5467faabc0f03c4ee5f35f11f0b6e14a94bb22
nathan <nathangray.bsc+github@gmail.com>
2022-08-22 13:45:06 -0600

    Et2ButtonIcon: Fix icons didn't show Must use SVGs here

--- api/js/etemplate/Et2Button/Et2ButtonIcon.ts
+++ api/js/etemplate/Et2Button/Et2ButtonIcon.ts
@@ -17,7 +17,23 @@ import {ButtonMixin} from "./ButtonMixin";
 
 export class Et2ButtonIcon extends ButtonMixin(Et2InputWidget(SlIconButton))
 {
+	set image(new_image : string)
+	{
+		let oldValue = this.__src;
+		if(new_image.indexOf("http") >= 0 || new_image.indexOf(this.egw().webserverUrl) >= 0)
+		{
+			this.src = new_image
+		}
+		else
+		{
+			this.src = this.egw().image(new_image);
+		}
+	}
 
+	get image()
+	{
+		return this.src;
+	}
 }
 
 customElements.define("et2-button-icon", Et2ButtonIcon);
