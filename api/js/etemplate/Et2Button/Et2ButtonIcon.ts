83dc0a95f856133af110b22a64a0df63bf7cd8e5
Hadi Nategh <hnategh@gmail.com>
2022-08-23 12:42:01 +0200

    Don't let button-icons grow/shrink

--- api/js/etemplate/Et2Button/Et2ButtonIcon.ts
+++ api/js/etemplate/Et2Button/Et2ButtonIcon.ts
@@ -13,10 +13,25 @@ import {Et2InputWidget} from "../Et2InputWidget/Et2InputWidget";
 import '../Et2Image/Et2Image';
 import {SlIconButton} from "@shoelace-style/shoelace";
 import {ButtonMixin} from "./ButtonMixin";
+import shoelace from "../Styles/shoelace";
+import {css} from "@lion/core";
 
 
 export class Et2ButtonIcon extends ButtonMixin(Et2InputWidget(SlIconButton))
 {
+	static get styles()
+	{
+		return [
+			...shoelace,
+			...(super.styles || []),
+			css`
+            :host {
+                flex: 0 0 auto !important;			
+			}
+            `,
+		];
+	}
+
 	set image(new_image : string)
 	{
 		let oldValue = this.__src;
