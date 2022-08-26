12ef8163b478f6923f1cf795558e80639d476f6c
nathan <nathangray.bsc+github@gmail.com>
2022-08-26 13:23:36 -0600

    Fix double nextmatch refresh if you search, press [Enter], then take focus out of the search

--- api/js/etemplate/et2_extension_nextmatch.ts
+++ api/js/etemplate/et2_extension_nextmatch.ts
@@ -3328,7 +3328,10 @@ export class et2_nextmatch_header_bar extends et2_DOMWidget implements et2_INext
 			overlay: (typeof settings.searchbox != 'undefined' && typeof settings.searchbox.overlay != 'undefined') ? settings.searchbox.overlay : false,
 			onchange: function()
 			{
-				self.nextmatch.applyFilters({search: this.get_value()});
+				if(this.value !== this.nextmatch.activeFilters.search)
+				{
+					self.nextmatch.applyFilters({search: this.get_value()});
+				}
 			},
 			value: settings.search || '',
 			fix: !egwIsMobile(),
