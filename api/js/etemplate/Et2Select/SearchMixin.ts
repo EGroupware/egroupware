EGroupware/egroupware

commit 4c00b812ee0e27cdd43fb91cac6c9e75e896519e

Fix edit inputbox covered new freeEntries

api/js/etemplate/Et2Select/SearchMixin.ts
@@ -948,6 +948,11 @@ export const Et2WithSearchMixin = <T extends Constructor<LitElement>>(superclass
 			{
 				this.handleMenuSlotChange();
 			});
+			// If we were overlapping edit inputbox with the value display, reset
+			if(this._activeControls.classList.contains("novalue"))
+			{
+				this.handleMenuShow();
+			}
 			return true;
 		}
 

commit c00c6f9e07e18e0d06c82efb76325aa3723c592a

readonly et2-select-mail caused error with 4c00b812ee0e27cdd43fb91cac6c9e75e896519e eg. in mail.display

api/js/etemplate/Et2Select/SearchMixin.ts
@@ -949,7 +949,7 @@ export const Et2WithSearchMixin = <T extends Constructor<LitElement>>(superclass
 				this.handleMenuSlotChange();
 			});
 			// If we were overlapping edit inputbox with the value display, reset
-			if(this._activeControls.classList.contains("novalue"))
+			if(!this.readonly && this._activeControls?.classList.contains("novalue"))
 			{
 				this.handleMenuShow();
 			}