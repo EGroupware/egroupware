EGroupware/egroupware

commit 5a20f5965e060db9bbd16bdb457ca5e0e72948cc

fix preprocessor to set default taglist autocomplete_url/searchUrl, if none is set and change autocomplte_params to searchOptions an empty autocomplete_url is preserved, as it was used to disable autocomplete

api/etemplate.php
@@ -228,15 +228,12 @@ function send_template()
 						$attrs['editModeEnabled'] = 'true';
 					}
 				}
-				if (isset($attrs['autocomplete_url']))
+				$attrs['searchUrl'] = $attrs['autocomplete_url'] ?? 'EGroupware\\Api\\Etemplate\\Widget\\Taglist::ajax_search';
+				if (isset($attrs['autocomplete_params']))
 				{
-					$attrs['searchUrl'] = $attrs['autocomplete_url'];
-					if (isset($attrs['autocomplete_params']))
-					{
-						$attrs['searchOptions'] = $attrs['autocomplete_params'];
-					}
-					unset($attrs['autocomplete_url'], $attrs['autocomplete_params']);
+					$attrs['searchOptions'] = $attrs['autocomplete_params'];
 				}
+				unset($attrs['autocomplete_url'], $attrs['autocomplete_params']);
 				if (isset($attrs['maxSelection']) && $attrs['maxSelection'] === '1')
 				{
 					unset($attrs['multiple'], $attrs['maxSelection']);

commit 12ab9b72a1e8b6a53162e9ac1d9c70b658e5ad43

Fix Et2SelectEmail search queried wrong URL broken by 5a20f5965e060db9bbd16bdb457ca5e0e72948cc

api/etemplate.php
@@ -228,13 +228,17 @@ function send_template()
 						$attrs['editModeEnabled'] = 'true';
 					}
 				}
-				$attrs['searchUrl'] = $attrs['autocomplete_url'] ?? 'EGroupware\\Api\\Etemplate\\Widget\\Taglist::ajax_search';
-				if (isset($attrs['autocomplete_params']))
+				if(isset($attrs['autocomplete_url']))
+				{
+					$attrs['searchUrl'] = $attrs['autocomplete_url'];
+				}
+
+				if(isset($attrs['autocomplete_params']))
 				{
 					$attrs['searchOptions'] = $attrs['autocomplete_params'];
 				}
 				unset($attrs['autocomplete_url'], $attrs['autocomplete_params']);
-				if (isset($attrs['maxSelection']) && $attrs['maxSelection'] === '1')
+				if(isset($attrs['maxSelection']) && $attrs['maxSelection'] === '1')
 				{
 					unset($attrs['multiple'], $attrs['maxSelection']);
 				}