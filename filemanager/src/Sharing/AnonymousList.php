EGroupware/egroupware

commit 28113048332c851691781c9d548c4662a9184336

Filemanager: Remove uneeded buttons from anonymous share nextmatch

filemanager/src/Sharing/AnonymousList.php
@@ -57,6 +57,10 @@ class AnonymousList extends filemanager_ui
 		// Override and take over get_rows so we can customize
 		$content['nm']['get_rows'] = '.' . get_class($this) . '.get_rows';
 		$content['nm']['show_refresh'] = true;
+		$content['nm']['no_filter'] = true;
+		$content['nm']['favorites'] = false;
+
+		$this->etemplate->setElementAttribute("nm", "header_right", '');
 
 		return parent::listview($content, $msg);
 	}

commit 38a82962b0e9f105c6f5b278da105f1c5fea21eb

Filemanager: Fix anonymous share stopped showing subdirectories on refresh
Broken by 28113048332c851691781c9d548c4662a9184336

filemanager/src/Sharing/AnonymousList.php
@@ -145,6 +145,9 @@ class AnonymousList extends filemanager_ui
 	 */
 	function get_rows(&$query, &$rows)
 	{
+		// We turned off filter but still need to use the value
+		$query['filter'] = '';
+
 		// Check for navigating outside share, redirect back to share
 		if (!empty($query['path']) && (!Vfs::stat($query['path'], false) || !Vfs::is_dir($query['path']) || !Vfs::check_access($query['path'], Vfs::READABLE)))
 		{