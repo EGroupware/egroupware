a70e470ffbd0dc11d3cb0d38ad11a026c5436406
ralf <rb@egroupware.org>
2022-12-02 10:36:07 +0100

    Changelog for 21.1.20221202

--- api/setup/setup.inc.php
+++ api/setup/setup.inc.php
@@ -14,7 +14,7 @@ $setup_info['api']['title']     = 'EGroupware API';
 $setup_info['api']['version']   = '21.1.003';
 $setup_info['api']['versions']['current_header'] = '1.29';
 // maintenance release in sync with changelog in doc/rpm-build/debian.changes
-$setup_info['api']['versions']['maintenance_release'] = '21.1.20220916';
+$setup_info['api']['versions']['maintenance_release'] = '21.1.20221202';
 $setup_info['api']['enable']    = 3;
 $setup_info['api']['app_order'] = 1;
 $setup_info['api']['license'] = 'GPL';
