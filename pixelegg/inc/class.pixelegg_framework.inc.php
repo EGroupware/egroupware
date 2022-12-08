95387d5a4c53f8c9cbda21e395a0a1b0f6982001
Hadi Nategh <hnategh@gmail.com>
2022-12-08 13:38:35 +0100

    Adopt some more elements for textsize

--- pixelegg/inc/class.pixelegg_framework.inc.php
+++ pixelegg/inc/class.pixelegg_framework.inc.php
@@ -132,8 +132,9 @@ class pixelegg_framework extends Api\Framework\Ajax
 			#egw_fw_sidebar #egw_fw_sidemenu .egw_fw_ui_sidemenu_entry_header h1,
 			#egw_fw_main #egw_fw_tabs .egw_fw_ui_tabs_header .egw_fw_ui_tab_header h1,
 			 #egw_fw_sidebar #egw_fw_sidemenu .egw_fw_ui_sidemenu_entry_content .egw_fw_ui_category_active h2,
+			 #egw_fw_sidebar #egw_fw_sidemenu .egw_fw_ui_sidemenu_entry_content .egw_fw_ui_category h2,
 			 table.egwGridView_grid, .calendar_calTimeRow .calendar_calTimeRowTime, .calendar_calGridHeader .calendar_calDayColHeader > div[data-date],
-			 .selectedTreeRow, .standartTreeRow, .standartTreeRow_lor{
+			 .selectedTreeRow, .standartTreeRow, .standartTreeRow_lor, .selectedTreeRow_lor{
 				font-size: {$textsize}px;
 			}
 			et2-lavatar, et2-avatar {font-size: 12px}
