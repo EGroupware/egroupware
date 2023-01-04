37dd18894f0fd3d376bdcb055dc9f9d9f25eba05
nathan <nathangray.bsc+github@gmail.com>
2023-01-04 15:33:48 -0700

    Get tests for Colorpicker passing again

--- api/js/etemplate/Et2Colorpicker/test/Et2Colorpicker.test.ts
+++ api/js/etemplate/Et2Colorpicker/test/Et2Colorpicker.test.ts
@@ -3,20 +3,34 @@
  */
 import {assert, fixture, html} from '@open-wc/testing';
 import {Et2Colorpicker} from "../Et2Colorpicker";
+import * as sinon from 'sinon';
 
 
 describe("Colorpicker widget", () =>
 {
 	// Reference to component under test
 	let element : Et2Colorpicker;
+	let egw_stub;
 
 	// Setup run before each test
 	beforeEach(async() =>
 	{
+		// Stub egw
+		// @ts-ignore
+		egw_stub = sinon.stub(Et2Colorpicker.prototype, "egw").returns({
+			lang: i => i,
+			tooltipUnbind: () => {}
+		});
+
 		// Create an element to test with, and wait until it's ready
 		element = await fixture<Et2Colorpicker>(html`
             <et2-colorpicker></et2-colorpicker>
 		`);
+	});
+
+	afterEach(() =>
+	{
+		egw_stub.restore();
 	});
 
 	it('is defined', () =>
@@ -29,7 +43,7 @@ describe("Colorpicker widget", () =>
 		// set a value
 		element.set_value("11111");
 		// trigger the clear button
-		element.__getClearButtonNode().dispatchEvent(new MouseEvent('click'));
+		element.shadowRoot.querySelector(".input__clear").dispatchEvent(new MouseEvent('click'));
 
 		assert.equal(element.getValue(), "");
 	});
