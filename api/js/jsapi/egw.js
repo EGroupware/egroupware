4e1b31bb3744ee0af843701a44a2aeb731e1624c
ralf <rb@egroupware.org>
2023-01-12 08:43:17 -0600

    also legacy-load old phpgwapi JS code like tabs using in KB

--- api/js/jsapi/egw.js
+++ api/js/jsapi/egw.js
@@ -78,7 +78,7 @@ window.app = {classes: {}};
 	window.egw_appName = egw_script.getAttribute('data-app');
 
 	// split includes in legacy js and modules
-	const legacy_js_regexp = /\/dhtmlx|jquery-ui|^etemplate\/|^phpbrain\//;
+	const legacy_js_regexp = /\/dhtmlx|jquery-ui|^etemplate\/|^phpbrain\/|^phpgwapi\//;
 
 	// check if egw object was injected by window open
 	if (typeof window.egw == 'undefined')
