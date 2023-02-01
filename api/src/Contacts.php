dba604ca596cdca20c51199726ee4345191041d5
ralf <rb@egroupware.org>
2023-02-01 08:43:07 +0100

    removed now unnecessary and not used lavatar parameter, as it caused multiple requests to same picture because auf different urls

--- api/src/Contacts.php
+++ api/src/Contacts.php
@@ -802,7 +802,6 @@ class Contacts extends Contacts\Storage
 		//error_log(__METHOD__."($id, ..., etag=$etag) ".  function_backtrace());
 		return $jpeg || !$default ? Egw::link('/api/avatar.php', array(
 			'contact_id' => $id,
-			'lavatar' => !$jpeg ? true : false
 		)+(isset($etag) ? array(
 			'etag'       => $etag
 		) : array())) : $default;
