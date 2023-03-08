13e7ff97b6a46720eee194d1422d9ee04de862e8
nathan <nathangray.bsc+github@gmail.com>
2023-03-08 11:53:48 -0700

    Fix description label did not get row value in nextmatch
    Fixes projectmanager price list showed "5 / $row_cont[pl_unit]" instead of "5 / h"

--- api/js/etemplate/Et2Description/Et2Description.ts
+++ api/js/etemplate/Et2Description/Et2Description.ts
@@ -250,7 +250,7 @@ export class Et2Description extends Et2Widget(LitElement) implements et2_IDetach
 
 	getDetachedAttributes(attrs)
 	{
-		attrs.push("id", "value", "class", "href");
+		attrs.push("id", "label", "value", "class", "href");
 	}
 
 	getDetachedNodes() : HTMLElement[]
