c93eb15fc0330f6b84e0c4e108ca18db00f56ce8
hadi <hnategh@gmail.com>
2023-04-27 14:20:49 +0200

    Fix mail preview failed to fetch text/calendar as attachment for smime signed messages

--- mail/inc/class.mail_ui.inc.php
+++ mail/inc/class.mail_ui.inc.php
@@ -3267,7 +3267,7 @@ $filter['before']= date("d-M-Y", $cutoffdate2);
 			{
 				$smime['msg'] = lang($smime['msg']);
 				$acc_smime = Mail\Smime::get_acc_smime($this->mail_bo->profileID);
-				$attachments = $this->mail_bo->getMessageAttachments($uid, $partID, $structure,true,false,true, $mailbox);
+				$attachments = $this->mail_bo->getMessageAttachments($uid, $partID, $structure,true,true,true, $mailbox);
 				$push = new Api\Json\Push($GLOBALS['egw_info']['user']['account_id']);
 				if (!empty($acc_smime) && !empty($smime['addtocontact'])) $push->call('app.mail.smime_certAddToContact', $smime);
 				if (is_array($attachments))
