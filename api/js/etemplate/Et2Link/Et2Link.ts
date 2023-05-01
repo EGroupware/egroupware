EGroupware/egroupware

commit 12e42d6396c22210b6136d660f8800800ab0eced

Fix Et2Link always opening in the same popup

api/js/etemplate/Et2Link/Et2Link.ts
@@ -295,7 +295,7 @@ export class Et2Link extends ExposeMixin<Et2Widget>(Et2Widget(LitElement)) imple
 			this.egw().open(Object.assign({
 				app: this.app,
 				id: this.entryId
-			}, this.dataset), "", this.linkHook, this.dataset.extra_args, this.targetApp || this.app, this.targetApp);
+			}, this.dataset), "", this.linkHook, this.dataset.extra_args, this.extraLinkTarget, this.targetApp || this.app);
 		}
 
 		_ev.stopImmediatePropagation();

commit 5cb6415fef4f8a698da2516821fae42fb921d517

Et2Link: Fix projectmanager did not open in pm tab
Broken by 12e42d6

api/js/etemplate/Et2Link/Et2Link.ts
@@ -295,7 +295,7 @@ export class Et2Link extends ExposeMixin<Et2Widget>(Et2Widget(LitElement)) imple
 			this.egw().open(Object.assign({
 				app: this.app,
 				id: this.entryId
-			}, this.dataset), "", this.linkHook, this.dataset.extra_args, this.extraLinkTarget, this.targetApp || this.app);
+			}, this.dataset), "", this.linkHook, this.dataset.extra_args, this.extraLinkTarget || this.app, this.targetApp || this.app);
 		}
 
 		_ev.stopImmediatePropagation();