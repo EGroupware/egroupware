48e0bd0e326c191cab004ced23a266d5d7fc3f8e
hadi <hnategh@gmail.com>
2023-05-25 13:14:28 +0200

    Apply nowrap white-space only to et2-link used in et2-link-list in order to let its content be shown in multiple lines

--- api/js/etemplate/Et2Link/Et2Link.ts
+++ api/js/etemplate/Et2Link/Et2Link.ts
@@ -47,8 +47,7 @@ export class Et2Link extends ExposeMixin<Et2Widget>(Et2Widget(LitElement)) imple
 				flex: 2 1 50%;
                 overflow: hidden;
                 text-overflow: ellipsis;
-                white-space: nowrap;
-				max-width: max-content;
+                max-width: max-content;
                 width: 0;
 			  }
 
@@ -56,7 +55,6 @@ export class Et2Link extends ExposeMixin<Et2Widget>(Et2Widget(LitElement)) imple
 				flex: 1 1 50%;
                 overflow: hidden;
                 text-overflow: ellipsis;
-                white-space: nowrap;
                 max-width: max-content;
                 width: 0;
 			  }
