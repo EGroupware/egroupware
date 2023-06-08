EGroupware/egroupware

commit e7e9c6fa22a09a3cd1d18ba4412d4b8b3515760c

Filemanager: Push was not correctly updating file list when directory was added/deleted

filemanager/inc/class.filemanager_ui.inc.php
@@ -1019,6 +1019,7 @@ class filemanager_ui
 		if($query['col_filter']['path'] && $vfs_options['name'])
 		{
 			// Query the requested path, in case it's nested
+			$query['col_filter']['path'][] = $query['path'];
 			foreach($query['col_filter']['path'] as $filter_path)
 			{
 				$query['col_filter']['dir'][] = Vfs::is_dir($filter_path) ? $filter_path : Vfs::dirname($filter_path);

filemanager/inc/class.filemanager_hooks.inc.php
@@ -354,7 +354,7 @@ class filemanager_hooks
 		$stat = isset($data['stat']) ? $data['stat'] : Vfs::stat($path);
 
 		// we ignore notifications about zero size files, created by some WebDAV clients prior to every real update!
-		if($stat['size'] || Vfs::is_dir($path))
+		if($stat['size'] || Vfs::is_dir($path) || in_array($data['location'], ['vfs_rmdir', 'vfs_unlink']))
 		{
 			self::push($data, $stat);
 		}

filemanager/js/filemanager.ts
@@ -20,7 +20,7 @@ import {et2_nextmatch_controller} from "../../api/js/etemplate/et2_extension_nex
 import {egw} from "../../api/js/jsapi/egw_global";
 import {et2_selectbox} from "../../api/js/etemplate/et2_widget_selectbox";
 import {et2_textbox} from "../../api/js/etemplate/et2_widget_textbox";
-import {ExposeMixin, MIME_REGEX} from "../../api/js/etemplate/Expose/ExposeMixin";
+import {MIME_REGEX} from "../../api/js/etemplate/Expose/ExposeMixin";
 
 /**
  * UI for filemanager
@@ -823,7 +823,7 @@ export class filemanagerAPP extends EgwApp
 	 */
 	_do_action_callback(_data)
 	{
-		if(_data.action == "delete" && this.egw.pushAvailable())
+		if(this.egw.pushAvailable())
 		{
 			// No need to refresh, push will handle it
 			this.egw.message(_data.msg)

commit 9b468805280be5ddb848f848142b559d9c450a6f

Filemanager: Fix "[] operator not supported for strings"
caused by e7e9c6f

filemanager/inc/class.filemanager_ui.inc.php
@@ -1020,6 +1020,10 @@ class filemanager_ui
 		{
 			// Query the requested path, in case it's nested
 			$query['col_filter']['path'][] = $query['path'];
+			if(!is_array($query['col_filter']['dir']))
+			{
+				$query['col_filter']['dir'] = $query['col_filter']['dir'] ? [$query['col_filter']['dir']] : [];
+			}
 			foreach($query['col_filter']['path'] as $filter_path)
 			{
 				$query['col_filter']['dir'][] = Vfs::is_dir($filter_path) ? $filter_path : Vfs::dirname($filter_path);