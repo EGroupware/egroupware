69455d725a9f67a04ee10f097a00a19d148e803d
nathan <nathangray.bsc+github@gmail.com>
2023-07-25 08:48:10 -0600

    Api: Fix Et2LinkSearch initialization with value failed, giving blank widget

--- api/js/etemplate/Et2Link/Et2LinkSearch.ts
+++ api/js/etemplate/Et2Link/Et2LinkSearch.ts
@@ -52,7 +52,7 @@ export class Et2LinkSearch extends Et2Select
 
 	get _appNode() : Et2LinkAppSelect
 	{
-		return this.parentNode.querySelector("et2-link-apps");
+		return this.parentNode?.querySelector("et2-link-apps");
 	}
 
 	/**
@@ -64,7 +64,7 @@ export class Et2LinkSearch extends Et2Select
 
 	protected remoteQuery(search : string, options : object)
 	{
-		let request = this.egw().request(this.searchUrl, [this._appNode.value, '', search, options]);
+		let request = this.egw().request(this.searchUrl, [this._appNode?.value ?? options.app, '', search, options]);
 		if(this.query && typeof this.query == "function")
 		{
 			if(!this.query(request, this))
