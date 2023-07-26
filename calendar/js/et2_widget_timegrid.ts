EGroupware/egroupware

commit df1a09907897282ca89613de5cac56747fce606f

Make sure we are not hiding unnecessarily the grid which could lead to grid invisibility in mobile view

calendar/js/et2_widget_timegrid.ts
@@ -699,7 +699,10 @@ export class et2_calendar_timegrid extends et2_calendar_view implements et2_IDet
 		var old_value = this.options.disabled;
 		this.disabled = disabled;
 
-		this.div.get(0).classList.toggle("hideme", disabled);
+		if (this.div.get(0).checkVisibility())
+		{
+			this.div.get(0).classList.toggle("hideme", disabled);
+		}
 
 		if(disabled)
 		{

commit 2bdc7092f0274cf157c082ed747613f1aec786a4

Fix commit df1a09907897282ca89613de5cac56747fce606f broke calendar in some browsers

calendar/js/et2_widget_timegrid.ts
@@ -698,8 +698,8 @@ export class et2_calendar_timegrid extends et2_calendar_view implements et2_IDet
 	{
 		var old_value = this.options.disabled;
 		this.disabled = disabled;
-
-		if (this.div.get(0).checkVisibility())
+		let style = window.getComputedStyle(this.div.get(0));
+		if (style.display !== 'none' && style.display !== '')
 		{
 			this.div.get(0).classList.toggle("hideme", disabled);
 		}