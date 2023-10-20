EGroupware/egroupware

commit e91751471310c115caac35a1b0b929c2b886d64e

Api: Fix customfield field attribute was not always applied
If attribute was set, sometimes it was overwritten and all cf were used.
Happened trying to get only some addressbook custom fields showing on registration page

api/src/Etemplate/Widget/Customfields.php
@@ -176,7 +176,7 @@ class Customfields extends Transformer
 			}
 
 			// Remove filtered fields
-			if (!empty($field_filters) && in_array($key, $negate_fields) && in_array($key, $field_filters))
+			if(!empty($field_filters) && !in_array($key, $field_filters) || !empty($negate_fields) && in_array($key, $negate_fields))
 			{
 				unset($fields[$key]);
 			}

commit b05af867e0bff34e09479afb0a996f63b6734c1a

Api: Fix custom field negated filter broken by e91751471310c115caac35a1b0b929c2b886d64e

api/src/Etemplate/Widget/Customfields.php
@@ -158,7 +158,10 @@ class Customfields extends Transformer
 					$f= substr($f,1);
 					$negate_fields[]= $f;
 				}
-				$field_filters []= $f;
+				else
+				{
+					$field_filters [] = $f;
+				}
 			}
 		}
 