EGroupware/egroupware

commit 80ad5dddb2a6fd980c1cad04f1cf77ec7dc52501

Et2Select: Fix selects with options from file were not showing their initial set value

api/js/etemplate/Et2Select/Et2Select.ts
@@ -508,7 +508,17 @@ export class Et2Select extends Et2WithSearchMixin(Et2WidgetWithSelect)
 		}
 		if(changedProperties.has("select_options") || changedProperties.has("value") || changedProperties.has("emptyLabel"))
 		{
-			this.updateComplete.then(() => this.fix_bad_value());
+			this.updateComplete.then(() =>
+			{
+				this.fix_bad_value();
+
+
+				// Force select to stay in sync, avoids value not shown when select options arrive late
+				if(this.select && this.value !== null && typeof this.value !== "undefined")
+				{
+					this.select.value = this.value;
+				}
+			});
 		}
 		if(changedProperties.has("select_options") && changedProperties.has("value"))
 		{

commit 519306c8d5e55458dcfb366b76eda25400b3f04b

Et2Select: Better fix for  80ad5dddb2a6fd980c1cad04f1cf77ec7dc52501 which caused problems with selecting options in Et2SelectEmail

api/js/etemplate/Et2Select/Et2Select.ts
@@ -511,17 +511,20 @@ export class Et2Select extends Et2WithSearchMixin(Et2WidgetWithSelect)
 			this.updateComplete.then(() =>
 			{
 				this.fix_bad_value();
-
-
-				// Force select to stay in sync, avoids value not shown when select options arrive late
-				if(this.select && this.value !== null && typeof this.value !== "undefined")
-				{
-					this.select.value = this.value;
-				}
 			});
 		}
+	}
+
+	update(changedProperties : PropertyValues)
+	{
+		super.update(changedProperties);
 		if(changedProperties.has("select_options") && changedProperties.has("value"))
 		{
+			// Force select to stay in sync, avoids value not shown when select options arrive late
+			if(this.select && this.value !== null && typeof this.value !== "undefined")
+			{
+				this.select.value = this.value;
+			}
 		}
 	}
 