EGroupware/egroupware

commit ba744d32923995aa0317a50afb32c04058d414fb

Remove Lion

api/js/etemplate/Et2Link/Et2LinkTo.ts
@@ -11,21 +11,19 @@
 
 
 import {Et2InputWidget} from "../Et2InputWidget/Et2InputWidget";
-import {FormControlMixin, ValidateMixin} from "@lion/form-core";
 import {css, html, LitElement, nothing} from "lit";
-import {ScopedElementsMixin} from "@lion/core";
 import {et2_createWidget, et2_widget} from "../et2_core_widget";
 import {et2_file} from "../et2_widget_file";
 import {Et2Button} from "../Et2Button/Et2Button";
 import {Et2LinkEntry} from "./Et2LinkEntry";
 import {egw} from "../../jsapi/egw_global";
 import {LinkInfo} from "./Et2Link";
-import type {ValidationType} from "@lion/form-core/types/validate/ValidateMixinTypes";
 import {ManualMessage} from "../Validators/ManualMessage";
 import {Et2Tabs} from "../Layout/Et2Tabs/Et2Tabs";
 import {Et2VfsSelectButton} from "../Et2Vfs/Et2VfsSelectButton";
 import {Et2LinkPasteDialog, getClipboardFiles} from "./Et2LinkPasteDialog";
 import {waitForEvent} from "../Et2Widget/event";
+import {classMap} from "lit/directives/class-map.js";
 
 /**
  * Choose an existing entry, VFS file or local file, and link it to the current entry.
@@ -33,7 +31,7 @@ import {waitForEvent} from "../Et2Widget/event";
  * If there is no "current entry", link information will be stored for submission instead
  * of being directly linked.
  */
-export class Et2LinkTo extends Et2InputWidget(ScopedElementsMixin(FormControlMixin(ValidateMixin(LitElement))))
+export class Et2LinkTo extends Et2InputWidget(LitElement)
 {
 	static get properties()
 	{
@@ -73,13 +71,10 @@ export class Et2LinkTo extends Et2InputWidget(ScopedElementsMixin(FormControlMix
 			.input-group__container {
 				flex: 1 1 auto;
 			}
-			.input-group {
+
+				.form-control-input {
 				display: flex;
 				width: 100%;
-				gap: 0.5rem;
-			}
-			.input-group__before {
-				display: flex;
 				gap: 0.5rem;
 			}
 			::slotted(.et2_file) {
@@ -160,13 +155,17 @@ export class Et2LinkTo extends Et2InputWidget(ScopedElementsMixin(FormControlMix
 		return html`
             <slot name="before"></slot>
             <et2-vfs-select
+                    id="link"
                     ?readonly=${this.readonly}
                     method=${method || nothing}
                     method-id=${method_id || nothing}
                     multiple
                     title=${this.egw().lang("select file(s) from vfs")}
                     .buttonLabel=${this.egw().lang('Link')}
-                    .onchange=${this.handleVfsSelected}
+                    @change=${async() =>
+                    {
+                        this.handleVfsSelected(await this.shadowRoot.getElementById("link")._dialog.getComplete());
+                    }}
             >
                 <et2-button slot="footer" image="copy" id="copy" style="order:3" noSubmit="true"
                             label=${this.egw().lang("copy")}></et2-button>
@@ -585,6 +584,35 @@ export class Et2LinkTo extends Et2InputWidget(ScopedElementsMixin(FormControlMix
 	static get validationTypes() : ValidationType[]
 	{
 		return ['error', 'success'];
+	}
+
+
+	render()
+	{
+		const labelTemplate = this._labelTemplate();
+		const helpTemplate = this._helpTextTemplate();
+
+		return html`
+            <div
+                    part="form-control"
+                    class=${classMap({
+                        'form-control': true,
+                        'form-control--medium': true,
+                        'form-control--has-label': labelTemplate !== nothing,
+                        'form-control--has-help-text': helpTemplate !== nothing
+                    })}
+            >
+                ${labelTemplate}
+                <div part="form-control-input" class="form-control-input" @sl-change=${() =>
+                {
+                    this.dispatchEvent(new Event("change", {bubbles: true}));
+                }}>
+                    ${this._inputGroupBeforeTemplate()}
+                    ${this._inputGroupInputTemplate()}
+                </div>
+                ${helpTemplate}
+            </div>
+		`;
 	}
 }
 

addressbook/js/app.ts
@@ -20,7 +20,7 @@ import {et2_selectbox} from "../../api/js/etemplate/et2_widget_selectbox";
 import {fetchAll, nm_action, nm_compare_field} from "../../api/js/etemplate/et2_extension_nextmatch_actions";
 import "./CRM";
 import {egw} from "../../api/js/jsapi/egw_global";
-import {LitElement} from "@lion/core";
+import {LitElement} from "lit";
 import {Et2SelectCountry} from "../../api/js/etemplate/Et2Select/Select/Et2SelectCountry";
 
 import {Et2SelectState} from "../../api/js/etemplate/Et2Select/Select/Et2SelectState";

admin/js/app.ts
@@ -17,13 +17,13 @@ import {etemplate2} from "../../api/js/etemplate/etemplate2";
 import {Et2Dialog} from "../../api/js/etemplate/Et2Dialog/Et2Dialog";
 import {egw} from "../../api/js/jsapi/egw_global.js";
 import {egwAction, egwActionObject} from '../../api/js/egw_action/egw_action';
-import {LitElement} from "@lion/core";
 import {et2_nextmatch} from "../../api/js/etemplate/et2_extension_nextmatch";
 import {et2_DOMWidget} from "../../api/js/etemplate/et2_core_DOMWidget";
 import {Et2SelectAccount} from "../../api/js/etemplate/Et2Select/Select/Et2SelectAccount";
 import {EgwAction} from "../../api/js/egw_action/EgwAction";
 import {EgwActionObject} from "../../api/js/egw_action/EgwActionObject";
 import type {Et2Button} from "../../api/js/etemplate/Et2Button/Et2Button";
+import {LitElement} from "lit";
 
 /**
  * UI for Admin

api/js/etemplate/Et2Avatar/Et2Avatar.ts
@@ -52,7 +52,6 @@ export class Et2Avatar extends Et2Widget(SlAvatar) implements et2_IDetachedDOM
 			/**
 			 * The label of the image
 			 * Actually not used as label, but we put it as title
-			 * Added here as there's no Lion parent
 			 */
 			label: {
 				type: String

api/js/etemplate/Et2Date/Et2Date.ts
@@ -9,20 +9,19 @@
  */
 
 
-import {css, html} from "lit";
+import {css, html, nothing} from "lit";
 import 'lit-flatpickr';
 import {dateStyles} from "./DateStyles";
 import type {Instance} from 'flatpickr/dist/types/instance';
 import {default as scrollPlugin} from "flatpickr/dist/plugins/scrollPlugin.js";
-import {default as ShortcutButtonsPlugin} from "shortcut-buttons-flatpickr/dist/shortcut-buttons-flatpickr";
+import {default as ShortcutButtonsPlugin} from "shortcut-buttons-flatpickr/dist/shortcut-buttons-flatpickr.js";
 import flatpickr from "flatpickr";
 import {egw} from "../../jsapi/egw_global";
-import type {HTMLElementWithValue} from "@lion/form-core/types/FormControlMixinTypes";
 import {Et2Textbox} from "../Et2Textbox/Et2Textbox";
-import {FormControlMixin} from "@lion/form-core";
 import {LitFlatpickr} from "lit-flatpickr";
 import {Et2InputWidget} from "../Et2InputWidget/Et2InputWidget";
 import shoelace from "../Styles/shoelace";
+import {classMap} from "lit/directives/class-map.js";
 
 // list of existing localizations from node_modules/flatpicker/dist/l10n directory:
 const l10n = [
@@ -224,7 +223,7 @@ export function parseDateTime(dateTimeString)
  * Format dates according to user preference
  *
  * @param {Date} date
- * @param {import('@lion/localize/types/LocalizeMixinTypes').FormatDateOptions} [options] Intl options are available
+ * @param [options] Intl options are available
  * 	set 'dateFormat': "Y-m-d" to specify a particular format
  * @returns {string}
  */
@@ -261,7 +260,7 @@ export function formatDate(date : Date, options = {dateFormat: ""}) : string
  * Format dates according to user preference
  *
  * @param {Date} date
- * @param {import('@lion/localize/types/LocalizeMixinTypes').FormatDateOptions} [options] Intl options are available
+ * @param  [options] Intl options are available
  * 	set 'timeFormat': "12" to specify a particular format
  * @returns {string}
  */
@@ -305,8 +304,7 @@ export function formatDateTime(date : Date, options = {dateFormat: "", timeForma
 	return formatDate(date, options) + " " + formatTime(date, options);
 }
 
-// !!! ValidateMixin !!!
-export class Et2Date extends Et2InputWidget(FormControlMixin(LitFlatpickr))
+export class Et2Date extends Et2InputWidget(LitFlatpickr)
 {
 	static get styles()
 	{
@@ -554,7 +552,7 @@ export class Et2Date extends Et2InputWidget(FormControlMixin(LitFlatpickr))
 		options.onChange = this._updateValueOnChange;
 		options.onReady = this._onReady;
 
-		// Remove Lion's inert attribute so we can work in Et2Dialog
+		// Remove inert attribute so we can work in Et2Dialog
 		options.onOpen = [() =>
 		{
 			this._instance.calendarContainer?.removeAttribute("inert")
@@ -968,17 +966,17 @@ export class Et2Date extends Et2InputWidget(FormControlMixin(LitFlatpickr))
 	 * This is an et2-textbox, which causes some problems with flatpickr
 	 * @protected
 	 */
-	get _inputNode() : HTMLElementWithValue
+	get _inputNode() : Et2Textbox
 	{
-		return this.querySelector('[slot="input"]');
+		return this.shadowRoot?.querySelector('et2-textbox');
 	}
 
 	/**
 	 * The holder of value for flatpickr
 	 */
-	get _valueNode() : HTMLElementWithValue
+	get _valueNode() : Et2Textbox
 	{
-		return this.querySelector('et2-textbox');
+		return this.shadowRoot?.querySelector('et2-textbox');
 	}
 
 	/**
@@ -1037,20 +1035,17 @@ export class Et2Date extends Et2InputWidget(FormControlMixin(LitFlatpickr))
 		this.setDate(date, false, null);
 	}
 
-	render()
-	{
-		return html`
-            <div part="form-control" class="form-control">
-                <div class="form-field__group-one" part="form-control-label">${this._groupOneTemplate()}</div>
-                <div class="form-field__group-two" part="form-control-input">${this._groupTwoTemplate()}</div>
-            </div>
-		`;
-	}
 
-	protected _inputGroupInputTemplate()
+	protected _inputTemplate()
 	{
+		if(typeof egwIsMobile == "function" && egwIsMobile())
+		{
+			// Plain input for mobile
+			return html`<input type=${this._mobileInputType()}></input>`;
+		}
+		// This element gets hidden and used for value, but copied by flatpickr and used for input
 		return html`
-            <slot name="input"></slot>
+            <et2-textbox type="text" placeholder=${this.placeholder} ?required=${this.required}></et2-textbox>
             ${this._incrementButtonTemplate()}
 		`;
 	}
@@ -1078,6 +1073,32 @@ export class Et2Date extends Et2InputWidget(FormControlMixin(LitFlatpickr))
                 >↓
                 </et2-button-icon>
             </div>`;
+	}
+
+	render()
+	{
+
+		const labelTemplate = this._labelTemplate();
+		const helpTemplate = this._helpTextTemplate();
+
+		return html`
+            ${super.render()}
+            <div
+                    part="form-control"
+                    class=${classMap({
+                        'form-control': true,
+                        'form-control--medium': true,
+                        'form-control--has-label': labelTemplate !== nothing,
+                        'form-control--has-help-text': helpTemplate !== nothing
+                    })}
+            >
+                ${labelTemplate}
+                <div part="form-control-input" class="form-control-input">
+                    ${this._inputTemplate()}
+                </div>
+                ${helpTemplate}
+            </div>
+		`;
 	}
 }
 

api/js/etemplate/Et2Date/Et2DateDuration.ts
@@ -9,12 +9,11 @@
  */
 
 
-import {css, html, LitElement} from "lit";
+import {css, html, LitElement, nothing} from "lit";
 import {classMap} from "lit/directives/class-map.js";
 import {Et2InputWidget} from "../Et2InputWidget/Et2InputWidget";
 import {sprintf} from "../../egw_action/egw_action_common";
 import {dateStyles} from "./DateStyles";
-import {FormControlMixin} from "@lion/form-core";
 import shoelace from "../Styles/shoelace";
 
 export interface formatOptions
@@ -112,7 +111,7 @@ export function formatDuration(value : number | string, options : formatOptions)
  * If not specified, the time is in assumed to be minutes and will be displayed with a calculated unit
  * but this can be specified with the properties.
  */
-export class Et2DateDuration extends Et2InputWidget(FormControlMixin(LitElement))
+export class Et2DateDuration extends Et2InputWidget(LitElement)
 {
 	static get styles()
 	{
@@ -125,7 +124,7 @@ export class Et2DateDuration extends Et2InputWidget(FormControlMixin(LitElement)
 				max-width: 100%;
 			  }
 
-			  .input-group {
+				.form-control-input {
 				display: flex;
 				flex-direction: row;
 				flex-wrap: nowrap;
@@ -399,35 +398,29 @@ export class Et2DateDuration extends Et2InputWidget(FormControlMixin(LitElement)
 
 	render()
 	{
-		return html`
-            <div part="form-control" class=${classMap({
-                'form-control': true,
-                'form-control--has-label': this.label.split("%")[0] || false
-            })}>
-                <div class="form-field__group-one form-control__label" part="form-control-label">
-                    ${this._groupOneTemplate()}
-                </div>
-                <div class="form-field__group-two" part="form-control-input">${this._groupTwoTemplate()}</div>
-            </div>
-		`;
-	}
 
-	/**
-	 * @return {TemplateResult}
-	 * @protected
-	 */
-	// eslint-disable-next-line class-methods-use-this
-	_inputGroupInputTemplate()
-	{
+		const labelTemplate = this._labelTemplate();
+		const helpTemplate = this._helpTextTemplate();
+
 		return html`
-            <div class="input-group__input" @sl-change=${() =>
-            {
-                this.dispatchEvent(new Event("change", {bubbles: true}));
-            }}>
-                <slot name="input">
+            <div
+                    part="form-control"
+                    class=${classMap({
+                        'form-control': true,
+                        'form-control--medium': true,
+                        'form-control--has-label': labelTemplate !== nothing,
+                        'form-control--has-help-text': helpTemplate !== nothing
+                    })}
+            >
+                ${labelTemplate}
+                <div part="form-control-input" class="form-control-input" @sl-change=${() =>
+                {
+                    this.dispatchEvent(new Event("change", {bubbles: true}));
+                }}>
                     ${this._inputTemplate()}
                     ${this._formatTemplate()}
-                </slot>
+                </div>
+                ${helpTemplate}
             </div>
 		`;
 	}

api/js/etemplate/Et2Date/Et2DateRange.ts
@@ -1,5 +1,4 @@
 import {Et2InputWidget} from "../Et2InputWidget/Et2InputWidget";
-import {FormControlMixin} from "@lion/form-core";
 import {css, html, LitElement, TemplateResult} from "lit";
 import {classMap} from "lit/directives/class-map.js";
 import {ifDefined} from "lit/directives/if-defined.js";
@@ -14,7 +13,7 @@ import {egw} from "../../jsapi/egw_global";
  * If not specified, the time is in assumed to be minutes and will be displayed with a calculated unit
  * but this can be specified with the properties.
  */
-export class Et2DateRange extends Et2InputWidget(FormControlMixin(LitElement))
+export class Et2DateRange extends Et2InputWidget(LitElement)
 {
 	static get styles()
 	{

api/js/etemplate/Et2Date/Et2DateTimeToday.ts
@@ -28,7 +28,6 @@ export class Et2DateTimeToday extends Et2DateReadonly
 	 * If the date is today, we show just the time.  Otherwise, the date.
 	 *
 	 * @param {Date} date
-	 * @param {import('@lion/localize/types/LocalizeMixinTypes').FormatDateOptions} [options] Intl options are available
 	 * @returns {string}
 	 */
 	formatDateTime(date : Date, options = {dateFormat: "", timeFormat: ""}) : string

commit e596d60395007807d62070766f2e333a24751abe

Et2LinkTo: Fix Link button did not show after selecting an entry
Broken by ba744d32923995aa0317a50afb32c04058d414fb

api/js/etemplate/Et2Link/Et2LinkTo.ts
@@ -437,7 +437,10 @@ export class Et2LinkTo extends Et2InputWidget(LitElement)
 	handleEntrySelected(event)
 	{
 		// Could be the app, could be they selected an entry
-		if(event.target == this.select._searchNode)
+		if(event.target == this.select && (
+			typeof this.select.value == "string" && this.select.value ||
+			typeof this.select.value == "object" && this.select.value.id
+		))
 		{
 			this.classList.add("can_link");
 			this.link_button.focus();