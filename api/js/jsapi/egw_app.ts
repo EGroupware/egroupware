b4c0ed9edc1585834d1f225d99022e16e8c6a24d
nathan <nathangray.bsc+github@gmail.com>
2024-07-09 08:57:35 -0600

    Fix Merge dialog "Save file" button did not actually save the file if one merge document was selected
    It opened it in Collabora instead, regardless of which button was clicked

--- api/js/jsapi/egw_app.ts
+++ api/js/jsapi/egw_app.ts
@@ -867,7 +867,7 @@ export abstract class EgwApp
 		{
 			vars.document = document.documents.map(f => f.path);
 		}
-		if(document.documents.length == 1 && !document.options.individual)
+		if(document.documents.length == 1 && !document.options.individual && !document.options.download)
 		{
 			// Only 1 document, we can open it
 			vars.id = JSON.stringify(ids);
