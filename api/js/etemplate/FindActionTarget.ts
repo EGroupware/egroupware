8cbecc5969b601f2a448a7467c4ac1011d6434bd
milan <mk@egroupware.org>
2024-07-26 12:05:03 +0200

    added Documentation for FindActionTarget.ts

--- api/js/etemplate/FindActionTarget.ts
+++ api/js/etemplate/FindActionTarget.ts
@@ -1,5 +1,11 @@
 import {EgwActionObject} from "../egw_action/EgwActionObject";
 
+/**
+ * if a class that registers actions implements this interface the Popup actions only bind one event handler on the parent item
+ * so not every child item will get its own event handler
+ * Currently only implemented by Et2Tree
+ * there the actions only bind one contextmenu event listener on the "et2-tree" htmlElement instead of one for each sl-tree-item
+ */
 export interface FindActionTarget
 {
     /**
