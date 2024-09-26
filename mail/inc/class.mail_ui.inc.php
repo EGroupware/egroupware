efdfc52da85f0c9239469f9e9f407654bbba465e
milan <mk@egroupware.org>
2024-09-26 13:31:50 +0200

    fix folder badge was not updated when rightclick 'mark all as read' was clicked
    (cherry picked from commit 1434694365d0292ab298ddf233212d6c22a6f1c6)

--- mail/inc/class.mail_ui.inc.php
+++ mail/inc/class.mail_ui.inc.php
@@ -5301,7 +5301,7 @@ $filter['before']= date("d-M-Y", $cutoffdate2);
 			}
 			else if ((isset($_messageList['all']) && $_messageList['all']) || ($query['filter'] && ($flag2check==$query['filter'] || stripos($query['filter'],$flag2check)!==false)))
 			{
-                self::ajax_setFolderStatus([$_messageList['msg']['0']],true);
+                self::ajax_setFolderStatus($_messageList['msg'],true);
 				$response->call('egw.refresh',lang('flagged %1 messages as %2 in %3',(isset($_messageList['all']) && $_messageList['all']?lang('all'):count($_messageList['msg'])),lang(($flag[$_flag]?$flag[$_flag]:$_flag)),lang($folder)),'mail');
 			}
 			else
