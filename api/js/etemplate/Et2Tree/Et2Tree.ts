c709c6315223b32d3e53d9aab8df06baa7f37684
nathan <nathangray.bsc+github@gmail.com>
2024-10-09 09:43:39 -0600

    Mail: Fix issues adding email account
    - Et2Tree.refreshItem() wrong return type

--- api/js/etemplate/Et2Tree/Et2Tree.ts
+++ api/js/etemplate/Et2Tree/Et2Tree.ts
@@ -599,7 +599,10 @@ export class Et2Tree extends Et2WidgetWithSelectMixin(LitElement) implements Fin
 		{
 			let item = this.getNode(_id);
 			// if the item does not exist in the tree yet no need to refresh
-			if(item == null) return
+			if(item == null)
+			{
+				return Promise.resolve();
+			}
 			return this.handleLazyLoading(item).then((result) => {
 				item.item = [...result.item]
 				this.requestUpdate("_selectOptions")
@@ -1123,6 +1126,7 @@ export class Et2Tree extends Et2WidgetWithSelectMixin(LitElement) implements Fin
             >
 				<sl-icon name="chevron-right" slot="expand-icon"></sl-icon>
 				<sl-icon name="chevron-down" slot="collapse-icon"></sl-icon>
+                <slot></slot>
                 ${repeat(this._selectOptions, (o) => o.value, this._optionTemplate)}
             </sl-tree>
 		`;
