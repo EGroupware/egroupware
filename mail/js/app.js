d4c819899cbbcae79bdf440a001ee6b03175e825
milan <mk@egroupware.org>
2024-12-05 18:06:50 +0100

    Mail Compose change "To" label to a dropdown to add bcc cc etc

--- mail/js/app.js
+++ mail/js/app.js
@@ -6244,7 +6244,7 @@ app.classes.mail = AppJS.extend(
 					var widget = this.et2.getWidgetById(actions[i]);
 					if (widget)
 					{
-						jQuery(widget.getDOMNode()).trigger('click');
+					//	jQuery(widget.getDOMNode()).trigger('click');
 					}
 				}
 			}
