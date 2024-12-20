46c4d20623983e9a0f223e1f6f3decedb2d6037f
nathan <nathangray.bsc+github@gmail.com>
2024-12-20 09:04:16 -0700

    Et2VfsPath: Fix Enter would not accept typed value in Firefox

--- api/js/etemplate/Et2Vfs/test/Et2VfsPath.test.ts
+++ api/js/etemplate/Et2Vfs/test/Et2VfsPath.test.ts
@@ -1,7 +1,8 @@
-import {assert, elementUpdated, fixture, html} from '@open-wc/testing';
+import {assert, elementUpdated, expect, fixture, html, oneEvent} from '@open-wc/testing';
 import * as sinon from 'sinon';
 import {inputBasicTests} from "../../Et2InputWidget/test/InputBasicTests";
 import {Et2VfsPath} from "../Et2VfsPath";
+import {sendKeys} from "@web/test-runner-commands";
 
 /**
  * Test file for Etemplate webComponent VfsPath
@@ -64,18 +65,80 @@ describe("Path widget basics", () =>
 		await elementUpdated(element);
 		assert.equal(element.shadowRoot.activeElement, element._edit, "Editable path did not get focus when widget got focus");
 	});
+});
+describe("User interactions", () =>
+{
+	// Setup run before each test
+	beforeEach(before);
 
 	it("blurring widget accepts current text", async() =>
 	{
+		const changeListener = oneEvent(element, "change");
 		const value = "/home/test/directory";
+
+		// Enter new value
 		element.focus();
 		await elementUpdated(element);
 		element._edit.value = value;
+
+		// Lose focus
 		element.blur();
 		await elementUpdated(element);
 
 		assert.equal(element.value, value, "Path was not accepted on blur");
+
+		// Make sure change event is fired
+		return changeListener;
 	});
+	it("[Enter] accepts current path", async() =>
+	{
+		const originalValue = "/home/different/directory";
+		const changedValue = "/home/test/directory";
+		element.value = originalValue;
+		element.focus();
+		await elementUpdated(element);
+		const changeListener = oneEvent(element, "change");
+
+		// Enter field, "type" a new value
+		element._edit.focus();
+		element._edit.value = changedValue;
+
+		// Press Enter to accept new value
+		await sendKeys({down: "Enter"});
+
+		// Wait for change event
+		const event = await changeListener;
+		expect(event).to.exist;
+
+		// Check value
+		assert.equal(element.value, changedValue, "Value did not change on [Enter]");
+	});
+
+	it("[Esc] rejects current path", async() =>
+	{
+		const originalValue = "/home/different/directory";
+		const changedValue = "/home/changed/directory";
+		element.value = originalValue;
+		element.focus();
+		await elementUpdated(element);
+
+		// Set up spy for change event
+		const handler = sinon.spy();
+		element.addEventListener("change", handler);
+
+		// Change the value
+		element._edit.focus();
+		element._edit.value = changedValue;
+
+		// Press Escape, cancel edit
+		await sendKeys({down: "Escape"});
+
+		// Check value
+		assert.equal(element.value, originalValue, "Value was changed when [Esc] was pressed");
+
+		// No change event
+		sinon.assert.notCalled(handler);
+	})
 });
 
 inputBasicTests(async() =>
