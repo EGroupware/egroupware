EGroupware/egroupware

commit e96f800ceddbb62f62c1160bc523e37dd80c6e63

Et2Details: Contents now scroll if they overflow

api/js/etemplate/Layout/Et2Details/Et2Details.ts
@@ -39,6 +39,7 @@ export class Et2Details extends Et2Widget(SlDetails)
 			css`
 				:host {
 					display: block;
+					min-height: 1em;
 				}
 				
 				:host([align="right"]) > div {
@@ -74,6 +75,14 @@ export class Et2Details extends Et2Widget(SlDetails)
 					border: var(--sl-panel-border-width) solid var(--sl-panel-border-color);
 					margin: 0px;
 					overflow: hidden;
+					height: 100%;
+					display: flex;
+					flex-direction: column;
+				}
+
+				.details__content {
+					height: 100%;
+					overflow-y: auto;
 				}
 
 				.details.hoist {

commit 6de1be92aa96466499f43d02daf16c64d0db4432

Api: Fix Et2Details did not properly size when small, giving scrollbars.
Broken by e96f800ceddbb62f62c1160bc523e37dd80c6e63

api/js/etemplate/Layout/Et2Details/Et2Details.ts
@@ -39,9 +39,7 @@ export class Et2Details extends Et2Widget(SlDetails)
 			css`
 				:host {
 					display: block;
-					min-height: var(--sl-input-height-medium);
 				}
-				
 				:host([align="right"]) > div {
 					justify-content: flex-end;
 				}
@@ -82,6 +80,7 @@ export class Et2Details extends Et2Widget(SlDetails)
 
 				.details__content {
 					height: 100%;
+					min-height: 1px;
 					overflow-y: auto;
 				}
 