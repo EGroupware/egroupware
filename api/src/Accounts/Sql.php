EGroupware/egroupware

commit 6378fb0c7c1a114fbf221ac3795da60b8e3a85d7

* Api: Improved account widget searching to work with +/- and quotes

api/src/Etemplate/Widget/Taglist.php
@@ -78,7 +78,12 @@ class Taglist extends Etemplate\Widget
 		{
 			$options['account_type'] = $_REQUEST['account_type'];
 			$options['tag_list'] = true;
-			$results = Api\Accounts::link_query($query, $options);
+			$options['filter'] ['contact_owner'] = 0;
+			$contacts = new Api\Contacts();
+			foreach($contacts->link_query($query, $options) as $value => $item)
+			{
+				$results[] = ['value' => $value] + $item;
+			}
 		}
 		else
 		{

commit 9fa35964e96083352e5ffa0c96005b5b4d83f298

Api: Fix searching for groups broken by  6378fb0
Reverted to Api\Accounts::link_query() as before, and advanced searching is now added into SQL account backend.

api/src/Accounts/Sql.php
@@ -531,17 +531,24 @@ class Sql
 		{
 			$filter[] = str_replace('UNIX_TIMESTAMP(NOW())',time(),Api\Contacts\Sql::ACOUNT_ACTIVE_FILTER);
 		}
+		$op = 'AND';
 		$criteria = array();
 		$wildcard = in_array($param['query_type'] ?? '', ['start', 'exact']) ? '' : '%';
 		if (($query = $param['query'] ?? null))
 		{
+			$op = $query[0] == '!' ? 'AND' : 'OR';
 			switch($param['query_type'])
 			{
+				case 'all':
+				default:
+					$op = '';
+					$search_cols = array_diff($search_cols, ['email']);
+					$storage = new Api\Storage\Base('api', $this->table, $this->db);
+					$criteria = $storage->search2criteria($query, $wildcard, $op, null, $search_cols);
+					break;
 				case 'start':
 					$query .= '*';
 					// fall-through
-				case 'all':
-				default:
 				case 'exact':
 					foreach($search_cols as $col)
 					{
@@ -578,7 +585,7 @@ class Sql
 			array_merge(array(1,'n_given','n_family','id','created','modified','files',$this->table.'.account_id AS account_id'),$email_cols),
 			$order, "account_lid,account_type,account_status,account_expires,account_primary_group,account_description".
 			",account_lastlogin,account_lastloginfrom,account_lastpwd_change,account_uuid,account_dn",
-			$wildcard,false,$query[0] == '!' ? 'AND' : 'OR',
+										$wildcard, false, $op,
 			!empty($param['offset']) ? array($param['start'], $param['offset']) : $param['start'] ?? false,
 			$filter,$join) ?? [] as $contact)
 		{

api/src/Etemplate/Widget/Taglist.php
@@ -78,13 +78,7 @@ class Taglist extends Etemplate\Widget
 		{
 			$options['account_type'] = $_REQUEST['account_type'];
 			$options['tag_list'] = true;
-			$options['filter'] ['contact_owner'] = 0;
-			$contacts = new Api\Contacts();
-			foreach($contacts->link_query($query, $options) as $contact_id => $item)
-			{
-				$contact = $contacts->read($contact_id);
-				$results[] = ['value' => $contact['account_id']] + $item;
-			}
+			$results = Api\Accounts::link_query($query, $options);
 		}
 		else
 		{