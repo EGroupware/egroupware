8105458a73bf84835c269762b6b4cd0b353a6baa
nathan <nathangray.bsc+github@gmail.com>
2025-06-26 16:50:59 -0600

    Work on course preferences

--- api/js/etemplate/Et2MenuItem/Et2MenuItem.ts
+++ api/js/etemplate/Et2MenuItem/Et2MenuItem.ts
@@ -1,8 +1,26 @@
 import {customElement} from "lit/decorators/custom-element.js";
 import {SlMenuItem} from "@shoelace-style/shoelace";
 import {Et2Widget} from "../Et2Widget/Et2Widget";
+import {property} from "lit/decorators/property.js";
+import {et2_IInput} from "../et2_core_interfaces";
 
 @customElement('et2-menu-item')
-export class Et2MenuItem extends Et2Widget(SlMenuItem)
+export class Et2MenuItem extends Et2Widget(SlMenuItem) implements et2_IInput
 {
+	@property({type: Boolean, reflect: true, attribute: 'checked'})
+	value = false;
+
+	getValue()
+	{
+		return this.value;
+	}
+
+	isDirty()
+	{
+		return false;
+	}
+
+	resetDirty() {}
+
+	isValid() {return true;}
 }
