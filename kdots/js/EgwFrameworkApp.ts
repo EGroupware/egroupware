EGroupware/egroupware

commit d09d10ad8212e408de92dc307100953285975466

kdots: tie logo / app dropdown size to active application's left side panel width

kdots/js/EgwFrameworkApp.ts
@@ -173,6 +173,18 @@ export class EgwFrameworkApp extends LitElement
 	connectedCallback()
 	{
 		super.connectedCallback();
+
+		// Get size preferences
+		// @ts-ignore preference() takes _callback = true
+		this.egw.preference(this.leftPanelInfo.preference, this.appName, true).then((value) =>
+		{
+			this.leftPanelInfo.preferenceWidth = value;
+		});
+		// @ts-ignore preference() takes _callback = true
+		this.egw.preference(this.rightPanelInfo.preference, this.appName, true).then((value) =>
+		{
+			this.rightPanelInfo.preferenceWidth = value;
+		});
 		this.addEventListener("load", this.handleEtemplateLoad);
 
 		// Work around sl-split-panel resizing to 0 when app is hidden
@@ -572,6 +584,8 @@ export class EgwFrameworkApp extends LitElement
 	 */
 	protected async handleSlide(event)
 	{
+		event.stopPropagation();
+
 		// Skip if there's no panelInfo - event is from the wrong place
 		// Skip if loading, or not active to avoid keeping changes while user is not interacting
 		if(typeof event.target?.dataset.panel == "undefined" || this.ignoreSplitterResize || this.loading || !this.hasAttribute("active"))
@@ -588,11 +602,28 @@ export class EgwFrameworkApp extends LitElement
 		{
 			return;
 		}
+		// Limit to maximum of actual width, splitter handles max
+		newPosition = Math.min(newPosition, split.querySelector(panelInfo.side == "left" ? "[slot='start']" : "[slot='end']").getBoundingClientRect().width);
 
 		// Update collapsed
 		this[`${panelInfo.side}Collapsed`] = newPosition == panelInfo.hiddenWidth;
 
 		let preferenceName = panelInfo.preference;
+
+		// Send it out with details, in case anyone cares
+		this.dispatchEvent(new CustomEvent("sl-reposition", {
+				detail: {
+					name: this.name,
+					side: panelInfo.side,
+					preference: preferenceName,
+					width: newPosition,
+				},
+				bubbles: true,
+				composed: true,
+			}
+		));
+
+		// Delay preference update & etemplate resize because they're expensive
 		if(!this[`${panelInfo.side}Collapsed`] && newPosition != panelInfo.preferenceWidth)
 		{
 			if(panelInfo.resizeTimeout)
@@ -924,6 +955,7 @@ export class EgwFrameworkApp extends LitElement
 	{
 		const hasLeftSlots = this.hasSideContent("left") || this.features?.favorites;
 		const hasRightSlots = this.hasSideContent("right");
+		const hasHeaderContent = this.hasSlotController.test("main-header");
 
 		const leftWidth = this.leftCollapsed || !hasLeftSlots ? this.leftPanelInfo.hiddenWidth :
 						  this.leftPanelInfo.preferenceWidth;
@@ -931,7 +963,10 @@ export class EgwFrameworkApp extends LitElement
 						   this.rightPanelInfo.preferenceWidth;
 		return html`
             <div class="egw_fw_app__header" part="app-header">
-                <div class="egw_fw_app__name" part="name">
+                <div class=${classMap({
+                    egw_fw_app__name: true,
+                    hasHeaderContent: hasHeaderContent,
+                })} part="name">
                     ${hasLeftSlots ? html`
                     <sl-icon-button name="${this.leftCollapsed ? "chevron-double-right" : "chevron-double-left"}"
                                     label="${this.leftCollapsed ? this.egw.lang("Show left area") : this.egw?.lang("Hide left area")}"

kdots/css/kdots.css
@@ -160,8 +160,10 @@ egw-framework#egw_fw_basecontainer::part(app-list-panel) {
   gap: var(--sl-spacing-medium);
 }
 egw-framework#egw_fw_basecontainer [slot="logo"] img {
-  max-width: 220px;
+  max-width: calc(var(--left-side-width, 220px) - var(--icon-size));
   max-height: var(--icon-size);
+  min-width: var(--icon-size);
+  flex: 1 1 auto;
   display: block;
   text-align: center;
 }
@@ -614,6 +616,8 @@ egw-app::part(name) {
   display: flex;
   align-items: center;
   text-wrap: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 egw-app::part(content) {
   flex-direction: column;
@@ -971,4 +975,3 @@ html[data-darkmode="1"] .nextmatch_sortheader,
   color: #96bcd9;
 }
 /** End theme customisations **/
-/*# sourceMappingURL=kdots.css.map */

kdots/css/src/application.less
@@ -7,6 +7,8 @@ egw-app {
 	display: flex;
 	align-items: center;
 	text-wrap: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
   }
 
   &::part(content) {

kdots/css/src/framework.less
@@ -72,8 +72,10 @@ egw-framework#egw_fw_basecontainer {
   /* Content slotted inside */
 
   [slot="logo"] img {
-	max-width: 220px;
+	max-width: calc(var(--left-side-width, 220px) - var(--icon-size));
 	max-height: var(--icon-size);
+	min-width: var(--icon-size);
+	flex: 1 1 auto;
 	display: block;
 	text-align: center;
   }

kdots/js/EgwFramework.styles.ts
@@ -11,6 +11,7 @@ export default css`
         --icon-size: 32px;
         --inactive-tab-opacity: 0.5;
         --header-icon-size: 1.5rem;
+		--left-side-width: 200px;
     }
 
     .egw_fw__layout-default {
@@ -129,6 +130,21 @@ export default css`
         color: inherit;
     }
 
+	.egw_fw__header .egw_fw__logo_apps {
+		container: logo / inline-size;
+		flex: 0 1 var(--left-side-width);
+		width: var(--left-side-width);
+		display: flex;
+		overflow: hidden;
+		justify-content: space-between;
+	}
+
+	/* Hide logo when things get small (no CSS vars or calc() here) */
+	@container logo (width < 150px) {
+		slot {
+			display: none;
+		}
+	}
     .egw_fw__header .egw_fw__app_list {
         flex: none;
         font-size: var(--header-icon-size, var(--sl-font-size-2x-large));

kdots/js/EgwFramework.ts
@@ -29,6 +29,7 @@ import {state} from "lit/decorators/state.js";
  * @csspart base - Wraps it all.
  * @csspart banner -
  * @csspart header -
+ * @csspart logo
  * @csspart open-applications - Tab group that has the currently open applications
  * @csspart app-list-panel - Dropdown containing the available applications
  * @csspart tab - Individual tabs
@@ -43,6 +44,7 @@ import {state} from "lit/decorators/state.js";
  * @cssproperty [--tab-icon-size=32] - Height of application icons in header bar
  * @cssproperty [--tab-icon-size-active=40] - Height of active application icon
  * @cssproperty [--<appname>-color] - Background color of the application tab
+ * @cssproperty [--left-side-width] - Width of the icon + application list, normally synced to left side menu width
  */
 @customElement('egw-framework')
 //@ts-ignore
@@ -851,6 +853,22 @@ export class EgwFramework extends LitElement
 		this.showTab(event.target.activeTab.panel);
 	}
 
+	/**
+	 * Listen to application sidemenu (left) position & update site logo size
+	 *
+	 * @param event
+	 * @protected
+	 */
+	protected handleSlide(event)
+	{
+		if(!event.detail.width)
+		{
+			return;
+		}
+		const size = event.detail.width ?? 200;
+		this.style.setProperty('--left-side-width', size + 'px');
+	}
+
 	public setActiveApp(appname : EgwFrameworkApp | string)
 	{
 		return this.showTab(typeof appname == "string" ? appname : appname.name);
@@ -1072,6 +1090,9 @@ export class EgwFramework extends LitElement
 		const statusSnap = (parseInt(iconSize) + 6) + 'px';
 		const statusPosition = this.egw?.preference("statusPosition", "common") ?? parseInt(statusSnap) ?? "36";
 
+		// Keep app list icon aligned with sidebox of current application
+		const leftSideWidth = this.activeApp?.leftSplitter?.positionInPixels;
+
 		const classes = {
 			"egw_fw__base": true
 		}
@@ -1086,15 +1107,17 @@ export class EgwFramework extends LitElement
                 </div>` : nothing
                 }
                 <header class="egw_fw__header" part="header">
-                    <slot name="logo"></slot>
-                    <sl-dropdown class="egw_fw__app_list" role="menu" exportparts="panel:app-list-panel">
-                        <sl-icon-button slot="trigger" name="grid-3x3-gap"
-                                        label="${this.egw.lang("Application list")}"
-                                        aria-hidden="true"
-                                        aria-description="${this.egw.lang("Activate for a list of applications")}"
-                        ></sl-icon-button>
-                        ${repeat(this.applicationList, (app) => this._applicationListAppTemplate(app))}
-                    </sl-dropdown>
+                    <div class="egw_fw__logo_apps">
+                        <slot name="logo" part="logo"></slot>
+                        <sl-dropdown class="egw_fw__app_list" role="menu" exportparts="panel:app-list-panel">
+                            <sl-icon-button slot="trigger" name="grid-3x3-gap"
+                                            label="${this.egw.lang("Application list")}"
+                                            aria-hidden="true"
+                                            aria-description="${this.egw.lang("Activate for a list of applications")}"
+                            ></sl-icon-button>
+                            ${repeat(this.applicationList, (app) => this._applicationListAppTemplate(app))}
+                        </sl-dropdown>
+                    </div>
 					<div class="spacer spacer_start"></div>
                     <sl-tab-group part="open-applications" class="egw_fw__open_applications" activation="manual"
                                   aria-label="${this.egw.lang("Open applications")}"
@@ -1115,7 +1138,9 @@ export class EgwFramework extends LitElement
                                     snap="150px ${statusSnap} 0px"
                                     snap-threshold="${Math.min(40, parseInt(iconSize) - 5)}"
                                     aria-label="Side menu resize">
-                        <main slot="start" part="main" class="egw_fw__main" id="main">
+                        <main slot="start" part="main" class="egw_fw__main" id="main"
+                              @sl-reposition=${this.handleSlide}
+                        >
                             <slot></slot>
                         </main>
                         <sl-icon slot="divider" name="grip-vertical"></sl-icon>
@@ -1124,7 +1149,9 @@ export class EgwFramework extends LitElement
                         </aside>
                     </sl-split-panel>
                 </div>` : html`
-                    <main part="main" class="egw_fw__main" id="main">
+                    <main part="main" class="egw_fw__main" id="main"
+                          @sl-reposition=${this.handleSlide}
+                    >
                         <slot></slot>
                     </main>`
                 }

kdots/js/EgwFrameworkApp.styles.ts
@@ -20,8 +20,10 @@ export default css`
 		display: flex;
 	}
 
-	.egw_fw_app__name {
-		max-width: 20vw;
+	.egw_fw_app__name.hasHeaderContent {
+		max-width: var(--left-side-width, 20vw);
+		/* Keep the collapse icon visible */
+		min-width: 2em;
 		flex: 1 1 20vw;
 	}
 

commit 371dbdc1cd5db0dcfe963c8920f1937d30119f19

kdots: Fix right side splitter sizing broken by d09d10ad8212e408de92dc307100953285975466

kdots/js/EgwFrameworkApp.ts
@@ -603,7 +603,10 @@ export class EgwFrameworkApp extends LitElement
 			return;
 		}
 		// Limit to maximum of actual width, splitter handles max
-		newPosition = Math.min(newPosition, split.querySelector(panelInfo.side == "left" ? "[slot='start']" : "[slot='end']").getBoundingClientRect().width);
+		if(panelInfo.side == "left")
+		{
+			newPosition = Math.min(newPosition, split.querySelector("[slot='start']").getBoundingClientRect().width);
+		}
 
 		// Update collapsed
 		this[`${panelInfo.side}Collapsed`] = newPosition == panelInfo.hiddenWidth;