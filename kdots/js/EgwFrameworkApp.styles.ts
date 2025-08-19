EGroupware/egroupware

commit a5a4c26785aec78d05eef807df7b78e5afdbe90f

kdots: Get responsive (< 800px wide) working better
- Add preference to open in popup window or dialog
- Cleaned up some egw_app stuff that is better handled by webcomponents & using slotting

kdots/js/EgwFrameworkApp.styles.ts
@@ -14,7 +14,7 @@ export default css`
         flex-direction: column;
 
         --application-color: var(--primary-background-color);
-        --left-min: 0%;
+		--left-min: 15em;
         --left-max: 20%;
     }
 
@@ -95,6 +95,10 @@ export default css`
         --min: calc(100% - var(--right-max, 50%));
     }
 
+	.egw_fw_app__panel.egw_fw_app--panel-collapsed {
+		--min: 0px;
+	}
+
     .egw_fw_app__innerSplit.no-content {
         --min: 100%;
     }
@@ -143,7 +147,10 @@ export default css`
         align-items: stretch;
         justify-content: stretch;
         overflow: hidden;
-    }
+
+		grid-template-columns: [start left] min-content [ main] 1fr [right] min-content [end];
+		grid-template-rows: [start sub-header] fit-content(2em) [main] auto [footer] fit-content(4em) [end];
+	}
 
     .egw_fw_app__filter_drawer [slot="header-actions"] {
         /* Fixes vertical alignment of et2-button-icon buttons in header actions */
@@ -179,10 +186,6 @@ export default css`
     }
 
     @media (min-width: 600px) {
-        .egw_fw_app__main {
-            grid-template-columns: [start left] min-content [ main] 1fr [right] min-content [end];
-            grid-template-rows: [start sub-header] fit-content(2em) [main] auto [footer] fit-content(4em) [end];
-        }
 
         .egw_fw_app__aside {
             overflow-y: hidden;
@@ -195,15 +198,6 @@ export default css`
             flex-direction: column;
         }
 
-        .egw_fw_app__main_content {
-            flex-direction: column;
-            align-items: stretch;
-        }
-
-        ::slotted(*) {
-            flex: 1 1 auto;
-        }
-
         ::slotted(iframe) {
             width: 100%;
         }
@@ -212,8 +206,9 @@ export default css`
         sl-split-panel::part(divider) {
             display: none;
         }
-    }
 
+		--left-max: fit-content;
+    }
     @media print {
         .content {
             overflow-y: visible !important;

api/js/etemplate/et2_extension_nextmatch_controller.ts
@@ -760,10 +760,62 @@ export class et2_nextmatch_controller extends et2_dataview_controller implements
 		}
 		if(!this._widget) return;
 
-		// inform mobile framework about nm selections, need to update status of header objects on selection
-		if (egwIsMobile()) window.framework.nm_onselect_ctrl(this._widget, action, senders);
+		// for mobile need to update status of header objects on selection
+		if(egwIsMobile())
+		{
+			this.nm_onselect_ctrl(this._widget, action, senders);
+		}
 
 		this._widget.onselect.call(this._widget, action,senders);
+	}
+
+	/**
+	 * Function runs after nextmatch selection callback gets called by object manager,
+	 * which we can update status of header DOM objects (eg. action_header, favorite, ...)
+	 *
+	 * @param {object} _widget nextmatch widget
+	 * @param {object} _action action object
+	 * @param {object} _senders selected row(s) action object
+	 */
+	nm_onselect_ctrl(_widget, _action, _senders)
+	{
+		var senders = _senders ? _senders : null;
+
+		// Update action_header status (3dots)
+		_widget.header.action_header.toggle(typeof _widget.getSelection().ids != 'undefined' && _widget.getSelection().ids.length > 0);
+
+		// Update selection counter in nm header
+		if(_widget._type == 'nextmatch' && _widget.getSelection().ids.length > 0)
+		{
+			if(senders && senders[0]?.actionLinks)
+			{
+				var delete_action = null;
+				for(var i = 0; i < senders[0].actionLinks.length; i++)
+				{
+					if(senders[0].actionLinks[i].actionId == 'delete')
+					{
+						delete_action = senders[0].actionLinks[i];
+					}
+				}
+				if(delete_action && delete_action.enabled)
+				{
+					_widget.header.delete_action
+						.show()
+						.off()
+						.click(function()
+						{
+							if(delete_action)
+							{
+								delete_action.actionObj.execute(senders);
+							}
+						});
+				}
+			}
+		}
+		else
+		{
+			_widget.header.delete_action.hide();
+		}
 	}
 
 	/** -- Implementation of et2_IDataProvider -- **/

api/js/etemplate/etemplate2.ts
@@ -791,6 +791,9 @@ export class etemplate2
 						{
 							this.focusOnFirstInput();
 						}
+						// Move top level slotted components out so they can get slotted
+						const slottedWidgets = this._widgetContainer.querySelectorAll(":scope > [slot]") ?? []
+						slottedWidgets.forEach(node => {this._DOMContainer.parentNode.appendChild(node);});
 
 						// Now etemplate is ready for others to interact with (eg: app.js)
 						this.ready = true;

api/js/jsapi/egw_app.ts
@@ -28,6 +28,7 @@ import type {Et2Details} from "../etemplate/Layout/Et2Details/Et2Details";
 import {Et2Checkbox} from "../etemplate/Et2Checkbox/Et2Checkbox";
 import {egw_globalObjectManager} from "../egw_action/egw_action";
 import type {EgwFrameworkApp} from "../../../kdots/js/EgwFrameworkApp";
+import {Et2ButtonIcon} from "../etemplate/Et2Button/Et2ButtonIcon";
 
 /**
  * Type for push-message
@@ -731,56 +732,15 @@ export abstract class EgwApp
 			delete self.viewTemplate;
 			delete self.viewContainer;
 			delete self.et2_view;
-			// we need to reference back into parent context this
-			for(var v in self)
-			{
-				this[v] = self[v];
-			}
 			app = null;
 		};
 
-		// view container
-		this.viewContainer = jQuery(document.createElement('div'))
-			.addClass('et2_mobile_view')
-			.attr('id', 'popupMainDiv')
-			.appendTo('body');
-		this.viewContainer[0].style.setProperty('--application-color', 'var(--' + this.appname + '-color,var(--primary-color))');
+		const mobileViewTemplate = (_action.data.mobileViewTemplate || 'edit').split('?');
+		const templateName = mobileViewTemplate[0];
+		const templateTimestamp = mobileViewTemplate[1];
+		const templateURL = egw.webserverUrl + '/' + this.appname + '/templates/mobile/' + templateName + '.xet' + '?' + templateTimestamp;
 
-		// close button
-		var close = jQuery(document.createElement('et2-button-icon'))
-			.attr("image", "close")
-			.addClass('egw_fw_mobile_popup_close loaded')
-			.click(function()
-			{
-				destroy.call(app[self.appname]);
-				//disable selected actions after close
-				egw_globalObjectManager.setAllSelected(false);
-			})
-			.appendTo(this.viewContainer);
-		if(!noEdit)
-		{
-			// edit button
-			var edit = jQuery(document.createElement('et2-button-icon'))
-				.attr("image", "edit")
-				.addClass('mobile-view-editBtn')
-				.click(function()
-				{
-					egw.open(rowID, self.appname);
-				})
-				.appendTo(this.viewContainer);
-		}
-		// view template main container (content)
-		this.viewTemplate = jQuery(document.createElement('div'))
-			.attr('id', this.appname + '-view')
-			.addClass('et2_mobile-view-container popupMainDiv')
-			.appendTo(this.viewContainer);
-
-		var mobileViewTemplate = (_action.data.mobileViewTemplate || 'edit').split('?');
-		var templateName = mobileViewTemplate[0];
-		var templateTimestamp = mobileViewTemplate[1];
-		var templateURL = egw.webserverUrl + '/' + this.appname + '/templates/mobile/' + templateName + '.xet' + '?' + templateTimestamp;
-
-		var data = {
+		const data = {
 			'content': content,
 			'readonlys': {'__ALL__': true, 'link_to': false},
 			'currentapp': this.appname,
@@ -790,17 +750,52 @@ export abstract class EgwApp
 			'validation_errors': this.et2.getArrayMgr('validation_errors').data
 		};
 
-		// etemplate2 object for view
-		this.et2_view = new etemplate2(this.viewTemplate[0], '');
-		framework.pushState('view');
-		if(templateName)
-		{
-			this.et2_view.load(this.appname + '.' + templateName, templateURL, data, typeof et2_callback == 'function' ? et2_callback : function() {}, app);
-		}
+		// view container
+		const viewContainer = <Et2Dialog>loadWebComponent('et2-dialog', {
+			class: "et2_mobile_view egw-popup",
+			id: "popupMainDiv",
+			destroyonclose: true,
+			template: templateURL,
+			value: data
+		}, this.et2);
 
-		// define a global close function for view template
-		// in order to be able to destroy view on action
-		this.et2_view.close = destroy;
+		// We probably already have the title for the entry, but we'll get it from the server if not
+		this.egw.link_title(this.appname, rowID, true).then(title =>
+		{
+			viewContainer.prepend(Object.assign(document.createElement('span'), {
+				innerText: title,
+				slot: "label"
+			}));
+		});
+		viewContainer.append(loadWebComponent('et2-number', {
+			slot: "label", readonly: true, class: "entry_id", precision: "0", align: "right", value: rowID
+		}, this.et2));
+		viewContainer.style.setProperty('--application-color', 'var(--' + this.appname + '-color,var(--primary-color))');
+		(framework?.activeApp ? framework.activeApp : document.body).append(viewContainer);
+		this.viewContainer = jQuery(viewContainer);
+
+		// close
+		viewContainer.getComplete().then(([button, value]) =>
+		{
+			destroy.call(app[self.appname]);
+			//disable selected actions after close
+			egw_globalObjectManager.setAllSelected(false);
+		});
+		if(!noEdit)
+		{
+			// edit button
+			const edit = <Et2ButtonIcon>loadWebComponent('et2-button-icon', {
+				image: "edit",
+				class: "mobile-view-editBtn",
+				noSubmit: true,
+				slot: "header-actions"
+			}, this.et2);
+			edit.addEventListener("click", () =>
+			{
+				egw.open(rowID, self.appname);
+			});
+			viewContainer.append(edit);
+		}
 	}
 
 	/**

infolog/templates/mobile/edit.xet
@@ -129,22 +129,20 @@
 			</rows>
 		</grid>
 	</template>
-	<template id="infolog.edit" template="" lang="" group="0" version="1.9.101">
+    <template id="infolog.edit" template="" lang="" group="0" version="1.9.101">
+        <et2-hbox disabled="@hidebuttons" slot="label">
+            <et2-button-icon statustext="Apply the changes" id="button[apply]" image="apply"></et2-button-icon>
+            <et2-button-icon statustext="delete this entry" id="button[delete]" onclick="if($cont[info_anz_subs]) return $cont[info_anz_subs]; Et2Dialog.confirm(widget,'Delete this entry','Delete');" image="delete" span="all"></et2-button-icon>
+            <et2-button-timestamp statustext="Insert timestamp into description field" id="timestamp" class="infologExtraButton infologTimestamp" image="timesheet" target="info_des"></et2-button-timestamp>
+            <et2-button-toggle label="notifications" id="no_notifications" class="notification_check" onIcon="bell-fill" offIcon="bell-slash"/>
+            <et2-number id="info_number" readonly="true" class="entry_id" precision="0"></et2-number>
+        </et2-hbox>
 		<grid border="1" width="100%">
 			<columns>
 				<column width = "120"/>
 				<column/>
 			</columns>
 			<rows>
-				<row disabled="@hidebuttons" class="dialogHeadbar" >
-					<et2-hbox  span="all" width="100%">
-						<et2-button-icon  statustext="Apply the changes" id="button[apply]" image="apply"></et2-button-icon>
-                        <et2-button-icon statustext="delete this entry" id="button[delete]" onclick="if($cont[info_anz_subs]) return $cont[info_anz_subs]; Et2Dialog.confirm(widget,'Delete this entry','Delete');" image="delete" span="all"></et2-button-icon>
-						<et2-button-timestamp  statustext="Insert timestamp into description field" id="timestamp" class="infologExtraButton infologTimestamp" image="timesheet" target="info_des"></et2-button-timestamp>
-                        <et2-button-toggle label="notifications" id="no_notifications" class="notification_check" onIcon="bell-fill" offIcon="bell-slash"/>
-						<et2-number  id="info_number" readonly="true" class="entry_id" precision="0"></et2-number>
-					</et2-hbox>
-				</row>
 				<row disabled="!@showsearchbuttons" class="dialogHeadbar">
 					<et2-hbox  span="all" width="100%">
 						<et2-button  label="Search" id="button[search]" image="search"></et2-button>

infolog/templates/mobile/view.xet
@@ -136,11 +136,6 @@
 				<column/>
 			</columns>
 			<rows>
-				<row class="dialogHeadbar" >
-					<et2-box>
-						<et2-number  id="info_number" readonly="true" class="entry_id" precision="0"></et2-number>
-					</et2-box>
-				</row>
 				<row>
 					<et2-description  value="Title" for="info_subject"></et2-description>
 					<et2-textbox  statustext="a short subject for the entry" id="info_subject" class="et2_required" maxlength="255" span="5" tabindex="1"></et2-textbox>

kdots/css/kdots.css
@@ -1083,14 +1083,8 @@ body #popupMainDiv.et2_mobile_view .dialogHeadbar .entry_id {
     padding: 0;
     display: flex;
     align-items: center;
-  }
-  body et2-dialog::part(title) et2-hbox,
-  body et2-dialog::part(title) et2-box {
-    flex: 1 1 100%;
-  }
-  body et2-dialog::part(title) et2-hbox ::part(base),
-  body et2-dialog::part(title) et2-box ::part(base) {
-    align-items: center;
+    /* Needs something so flex will work right */
+    min-width: 5em;
   }
   body et2-dialog::part(header-actions) {
     flex-direction: row-reverse;
@@ -1110,8 +1104,20 @@ body #popupMainDiv.et2_mobile_view .dialogHeadbar .entry_id {
     /* Allow buttons to wrap */
     flex-wrap: wrap;
   }
+  body et2-dialog et2-hbox[slot="label"],
+  body et2-dialog et2-box[slot="label"] {
+    flex: 1 1 100%;
+  }
+  body et2-dialog et2-hbox[slot="label"]::part(base),
+  body et2-dialog et2-box[slot="label"]::part(base) {
+    align-items: center;
+  }
   body et2-dialog span[slot="label"] {
     flex-shrink: 1;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding-inline-end: var(--sl-spacing-small);
   }
   body et2-dialog et2-number[slot="label"] {
     display: inline-flex;

kdots/css/src/mobile.css
@@ -85,14 +85,8 @@ body #popupMainDiv.et2_mobile_view .dialogHeadbar .entry_id {
     padding: 0;
     display: flex;
     align-items: center;
-  }
-  body et2-dialog::part(title) et2-hbox,
-  body et2-dialog::part(title) et2-box {
-    flex: 1 1 100%;
-  }
-  body et2-dialog::part(title) et2-hbox ::part(base),
-  body et2-dialog::part(title) et2-box ::part(base) {
-    align-items: center;
+    /* Needs something so flex will work right */
+    min-width: 5em;
   }
   body et2-dialog::part(header-actions) {
     flex-direction: row-reverse;
@@ -112,8 +106,20 @@ body #popupMainDiv.et2_mobile_view .dialogHeadbar .entry_id {
     /* Allow buttons to wrap */
     flex-wrap: wrap;
   }
+  body et2-dialog et2-hbox[slot="label"],
+  body et2-dialog et2-box[slot="label"] {
+    flex: 1 1 100%;
+  }
+  body et2-dialog et2-hbox[slot="label"]::part(base),
+  body et2-dialog et2-box[slot="label"]::part(base) {
+    align-items: center;
+  }
   body et2-dialog span[slot="label"] {
     flex-shrink: 1;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding-inline-end: var(--sl-spacing-small);
   }
   body et2-dialog et2-number[slot="label"] {
     display: inline-flex;

commit 62d84c8d685629a1b5fc6cdc0ce68638f6b569bd

kdots: Fix template height broken by a5a4c26785aec78d05eef807df7b78e5afdbe90f

kdots/js/EgwFrameworkApp.styles.ts
@@ -198,11 +198,18 @@ export default css`
             flex-direction: column;
         }
 
+		::slotted(*) {
+			flex: 1 1 auto;
+		}
+
         ::slotted(iframe) {
             width: 100%;
         }
     }
     @media (max-width: 799px) {
+		.egw_fw_app--no_mobile {
+			display: none;
+		}
         sl-split-panel::part(divider) {
             display: none;
         }