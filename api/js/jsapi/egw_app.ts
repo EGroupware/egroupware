EGroupware/egroupware

commit 954f26fc13c004789b9ecde1973317b312e7f0e1

move code to keep filters in app-toolbar in sync into egw_app.ts: - filters must have the id of a column-filter, or "filter", "filter2", "cat_id" or "search" - for toggle-buttons (instead of filters) you can extend checkNmFilterChanged(app_toolbar, id, value) to keep them update (you still need to call this.nm.applyFilters() onchange!) --> see developer app for an example

api/js/jsapi/egw_app.ts
@@ -12,7 +12,7 @@
 
 import {etemplate2} from "../etemplate/etemplate2";
 import type {Et2Template} from "../etemplate/Et2Template/Et2Template";
-import {et2_nextmatch} from "../etemplate/et2_extension_nextmatch";
+import type {et2_nextmatch} from "../etemplate/et2_extension_nextmatch";
 import {et2_createWidget} from "../etemplate/et2_core_widget";
 import type {IegwAppLocal} from "./egw_global";
 import Sortable from 'sortablejs/modular/sortable.complete.esm.js';
@@ -29,6 +29,8 @@ import {Et2Checkbox} from "../etemplate/Et2Checkbox/Et2Checkbox";
 import {egw_globalObjectManager} from "../egw_action/egw_action";
 import type {EgwFrameworkApp} from "../../../kdots/js/EgwFrameworkApp";
 import {Et2ButtonIcon} from "../etemplate/Et2Button/Et2ButtonIcon";
+import type {Et2Widget} from "../etemplate/Et2Widget/Et2Widget";
+import type {Et2Select} from "../etemplate/Et2Select/Et2Select";
 
 /**
  * Type for push-message
@@ -175,6 +177,11 @@ export abstract class EgwApp
 	protected push_filter_fields : string[];
 
 	/**
+	 * reference to nextmatch with id="nm"
+	 */
+	nm : et2_nextmatch = null;
+
+	/**
 	 * Initialization and setup goes here, but the etemplate2 object
 	 * is not yet ready.
 	 */
@@ -182,6 +189,8 @@ export abstract class EgwApp
 	{
 		this.appname = appname;
 		this.egw = egw(this.appname, _wnd || window);
+		this.nm = null;
+		this.nmFilterChange = this.nmFilterChange.bind(this);
 
 		// Initialize sidebox for non-popups.
 		// ID set server side
@@ -250,6 +259,12 @@ export abstract class EgwApp
 		{
 			EgwApp._instances.splice(index, 1);
 		}
+
+		if (this.nm && this.nm.getDOMNode())
+		{
+			this.nm.getDOMNode().removeEventListener('et2-filter', this.nmFilterChange);
+		}
+		this.nm = null;
 	}
 
 	/**
@@ -290,8 +305,100 @@ export abstract class EgwApp
 			}
 		}
 
+		// if we have a NM widget: make it available as this.nm and install event-listener for this.nmFilterChange
+		if (!this.nm && (this.nm = this.et2.getWidgetById('nm')))
+		{
+			this.nm = this.et2.getWidgetById('nm');
+			this.nm.getDOMNode().addEventListener('et2-filter', this.nmFilterChange);
+			// update values in toolbar
+			window.setTimeout(() =>
+			{
+				this.nmFilterChange({detail: { activeFilters: this.nm.activeFilters}});
+			});
+		}
+
 		// Highlights the favorite based on initial list state
 		this.highlight_favorite();
+	}
+
+	/**
+	 * Keep filters in the application toolbar in sync with NM / callback bound on et2-filter event of NM
+	 *
+	 * @param _ev : Event
+	 */
+	nmFilterChange(_ev : Event)
+	{
+		const app_toolbar = document.querySelector('egw-app#'+this.appname+' [slot="main-header"]');
+		const activeFilters = _ev.detail?.activeFilters;
+		if (app_toolbar && activeFilters)
+		{
+			for(const attr in activeFilters)
+			{
+				switch (attr)
+				{
+					case 'col_filter':
+						for(const attr in activeFilters.col_filter)
+						{
+							let widget = app_toolbar.getWidgetById(attr);
+							if (widget && widget.value != activeFilters.col_filter[attr])
+							{
+								widget.value = activeFilters.col_filter[attr];
+							}
+						}
+						break;
+					case 'filter':
+					case 'filter2':
+					case 'cat_id':
+					case 'search':
+						this.checkNmFilterChanged(app_toolbar, attr, activeFilters[attr]);
+						break;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Check if any NM filter or search in app-toolbar needs to be updated to reflect NM internal state
+	 *
+	 * @param app_toolbar
+	 * @param id
+	 * @param value
+	 */
+	checkNmFilterChanged(app_toolbar, id : string, value : string)
+	{
+		let widget = app_toolbar.getWidgetById(id);
+		if (widget && widget.value != value)
+		{
+			widget.value = value;
+		}
+	}
+
+	/**
+	 * Propagate filters in app_toolbar to NM and filter thingy
+	 *
+	 * Use as onchange on these filters (named like the ones in NM!)
+	 *
+	 * @param _ev
+	 * @param _widget
+	 */
+	changeNmFilter(_ev : Event, _widget : Et2Select|Et2Widget)
+	{
+		if (!this.nm || !_widget.id) return;
+
+		const filters = {};
+		switch(_widget.id)
+		{
+			case 'filter':
+			case 'filter2':
+			case 'cat_id':
+			case 'search':
+				filters[_widget.id] = _widget.value;
+				break;
+			default:
+				filters.col_filter = {};
+				filters.col_filter[_widget.id] = _widget.value;
+		}
+		this.nm && this.nm.applyFilters(filters);
 	}
 
 	/**

admin/js/app.ts
@@ -44,13 +44,6 @@ class AdminApp extends EgwApp
 	iframe : any = null;
 
 	/**
-	 * reference to nextmatch
-	 *
-	 * {et2_extension_nextmatch}
-	 */
-	nm : any = null;
-
-	/**
 	 * Reference to div to hold AJAX loadable pages
 	 *
 	 * {et2_box}
@@ -81,7 +74,6 @@ class AdminApp extends EgwApp
 	destroy(_app)
 	{
 		this.iframe = null;
-		this.nm = null;
 		this.acl_dialog = null;
 		this.tree?.destroy && this.tree.destroy();
 		this.tree?.remove && this.tree.remove();
@@ -108,7 +100,6 @@ class AdminApp extends EgwApp
 		{
 			case 'admin.index':
 				var iframe = this.iframe = this.et2.getWidgetById('iframe');
-				this.nm = this.et2.getWidgetById('nm');
 				this.groups = this.et2.getWidgetById('groups');
 				this.groups.set_disabled(true);
 				this.ajax_target = this.et2.getWidgetById('ajax_target');

commit 1772a6e33136690f14657ff391874f3e2ce5b179

Fix ReferenceError: et2_nextmatch is not defined from egw.refresh()
Broken by 954f26f

api/js/jsapi/egw_app.ts
@@ -12,7 +12,7 @@
 
 import {etemplate2} from "../etemplate/etemplate2";
 import type {Et2Template} from "../etemplate/Et2Template/Et2Template";
-import type {et2_nextmatch} from "../etemplate/et2_extension_nextmatch";
+import {et2_nextmatch} from "../etemplate/et2_extension_nextmatch";
 import {et2_createWidget} from "../etemplate/et2_core_widget";
 import type {IegwAppLocal} from "./egw_global";
 import Sortable from 'sortablejs/modular/sortable.complete.esm.js';
@@ -20,6 +20,7 @@ import {et2_valueWidget} from "../etemplate/et2_core_valueWidget";
 import {fetchAll, nm_action} from "../etemplate/et2_extension_nextmatch_actions";
 import {Et2Dialog} from "../etemplate/Et2Dialog/Et2Dialog";
 import {Et2Favorites} from "../etemplate/Et2Favorites/Et2Favorites";
+import type {Et2Widget} from "../etemplate/Et2Widget/Et2Widget";
 import {loadWebComponent} from "../etemplate/Et2Widget/Et2Widget";
 import type {EgwAction} from "../egw_action/EgwAction";
 import {Et2MergeDialog} from "../etemplate/Et2Dialog/Et2MergeDialog";
@@ -29,7 +30,6 @@ import {Et2Checkbox} from "../etemplate/Et2Checkbox/Et2Checkbox";
 import {egw_globalObjectManager} from "../egw_action/egw_action";
 import type {EgwFrameworkApp} from "../../../kdots/js/EgwFrameworkApp";
 import {Et2ButtonIcon} from "../etemplate/Et2Button/Et2ButtonIcon";
-import type {Et2Widget} from "../etemplate/Et2Widget/Et2Widget";
 import type {Et2Select} from "../etemplate/Et2Select/Et2Select";
 
 /**