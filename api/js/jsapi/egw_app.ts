9e5a7895f5f9465bcf387112e5ec16883575fe8b
ralf <rb@egroupware.org>
2025-09-09 14:18:48 +0200

    fix how egw-app object is found / searched to work for apps like CRM-view or call-list having NOT their app-name as id

--- api/js/jsapi/egw_app.ts
+++ api/js/jsapi/egw_app.ts
@@ -328,7 +328,7 @@ export abstract class EgwApp
 	 */
 	nmFilterChange(_ev : Event)
 	{
-		let app_toolbar = document.querySelector('egw-app#' + this.appname + ' [slot="main-header"]');
+		let app_toolbar = this.et2.closest('egw-app').querySelector('[slot="main-header"]');
 		if(app_toolbar && app_toolbar.localName != "et2-template")
 		{
 			app_toolbar = app_toolbar?.querySelector("et2-template");
