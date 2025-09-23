850bbfbc4649ffb8d048a1a6407fee78ed3a86ad
nathan <nathangray.bsc+github@gmail.com>
2025-09-23 10:20:35 -0600

    Make dates on mobile look more like other inputs

--- api/js/etemplate/Et2Date/DateStyles.ts
+++ api/js/etemplate/Et2Date/DateStyles.ts
@@ -9,28 +9,45 @@ import {cssImage} from "../Et2Widget/Et2Widget";
 export const dateStyles = [
 	colorsDefStyles,
 	css`
-	:host {
-		display: block;
-		white-space: nowrap;
-		min-width: fit-content;
-		background-color: transparent;
-	}
-	.overdue {
-		color: red; // var(--whatever the theme color)
-	}
-	input.flatpickr {
-		border: 1px solid;
-		border-color: var(--input-border-color);
-		color: var(--input-text-color);
-		padding-top: 4px;
-		padding-bottom: 4px;
-		flex: 1 1 auto;
-	}
-	input.flatpickr:hover {
-		background-image: ${cssImage("datepopup")};
-		background-repeat: no-repeat;
-		background-position-x: right;
-		background-position-y: 1px;
-		background-size: 18px;
-	}
+		:host {
+			display: block;
+			white-space: nowrap;
+			min-width: fit-content;
+			background-color: transparent;
+		}
+
+		/* Style input directly for mobile */
+
+		.form-control-input input[type=date]:only-child {
+			font-size: var(--sl-input-font-size-medium);
+			line-height: var(--sl-input-height-medium);
+			border: var(--sl-input-border-width) solid var(--sl-input-border-color);
+			border-radius: var(--sl-input-border-radius-medium);
+			padding: 0 var(--sl-input-spacing-medium);
+		}
+
+		.overdue {
+			color: red; // var(--whatever the theme color)
+		}
+
+		input[type="date"] {
+			padding: 0 var(--sl-input-spacing-medium);
+		}
+
+		input.flatpickr {
+			border: 1px solid;
+			border-color: var(--input-border-color);
+			color: var(--input-text-color);
+			padding-top: 4px;
+			padding-bottom: 4px;
+			flex: 1 1 auto;
+		}
+
+		input.flatpickr:hover {
+			background-image: ${cssImage("datepopup")};
+			background-repeat: no-repeat;
+			background-position-x: right;
+			background-position-y: 1px;
+			background-size: 18px;
+		}
 `];
