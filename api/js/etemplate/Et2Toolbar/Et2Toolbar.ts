EGroupware/egroupware

commit deb76595d7bf283b2dffddce3a2615e41bff118a

Et2Toolbar: Fix elements in the prefix slot affected the sizing calculation, unnecessarily hiding elements

api/js/etemplate/Et2Toolbar/Et2Toolbar.ts
@@ -524,6 +524,7 @@ export class Et2Toolbar extends Et2InputWidget(Et2Box)
 			}
 		});
 
+		// Sort & check sizing
 		elements = Array.from(this.querySelectorAll(':scope > *:not([slot="prefix"]):not([slot="suffix"])'));
 		elements.sort((a : HTMLElement, b : HTMLElement) => parseInt(b.dataset.order) - parseInt(a.dataset.order));
 		elements.forEach((el : HTMLElement) =>
@@ -554,12 +555,13 @@ export class Et2Toolbar extends Et2InputWidget(Et2Box)
 	 */
 	protected _organiseChild(child : HTMLElement)
 	{
-		if(!this.shadowRoot.querySelector(".toolbar-buttons"))
+		const buttonDiv = <HTMLElement>this.shadowRoot.querySelector(".toolbar-buttons");
+		if(!buttonDiv)
 		{
 			// Not ready yet
 			return;
 		}
-		const isOverflowed = (child.offsetWidth + child.offsetLeft) > (<HTMLElement>this.shadowRoot.querySelector(".toolbar-buttons")).offsetWidth;
+		const isOverflowed = (child.offsetWidth + child.offsetLeft - buttonDiv.offsetLeft) > buttonDiv.offsetWidth;
 		if(isOverflowed || this._preference[child.id])
 		{
 			this._isOverflowed = this._isOverflowed || isOverflowed;
@@ -702,7 +704,7 @@ export class Et2Toolbar extends Et2InputWidget(Et2Box)
 	protected settingsOptions()
 	{
 		const options = [];
-		this.querySelectorAll("[id]:not([slot=\"prefix\"]):not([slot=\"suffix\"])").forEach((child : HTMLElement) =>
+		this.querySelectorAll(":scope > [id]:not([slot=\"prefix\"]):not([slot=\"suffix\"])").forEach((child : HTMLElement) =>
 		{
 			const option : SelectOption = {
 				value: child.id,

commit b881f3635cbdb31169a83ef14b36f1e5c94364a0

Et2Toolbar: Fix widget / group sorting
Broken by deb76595d7bf283b2dffddce3a2615e41bff118a

api/js/etemplate/Et2Toolbar/Et2Toolbar.ts
@@ -238,7 +238,8 @@ export class Et2Toolbar extends Et2InputWidget(Et2Box)
 				{
 					last_group = document.createElement("sl-button-group");
 					last_group.dataset.group = action.group;
-					last_group.dataset.order = domChildCount + action.group;
+					const order = !action.group || isNaN(action.group) ? last_group_id + 1 : action.group;
+					last_group.dataset.order = domChildCount + order;
 					this.append(last_group);
 				}
 
@@ -525,17 +526,26 @@ export class Et2Toolbar extends Et2InputWidget(Et2Box)
 		});
 
 		// Sort & check sizing
+		const orderSort = (a : HTMLElement, b : HTMLElement) => parseInt(a.dataset.order) - parseInt(b.dataset.order);
 		elements = Array.from(this.querySelectorAll(':scope > *:not([slot="prefix"]):not([slot="suffix"])'));
-		elements.sort((a : HTMLElement, b : HTMLElement) => parseInt(b.dataset.order) - parseInt(a.dataset.order));
+		elements.sort(orderSort);
 		elements.forEach((el : HTMLElement) =>
 		{
 			if(typeof el.dataset.group !== "undefined")
 			{
-				Array.from(el.childNodes).reverse().forEach((c : HTMLElement) => this._organiseChild(c));
+				Array.from(el.childNodes).sort(orderSort).forEach((c : HTMLElement) =>
+				{
+					this._organiseChild(c)
+					el.appendChild(c);
+				});
 			}
 			else
 			{
 				this._organiseChild(el);
+			}
+			if(el.parentNode == this)
+			{
+				this.appendChild(el);
 			}
 		});
 