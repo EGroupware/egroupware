0fb861ec20b080582140f42b95c9af80e73982e2
ralf <rb@egroupware.org>
2026-01-12 16:40:06 +0100

    avoid white-space:pre-wrap, if dialog has a template

--- api/js/etemplate/Et2Dialog/Et2Dialog.ts
+++ api/js/etemplate/Et2Dialog/Et2Dialog.ts
@@ -175,8 +175,10 @@ export class Et2Dialog extends Et2Widget(SlDialog)
 
 				::slotted(.dialog_content) {
 					height: var(--height, 100%);
-					white-space: pre-wrap;
 				}
+                ::slotted(.dialog_content:not(.dialog--has_template)) {
+                    white-space: pre-wrap;
+                }
 
 			  /* Non-modal dialogs don't have an overlay */
 
