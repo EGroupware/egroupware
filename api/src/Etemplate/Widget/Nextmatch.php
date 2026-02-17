EGroupware/egroupware

commit a821bf0f40999ffddb27ad527afd99de287027ad

deferring get_rows call and fixing favorites not displaying currently active one: - get_rows is no longer called in initial request, but initiated via ajax_get_rows from client-side to speed up initial display (without rows) - apps can set num_rows to any number >0 to opt out of this new behavior, which will then be used to query the  given number of rows - only set implizit sorting preference, on the initial request (determined by num_rows NOT being set!) - app.ts method fixState() to fix previous filter-values on a per-app basis - fixState() implemented in tracker to cope with previous value of "0", which uses now "" - added extra noFilter (name="blank") check, which succeeds if all filters have an empty (or undefined) value --> setting a favorite and reloading now keeps the set favorite highlighted (at least in Tracker and InfoLog so far)

api/src/Etemplate/Widget/Nextmatch.php
@@ -121,27 +121,28 @@ class Nextmatch extends Etemplate\Widget
 		$form_name = self::form_name($cname, $this->id, $expand);
 		$value = self::get_array(self::$request->content, $form_name, true);
 
-		$value['start'] = 0;
-		if(!array_key_exists('num_rows', $value))
-		{
-			$value['num_rows'] = self::INITIAL_ROWS;
-		}
-
-		$value['rows'] = array();
-
-		$send_value = $value;
-
 		list($app) = explode('.', $value['get_rows']);
 		if(empty($GLOBALS['egw_info']['apps'][$app]))
 		{
 			list($app) = explode('.', $this->attrs['template']);
 		}
-
 		// Check for sort preference.  We only apply this on first load, so it can be changed
-		if(array_key_exists($this->attrs['template'] . "_sort", $GLOBALS['egw_info']['user']['preferences'][$app] ?? []))
+		// First load is detected on a NOT set num_rows!
+		if((empty($send_value['sort']) || !isset($send_value['num_rows'])) &&
+			array_key_exists($this->attrs['template'] . "_sort", $GLOBALS['egw_info']['user']['preferences'][$app] ?? []))
 		{
 			$send_value['sort'] = $GLOBALS['egw_info']['user']['preferences'][$app][$this->attrs['template'] . "_sort"];
 		}
+
+		$value['start'] = 0;
+		if(!array_key_exists('num_rows', $value))
+		{
+			$value['num_rows'] = 0; //self::INITIAL_ROWS;
+		}
+
+		$value['rows'] = array();
+
+		$send_value = $value;
 
 		// Check for a favorite in URL
 		if(!empty($_GET['favorite']) && !empty($value['favorites']))

api/js/etemplate/Et2Favorites/Et2FavoritesMenu.ts
@@ -9,6 +9,7 @@ import {createRef, Ref, ref} from "lit/directives/ref.js";
 import Sortable from "sortablejs/modular/sortable.complete.esm.js";
 import {SlMenu} from "@shoelace-style/shoelace";
 import {state} from "lit/decorators/state.js";
+import {app} from "../../jsapi/egw_global";
 
 /**
  * @summary A menu listing a user's favorites.  Populated from the user's preferences.
@@ -174,13 +175,9 @@ export class Et2FavoritesMenu extends Et2Widget(LitElement)
 
 		Object.entries(this.favorites).forEach(([name, favorite]) =>
 		{
-			// Handle old style by making it like current style
-			// @ts-ignore
-			if(favorite.filter && !favorite.state)
-			{
-				//@ts-ignore
-				favorite.state = favorite.filter;
-			}
+			const app_object = app[this.application];
+			if (app_object) favorite = app_object.fixState(favorite);
+
 			let match_count = 0;
 			let extra_keys = Object.keys(favorite.state);
 
@@ -257,6 +254,14 @@ export class Et2FavoritesMenu extends Et2Widget(LitElement)
 				{
 					return;
 				}
+			}
+			// match noFilter, if none is set to a non-empty value
+			if (name === 'blank')
+			{
+				match_count = !currentState.filter && !currentState.filter2 && !currentState.cat_id && !currentState.search ? 9 : 0;
+				Object.entries(currentState.col_filter).forEach(([name, value]) => {
+					if (value) match_count=0;
+				});
 			}
 			// Better match?  Hold on to it and keep looking
 			if(match_count > best_count)

api/js/jsapi/egw_app.ts
@@ -684,6 +684,29 @@ export abstract class EgwApp
 	}
 
 	/**
+	 * Fix old favorite or state, can be overridden by apps
+	 *
+	 * Should always be used, before comparing or setting a stored favorite
+	 *
+	 * @param {{name: string, state: object}|string} state Object (or JSON string) for a state.
+	 *	Only state is required, and its contents are application specific.
+	 * @return {{name: string, state: object}} state Object (or JSON string) for a state.
+	 */
+	fixState(state): { name: string, state: object, group: number|false }
+	{
+		if(typeof state == "string" && (state.indexOf('{') != -1 || state === 'null'))
+		{
+			state = JSON.parse(state);
+		}
+		// Handle old style by making it like current style
+		if(state.filter && !state.state)
+		{
+			state.state = state.filter;
+		}
+		return state;
+	}
+
+	/**
 	 * Set the application's state to the given state.
 	 *
 	 * While not pretending to implement the history API, it is patterned similarly
@@ -699,14 +722,8 @@ export abstract class EgwApp
 	 */
 	setState(state, template? : string) : string | false | void
 	{
-		// State should be an object, not a string, but we'll parse
-		if(typeof state == "string")
-		{
-			if(state.indexOf('{') != -1 || state == 'null')
-			{
-				state = JSON.parse(state);
-			}
-		}
+		state = this.fixState(state);
+
 		if(typeof state != "object")
 		{
 			egw.debug('error', 'Unable to set state to %o, needs to be an object', state);

commit 71ad5e643e8d32102d4061f21908e0c410dfb8b4

* Api: Fix implicit sort preference was no longer applied
Broken by a821bf0f40999ffddb27ad527afd99de287027ad

api/src/Etemplate/Widget/Nextmatch.php
@@ -129,10 +129,10 @@ class Nextmatch extends Etemplate\Widget
 		}
 		// Check for sort preference.  We only apply this on first load, so it can be changed
 		// First load is detected on a NOT set num_rows!
-		if((empty($send_value['sort']) || !isset($send_value['num_rows'])) &&
+		if(!isset($value['num_rows']) &&
 			array_key_exists($this->attrs['template'] . "_sort", $GLOBALS['egw_info']['user']['preferences'][$app] ?? []))
 		{
-			$send_value['sort'] = $GLOBALS['egw_info']['user']['preferences'][$app][$this->attrs['template'] . "_sort"];
+			$value['sort'] = $GLOBALS['egw_info']['user']['preferences'][$app][$this->attrs['template'] . "_sort"];
 		}
 
 		$value['start'] = 0;