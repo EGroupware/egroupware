c70dfd466d7e703d596ac34b63c78e9ff5152905
ralf <rb@egroupware.org>
2026-02-24 14:29:58 +0000

    Changelog for 26.2.20260224

--- api/setup/setup.inc.php
+++ api/setup/setup.inc.php
@@ -14,7 +14,7 @@ $setup_info['api']['title']     = 'EGroupware API';
 $setup_info['api']['version'] = '26.1';
 $setup_info['api']['versions']['current_header'] = '1.29';
 // maintenance release in sync with changelog in doc/rpm-build/debian.changes
-$setup_info['api']['versions']['maintenance_release'] = '26.2.20260216';
+$setup_info['api']['versions']['maintenance_release'] = '26.2.20260224';
 $setup_info['api']['enable']    = 3;
 $setup_info['api']['app_order'] = 1;
 $setup_info['api']['license'] = 'GPL';
