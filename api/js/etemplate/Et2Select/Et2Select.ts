76cf25d5f450596127d931cedc165d436bbd5af9
milan <mk@egroupware.org>
2026-02-25 15:50:06 +0100

    Et2Select: add padding to ::part(tags) so we do not get a scrollbar only scrolls 2 px

--- api/js/etemplate/Et2Select/Et2Select.ts
+++ api/js/etemplate/Et2Select/Et2Select.ts
@@ -138,6 +138,7 @@ export class Et2Select extends Et2WithSearchMixin(Et2WidgetWithSelect)
 
 				::part(tags) {
 					max-width: 100%;
+					padding: var(--sl-spacing-2x-small); 
 				}
 
 				.select__label {
