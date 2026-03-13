eedebe8f3a1532a5b85d372a3bd04a7e77d28c94
nathan <nathangray.bsc+github@gmail.com>
2026-03-13 11:03:47 -0600

    * Admin: Fix categories Add button & placeholder ("No matches found") context menu add
    - Also removed old actions column from the category list

--- admin/js/app.ts
+++ admin/js/app.ts
@@ -86,7 +86,7 @@ class AdminApp extends EgwApp
 		this.tree?.remove && this.tree.remove();
 		this.tree = null;
 
-		this.nm2?.getDOMNode()?.removeEventListner('et2-filter', this.nmFilterChange);
+		this.nm2?.getDOMNode()?.removeEventListener('et2-filter', this.nmFilterChange);
 
 		// call parent
 		super.destroy(_app);
@@ -248,7 +248,10 @@ class AdminApp extends EgwApp
 		{
 			this.egw.app_header('');
 			// blank iframe, to not keep something running there
-			this.iframe.getDOMNode().contentDocument.location.href = 'about:blank';
+			if(this.iframe && this.iframe.getDOMNode())
+			{
+				this.iframe.getDOMNode().contentDocument.location.href = 'about:blank';
+			}
 		}
 		this.iframe.set_disabled(!_url || ajax);
 		this.nm.set_disabled(!!_url || ajax);
