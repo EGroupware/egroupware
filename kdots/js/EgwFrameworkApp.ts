ac92399cf6d5b7ab32298a98232136f063a6533e
nathan <nathangray.bsc+github@gmail.com>
2026-04-13 15:57:15 -0600

    KDots: Prevent bad panel calculation from putting left side content on the right

--- kdots/js/EgwFrameworkApp.ts
+++ kdots/js/EgwFrameworkApp.ts
@@ -1069,11 +1069,11 @@ export class EgwFrameworkApp extends LitElement
 				{
 					if(splitter && panelInfo.side == "right")
 					{
-						splitter.position = this[collapsed] ? panelInfo.hiddenWidth : parseInt(<string>panelInfo.preferenceWidth);
+						splitter.position = this[collapsed] ? panelInfo.hiddenWidth : parseInt(<string>panelInfo.preferenceWidth) ?? panelInfo.defaultWidth;
 					}
 					else if(splitter && panelInfo.side == "left")
 					{
-						splitter.positionInPixels = this[collapsed] ? panelInfo.hiddenWidth : parseInt(<string>panelInfo.preferenceWidth);
+						splitter.positionInPixels = this[collapsed] ? panelInfo.hiddenWidth : parseInt(<string>panelInfo.preferenceWidth) ?? panelInfo.defaultWidth;
 					}
 					this.ignoreSplitterResize = false;
 				}, 0);
