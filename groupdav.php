49b2be4274b8a763664cbca90a69298459f8e736
ralf <rb@egroupware.org>
2026-05-15 14:19:04 +0200

    for unauthenticated requests to CalDAV/CardDAV/REST endpoint return 401 Unauthorized directly / without loading EGroupware

--- groupdav.php
+++ groupdav.php
@@ -20,6 +20,19 @@
 
 use EGroupware\Api;
 
+// check we either have a session cookie, or an Authorization header, otherwise directly return 401 Unauthorized
+if (!preg_match('#/groupdav.php/openapi.json($|\?)#', $_SERVER['REQUEST_URI']) &&
+	empty($_COOKIE['sessionid']) && empty($_SERVER['PHP_AUTH_DIGEST']))
+{
+	error_log($_SERVER['REQUEST_METHOD'].' '.(empty($_SERVER['HTTPS']) ? 'http://' : 'https://').$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].
+		(isset($_SERVER['HTTP_AUTHORIZATION']) ? ': Authorization: '.$_SERVER['HTTP_AUTHORIZATION'] : ': sessionid='.($_COOKIE['sessionid']??'NULL')).
+		': '.($_SERVER['X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'] ?? 'NULL').' --> 401 Unauthorized');
+
+	header('WWW-Authenticate: Basic realm="EGroupware CalDAV/CardDAV/GroupDAV server"');   // cant use CalDAV::REALM as autoloading and include path not yet setup!
+	http_response_code(401);
+	die("Unauthorized, you need to authenticate first!\n");
+}
+
 $starttime = microtime(true);
 
 $GLOBALS['egw_info'] = array(
@@ -28,7 +41,7 @@ $GLOBALS['egw_info'] = array(
 		'currentapp' => $_SERVER['REQUEST_METHOD'] === 'GET' && preg_match('#/groupdav.php/openapi.json($|\?)#', $_SERVER['REQUEST_URI']) ? 'login' : 'groupdav',
 		'no_exception_handler' => 'basic_auth',	// we use a basic auth exception handler (sends exception message as basic auth realm)
 		'autocreate_session_callback' => 'EGroupware\\Api\\Header\\Authenticate::autocreate_session_callback',
-		'auth_realm' => 'EGroupware CalDAV/CardDAV/GroupDAV server',	// cant use groupdav::REALM as autoloading and include path not yet setup!
+		'auth_realm' => 'EGroupware CalDAV/CardDAV/GroupDAV server',	// cant use CalDAV::REALM as autoloading and include path not yet setup!
 	)
 );
 // if you move this file somewhere else, you need to adapt the path to the header!
