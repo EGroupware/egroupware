c739b51f276b86fc806c8c0fa04bcd1c45a04f7d
ralf <rb@egroupware.org>
2026-05-16 10:52:13 +0200

    directly send 401 Unauthorized for empty Basic auth user or PW too add no caching and x-webdav-status header plus further logging of request body, cookies and server-vars

--- groupdav.php
+++ groupdav.php
@@ -22,13 +22,22 @@ use EGroupware\Api;
 
 // check we either have a session cookie, or an Authorization header, otherwise directly return 401 Unauthorized
 if (!preg_match('#/groupdav.php/openapi.json($|\?)#', $_SERVER['REQUEST_URI']) &&
-	empty($_COOKIE['sessionid']) && empty($_SERVER['HTTP_AUTHORIZATION']))
+	empty($_COOKIE['sessionid']) && (empty($_SERVER['HTTP_AUTHORIZATION']) ||
+		// also disallow empty Basic auth user or PW
+		str_starts_with($_SERVER['HTTP_AUTHORIZATION'], 'Basic ') && (empty($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_PW']))))
 {
 	error_log($_SERVER['REQUEST_METHOD'].' '.(empty($_SERVER['HTTPS']) ? 'http://' : 'https://').$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].
 		(isset($_SERVER['HTTP_AUTHORIZATION']) ? ': Authorization: '.$_SERVER['HTTP_AUTHORIZATION'] : ': sessionid='.($_COOKIE['sessionid']??'NULL')).
-		': '.($_SERVER['X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'] ?? 'NULL').' --> 401 Unauthorized');
+		': '.($_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'] ?? 'NULL').' '.$_SERVER['HTTP_USER_AGENT'].' --> 401 Unauthorized'."\n");
+	if (!empty($_SERVER['CONTENT_LENGTH'])) error_log('body: '.file_get_contents('php://input'));
+	error_log('_COOKIE='.json_encode($_COOKIE).', PHP_AUTH_USER='.json_encode($_SERVER['PHP_AUTH_USER']??null).', _SERVER='.json_encode($_SERVER));
+
+	header('Cache-Control: no-store, no-cache, must-revalidate');
+	header('Expires: Thu, 19 Nov 1981 08:52:00 GMT');
+	header('Pragma: no-cache');
 
 	header('WWW-Authenticate: Basic realm="EGroupware CalDAV/CardDAV/GroupDAV server"');   // cant use CalDAV::REALM as autoloading and include path not yet setup!
+	header('X-Webdav-Status: 401 Unauthorized');
 	http_response_code(401);
 	die("Unauthorized, you need to authenticate first!\n");
 }
