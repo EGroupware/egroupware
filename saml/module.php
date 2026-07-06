7b11cf65b08f0268a8b27ad1fc64c5b66abde559
ralf <rb@egroupware.org>
2026-07-06 12:16:44 +0200

    WIP updating SAML / Shibboleth to new SimpleSAMLphp: re-add Univention fix

--- saml/module.php
+++ saml/module.php
@@ -14,11 +14,21 @@ require_once('_include.php');
 $config = Configuration::getInstance();
 $headers = $config->getOptionalArray('headers.security', Configuration::DEFAULT_SECURITY_HEADERS);
 
-$response = Module::process();
-foreach ($headers as $header => $value) {
-    // Some pages may have specific requirements that we must follow. Don't touch them.
-    if (!$response->headers->has($header)) {
-        $response->headers->set($header, $value);
-    }
+try {
+	$response = Module::process();
+	foreach ($headers as $header => $value) {
+	    // Some pages may have specific requirements that we must follow. Don't touch them.
+	    if (!$response->headers->has($header)) {
+	        $response->headers->set($header, $value);
+	    }
+	}
+	$response->send();
 }
-$response->send();
+catch(\SimpleSAML\Error\NoState $e) {
+	// fix/hack NOSTATE error caused by EGroupware and therefore SimpleSAMLphp session lost due logout
+	if (strpos($_SERVER['PHP_SELF'], '/saml/module.php/saml/sp/saml2-logout.php/default-sp') !== false)
+	{
+		\EGroupware\Api\Egw::redirect(str_replace('/saml/module.php/saml/sp/saml2-logout.php/default-sp', '/logout.php', $_SERVER['PHP_SELF']));
+	}
+	throw $e;
+}
