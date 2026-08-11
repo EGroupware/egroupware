e8d69d4991974c9a8dc4f17052953ddb7d8ee894
nathan <nathangray.bsc+github@gmail.com>
2026-08-11 10:59:36 -0600

    Fix search could get stuck in session leading to constant hidden filtering

--- api/src/Etemplate/Widget/Nextmatch.php
+++ api/src/Etemplate/Widget/Nextmatch.php
@@ -413,7 +413,7 @@ class Nextmatch extends Etemplate\Widget
 			$filters['col_filter'] = array_filter($filters['col_filter'], fn($key) => !is_int($key), ARRAY_FILTER_USE_KEY);
 		}
 		// remove non-string search patterns not used in UI
-		if (empty($filters['search']) || !is_string($filters['search']))
+		if (!is_string($filters['search']))
 		{
 			unset($filters['search']);
 		}
