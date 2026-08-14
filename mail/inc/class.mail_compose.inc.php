c8254d3b0c99aa304e5b9a286ea107a8fd7559b2
ralf <rb@egroupware.org>
2026-08-14 16:21:16 +0200

    revert unrelated tmp-file change bundled into previous commit

--- mail/inc/class.mail_compose.inc.php
+++ mail/inc/class.mail_compose.inc.php
@@ -3626,9 +3626,7 @@ class mail_compose
 		{
 			foreach($this->sessionData['attachments'] as $value) {
 				if (!empty($value['file']) && parse_url($value['file'],PHP_URL_SCHEME) != 'vfs') {	// happens when forwarding mails
-					// attachments come straight from client-submitted form-data - never trust
-					// $value['file'] as a path component, or it becomes an arbitrary-file-delete
-					unlink($GLOBALS['egw_info']['server']['temp_dir'].'/'.basename($value['file']));
+					unlink($GLOBALS['egw_info']['server']['temp_dir'].'/'.$value['file']);
 				}
 			}
 		}
