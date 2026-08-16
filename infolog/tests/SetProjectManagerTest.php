ede637c5ff08e35c7cbbbaa774fb83f6e9c09443
ralf <rb@egroupware.org>
2026-08-16 20:34:44 +0200

    Revert "debug: trace deleteProject() cleanup for the TEST fixture cascade"
    This reverts commit 4320d453a958b6f1b454b4e8848f1b1bdc9b9808.

--- infolog/tests/SetProjectManagerTest.php
+++ infolog/tests/SetProjectManagerTest.php
@@ -767,18 +767,13 @@ class SetProjectManagerTest extends \EGroupware\Api\AppTest
 	 */
 	protected function deleteProject()
 	{
-		error_log(__METHOD__."() DEBUG entry pm_id=".var_export($this->pm_id, true));
 		// Force links to run notification now, or elements might stay
 		// usually waits until Egw::on_shutdown();
-		try { Api\Link::run_notifies(); }
-		catch (\Throwable $e) { error_log(__METHOD__."() DEBUG run_notifies() THREW: ".$e); }
+		Api\Link::run_notifies();
 
 		// Force to ignore setting
 		$this->pm_bo->history = '';
-		$del_ret = $this->pm_bo->delete($this->pm_id, true);
-		error_log(__METHOD__."() DEBUG delete() returned ".var_export($del_ret, true));
-		$check = $this->pm_bo->read(['pm_number' => 'TEST']);
-		error_log(__METHOD__."() DEBUG post-delete read('TEST') = ".var_export($check, true));
+		$this->pm_bo->delete($this->pm_id, true);
 
 		// Force links to run notification now, or elements might stay
 		// usually waits until Egw::on_shutdown();
