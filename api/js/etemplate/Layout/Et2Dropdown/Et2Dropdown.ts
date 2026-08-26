d212c6896e754bf53c20fde018466475c37a01d6
milan <milan@egroupware.org>
2026-08-26 15:17:18 +0200

    * Api: Vacation notice is shown on hover again.  --reimplement toggleOnHover attribute from et2Details also for Et2Dropdown

--- api/js/etemplate/Layout/Et2Dropdown/Et2Dropdown.ts
+++ api/js/etemplate/Layout/Et2Dropdown/Et2Dropdown.ts
@@ -1,6 +1,7 @@
 import {Et2Widget} from "../../Et2Widget/Et2Widget";
 import {SlDropdown} from "@shoelace-style/shoelace";
 import {customElement} from "lit/decorators/custom-element.js";
+import {property} from "lit/decorators/property.js";
 import {css} from "lit";
 
 @customElement("et2-dropdown")
@@ -21,5 +22,40 @@ export class Et2Dropdown extends Et2Widget(SlDropdown)
 				}
 			`
 		];
+	}
+
+	/**
+	 * Open the panel on mouse hover, instead of requiring a click on the trigger
+	 */
+	@property({type: Boolean})
+	toggleOnHover = false;
+
+	constructor()
+	{
+		super();
+		this._mouseOutEvent = this._mouseOutEvent.bind(this);
+	}
+
+	connectedCallback()
+	{
+		super.connectedCallback();
+
+		this.updateComplete.then(() =>
+		{
+			if(this.toggleOnHover)
+			{
+				this.addEventListener("mouseover", this.show);
+				this.addEventListener('mouseout', this._mouseOutEvent);
+			}
+		});
+	}
+
+	/**
+	 * Close the dropdown once the mouse leaves it
+	 * @param event
+	 */
+	_mouseOutEvent(event)
+	{
+		if(!this.getDOMNode().contains(event.relatedTarget)) this.hide();
 	}
 }
