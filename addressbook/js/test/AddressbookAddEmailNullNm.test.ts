EGroupware/egroupware

commit cd326f99042861a5be6db4f9716a7e169e59c2e6

Addressbook: Switch to Et2Nextmatch webComponent

addressbook/js/app.ts
@@ -17,7 +17,6 @@ import {EgwApp, PushData} from '../../api/js/jsapi/egw_app';
 import {etemplate2} from "../../api/js/etemplate/etemplate2";
 import {Et2Dialog} from "../../api/js/etemplate/Et2Dialog/Et2Dialog";
 import {et2_selectbox} from "../../api/js/etemplate/et2_widget_selectbox";
-import {fetchAll, nm_action, nm_compare_field} from "../../api/js/etemplate/et2_extension_nextmatch_actions";
 import "./CRM";
 import {egw} from "../../api/js/jsapi/egw_global";
 import {LitElement} from "lit";
@@ -198,8 +197,9 @@ class AddressbookApp extends EgwApp
 				// No data on the event, we'll have to reload if calendar column is visible
 				// to get the updated information
 				var nm = etemplate2.getById('addressbook-index').widgetContainer.getWidgetById('nm');
-				var pref = nm ? nm._getPreferences() : false;
-				if(pref && pref.visible.indexOf('calendar_calendar') > -1)
+				var visible = nm ? (nm.getValue().selectcols || []) : [];
+				if(typeof visible === 'string') visible = visible.split(',');
+				if(visible.indexOf('calendar_calendar') > -1)
 				{
 					nm.refresh(null,'update');
 				}
@@ -483,7 +483,7 @@ class AddressbookApp extends EgwApp
 		}
 	}
 	/**
-	 * Add appointment or show calendar for selected contacts, call default nm_action after some checks
+	 * Add appointment or show calendar for selected contacts
 	 *
 	 * @param _action
 	 * @param _senders
@@ -493,8 +493,11 @@ class AddressbookApp extends EgwApp
 		if (!_senders[0].id.match(/^(?:addressbook::)?[0-9]+$/))
 		{
 			// send org-view requests to server
-			_action.data.nm_action = "submit";
-			nm_action(_action, _senders);
+			const nm = _action.data?.nextmatch || _action.parent?.data?.nextmatch;
+			return nm.executeAction(_action.id, {
+				ids: _senders.map((sender) => sender.id),
+				all: nm.getSelection().all === true
+			}, {nmAction: "submit"});
 		}
 		else
 		{
@@ -563,24 +566,26 @@ class AddressbookApp extends EgwApp
 	}
 
 	/**
-	 * Add task for selected contacts, call default nm_action after some checks
+	 * Add task for selected contacts
 	 *
 	 * @param _action
 	 * @param _senders
 	 */
 	add_task(_action, _senders)
 	{
+		const nm = _action.data?.nextmatch || _action.parent?.data?.nextmatch;
 		if (!_senders[0].id.match(/^(addressbook::)?[0-9]+$/))
 		{
 			// send org-view requests to server
-			_action.data.nm_action = "submit";
+			return nm.executeAction(_action.id, {
+				ids: _senders.map((sender) => sender.id),
+				all: nm.getSelection().all === true
+			}, {nmAction: "submit"});
 		}
-		else
-		{
-			// call nm_action's popup
-			_action.data.nm_action = "popup";
-		}
-		nm_action(_action, _senders);
+		return nm.executeAction(_action.id, {
+			ids: _senders.map((sender) => sender.id),
+			all: nm.getSelection().all === true
+		}, {nmAction: "popup"});
 	}
 
 	/**
@@ -792,10 +797,11 @@ class AddressbookApp extends EgwApp
 			owner = filter.getValue()||egw.preference('add_default','addressbook');
 		}
 		var contacts = [];
-		if(selected && selected[0] && selected[0].getAllSelected())
+		const nm = this.et2.getWidgetById('nm');
+		if(nm.getSelection().all)
 		{
 			// Action says all contacts selected, better ask the server for _all_ the IDs
-			var fetching = fetchAll(selected, this.et2.getWidgetById('nm'), jQuery.proxy(
+			var fetching = this._fetchAllSelected(nm, jQuery.proxy(
 				function(contacts) {
 					this._add_new_list_prompt(owner, contacts);
 				}, this));
@@ -890,6 +896,18 @@ class AddressbookApp extends EgwApp
 		document.body.appendChild(dialog);
 	}
 
+	_fetchAllSelected(nm, callback)
+	{
+		if(nm.getSelection().all)
+		{
+			nm.fetchAllIds()
+				.then((ids) => callback.call(this, ids))
+				.catch(() => {});
+			return true;
+		}
+		return false;
+	}
+
 	/**
 	 * Rename the current distribution list selected in the nextmatch filter2
 	 *
@@ -974,7 +992,7 @@ class AddressbookApp extends EgwApp
 
 		if(filter2_val == 'add')
 		{
-			this.add_new_list(typeof widget == 'undefined' ? this.et2.getWidgetById('filter').value : widget.header.filter.get_value());
+			this.add_new_list(filter.getValue?.() || filter.get_value?.() || filter.value);
 			filter2.set_value('');
 		}
 		// automatic switch to accounts addressbook or all addressbooks depending on distribution list is a group
@@ -1008,7 +1026,25 @@ class AddressbookApp extends EgwApp
 		if (field) var val = field.get_value();
 		if (val)
 		{
-			return nm_compare_field;
+			return (_action) =>
+			{
+				let field : any = document.getElementById(_action.data.fieldId);
+				let value;
+				if(field)
+				{
+					value = jQuery(field).val();
+				}
+				else
+				{
+					field = _action.data.nextmatch?.getWidgetById(_action.data.fieldId);
+					value = field?.getValue?.();
+				}
+				if(!field) return false;
+
+				return _action.data.fieldValue.substr(0,1) == '!' ?
+					value != _action.data.fieldValue.substr(1) :
+					value == _action.data.fieldValue;
+			};
 		}
 		else
 		{
@@ -1038,7 +1074,7 @@ class AddressbookApp extends EgwApp
 			return false;
 		}
 		// Reset filters first
-		nm.activeFilters = {};
+		nm.applyFilters({}, {reload: false});
 		nm.applyFilters(filters);
 		return false;
 	}
@@ -1054,7 +1090,7 @@ class AddressbookApp extends EgwApp
 		var link = {'preset[type]':[], 'preset[file]':[]};
 		var content = {data:{files:{file:[], type:[]}}};
 		var nm = this.et2.getWidgetById('nm');
-		if(fetchAll(_elems, nm, jQuery.proxy(function(ids) {
+		if(this._fetchAllSelected(nm, jQuery.proxy(function(ids) {
 			this.adb_mail_vcard(_action, ids.map(function(num) {return {id:'addressbook::'+num};}));
 		}, this)))
 		{
@@ -1115,8 +1151,8 @@ class AddressbookApp extends EgwApp
 	{
 		// Check for all selected.
 		if (typeof(nm) === "undefined") nm = this.et2.getWidgetById('nm');
-		if(fetchAll(selected, nm, (ids) => {
-			// fetchAll() returns just the ID, no prefix, so map it to match normal selected
+		if(this._fetchAllSelected(nm, (ids) => {
+			// fetchAllIds() returns just the ID, no prefix, so map it to match normal selected
 			this.addEmail(action, ids.map((num) => { return {id:'addressbook::'+num}; }), nm, which);
 		}))
 		{
@@ -1222,9 +1258,9 @@ class AddressbookApp extends EgwApp
 
 		// Check for all selected, don't resolve until all done
 		let nm = this.et2.getWidgetById('nm');
-		let all = new Promise(function(resolve)
+		let all = new Promise((resolve) =>
 		{
-			let fetching = fetchAll(selected, nm, ids => {resolve(ids.map(function(num) {return {id: 'addressbook::' + num};}))});
+			let fetching = this._fetchAllSelected(nm, ids => {resolve(ids.map(function(num) {return {id: 'addressbook::' + num};}))});
 			if(!fetching)
 			{
 				resolve(selected);
@@ -1431,9 +1467,7 @@ class AddressbookApp extends EgwApp
 			))
 			{
 				const nm = index.widgetContainer.getWidgetById('nm');
-				const action = nm.controller._actionManager.getActionById('view_org');
-				const senders = [{_context: {_widget: nm}}];
-				return nm_action(action, senders, {}, {ids: [state.state.grouped_view]});
+				return nm.executeAction("view_org", {ids: [state.state.grouped_view], all: false}, {nmAction: "submit"});
 			}
 			grouped.value = state.state.grouped_view;
 		}

addressbook/templates/default/index.duplicate_rows.xet
@@ -14,35 +14,29 @@
 				<row class="th">
 					<nextmatch-header label="Type" id="type"/>
 					<nextmatch-header label="#" align="center" id="group_count"/>
-					<grid>
-						<columns>
-							<column/>
-							<column/>
-						</columns>
-						<rows>
-							<row disabled="!@order=n_fileas">
-								<nextmatch-sortheader label="own sorting" id="n_fileas" span="all"/>
-							</row>
-							<row disabled="!@order=n_given">
-								<nextmatch-sortheader label="Firstname" id="n_given"/>
-								<nextmatch-sortheader label="Name" id="n_family"/>
-							</row>
-							<row disabled="!@order=n_family">
-								<nextmatch-sortheader label="Name" id="n_family"/>
-								<nextmatch-sortheader label="Firstname" id="n_given"/>
-							</row>
-							<row>
-								<nextmatch-sortheader label="Organisation" id="org_name" span="all"/>
-							</row>
-							<row disabled="!@order=/^(org_name|n_fileas|adr_one_postalcode|contact_modified|contact_created|#)/">
-								<nextmatch-sortheader label="Name" id="n_family"/>
-								<nextmatch-sortheader label="Firstname" id="n_given" class="leftPad5"/>
-							</row>
-							<row disabled="@order=n_fileas">
-								<nextmatch-sortheader label="own sorting" id="n_fileas" span="all"/>
-							</row>
-						</rows>
-					</grid>
+                    <et2-vbox>
+                        <et2-hbox disabled="!@order=n_fileas">
+                            <et2-nextmatch-sortheader label="own sorting" id="n_fileas" span="all"/>
+                        </et2-hbox>
+                        <et2-hbox disabled="!@order=n_given">
+                            <et2-nextmatch-sortheader label="Firstname" id="n_given"/>
+                            <et2-nextmatch-sortheader label="Name" id="n_family"/>
+                        </et2-hbox>
+                        <et2-hbox disabled="!@order=n_family">
+                            <et2-nextmatch-sortheader label="Name" id="n_family"/>
+                            <et2-nextmatch-sortheader label="Firstname" id="n_given"/>
+                        </et2-hbox>
+                        <et2-hbox>
+                            <et2-nextmatch-sortheader label="Organisation" id="org_name" span="all"/>
+                        </et2-hbox>
+                        <et2-hbox disabled="!@order=/^(org_name|n_fileas|adr_one_postalcode|contact_modified|contact_created|#)/">
+                            <et2-nextmatch-sortheader label="Name" id="n_family"/>
+                            <et2-nextmatch-sortheader label="Firstname" id="n_given" class="leftPad5"/>
+                        </et2-hbox>
+                        <et2-hbox disabled="@order=n_fileas">
+                            <et2-nextmatch-sortheader label="own sorting" id="n_fileas" span="all"/>
+                        </et2-hbox>
+                    </et2-vbox>
 					<nextmatch-header label="Business address" id="business"/>
 					<!--
 					<et2-vbox>

addressbook/templates/default/index.org_rows.xet
@@ -14,9 +14,9 @@
 			</columns>
 			<rows>
 				<row class="th">
-					<nextmatch-header label="Type" id="type" options="1"/>
-					<nextmatch-header label="#" align="center" id="number" options="Number"/>
-					<nextmatch-sortheader id="org_name" label="Organisation"/>
+                    <nextmatch-header label="Type" id="type"/>
+                    <nextmatch-header label="#" align="center" id="number"/>
+                    <et2-nextmatch-sortheader id="org_name" label="Organisation"/>
 					<nextmatch-header label="Department" id="org_unit"/>
 					<nextmatch-header label="Business address" id="business"/>
 					<et2-vbox>

commit 32ecf10da80c3772eed40940166e3c061e55666e

* addressbook: fix "Add to To/Cc/Bcc" context-menu action silently doing nothing
Found live 2026-09-14 (ralf, relaying a real user's report, Claus
Brinkmann): right-clicking selected contacts -> Email -> "Add to Bcc"
(used weekly to build a collective mailing) showed in the context
menu but silently did nothing.

Root cause: EgwAction.execute(_senders, _target=null) always calls
onExecute.exec(this, _senders, _target) - for a plain (non-drop)
context-menu action, _target is null, never undefined.
AddressbookApp.addEmail()'s own fallback
(`if (typeof(nm) === "undefined") nm = this.et2.getWidgetById('nm')`)
never triggers for null, so nm stayed null into
_fetchAllSelected(nm, ...), which called nm.getSelection()
unconditionally and threw - silently, inside a menu click handler.

This is a real regression from cd326f99042 (2026-06-25, "Addressbook:
Switch to Et2Nextmatch webComponent"), which replaced the old
fetchAll() utility (had its own
`if(!nextmatch || !nextmatch.controller) return false;` guard) with
_fetchAllSelected(), dropping that guard. Affects all three "Add to
To/Cc/Bcc" actions identically.

Fixed addEmail()'s own guard (`!nm` instead of typeof check) and
restored the dropped null-guard in _fetchAllSelected() itself for any
other caller.

Also hardened a related latent issue found while investigating:
egw_open.ts's mailto() called window.app.mail?.composeMailto()
directly for the "open a new compose" case - if mail's own JS was
never loaded in that browser tab, this silently no-op'd instead of
opening a popup. Now resolved via egw.applyFunc(), the same
dotted-path resolver every 'javaScript:...' action string already
uses, which lazy-loads the app bundle first if needed.

3 new tests in addressbook/js/test/AddressbookAddEmailNullNm.test.ts -
confirmed they fail against the pre-fix code with the exact reported
exception, pass after. Live-verified end-to-end against
boulder.egroupware.org: a real compose popup now opens with the

addressbook/js/app.ts
@@ -928,6 +928,15 @@ class AddressbookApp extends EgwApp
 
 	_fetchAllSelected(nm, callback)
 	{
+		// The legacy fetchAll() this replaced (cd326f99042, 2026-06-25) guarded against a null/
+		// undefined nextmatch (`if(!nextmatch || !nextmatch.controller) return false;`) - restored
+		// here since a caller can legitimately be handed one (addEmail()'s own `nm` param is
+		// `et2_nextmatch|null` by design, and EgwAction.execute()'s default `_target=null` means a
+		// plain, non-drop context-menu action always arrives that way).
+		if(!nm)
+		{
+			return false;
+		}
 		if(nm.getSelection().all)
 		{
 			nm.fetchAllIds()
@@ -1208,7 +1217,12 @@ class AddressbookApp extends EgwApp
 	addEmail(action, selected, nm?, which?, setCompose?)
 	{
 		// Check for all selected.
-		if (typeof(nm) === "undefined") nm = this.et2.getWidgetById('nm');
+		// EgwAction.execute(_senders, _target=null) always calls onExecute.exec(this, _senders,
+		// _target) - for a plain (non-drop) context-menu action like this one, _target is null,
+		// not undefined, so this must fall back for null too (real user report 2026-09-14,
+		// Claus Brinkmann: "Add to Bcc" showed in the menu but silently did nothing - nm stayed
+		// null and _fetchAllSelected(null, ...) threw reading null.getSelection()).
+		if (!nm) nm = this.et2.getWidgetById('nm');
 		if(this._fetchAllSelected(nm, (ids) => {
 			// fetchAllIds() returns just the ID, no prefix, so map it to match normal selected
 			this.addEmail(action, ids.map((num) => { return {id:'addressbook::'+num}; }), nm, which);

api/js/jsapi/egw_open.ts
@@ -245,9 +245,17 @@ function mailto(uri : string) : void
 	// classic-menuaction fallback - mailto: itself already parses to/cc/bcc entirely client-side
 	// above, so there was never anything server-side left for THIS entry point to depend on
 	// (doc/ai/projects/mail-compose-jmap-migration.md, Step 10's own "explicitly deferred" list).
+	//
+	// Resolved via egw.applyFunc() (the same dotted-path resolution every `onExecute:
+	// 'javaScript:app.x.y'` action string already uses), not a direct `window.app.mail?.` read -
+	// a caller whose current tab never happened to load/visit the mail app (eg. addressbook's
+	// own "Add to Bcc" context-menu action, a real 2026-09-14 user report: the action showed but
+	// silently did nothing) has `window.app.mail` undefined, so the optional-chaining call used
+	// to just no-op with no popup and no error. applyFunc() lazy-loads mail's own JS bundle (and
+	// instantiates MailApp) first if it isn't loaded yet, then calls composeMailto() for real.
 	egw.openWithinWindow ("mail", "setCompose", content, {'preset[mailto]':uri},
 		/\/mail\/compose\.php/, undefined,
-		() => (<any>window).app.mail?.composeMailto(content));
+		() => egw.applyFunc('app.mail.composeMailto', [content]));
 
 	for (var index in content)
 	{

addressbook/js/test/AddressbookAddEmailNullNm.test.ts
@@ -1,0 +1,95 @@
+import {assert} from "@open-wc/testing";
+import "./AddressbookAppImportStub";
+// breaks the et2_core_widget <-> Et2Widget import cycle (ClassWithAttributes TDZ) the same
+// way the mail app.ts tests do, before app.ts pulls it in transitively
+import "../../../api/js/etemplate/Et2Widget/Et2Widget";
+
+/**
+ * app.ts has to be loaded through its explicit source path (see MailVcardMessage.test.ts's own
+ * docblock for why): a plain `import ... from "../app"` resolves to the gitignored, stale tsc
+ * output, not the live source.
+ */
+const APP_SOURCE = '/addressbook/js/app.ts';
+
+/**
+ * Regression coverage for a real user report (2026-09-14, Claus Brinkmann): right-clicking
+ * selected contacts -> Email -> "Add to Bcc" (also To/Cc) showed in the context menu but
+ * silently did nothing.
+ *
+ * Root cause: `EgwAction.execute(_senders, _target = null)` always calls
+ * `onExecute.exec(this, _senders, _target)` - for a plain (non-drop) context-menu action,
+ * `_target` is `null`, not `undefined`. `addEmail(action, selected, nm?, ...)`'s own fallback
+ * (`if (typeof(nm) === "undefined") nm = this.et2.getWidgetById('nm');`) never triggered for a
+ * `null` nm, so `nm` stayed `null` all the way into `_fetchAllSelected(nm, ...)`, which (unlike
+ * the legacy `fetchAll()` utility it replaced in cd326f99042, 2026-06-25 - that one guarded with
+ * `if(!nextmatch || !nextmatch.controller) return false;`) called `nm.getSelection()`
+ * unconditionally and threw `TypeError: Cannot read properties of null (reading 'getSelection')`
+ * - silently, inside a menu-item click handler, so the user never saw an error at all.
+ *
+ * Setup mirrors MailVcardMessage.test.ts: a bare `Object.create(prototype)` app object, no real
+ * EgwApp/framework/etemplate needed for addEmail()'s own logic.
+ */
+function createFakeEt2(nmSelection : { ids : string[], all : boolean })
+{
+	const nm = {
+		getSelection: () => nmSelection,
+		fetchAllIds: () => Promise.resolve(nmSelection.ids),
+	};
+	return {getWidgetById: (id : string) => id === 'nm' ? nm : null};
+}
+
+describe('AddressbookApp.addEmail() with a null nm (plain context-menu action)', () =>
+{
+	let app : any;
+	let openLinkCalls : string[];
+	let AddressbookApp : any;
+
+	before(async function()
+	{
+		this.timeout(15000);
+		await import(APP_SOURCE);
+		AddressbookApp = (<any>window).app.classes.addressbook;
+	});
+
+	beforeEach(() =>
+	{
+		openLinkCalls = [];
+		const egw : any = {
+			lang: (msg : string, ...args : any[]) => args.reduce((s : string, arg, i) => s.replace('%' + (i + 1), String(arg)), msg),
+			preference: () => undefined,
+			message: () => {},
+			open_link: (uri : string) => openLinkCalls.push(uri),
+			dataGetUIDdata: (id : string) => ({data: {n_fn: 'Test Contact', email: 'contact@example.org'}}),
+		};
+		(<any>window).egw = egw;
+
+		app = Object.create(AddressbookApp.prototype);
+		Object.assign(app, {
+			appname: 'addressbook', egw: egw,
+			et2: createFakeEt2({ids: ['1'], all: false}),
+		});
+	});
+
+	const action = {id: 'add_to_bcc', getManager: () => ({getActionById: () => ({checked: false})})};
+	const selected = [{id: 'addressbook::1'}];
+
+	it('does not throw when nm is explicitly null (EgwAction.execute()\'s own default target)', () =>
+	{
+		assert.doesNotThrow(() => app.addEmail(action, selected, null));
+	});
+
+	it('still opens the mailto: bcc link when nm is null, falling back to this.et2\'s own nm', () =>
+	{
+		app.addEmail(action, selected, null);
+
+		assert.equal(openLinkCalls.length, 1);
+		assert.match(openLinkCalls[0], /^mailto:\?bcc=/);
+		assert.include(openLinkCalls[0], 'contact@example.org');
+	});
+
+	it('_fetchAllSelected() itself also tolerates a null/undefined nm (defense in depth)', () =>
+	{
+		assert.equal(app._fetchAllSelected(null, () => {}), false);
+		assert.equal(app._fetchAllSelected(undefined, () => {}), false);
+	});
+});